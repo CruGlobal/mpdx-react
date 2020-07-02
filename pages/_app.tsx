d36f3f9402c9a5550c6c91a2b430253da914b953
Tataihono Nikora <tataihono.nikora@gmail.com>
2020-07-02 13:57:32 +1200

    move things into document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import React, { ReactElement, useEffect } from 'react';
 import { AppProps } from 'next/app';
-import Head from 'next/head';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ApolloProvider } from '@apollo/client';
@@ -31,34 +30,6 @@ const App = ({ Component, pageProps, router }: AppProps): ReactElement => {
     return (
         <Provider options={{ site: process.env.SITE_URL }} session={session}>
             <ApolloProvider client={client}>
-                <Head>
-                    <title>MPDX | Fundraising software built for God’s people</title>
-                    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-                    <meta
-                        name="viewport"
-                        content="viewport-fit=cover,width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-                    />
-                    <link rel="manifest" href="/manifest.json" />
-                    <link href="/favicon.png" rel="icon" type="image/png" sizes="32x32" />
-                    <link rel="apple-touch-icon" href="/icons/apple-touch-icon-iphone-60x60.png" />
-                    <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-touch-icon-ipad-76x76.png" />
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="114x114"
-                        href="/icons/apple-touch-icon-iphone-retina-120x120.png"
-                    />
-                    <link
-                        rel="apple-touch-icon"
-                        sizes="144x144"
-                        href="/icons/apple-touch-icon-ipad-retina-152x152.png"
-                    />
-                    <meta name="theme-color" content="#05699b" />
-                    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-                    <link
-                        href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;700&display=swap"
-                        rel="stylesheet"
-                    />
-                </Head>
                 <ThemeProvider theme={theme}>
                     <CssBaseline />
                     <Chrome>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,30 @@ class MyDocument extends Document {
             <html lang="en">
                 <Head>
                     <meta name="theme-color" content={theme.palette.primary.main} />
+                    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                    <meta
+                        name="viewport"
+                        content="viewport-fit=cover,width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+                    />
+                    <link rel="manifest" href="/manifest.json" />
+                    <link href="/favicon.png" rel="icon" type="image/png" sizes="32x32" />
+                    <link rel="apple-touch-icon" href="/icons/apple-touch-icon-iphone-60x60.png" />
+                    <link rel="apple-touch-icon" sizes="60x60" href="/icons/apple-touch-icon-ipad-76x76.png" />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="114x114"
+                        href="/icons/apple-touch-icon-iphone-retina-120x120.png"
+                    />
+                    <link
+                        rel="apple-touch-icon"
+                        sizes="144x144"
+                        href="/icons/apple-touch-icon-ipad-retina-152x152.png"
+                    />
+                    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+                    <link
+                        href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;700&display=swap"
+                        rel="stylesheet"
+                    />
                 </Head>
                 <body>
                     <Main />
