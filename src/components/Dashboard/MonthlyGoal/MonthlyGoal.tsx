d4a86051aa5917d9eb643dbafcf7d6c44bf21db3
Tataihono Nikora <tataihono.nikora@gmail.com>
2020-07-07 12:11:53 +1200

    smaller monthly goal view on mobile

--- src/components/Dashboard/MonthlyGoal/MonthlyGoal.tsx
+++ src/components/Dashboard/MonthlyGoal/MonthlyGoal.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
-import { Typography, makeStyles, Theme, Grid, CardContent, Box } from '@material-ui/core';
+import { Typography, makeStyles, Theme, Grid, CardContent, Box, Hidden } from '@material-ui/core';
 import { Skeleton } from '@material-ui/lab';
 import { currencyFormat, percentageFormat } from '../../../lib/intlFormat';
 import AnimatedCard from '../../AnimatedCard';
@@ -69,7 +69,14 @@ const MonthlyGoal = ({ loading, goal, received, pledged, currencyCode = 'USD' }:
         <>
             <Box my={{ xs: 1, sm: 2 }}>
                 <AnimatedBox>
-                    <Typography variant="h6">Monthly Goal</Typography>
+                    <Typography variant="h6">
+                        <Box display="flex">
+                            <Box flexGrow={1}>Monthly Goal</Box>
+                            <Hidden smUp>
+                                <Box>{!loading && currencyFormat(goal, currencyCode)}</Box>
+                            </Hidden>
+                        </Box>
+                    </Typography>
                 </AnimatedBox>
             </Box>
             <AnimatedCard>
@@ -91,15 +98,17 @@ const MonthlyGoal = ({ loading, goal, received, pledged, currencyCode = 'USD' }:
                         )}
                     </div>
                     <Grid container spacing={2}>
-                        <Grid xs={6} md={3} item>
-                            <Typography component="div" color="textSecondary">
-                                <div className={[classes.indicator, classes.goal].join(' ')} />
-                                Goal
-                            </Typography>
-                            <Typography variant="h5">
-                                {loading ? <Skeleton variant="text" /> : currencyFormat(goal, currencyCode)}
-                            </Typography>
-                        </Grid>
+                        <Hidden xsDown>
+                            <Grid sm={6} md={3} item>
+                                <Typography component="div" color="textSecondary">
+                                    <div className={[classes.indicator, classes.goal].join(' ')} />
+                                    Goal
+                                </Typography>
+                                <Typography variant="h5">
+                                    {loading ? <Skeleton variant="text" /> : currencyFormat(goal, currencyCode)}
+                                </Typography>
+                            </Grid>
+                        </Hidden>
                         <Grid xs={6} md={3} item>
                             <Typography component="div" color="textSecondary">
                                 <div className={[classes.indicator, classes.received].join(' ')} />
@@ -136,23 +145,25 @@ const MonthlyGoal = ({ loading, goal, received, pledged, currencyCode = 'USD' }:
                                 {loading ? <Skeleton variant="text" /> : currencyFormat(pledged, currencyCode)}
                             </Typography>
                         </Grid>
-                        <Grid xs={6} md={3} item>
-                            <Typography component="div" color="textSecondary">
-                                Below Goal
-                            </Typography>
-                            <Typography variant="h5">
-                                {loading ? (
-                                    <Skeleton variant="text" />
-                                ) : isNaN(belowGoalPercentage) ? (
-                                    '- '
-                                ) : (
-                                    percentageFormat(belowGoalPercentage)
-                                )}
-                            </Typography>
-                            <Typography component="small">
-                                {loading ? <Skeleton variant="text" /> : currencyFormat(belowGoal, currencyCode)}
-                            </Typography>
-                        </Grid>
+                        <Hidden xsDown>
+                            <Grid sm={6} md={3} item>
+                                <Typography component="div" color="textSecondary">
+                                    Below Goal
+                                </Typography>
+                                <Typography variant="h5">
+                                    {loading ? (
+                                        <Skeleton variant="text" />
+                                    ) : isNaN(belowGoalPercentage) ? (
+                                        '- '
+                                    ) : (
+                                        percentageFormat(belowGoalPercentage)
+                                    )}
+                                </Typography>
+                                <Typography component="small">
+                                    {loading ? <Skeleton variant="text" /> : currencyFormat(belowGoal, currencyCode)}
+                                </Typography>
+                            </Grid>
+                        </Hidden>
                     </Grid>
                 </CardContent>
             </AnimatedCard>
