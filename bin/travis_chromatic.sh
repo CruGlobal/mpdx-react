5efe93ac877ce6869329141a0c1d340eb8c6e835
Tataihono Nikora <tataihono.nikora@gmail.com>
2020-07-17 17:16:11 +1200

    speed up travis

--- bin/travis_chromatic.sh
+++ bin/travis_chromatic.sh
@@ -1,6 +1,6 @@
 if ["$TRAVIS_PULL_REQUEST" = "false" ];
 then
-  npm run chromatic -- --auto-accept-changes
+  npm run chromatic -- --auto-accept-changes --skip 'dependabot/**' --exit-once-uploaded
 else
-  npm run chromatic -- --exit-zero-on-changes
+  npm run chromatic -- --exit-zero-on-changes --skip 'dependabot/**' --exit-once-uploaded
 fi
