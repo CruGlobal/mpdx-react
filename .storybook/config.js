ccec2fa071e027d3b6c787d67ebc8501ad8fc289
Tataihono Nikora <tataihono.nikora@gmail.com>
2020-07-27 16:44:45 +1200

    set time for chromatic

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,10 +2,21 @@ import React from 'react';
 import { configure } from '@storybook/react';
 import { addDecorator } from '@storybook/react';
 import { ThemeProvider, CssBaseline } from '@material-ui/core';
-import { withKnobs } from "@storybook/addon-knobs";
+import { withKnobs } from '@storybook/addon-knobs';
+import MockDate from 'mockdate';
+import isChromatic from 'chromatic/isChromatic';
 import theme from '../src/theme';
 
-addDecorator(storyFn => <ThemeProvider theme={theme}><CssBaseline />{storyFn()}</ThemeProvider>);
+if(isChromatic()) {
+    MockDate.set(new Date(2020, 1, 1));
+}
+
+addDecorator((storyFn) => (
+    <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {storyFn()}
+    </ThemeProvider>
+));
 addDecorator(withKnobs);
 
 // automatically import all files ending in *.stories.tsx
