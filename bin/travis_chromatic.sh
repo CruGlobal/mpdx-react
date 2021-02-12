f824d705091c92a2164ce1b20bbaeb6c530aeefb
Scotty Waggoner <ozzieorca@gmail.com>
2021-02-11 17:38:29 -0800

    Update Travis to use Yarn

--- bin/travis_chromatic.sh
+++ bin/travis_chromatic.sh
@@ -1,6 +1,6 @@
 if ["$TRAVIS_PULL_REQUEST" = "false" ];
 then
-  npm run chromatic -- --auto-accept-changes --skip 'dependabot/**' --exit-once-uploaded
+  yarn chromatic --auto-accept-changes --skip 'dependabot/**' --exit-once-uploaded
 else
-  npm run chromatic -- --exit-zero-on-changes --skip 'dependabot/**' --exit-once-uploaded
+  yarn chromatic --exit-zero-on-changes --skip 'dependabot/**' --exit-once-uploaded
 fi
