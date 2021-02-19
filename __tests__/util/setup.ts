1823f98511a14458859291741e0226779c94ff0d
Robert Eldredge <robert.eldredge@cru.org>
2021-02-19 13:41:21 -0500

    Make mock date global

--- __tests__/util/setup.ts
+++ __tests__/util/setup.ts
@@ -1,4 +1,5 @@
 import '@testing-library/jest-dom';
+import { Settings } from 'luxon';
 
 window.document.createRange = (): Range =>
   (({
@@ -11,3 +12,11 @@ window.document.createRange = (): Range =>
   } as unknown) as Range);
 
 window.HTMLElement.prototype.scrollIntoView = jest.fn();
+
+global.beforeEach(() => {
+  Settings.now = () => new Date(2020, 1, 1).valueOf();
+});
+
+global.afterEach(() => {
+  Settings.resetCaches();
+});
--- src/components/Dashboard/ThisWeek/LateCommitments/LateCommitments.test.tsx
+++ src/components/Dashboard/ThisWeek/LateCommitments/LateCommitments.test.tsx
@@ -39,64 +39,54 @@ describe('LateCommitments', () => {
     ).not.toBeInTheDocument();
   });
 
-  describe('MockDate', () => {
-    beforeEach(() => {
-      Settings.now = () => new Date(2020, 1, 1).valueOf();
-    });
-    afterEach(() => {
-      Settings.resetCaches();
-    });
-    it('props', () => {
-      const latePledgeContacts = {
-        nodes: [
-          {
-            id: 'contact1',
-            name: 'Smith, Sarah',
-            lateAt: '2012-10-01',
-          },
-          {
-            id: 'contact2',
-            name: 'Smith, John',
-            lateAt: '2015-12-01',
-          },
-        ],
-        totalCount: 1595,
-      };
-      const { getByTestId, queryByTestId } = render(
-        <LateCommitments latePledgeContacts={latePledgeContacts} />,
-      );
-      expect(
-        queryByTestId('LateCommitmentsCardContentEmpty'),
-      ).not.toBeInTheDocument();
-      expect(
-        queryByTestId('LateCommitmentsDivLoading'),
-      ).not.toBeInTheDocument();
-      const buttonElement = getByTestId('LateCommitmentsButtonViewAll');
-      expect(buttonElement.textContent).toEqual('View All (1,595)');
-      expect(buttonElement).toHaveAttribute(
-        'href',
-        'https://stage.mpdx.org/contacts?filters=%7B%22late_at%22%3A%221970-01-01..2020-02-01%22%2C%22status%22%3A%22Partner%20-%20Financial%22%7D',
-      );
-      const contact1Element = getByTestId(
-        'LateCommitmentsListItemContact-contact1',
-      );
-      expect(contact1Element).toHaveAttribute(
-        'href',
-        'https://stage.mpdx.org/contacts/contact1',
-      );
-      expect(contact1Element.textContent).toEqual(
-        'Smith, SarahTheir gift is 2,679 days late.',
-      );
-      const contact2Element = getByTestId(
-        'LateCommitmentsListItemContact-contact2',
-      );
-      expect(contact2Element).toHaveAttribute(
-        'href',
-        'https://stage.mpdx.org/contacts/contact2',
-      );
-      expect(contact2Element.textContent).toEqual(
-        'Smith, JohnTheir gift is 1,523 days late.',
-      );
-    });
+  it('props', () => {
+    const latePledgeContacts = {
+      nodes: [
+        {
+          id: 'contact1',
+          name: 'Smith, Sarah',
+          lateAt: '2012-10-01',
+        },
+        {
+          id: 'contact2',
+          name: 'Smith, John',
+          lateAt: '2015-12-01',
+        },
+      ],
+      totalCount: 1595,
+    };
+    const { getByTestId, queryByTestId } = render(
+      <LateCommitments latePledgeContacts={latePledgeContacts} />,
+    );
+    expect(
+      queryByTestId('LateCommitmentsCardContentEmpty'),
+    ).not.toBeInTheDocument();
+    expect(queryByTestId('LateCommitmentsDivLoading')).not.toBeInTheDocument();
+    const buttonElement = getByTestId('LateCommitmentsButtonViewAll');
+    expect(buttonElement.textContent).toEqual('View All (1,595)');
+    expect(buttonElement).toHaveAttribute(
+      'href',
+      'https://stage.mpdx.org/contacts?filters=%7B%22late_at%22%3A%221970-01-01..2020-02-01%22%2C%22status%22%3A%22Partner%20-%20Financial%22%7D',
+    );
+    const contact1Element = getByTestId(
+      'LateCommitmentsListItemContact-contact1',
+    );
+    expect(contact1Element).toHaveAttribute(
+      'href',
+      'https://stage.mpdx.org/contacts/contact1',
+    );
+    expect(contact1Element.textContent).toEqual(
+      'Smith, SarahTheir gift is 2,679 days late.',
+    );
+    const contact2Element = getByTestId(
+      'LateCommitmentsListItemContact-contact2',
+    );
+    expect(contact2Element).toHaveAttribute(
+      'href',
+      'https://stage.mpdx.org/contacts/contact2',
+    );
+    expect(contact2Element.textContent).toEqual(
+      'Smith, JohnTheir gift is 1,523 days late.',
+    );
   });
 });
