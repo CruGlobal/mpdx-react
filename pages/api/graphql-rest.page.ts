698a63a1d7d53bb9057e4fbbb5f9f51fae27fadb
Scotty Waggoner <ozzieorca@gmail.com>
2021-03-05 10:48:06 -0800

    Adjust ContactFilter type to match real data
    - Parse application/vnd.api+json as JSON
    - Add data source to context type

--- pages/api/graphql-rest.page.ts
+++ pages/api/graphql-rest.page.ts
@@ -2,7 +2,11 @@ import { ApolloServer } from '@saeris/apollo-server-vercel';
 import { buildFederatedSchema } from '@apollo/federation';
 import { gql } from 'graphql-tag';
 import { NextApiRequest } from 'next';
-import { RESTDataSource } from 'apollo-datasource-rest';
+import {
+  RequestOptions,
+  Response,
+  RESTDataSource,
+} from 'apollo-datasource-rest';
 import { Resolvers } from './graphql-rest.page.generated';
 
 const typeDefs = gql`
@@ -14,13 +18,19 @@ const typeDefs = gql`
     id: ID!
     type: String!
     filterType: String!
-    default_selection: Boolean!
+    defaultSelection: [String]!
     featured: Boolean!
     multiple: Boolean!
     name: String!
-    options: [String!]!
-    parent: String!
+    options: [ContactFilterOption!]!
+    parent: String
     title: String!
+  }
+
+  type ContactFilterOption {
+    id: String
+    name: String!
+    placeholder: String
   }
 `;
 
@@ -38,20 +48,58 @@ class MpdxRestApi extends RESTDataSource {
     this.baseURL = process.env.REST_API_URL;
   }
 
-  willSendRequest(request) {
+  willSendRequest(request: RequestOptions) {
     request.headers.set('Authorization', this.context.authHeader);
     request.headers.set('content-type', 'application/vnd.api+json');
   }
 
+  // Overridden to accept JSON API Content Type application/vnd.api+json
+  // Taken from https://github.com/apollographql/apollo-server/blob/baf9b252dfab150ec828ef83345a7ceb2d34a6a3/packages/apollo-datasource-rest/src/RESTDataSource.ts#L110-L126
+  protected parseBody(
+    response: Response,
+  ): Promise<Record<string, unknown> | string> {
+    const contentType = response.headers.get('Content-Type');
+    const contentLength = response.headers.get('Content-Length');
+    if (
+      // As one might expect, a "204 No Content" is empty! This means there
+      // isn't enough to `JSON.parse`, and trying will result in an error.
+      response.status !== 204 &&
+      contentLength !== '0' &&
+      contentType &&
+      (contentType.startsWith('application/json') ||
+        contentType.startsWith('application/vnd.api+json') ||
+        contentType.startsWith('application/hal+json'))
+    ) {
+      return response.json();
+    } else {
+      return response.text();
+    }
+  }
+
   async getContactFilters(accountListId) {
-    return this.get(
+    const { data } = await this.get(
       `contacts/filters?filter[account_list_id]=${accountListId}`,
+    );
+    return data.map(
+      ({
+        attributes: { type, default_selection, ...attributes },
+        ...filter
+      }) => ({
+        ...filter,
+        ...attributes,
+        filterType: type,
+        defaultSelection:
+          typeof default_selection === 'string'
+            ? default_selection.split(/,\s?/)
+            : [default_selection],
+      }),
     );
   }
 }
 
 export interface Context {
   authHeader: string;
+  dataSources: { mpdxRestApi: MpdxRestApi };
 }
 
 const server = new ApolloServer({
@@ -61,7 +109,7 @@ const server = new ApolloServer({
       mpdxRestApi: new MpdxRestApi(),
     };
   },
-  context: ({ req }: { req: NextApiRequest }): Context => {
+  context: ({ req }: { req: NextApiRequest }): Partial<Context> => {
     return { authHeader: req.headers.authorization };
   },
 });
