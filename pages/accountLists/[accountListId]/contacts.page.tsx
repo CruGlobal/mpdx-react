05390542c5a6e24b59770e2f2b0dd0ab828845b3
Scotty Waggoner <ozzieorca@gmail.com>
2021-03-05 11:05:29 -0800

    Load contact filters on frontend

--- pages/accountLists/[accountListId]/contacts.page.tsx
+++ pages/accountLists/[accountListId]/contacts.page.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head';
 import { useTranslation } from 'react-i18next';
 import { useRouter } from 'next/router';
 import { useContactsQuery } from './Contacts.generated';
+import { useContactFiltersLazyQuery } from './ContactFilters.generated';
 
 const ContactsPage: React.FC = () => {
   const { t } = useTranslation();
@@ -12,24 +13,57 @@ const ContactsPage: React.FC = () => {
   const { data, loading, error } = useContactsQuery({
     variables: { accountListId: accountListId as string },
   });
+  const [
+    loadContactFilters,
+    {
+      data: contactFilters,
+      loading: contactFiltersLoading,
+      error: contactFiltersError,
+    },
+  ] = useContactFiltersLazyQuery({
+    variables: { accountListId: accountListId as string },
+  });
 
   return (
     <>
       <Head>
         <title>MPDX | {t('Contacts')}</title>
       </Head>
-      {error && <p>Error: {error.toString()}</p>}
-      {loading ? (
-        <p>Loading</p>
-      ) : !data?.contacts?.nodes ? (
-        <p>No data</p>
-      ) : (
-        <ul>
-          {data.contacts.nodes.map(({ id, name }) => (
-            <li key={id}>{name}</li>
-          ))}
-        </ul>
-      )}
+      <div style={{ display: 'flex' }}>
+        <div style={{ margin: 5 }}>
+          <h2>Filters</h2>
+          <button onClick={() => loadContactFilters()}>Load Filters</button>
+          {contactFiltersError && (
+            <p>Error: {contactFiltersError.toString()}</p>
+          )}
+          {contactFiltersLoading ? (
+            <p>Loading Filters</p>
+          ) : !contactFilters?.contactFilters ? (
+            <p>No filters</p>
+          ) : (
+            <ul>
+              {contactFilters.contactFilters.map(({ id, name }) => (
+                <li key={id}>{name}</li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <div style={{ margin: 5 }}>
+          <h2>Contacts</h2>
+          {error && <p>Error: {error.toString()}</p>}
+          {loading ? (
+            <p>Loading</p>
+          ) : !data?.contacts?.nodes ? (
+            <p>No data</p>
+          ) : (
+            <ul>
+              {data.contacts.nodes.map(({ id, name }) => (
+                <li key={id}>{name}</li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
     </>
   );
 };
