a730cb773d596a5cbc505af6d3f92643dfb7d9f4
Scotty Waggoner <ozzieorca@gmail.com>
2021-03-08 16:36:56 -0800

    Add GraphQL automock tooling
    - Add graphql-ergonomock
    - Add helper function to make graphql-ergonomock support fragments and generated types

---
+++ testUtils/graphqlMocking.ts
@@ -0,0 +1,110 @@
+import {
+  buildSchema,
+  DocumentNode,
+  ExecutionResult,
+  GraphQLInterfaceType,
+  GraphQLObjectType,
+  GraphQLSchema,
+} from 'graphql';
+import { ergonomock, ErgonomockOptions } from 'graphql-ergonomock';
+import { mergeSchemas } from '@graphql-tools/merge';
+import { gql } from 'graphql-tag';
+import { DeepPartial } from 'ts-essentials';
+import schema from '../graphql/schema.graphql';
+
+// Adapted from https://github.com/smooth-code/fraql/blob/65674bc6a7f523ea3b20d8ecd34007a820cf5c67/src/mock.js#L16-L43
+const generateSchemaWithFragmentsAsQueries = (
+  schemaDocument: DocumentNode,
+): GraphQLSchema => {
+  const originalSchema = buildSchema(schemaDocument.loc.source.body);
+  const typeMap = originalSchema.getTypeMap();
+  const fields = Object.keys(typeMap).reduce((fields, typeName) => {
+    const type = typeMap[typeName];
+    if (
+      typeName.startsWith('__') ||
+      typeName === 'Query' ||
+      (!(type instanceof GraphQLObjectType) &&
+        !(type instanceof GraphQLInterfaceType))
+    ) {
+      return fields;
+    }
+    return {
+      ...fields,
+      [`fraql__${typeName}`]: {
+        type: typeMap[typeName],
+      },
+    };
+  }, {});
+
+  const fraqlSchema = new GraphQLSchema({
+    query: new GraphQLObjectType({ name: 'Query', fields }),
+  });
+
+  return mergeSchemas({
+    schemas: [originalSchema, fraqlSchema],
+  });
+};
+
+// Adapted from https://github.com/smooth-code/fraql/blob/65674bc6a7f523ea3b20d8ecd34007a820cf5c67/src/mock.js#L52-L78
+const ergonomockFragment = <TData>(
+  schema: DocumentNode,
+  query: DocumentNode,
+  options?: ErgonomockOptions,
+): TData => {
+  const fragmentDefinition = query.definitions[0];
+  if (fragmentDefinition.kind !== 'FragmentDefinition') {
+    throw new Error('ergonomockFragment only supports fragments');
+  }
+  const typeName = fragmentDefinition.typeCondition.name.value;
+  const fieldName = `fraql__${typeName}`;
+  const wrappedQuery = gql`
+    query {
+      ${fieldName} {
+        ...${fragmentDefinition.name.value}
+      }
+    }
+    ${query}
+  `;
+  const res = ergonomock(
+    generateSchemaWithFragmentsAsQueries(schema),
+    wrappedQuery,
+    { ...options, mocks: { [fieldName]: options.mocks } },
+  ) as ExecutionResult;
+
+  if (res.errors && res.errors.length) {
+    throw res.errors[0];
+  }
+
+  if (res.data[fieldName] === undefined) {
+    throw new Error(`fraql: type "${typeName}" not found`);
+  }
+  return res.data[fieldName];
+};
+
+const ergonomockQuery = <TData>(
+  schema: DocumentNode,
+  query: DocumentNode,
+  options?: ErgonomockOptions,
+): TData => {
+  const res = ergonomock(schema, query, options) as ExecutionResult;
+
+  if (res.errors && res.errors.length) {
+    throw res.errors[0];
+  }
+
+  return res.data as TData;
+};
+
+const documentContainsNonFragments = (document: DocumentNode): boolean =>
+  document.definitions.some(({ kind }) => kind !== 'FragmentDefinition');
+
+export const gqlMock = <TData, TVariables = never>(
+  query: DocumentNode,
+  options?: ErgonomockOptions & {
+    mocks?: DeepPartial<TData>;
+    variables?: TVariables;
+  },
+): TData =>
+  documentContainsNonFragments(query)
+    ? ergonomockQuery(schema, query, options)
+    : ergonomockFragment(schema, query, options);
