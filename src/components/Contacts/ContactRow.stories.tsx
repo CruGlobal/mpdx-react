3b4f1e85429ed919483edfaa3985d8327ba5da3f
Scotty Waggoner <ozzieorca@gmail.com>
2021-03-08 17:04:06 -0800

    Fix type error temporarily

--- src/components/Contacts/ContactRow.stories.tsx
+++ src/components/Contacts/ContactRow.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Story, Meta } from '@storybook/react';
 import { withDesign } from 'storybook-addon-designs';
 import { ContactRow } from './ContactRow';
+import { ContactRowFragment } from './ContactRow.generated';
 
 export default {
   title: 'Contacts/ContactRow',
@@ -10,7 +11,9 @@ export default {
 } as Meta;
 
 export const Default: Story = () => {
-  return <ContactRow contact={{ name: 'Contact Name' }} />;
+  return (
+    <ContactRow contact={{ name: 'Contact Name' } as ContactRowFragment} />
+  );
 };
 
 Default.parameters = {
