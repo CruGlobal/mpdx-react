f7b49a82ff7dedaeb153afe69cb8a50eb94547aa
Gyasi Story <gyasi.story@cru.org>
2021-03-10 12:06:07 -0500

    Create giftStatus.stories.tsx

---
+++ src/components/GiftStatus/giftStatus.stories.tsx
@@ -0,0 +1,39 @@
+import React, { ReactElement } from 'react';
+import { Box } from '@material-ui/core';
+import { boolean } from '@storybook/addon-knobs';
+import GiftStatus from '.';
+
+export default {
+  title: 'GiftStatus',
+};
+
+export const Default = (): ReactElement => {
+  return (
+    <Box m={2}>
+      <GiftStatus
+        isHidden={boolean('isHidden', true)}
+        isLate={boolean('isLate', false)}
+      />
+    </Box>
+  );
+};
+export const LateStatus = (): ReactElement => {
+  return (
+    <Box m={2}>
+      <GiftStatus
+        isHidden={boolean('isHidden', false)}
+        isLate={boolean('isLate', true)}
+      />
+    </Box>
+  );
+};
+export const OnTimeStatus = (): ReactElement => {
+  return (
+    <Box m={2}>
+      <GiftStatus
+        isHidden={boolean('isHidden', false)}
+        isLate={boolean('isLate', false)}
+      />
+    </Box>
+  );
+};
