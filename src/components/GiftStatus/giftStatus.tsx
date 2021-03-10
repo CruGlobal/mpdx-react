cf09245391dbff87d26a2312a79b321dcba37fa9
Gyasi Story <gyasi.story@cru.org>
2021-03-10 12:23:05 -0500

    Create giftStatus.tsx

---
+++ src/components/GiftStatus/giftStatus.tsx
@@ -0,0 +1,34 @@
+import React, { ReactElement } from 'react';
+import ErrorIcon from '@material-ui/icons/Error';
+import Circle from '@material-ui/icons/FiberManualRecord';
+import { Box, hexToRgb } from '@material-ui/core';
+
+interface Props {
+  isHidden?: boolean;
+  isLate?: boolean;
+}
+const GiftStatus = ({ isHidden, isLate }: Props): ReactElement => {
+  return (
+    <div>
+      {isHidden ? (
+        <Box data-testid="giftStatushidden" />
+      ) : (
+        <>
+          {isLate ? (
+            <ErrorIcon
+              style={{ color: hexToRgb('#F44336') }}
+              data-testid="giftStatusLate"
+            />
+          ) : (
+            <Circle
+              style={{ color: hexToRgb('#00CA99') }}
+              data-testid="giftStatusOnTime"
+            />
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default GiftStatus;
