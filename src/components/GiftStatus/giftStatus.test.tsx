f37c38a1833312d2a381c883bce0fd409377c6ab
Gyasi Story <gyasi.story@cru.org>
2021-03-10 12:23:09 -0500

    Create giftStatus.test.tsx

---
+++ src/components/GiftStatus/giftStatus.test.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import GiftStatus from '.';
+
+describe('GiftStatus', () => {
+  it('is Hidden', () => {
+    const { queryByTestId } = render(
+      <GiftStatus isHidden={true} isLate={false} />,
+    );
+    expect(queryByTestId('giftStatusLate')).toBeNull();
+    expect(queryByTestId('giftStatusOnTime')).toBeNull();
+  });
+  it('is Late', () => {
+    const { queryByTestId } = render(
+      <GiftStatus isHidden={true} isLate={false} />,
+    );
+    expect(queryByTestId('giftStatusHidden')).toBeNull();
+    expect(queryByTestId('giftStatusOnTime')).toBeNull();
+  });
+  it('is On Time', () => {
+    const { queryByTestId } = render(
+      <GiftStatus isHidden={true} isLate={false} />,
+    );
+    expect(queryByTestId('giftStatusHidden')).toBeNull();
+    expect(queryByTestId('giftStatusLate')).toBeNull();
+  });
+});
