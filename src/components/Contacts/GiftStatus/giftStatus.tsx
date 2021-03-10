3cfb1ae43535da9e53dffb823c3ee730df64d3f9
Gyasi Story <gyasi.story@cru.org>
2021-03-10 14:31:17 -0500

    using hex value

--- src/components/Contacts/GiftStatus/giftStatus.tsx
+++ src/components/Contacts/GiftStatus/giftStatus.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ErrorIcon from '@material-ui/icons/Error';
 import Circle from '@material-ui/icons/FiberManualRecord';
-import { Box, hexToRgb } from '@material-ui/core';
+import { Box } from '@material-ui/core';
 
 interface Props {
   isHidden?: boolean;
@@ -16,12 +16,12 @@ export const GiftStatus: React.FC<Props> = ({ isHidden, isLate }) => {
         <>
           {isLate ? (
             <ErrorIcon
-              style={{ color: hexToRgb('#F44336') }}
+              style={{ color: '#F44336' }}
               data-testid="giftStatusLate"
             />
           ) : (
             <Circle
-              style={{ color: hexToRgb('#00CA99') }}
+              style={{ color: '#00CA99' }}
               data-testid="giftStatusOnTime"
             />
           )}
