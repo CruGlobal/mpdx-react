d2829ab6cce9932da1ab35eba1acd4cc753ffafd
Gyasi Story <gyasi.story@cru.org>
2021-03-12 10:42:43 -0500

    MPDX-6947    Create Initial Checkbox View

--- src/components/Contacts/ContactRow.tsx
+++ src/components/Contacts/ContactRow.tsx
@@ -1,10 +1,25 @@
+import { makeStyles, Theme } from '@material-ui/core';
+import { CheckBox } from '@material-ui/icons';
 import React from 'react';
 import { ContactRowFragment } from './ContactRow.generated';
 
+const useStyles = makeStyles((theme: Theme) => ({
+  checkbox: {
+    width: '24px',
+    height: '24px',
+    left: '16px',
+    background: '#9C9FA1',
+  },
+}));
 interface Props {
   contact: ContactRowFragment;
 }
 
 export const ContactRow: React.FC<Props> = ({ contact }) => {
-  return <div>{contact.name}</div>;
+  const classes = useStyles();
+  return (
+    <div>
+      <CheckBox className={classes.checkbox} color="secondary" />
+    </div>
+  );
 };
