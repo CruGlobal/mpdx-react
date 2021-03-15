2fc62f59a07915df2c78106ad7d9aa51465ec14e
Gyasi Story <gyasi.story@cru.org>
2021-03-15 16:33:30 -0400

    MPDX-6947  use Flex CSS

--- src/components/Contacts/ContactRow.tsx
+++ src/components/Contacts/ContactRow.tsx
@@ -32,73 +32,36 @@ interface Props {
 export const ContactRow: React.FC<Props> = ({ contact }) => {
   const classes = useStyles();
   return (
-    <Box
-      style={{
-        height: '72px',
-        width: '100%',
-      }}
-    >
-      <CheckBox className={classes.checkbox} color="secondary" />
+    <Box style={{ position: 'relative', width: '100%' }}>
       <Box
         style={{
-          display: 'inline-block',
-          width: '40%',
-          padding: '0',
-          marginTop: '14px',
-          marginBottom: '14px',
-          marginLeft: '35px',
+          height: '72px',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-evenly',
+          alignItems: 'center',
         }}
       >
-        <p
-          style={{
-            fontSize: '16px',
-            lineHeight: '24px',
-            letterSpacing: '0.15px',
-          }}
-          className={classes.contactText}
-        >
-          {contact.name}
-        </p>
-        <p
-          style={{
-            lineHeight: '20px',
-            fontSize: '14px',
-            letterSpacing: '0.25',
-          }}
-          className={classes.contactText}
-        >
-          123 Seaseme Street
-        </p>
-      </Box>
-
-      <Hidden smDown>
-        <Box style={{ display: 'inline-block', margin: '10px' }}>
-          <CelebrationIcons hasBirthday={true} hasAnniversary={true} />
-        </Box>
-      </Hidden>
-
-      <Box style={{ display: 'inline-block', margin: '10px' }}>
-        <GiftStatus status={GiftStatusEnum.Late} />
-      </Box>
-
-      <Hidden mdDown>
+        <CheckBox className={classes.checkbox} color="secondary" />
         <Box
           style={{
             display: 'inline-block',
-            width: '40%',
-            minWidth: '400px',
-            margin: '10px',
+            flexGrow: 4,
+            padding: '0',
+            marginTop: '14px',
+            marginBottom: '14px',
+            marginLeft: '35px',
           }}
         >
           <p
             style={{
-              lineHeight: '20px',
-              fontSize: '14px',
-              letterSpacing: '0.25',
+              fontSize: '16px',
+              lineHeight: '24px',
+              letterSpacing: '0.15px',
             }}
             className={classes.contactText}
           >
-            Partner - Financial
+            {contact.name}
           </p>
           <p
             style={{
@@ -108,12 +71,53 @@ export const ContactRow: React.FC<Props> = ({ contact }) => {
             }}
             className={classes.contactText}
           >
-            1250 CAD Monthly (60+ days late)
+            123 Seaseme Street
           </p>
         </Box>
-      </Hidden>
 
-      <StarContactIcon hasStar={true} />
+        <Hidden smDown>
+          <Box style={{ display: 'inline-block', margin: '10px' }}>
+            <CelebrationIcons hasBirthday={true} hasAnniversary={true} />
+          </Box>
+        </Hidden>
+
+        <Box style={{ display: 'inline-block', margin: '10px' }}>
+          <GiftStatus status={GiftStatusEnum.Late} />
+        </Box>
+
+        <Hidden mdDown>
+          <Box
+            style={{
+              display: 'inline-block',
+              flexGrow: 4,
+              margin: '10px',
+            }}
+          >
+            <p
+              style={{
+                lineHeight: '20px',
+                fontSize: '14px',
+                letterSpacing: '0.25',
+              }}
+              className={classes.contactText}
+            >
+              Partner - Financial
+            </p>
+            <p
+              style={{
+                lineHeight: '20px',
+                fontSize: '14px',
+                letterSpacing: '0.25',
+              }}
+              className={classes.contactText}
+            >
+              1250 CAD Monthly (60+ days late)
+            </p>
+          </Box>
+        </Hidden>
+
+        <StarContactIcon hasStar={true} />
+      </Box>
       <hr
         style={{
           display: 'block',
