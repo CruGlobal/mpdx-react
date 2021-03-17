24ed7fbc4b8f44f17ce242e09b31de833c49924a
Robert Eldredge <robert.eldredge@cru.org>
2021-03-17 15:49:06 -0400

    ContactsTable

---
+++ src/components/Contacts/ContactsTable/ContactsTable.tsx
@@ -0,0 +1,51 @@
+import { Box, colors, Table } from '@material-ui/core';
+import React from 'react';
+import { ContactRow } from '../ContactRow';
+import { ContactRowFragment } from '../ContactRow/ContactRow.generated';
+
+interface Props {
+  contacts: ContactRowFragment[] | undefined;
+  loading: boolean;
+  error: Error;
+}
+
+const ContactsTable: React.FC<Props> = ({
+  contacts = [],
+  loading,
+  error,
+}: Props) => {
+  const renderLoading = () => (
+    <Box flex={1} height={'100%'} bgcolor={colors.green[600]}>
+      <p>Loading</p>
+    </Box>
+  );
+
+  const renderEmpty = () => (
+    <div style={{ backgroundColor: colors.yellow[600] }}>No Data</div>
+  );
+
+  const renderError = () => (
+    <div style={{ backgroundColor: colors.red[600] }}>
+      Error: {error.toString()}
+    </div>
+  );
+
+  return (
+    <Box flex={1}>
+      {error && renderError()}
+      {!loading ? (
+        renderLoading()
+      ) : contacts.length === 0 ? (
+        renderEmpty()
+      ) : (
+        <Table>
+          {contacts.map((contact) => (
+            <ContactRow key={contact.id} contact={contact} />
+          ))}
+        </Table>
+      )}
+    </Box>
+  );
+};
+
+export default ContactsTable;
