a0992aaa1ba5e5f40bf8b133f0ce6e0ed39f40ce
Robert Eldredge <robert.eldredge@cru.org>
2021-03-17 16:12:05 -0400

    contacts screen

--- pages/accountLists/[accountListId]/contacts.page.tsx
+++ pages/accountLists/[accountListId]/contacts.page.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import Head from 'next/head';
 import { useTranslation } from 'react-i18next';
 import { useRouter } from 'next/router';
-import { ContactRow } from '../../../src/components/Contacts/ContactRow';
+import Contacts from '../../../src/components/Contacts/Contacts';
 import { useApp } from '../../../src/components/App';
 import { useContactsQuery } from './Contacts.generated';
 import { useContactFiltersLazyQuery } from './ContactFilters.generated';
@@ -13,13 +13,17 @@ const ContactsPage: React.FC = () => {
   const {
     query: { accountListId },
   } = useRouter();
-  const { data, loading, error } = useContactsQuery({
+  const {
+    data: contactsData,
+    loading: contactsLoading,
+    error: contactsError,
+  } = useContactsQuery({
     variables: { accountListId: accountListId as string },
   });
   const [
     loadContactFilters,
     {
-      data: contactFilters,
+      data: contactFiltersData,
       loading: contactFiltersLoading,
       error: contactFiltersError,
     },
@@ -36,39 +40,15 @@ const ContactsPage: React.FC = () => {
       <Head>
         <title>MPDX | {t('Contacts')}</title>
       </Head>
-      <div style={{ display: 'flex' }}>
-        <div style={{ margin: 5 }}>
-          <h2>Filters</h2>
-          <button onClick={() => loadContactFilters()}>Load Filters</button>
-          {contactFiltersError && (
-            <p>Error: {contactFiltersError.toString()}</p>
-          )}
-          {contactFiltersLoading ? (
-            <p>Loading Filters</p>
-          ) : !contactFilters?.contactFilters ? (
-            <p>No filters</p>
-          ) : (
-            <ul>
-              {contactFilters.contactFilters.map(({ id, name }) => (
-                <li key={id}>{name}</li>
-              ))}
-            </ul>
-          )}
-        </div>
-        <div style={{ margin: 5 }}>
-          <h2>Contacts</h2>
-          {error && <p>Error: {error.toString()}</p>}
-          {loading ? (
-            <p>Loading</p>
-          ) : !data?.contacts?.nodes ? (
-            <p>No data</p>
-          ) : (
-            data.contacts.nodes.map((contact) => (
-              <ContactRow key={contact.id} contact={contact} />
-            ))
-          )}
-        </div>
-      </div>
+      <Contacts
+        contactsData={contactsData}
+        contactsLoading={contactsLoading}
+        contactsError={contactsError}
+        filtersData={contactFiltersData}
+        filtersLoading={contactFiltersLoading}
+        filtersError={contactFiltersError}
+        loadContactFilters={loadContactFilters}
+      />
     </>
   );
 };
---
+++ src/components/Contacts/Contacts.tsx
@@ -0,0 +1,55 @@
+import React, { ReactElement } from 'react';
+import { QueryLazyOptions } from '@apollo/client';
+import { Box, colors } from '@material-ui/core';
+import { Exact } from '../../../graphql/types.generated';
+import { ContactsQuery } from '../../../pages/accountLists/[accountListId]/Contacts.generated';
+import { ContactFiltersQuery } from '../../../pages/accountLists/[accountListId]/ContactFilters.generated';
+import ContactFilters from './ContactFilters/ContactFilters';
+import ContactsHeader from './ContactsHeader/ContactsHeader';
+import ContactsTable from './ContactsTable/ContactsTable';
+
+interface Props {
+  contactsData: ContactsQuery;
+  contactsLoading: boolean;
+  contactsError: Error;
+  filtersData: ContactFiltersQuery;
+  filtersLoading: boolean;
+  filtersError: Error;
+  loadContactFilters: (
+    options: QueryLazyOptions<Exact<{ accountListId: string }>>,
+  ) => void;
+}
+
+const Contacts = ({
+  contactsData,
+  contactsLoading,
+  contactsError,
+  filtersData,
+  filtersLoading,
+  filtersError,
+  loadContactFilters,
+}: Props): ReactElement => {
+  return (
+    <Box display="flex" border={1} borderColor={colors.red[600]}>
+      <Box flex={1}>
+        <ContactFilters
+          style={{ flex: 1 }}
+          data={filtersData}
+          loading={filtersLoading}
+          error={filtersError}
+          loadFilters={loadContactFilters}
+        />
+      </Box>
+      <Box flex={4} flexDirection={'column'}>
+        <ContactsHeader />
+        <ContactsTable
+          contacts={contactsData?.contacts.nodes}
+          loading={contactsLoading}
+          error={contactsError}
+        />{' '}
+      </Box>
+    </Box>
+  );
+};
+
+export default Contacts;
