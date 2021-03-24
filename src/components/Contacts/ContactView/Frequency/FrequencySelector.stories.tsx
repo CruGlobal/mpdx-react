d4b46f17ae6979c49c91d2c3e40a921ca4af9881
Gyasi Story <gyasi.story@cru.org>
2021-03-24 16:15:28 -0400

    Create FrequencySelector.stories.tsx

---
+++ src/components/Contacts/ContactView/Frequency/FrequencySelector.stories.tsx
@@ -0,0 +1,22 @@
+import { Box } from '@material-ui/core';
+import { array, text } from '@storybook/addon-knobs';
+import { ReactElement } from 'react';
+import { FrequencySelector } from './FrequencySelector';
+
+export default {
+  title: 'Contacts/ContactView/Widgets/FrequencySelector',
+};
+
+export const Default = (): ReactElement => {
+  const values = ['Monthly', 'Quarterly', 'Annually'];
+  const knobValue = array('frequencies', values);
+  return (
+    <Box m={2}>
+      <FrequencySelector
+        frequencies={knobValue}
+        selectedFrequency={text('frequency', 'Monthly')}
+        saveFrequency={null}
+      />
+    </Box>
+  );
+};
