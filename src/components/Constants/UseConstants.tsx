377d518478ba92883ee13c1cd9b12b2ef03afc1e
Gyasi Story <gyasi.story@cru.org>
2021-03-25 14:53:55 -0400

    MPDX-6958 add comment for use

--- src/components/Constants/UseConstants.tsx
+++ src/components/Constants/UseConstants.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@apollo/client';
 import { LoadConstantsDocument } from './LoadConstants.generated';
 
+// Use this call to force loading from cache storage first
 export const UseApiConstants = useQuery(LoadConstantsDocument, {
   fetchPolicy: 'cache-first',
 });
