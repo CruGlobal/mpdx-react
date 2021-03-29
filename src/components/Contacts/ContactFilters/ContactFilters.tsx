ab382d801b1648bb592c39fe813184b08646b66a
Daniel Frett <daniel.frett@ccci.org>
2021-03-29 16:09:46 -0400

    add nesting to the contact filter menu
    this attempts to use a Slide transition to slide between the menus, but there is currently some CSS weirdness with it

--- src/components/Contacts/ContactFilters/ContactFilters.tsx
+++ src/components/Contacts/ContactFilters/ContactFilters.tsx
@@ -1,15 +1,24 @@
 import {
   Box,
-  Button,
   CircularProgress,
   Collapse,
+  IconButton,
   List,
   ListItem,
+  ListItemIcon,
+  ListItemSecondaryAction,
   ListItemText,
+  Slide,
   Typography,
 } from '@material-ui/core';
-import { ExpandLess, ExpandMore } from '@material-ui/icons';
-import React, { useState } from 'react';
+import {
+  ArrowBackIos,
+  ArrowForwardIos,
+  ExpandLess,
+  ExpandMore,
+} from '@material-ui/icons';
+import React, { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { useContactFiltersLazyQuery } from './ContactFilters.generated';
 
 interface Props {
@@ -21,59 +30,112 @@ export const ContactFilters: React.FC<Props> = ({ accountListId }: Props) => {
     loadContactFilters,
     { data, loading, error },
   ] = useContactFiltersLazyQuery({ variables: { accountListId } });
+  const { t } = useTranslation();
+
   const [showAll, setShowAll] = useState(false);
+  const [showSubMenu, setShowSubMenu] = useState<null | string>(null);
+
+  useEffect(() => {
+    loadContactFilters();
+  }, []);
 
   return (
-    <>
-      <Box px={2} pt={2}>
-        <Typography variant="h6">Filter</Typography>
-        {error && (
-          <Typography variant="subtitle1" color="error" data-testID="ErrorText">
-            Error: {error.toString()}
-          </Typography>
-        )}
-        {/* {!data && (
-          <button onClick={() => loadContactFilters()}>Load Filters</button>
-        )} */}
-      </Box>
-      {loading ? (
-        <Box px={2}>
-          <CircularProgress />
-        </Box>
-      ) : !data ? (
-        <Box px={2}>
-          <Button color="primary" onClick={() => loadContactFilters()}>
-            Load Filters
-          </Button>
-        </Box>
-      ) : !data || data.contactFilters.length === 0 ? (
-        <Box px={2}>No Filters</Box>
-      ) : (
-        <List dense={true} data-testID="FiltersList">
-          {data.contactFilters.map(({ id, title, featured }) => (
-            <Collapse key={id} in={showAll || featured}>
-              <ListItem>
-                <Typography variant="subtitle1">{title}</Typography>
+    <div>
+      <div style={{ overflow: 'hidden' }}>
+        <Slide
+          in={showSubMenu == null}
+          direction="right"
+          appear={false}
+          mountOnEnter
+          unmountOnExit
+        >
+          <div>
+            <Box px={2} pt={2}>
+              <Typography variant="h6">{t('Filter')}</Typography>
+            </Box>
+            <List dense={true} data-testID="FiltersList">
+              <ListItem button onClick={() => setShowSubMenu('tags')}>
+                <ListItemText
+                  primary="Tags"
+                  primaryTypographyProps={{ variant: 'subtitle1' }}
+                />
+                <ArrowForwardIos fontSize="small" color="disabled" />
               </ListItem>
-            </Collapse>
-          ))}
-          <ListItem button onClick={() => setShowAll(!showAll)}>
-            <ListItemText
-              color="primary"
-              primary={showAll ? 'See Fewer Filters' : 'See More Filters'}
-              primaryTypographyProps={{
-                color: 'primary',
-                variant: 'subtitle1',
-              }}
-            />
-            {showAll ? (
-              <ExpandLess color="primary" />
-            ) : (
-              <ExpandMore color="primary" />
+              <ListItem button onClick={() => setShowSubMenu('other')}>
+                <ListItemText
+                  primary="Other"
+                  primaryTypographyProps={{ variant: 'subtitle1' }}
+                />
+                <ListItemSecondaryAction>
+                  <IconButton edge="end" aria-label="delete">
+                    <ArrowForwardIos fontSize="small" color="disabled" />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+              <ListItem button onClick={() => setShowAll(!showAll)}>
+                <ListItemText
+                  color="primary"
+                  primary={showAll ? 'See Fewer Filters' : 'See More Filters'}
+                  primaryTypographyProps={{
+                    color: 'primary',
+                    variant: 'subtitle1',
+                  }}
+                />
+                {showAll ? (
+                  <ExpandLess color="primary" />
+                ) : (
+                  <ExpandMore color="primary" />
+                )}
+              </ListItem>
+            </List>
+          </div>
+        </Slide>
+        <Slide
+          in={showSubMenu != null}
+          direction="left"
+          mountOnEnter
+          unmountOnExit
+        >
+          <List dense={true} data-testID="FiltersList">
+            <ListItem button onClick={() => setShowSubMenu(null)}>
+              <ListItemIcon>
+                <ArrowBackIos fontSize="small" />
+              </ListItemIcon>
+              <ListItemText
+                primary="Tags"
+                primaryTypographyProps={{ variant: 'h6' }}
+              />
+            </ListItem>
+            {error && (
+              <ListItem>
+                <ListItemText
+                  primary="Error: {error.toString()}"
+                  primaryTypographyProps={{
+                    variant: 'subtitle1',
+                    color: 'error',
+                  }}
+                />
+              </ListItem>
             )}
-          </ListItem>
-        </List>
-      )}
-    </>
+            {loading ? (
+              <ListItem>
+                <CircularProgress />
+              </ListItem>
+            ) : (
+              data?.contactFilters?.map(({ id, title, featured }) => (
+                <Collapse key={id} in={showAll || featured}>
+                  <ListItem>
+                    <ListItemText
+                      primary={title}
+                      primaryTypographyProps={{ variant: 'subtitle1' }}
+                    />
+                  </ListItem>
+                </Collapse>
+              ))
+            )}
+          </List>
+        </Slide>
+      </div>
+    </div>
   );
 };
