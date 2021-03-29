05e04155f38a216a682e9b049966901c90683ac3
Daniel Frett <daniel.frett@ccci.org>
2021-03-29 11:55:28 -0400

    update the contact filters to utilize Material UI

--- src/components/Contacts/ContactFilters/ContactFilters.tsx
+++ src/components/Contacts/ContactFilters/ContactFilters.tsx
@@ -1,5 +1,15 @@
-import { colors } from '@material-ui/core';
-import React from 'react';
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Collapse,
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+} from '@material-ui/core';
+import { ExpandLess, ExpandMore } from '@material-ui/icons';
+import React, { useState } from 'react';
 import { useContactFiltersLazyQuery } from './ContactFilters.generated';
 
 interface Props {
@@ -10,26 +20,60 @@ export const ContactFilters: React.FC<Props> = ({ accountListId }: Props) => {
   const [
     loadContactFilters,
     { data, loading, error },
-  ] = useContactFiltersLazyQuery({
-    variables: { accountListId },
-  });
+  ] = useContactFiltersLazyQuery({ variables: { accountListId } });
+  const [showAll, setShowAll] = useState(false);
 
   return (
-    <div style={{ backgroundColor: colors.amber[600] }}>
-      <h2>Filters</h2>
-      <button onClick={() => loadContactFilters()}>Load Filters</button>
-      {error && <p data-testID="ErrorText">Error: {error.toString()}</p>}
+    <>
+      <Box px={2} pt={2}>
+        <Typography variant="h6">Filter</Typography>
+        {error && (
+          <Typography variant="subtitle1" color="error" data-testID="ErrorText">
+            Error: {error.toString()}
+          </Typography>
+        )}
+        {/* {!data && (
+          <button onClick={() => loadContactFilters()}>Load Filters</button>
+        )} */}
+      </Box>
       {loading ? (
-        <p>Loading Filters</p>
+        <Box px={2}>
+          <CircularProgress />
+        </Box>
+      ) : !data ? (
+        <Box px={2}>
+          <Button color="primary" onClick={() => loadContactFilters()}>
+            Load Filters
+          </Button>
+        </Box>
       ) : !data || data.contactFilters.length === 0 ? (
-        <p>No Filters</p>
+        <Box px={2}>No Filters</Box>
       ) : (
-        <ul data-testID="FiltersList">
-          {data.contactFilters.map(({ id, name }) => (
-            <li key={id}>{name}</li>
+        <List dense={true} data-testID="FiltersList">
+          {data.contactFilters.map(({ id, title, featured }) => (
+            <Collapse key={id} in={showAll || featured}>
+              <ListItem>
+                <Typography variant="subtitle1">{title}</Typography>
+              </ListItem>
+            </Collapse>
           ))}
-        </ul>
+          <ListItem button onClick={() => setShowAll(!showAll)}>
+            <ListItemText
+              color="primary"
+              primary={showAll ? 'See Fewer Filters' : 'See More Filters'}
+              primaryTypographyProps={{
+                color: 'primary',
+                variant: 'subtitle1',
+              }}
+            />
+            {showAll ? (
+              <ExpandLess color="primary" />
+            ) : (
+              <ExpandMore color="primary" />
+            )}
+          </ListItem>
+        </List>
       )}
-    </div>
+    </>
   );
 };
