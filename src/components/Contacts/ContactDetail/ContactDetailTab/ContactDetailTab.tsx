4776e4437ab8901f4c4dbd43d6aae7d48b489cdc
Gyasi Story <gyasi.story@cru.org>
2021-04-01 15:50:33 -0400

    MPDX-6975  Clean up design

--- src/components/Contacts/ContactDetail/ContactDetailTab/ContactDetailTab.tsx
+++ src/components/Contacts/ContactDetail/ContactDetailTab/ContactDetailTab.tsx
@@ -1,35 +1,44 @@
-import {
-  Box,
-  Button,
-  makeStyles,
-  Theme,
-  ThemeProvider,
-} from '@material-ui/core';
+import { Box, Button, makeStyles, styled } from '@material-ui/core';
 import { Create } from '@material-ui/icons';
 import React from 'react';
-import theme from '../../../../theme';
+import { useTranslation } from 'react-i18next';
 import { ContactDetailQuery } from '../ContactDetail.generated';
 
-const useStyles = makeStyles((theme: Theme) => ({
-  contactDetailHeadingContainer: {
-    display: 'flex',
-    flexDirection: 'row',
-    alignContent: 'center',
-    alignItems: 'center',
-  },
+const ContactDetailSectionContainer = styled(Box)(({ theme }) => ({
+  padding: theme.spacing(0),
+  paddingTop: '4px',
+  margin: 0,
+  alignContent: 'start',
+}));
+
+const ContactDetailHeadingContainer = styled(Box)(() => ({
+  display: 'flex',
+  flexDirection: 'row',
+  alignContent: 'center',
+  alignItems: 'center',
+}));
+
+const ContactDetailHeadingIcon = styled(Create)(({ theme }) => ({
+  width: '18px',
+  height: '18px',
+  margin: theme.spacing(0),
+  color: '#9C9FA1',
+}));
+
+const ContactDeleteButton = styled(Button)(({ theme }) => ({
+  display: 'block',
+  padding: theme.spacing(2),
+  margin: theme.spacing(5),
+  marginLeft: 'auto',
+  marginRight: 'auto',
+}));
+
+const useStyles = makeStyles(() => ({
   contactDetailHeadingText: {
     flexGrow: 5,
   },
-  contactDetailHeadingIcon: {
-    width: '18px',
-    height: '18px',
-    margin: theme.spacing(0),
-    color: '#9C9FA1',
-  },
-  contactDetailContainer: {
-    padding: theme.spacing(1),
-  },
 }));
+
 interface ContactDetailTabProps {
   contact: ContactDetailQuery;
 }
@@ -39,49 +48,50 @@ export const ContactDetailTab: React.FC<ContactDetailTabProps> = ({
 }) => {
   const classes = useStyles();
   const calledContact = contact.contact;
+  const { t } = useTranslation();
 
   return (
-    <ThemeProvider theme={theme}>
-      <Box style={{ width: '100%', paddingRight: '60px', paddingLeft: '60px' }}>
-        <Box
-          id="contact-detail-tags"
-          className={classes.contactDetailContainer}
-        ></Box>
-        <hr />
-        <Box
-          id="contact-detail-people"
-          className={classes.contactDetailContainer}
-        >
-          <Box className={classes.contactDetailHeadingContainer}>
-            <h6 className={classes.contactDetailHeadingText}>
-              {calledContact.name}
-            </h6>
-            <Create className={classes.contactDetailHeadingIcon} />
-          </Box>
-        </Box>
-        <hr />
-        <Box
-          id="contact-detail-mailing"
-          className={classes.contactDetailContainer}
-        >
-          <Box className={classes.contactDetailHeadingContainer}>
-            <h6 className={classes.contactDetailHeadingText}>Mailing</h6>
-            <Create className={classes.contactDetailHeadingIcon} />
-          </Box>
-        </Box>
-        <hr />
-        <Box
-          id="contact-detail-other"
-          className={classes.contactDetailContainer}
-        >
-          <Box className={classes.contactDetailHeadingContainer}>
-            <h6 className={classes.contactDetailHeadingText}>Other</h6>
-            <Create className={classes.contactDetailHeadingIcon} />
-          </Box>
-        </Box>
-        <hr />
-        <Button>DELETE CONTACT</Button>
-      </Box>
-    </ThemeProvider>
+    <Box style={{ width: '100%', paddingRight: '5%', paddingLeft: '5%' }}>
+      {
+        // Tag Section
+      }
+      <ContactDetailHeadingContainer></ContactDetailHeadingContainer>
+      <hr />
+      {
+        // People Section
+      }
+      <ContactDetailSectionContainer>
+        <ContactDetailHeadingContainer>
+          <h6 className={classes.contactDetailHeadingText}>
+            {calledContact.name}
+          </h6>
+          <ContactDetailHeadingIcon />
+        </ContactDetailHeadingContainer>
+      </ContactDetailSectionContainer>
+      <hr />
+      {
+        // Mailing Section
+      }
+      <ContactDetailSectionContainer>
+        <ContactDetailHeadingContainer>
+          <h6 className={classes.contactDetailHeadingText}>{t('Mailing')}</h6>
+          <ContactDetailHeadingIcon />
+        </ContactDetailHeadingContainer>
+      </ContactDetailSectionContainer>
+      <hr />
+      {
+        // other Section
+      }
+      <ContactDetailSectionContainer>
+        <ContactDetailHeadingContainer>
+          <h6 className={classes.contactDetailHeadingText}>{t('Other')}</h6>
+          <ContactDetailHeadingIcon />
+        </ContactDetailHeadingContainer>
+      </ContactDetailSectionContainer>
+      <hr />
+      <ContactDeleteButton variant="outlined" color="default">
+        {t('DELETE CONTACT').toLocaleUpperCase()}
+      </ContactDeleteButton>
+    </Box>
   );
 };
