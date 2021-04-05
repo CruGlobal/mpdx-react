897fb82c525c6adf17e1107c84011dc25f7011e7
Robert Eldredge <robert.eldredge@cru.org>
2021-04-05 14:00:02 -0400

    Work on top of header

--- src/components/Contacts/ContactDetails/ContactDetailsHeader/ContactDetailsHeader.tsx
+++ src/components/Contacts/ContactDetails/ContactDetailsHeader/ContactDetailsHeader.tsx
@@ -1,4 +1,5 @@
 import { Box } from '@material-ui/core';
+import { Close, MoreVert, StarOutline } from '@material-ui/icons';
 import React from 'react';
 
 import { useGetContactDetailsHeaderQuery } from './ContactDetailsHeader.generated';
@@ -16,14 +17,47 @@ export const ContactDetailsHeader: React.FC<Props> = ({
     variables: { accountListId, contactId },
   });
 
+  if (loading) {
+    return <p>loading</p>;
+  }
+
+  const { contact } = data;
+
+  if (!contact) {
+    return null;
+  }
+
   return (
-    <Box>
-      {/*TODO: Build Header*/}
-      {loading ? (
-        <p>loading</p>
-      ) : (
-        <p role="contactName">{data?.contact.name}</p>
-      )}
+    <Box
+      style={{
+        display: 'flex',
+      }}
+    >
+      <Box
+        style={{
+          flex: 1,
+          alignItems: 'center',
+        }}
+      >
+        <Box
+          style={{
+            backgroundColor: '#000000',
+            height: 64,
+            width: 64,
+            borderRadius: 32,
+            display: 'inline-block',
+          }}
+        />
+        <p
+          style={{ display: 'inline-block' }}
+        >{`${contact.primaryPerson?.firstName} ${contact.primaryPerson?.lastName}`}</p>
+        <p style={{ display: 'inline-block' }}>{'- Primary'}</p>
+      </Box>
+      <Box alignItems="center">
+        <StarOutline style={{ display: 'inline-block' }} />
+        <MoreVert style={{ display: 'inline-block' }} />
+        <Close style={{ display: 'inline-block' }} />
+      </Box>
     </Box>
   );
 };
