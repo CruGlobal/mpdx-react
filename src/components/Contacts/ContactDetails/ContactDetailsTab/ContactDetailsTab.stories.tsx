a4532be326b4b172290cf8460824b76f2e1ac31e
Gyasi Story <gyasi.story@cru.org>
2021-04-06 14:33:58 -0400

    MPDX-6975 add loading Story

--- src/components/Contacts/ContactDetails/ContactDetailsTab/ContactDetailsTab.stories.tsx
+++ src/components/Contacts/ContactDetails/ContactDetailsTab/ContactDetailsTab.stories.tsx
@@ -1,7 +1,12 @@
+import { MockedProvider } from '@apollo/client/testing';
 import React, { ReactElement } from 'react';
 import { GqlMockedProvider } from '../../../../../__tests__/util/graphqlMocking';
 import { ContactDetailsTab } from './ContactDetailsTab';
-import { ContactDetailsTabQuery } from './ContactDetailsTab.generated';
+import {
+  ContactDetailsTabDocument,
+  ContactDetailsTabQuery,
+  useContactDetailsTabQuery,
+} from './ContactDetailsTab.generated';
 
 export default {
   title: 'Contacts/Tab/ContactDetailsTab',
@@ -16,5 +21,27 @@ export const Default = (): ReactElement => {
     <GqlMockedProvider<ContactDetailsTabQuery>>
       <ContactDetailsTab accountListId={accountListId} contactId={contactId} />
     </GqlMockedProvider>
+  );
+};
+
+export const Loading = (): ReactElement => {
+  return (
+    <MockedProvider
+      mocks={[
+        {
+          request: {
+            query: ContactDetailsTabDocument,
+            variables: {
+              accountListId: accountListId,
+              contactId: contactId,
+            },
+          },
+          result: {},
+          delay: 8640000,
+        },
+      ]}
+    >
+      <ContactDetailsTab accountListId={accountListId} contactId={contactId} />
+    </MockedProvider>
   );
 };
