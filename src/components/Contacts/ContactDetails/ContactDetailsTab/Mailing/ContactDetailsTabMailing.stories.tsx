c7c296348bf843c625f4445fe8f99ffae1b4f789
Gyasi Story <gyasi.story@cru.org>
2021-04-07 09:45:38 -0400

    MPDX-6975 add mock data to Storybook

--- src/components/Contacts/ContactDetails/ContactDetailsTab/Mailing/ContactDetailsTabMailing.stories.tsx
+++ src/components/Contacts/ContactDetails/ContactDetailsTab/Mailing/ContactDetailsTabMailing.stories.tsx
@@ -1,15 +1,29 @@
-import { Box } from '@material-ui/core';
+import { Box, CssBaseline, MuiThemeProvider } from '@material-ui/core';
 import React, { ReactElement } from 'react';
+import { gqlMock } from '../../../../../../__tests__/util/graphqlMocking';
+import theme from '../../../../../theme';
+import {
+  ContactDetailsTabDocument,
+  ContactDetailsTabQuery,
+  ContactDetailsTabQueryVariables,
+} from '../ContactDetailsTab.generated';
 import { ContactDetailsTabMailing } from './ContactDetailsTabMailing';
 
 export default {
-  title: 'Contacts/Tab/ContactDetails/Mailing',
+  title: 'Contacts/Tab/ContactDetailsTab/Mailing',
 };
 
 export const Default = (): ReactElement => {
+  const mock = gqlMock<ContactDetailsTabQuery, ContactDetailsTabQueryVariables>(
+    ContactDetailsTabDocument,
+    { variables: { accountListId: '111', contactId: '222' } },
+  );
   return (
     <Box m={2}>
-      <ContactDetailsTabMailing />
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <ContactDetailsTabMailing data={mock} />
+      </MuiThemeProvider>
     </Box>
   );
 };
