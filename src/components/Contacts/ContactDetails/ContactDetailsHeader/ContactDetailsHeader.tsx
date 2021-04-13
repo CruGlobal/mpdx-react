0f725b710a04e01d67f53fb441efa9f22f633b93
Robert Eldredge <robert.eldredge@cru.org>
2021-04-13 16:51:03 -0400

    remove unneeded reference

--- src/components/Contacts/ContactDetails/ContactDetailsHeader/ContactDetailsHeader.tsx
+++ src/components/Contacts/ContactDetails/ContactDetailsHeader/ContactDetailsHeader.tsx
@@ -1,6 +1,5 @@
 import { Box } from '@material-ui/core';
 import React from 'react';
-import theme from '../../../../theme';
 
 import { useGetContactDetailsHeaderQuery } from './ContactDetailsHeader.generated';
 
