9f595187ade4f19f610eec64d1d4bc10b067aec2
Robert Eldredge <robert.eldredge@cru.org>
2021-04-16 13:39:42 -0400

    Get the elements in there

--- src/components/Contacts/ContactsHeader/ContactsHeader.tsx
+++ src/components/Contacts/ContactsHeader/ContactsHeader.tsx
@@ -1,10 +1,92 @@
-import { Box, colors } from '@material-ui/core';
-import React from 'react';
+import { Box, styled, Checkbox, IconButton } from '@material-ui/core';
+import { FilterList } from '@material-ui/icons';
+import React, { useState } from 'react';
+import theme from '../../../theme';
+import { StarContactIcon } from '../StarContactIcon/StarContactIcon';
+
+const HeaderWrap = styled(Box)(({}) => ({
+  height: 96,
+  backgroundColor: theme.palette.background.default,
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  alignContent: 'center',
+}));
+const StyledCheckBox = styled(Checkbox)(({}) => ({
+  display: 'inline-block',
+  width: '18px',
+  height: '18px',
+  margin: theme.spacing(1),
+  color: theme.palette.secondary.dark,
+  checked: {
+    color: theme.palette.primary.dark,
+  },
+}));
+const FilterButton = styled(IconButton)(({}) => ({
+  display: 'inline-block',
+  width: 48,
+  height: 48,
+  borderRadius: 24,
+  margin: theme.spacing(1),
+  backgroundColor: '#FFCF07',
+}));
+const FilterIcon = styled(FilterList)(({}) => ({
+  width: 24,
+  height: 24,
+  color: theme.palette.primary.dark,
+}));
+const PlaceholderSearchBar = styled(Box)(({}) => ({
+  display: 'inline-block',
+  width: 256,
+  height: 48,
+  margin: theme.spacing(1),
+  backgroundColor: 'red',
+}));
+const ContactsShowingText = styled('p')(({}) => ({}));
+const PlaceholderActionsDropdown = styled(Box)(({}) => ({
+  display: 'inline-block',
+  width: 114,
+  height: 48,
+  margin: theme.spacing(1),
+  backgroundColor: 'red',
+}));
+const DisplayOptionsWrap = styled(Box)(({}) => ({
+  display: 'inline-block',
+  width: 96,
+  height: 48,
+  margin: theme.spacing(1),
+  border: '1px solid #000000',
+}));
+const DisplayOptionButton = styled(IconButton)(({}) => ({
+  display: 'inline-block',
+  width: 48,
+  height: 48,
+}));
 
 export const ContactsHeader: React.FC = () => {
+  const [allContactsChecked, setAllContactsChecked] = useState(false);
+
+  const toggleAllContactsCheckbox = () => {
+    setAllContactsChecked(!allContactsChecked);
+  };
+
   return (
-    <Box height={200} bgcolor={colors.blue[600]}>
-      {/*TODO: Build the Header */}
-    </Box>
+    <HeaderWrap>
+      <StyledCheckBox
+        checked={allContactsChecked}
+        onChange={toggleAllContactsCheckbox}
+      />
+      <FilterButton>
+        <FilterIcon />
+      </FilterButton>
+      <PlaceholderSearchBar />
+      <ContactsShowingText>Showing 43</ContactsShowingText>
+      <PlaceholderActionsDropdown />
+      <DisplayOptionsWrap>
+        <DisplayOptionButton />
+        <DisplayOptionButton />
+      </DisplayOptionsWrap>
+      <StarContactIcon hasStar={false} />
+    </HeaderWrap>
   );
 };
