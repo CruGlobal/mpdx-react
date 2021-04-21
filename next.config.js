8c97f2d415d6b16049821961115fff4162797769
Scotty Waggoner <ozzieorca@gmail.com>
2021-04-21 13:47:39 -0700

    Enable production sourcemaps

--- next.config.js
+++ next.config.js
@@ -51,5 +51,6 @@ module.exports = withPlugins([
     },
     // Force .page prefix on page files (ex. index.page.tsx) so generated files can be included in /pages directory without Next.js throwing build errors
     pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+    productionBrowserSourceMaps: true,
   },
 ]);
