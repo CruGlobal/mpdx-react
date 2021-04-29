b06f4aba024a22aa7498e56f9ed934d7693180e3
Gyasi Story <gyasi.story@cru.org>
2021-04-29 13:36:18 -0400

    MPDX-6974 update test to cover View

--- src/components/Contacts/ContactDetails/ContactReferralTab/ContactReferralTab.test.tsx
+++ src/components/Contacts/ContactDetails/ContactReferralTab/ContactReferralTab.test.tsx
@@ -1,6 +1,12 @@
+/* eslint-disable react/react-in-jsx-scope */
 import { renderHook } from '@testing-library/react-hooks';
 import { GqlMockedProvider } from '../../../../../__tests__/util/graphqlMocking';
-import { useContactReferralTabQuery } from './ContactReferralTab.generated';
+import { render } from '../../../../../__tests__/util/testingLibraryReactMock';
+import { ContactReferralTab } from './ContactReferralTab';
+import {
+  ContactReferralTabQuery,
+  useContactReferralTabQuery,
+} from './ContactReferralTab.generated';
 
 describe('ContactReferralTab', () => {
   it('test query', async () => {
@@ -24,5 +30,21 @@ describe('ContactReferralTab', () => {
         "contactId": "contact-id",
       }
     `);
+  });
+
+  it('test render', async () => {
+    const { findByText } = render(
+      <GqlMockedProvider<ContactReferralTabQuery>
+        mocks={{
+          contact: { id: 'contact-id' },
+        }}
+      >
+        <ContactReferralTab
+          accountListId="accountList-id"
+          contactId="contact-id"
+        />
+      </GqlMockedProvider>,
+    );
+    expect(await findByText('No Referrals')).toBeVisible();
   });
 });
