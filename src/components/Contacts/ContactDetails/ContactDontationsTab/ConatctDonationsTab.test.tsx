7d07f27fbd0e09d988f506bc4d59fd8334bfa49b
Gyasi Story <gyasi.story@cru.org>
2021-05-11 15:18:05 -0400

    MPDX-6973  add render test to DonationsContactTab

--- src/components/Contacts/ContactDetails/ContactDontationsTab/ConatctDonationsTab.test.tsx
+++ src/components/Contacts/ContactDetails/ContactDontationsTab/ConatctDonationsTab.test.tsx
@@ -1,6 +1,12 @@
 import { renderHook } from '@testing-library/react-hooks';
+import React from 'react';
 import { GqlMockedProvider } from '../../../../../__tests__/util/graphqlMocking';
-import { useGetContactDonationsQuery } from './ContactDonationsTab.generated';
+import { render } from '../../../../../__tests__/util/testingLibraryReactMock';
+import { ContactDonationsTab } from './ContactDonationsTab';
+import {
+  GetContactDonationsQuery,
+  useGetContactDonationsQuery,
+} from './ContactDonationsTab.generated';
 
 const accountListId = 'account-list-1';
 const contactId = 'contact-id-1';
@@ -27,5 +33,16 @@ describe('ContactDonationsTab', () => {
     expect(
       result.current.data?.contact.donations.nodes.length,
     ).toMatchInlineSnapshot(`undefined`);
+  });
+  it('test renderer', async () => {
+    const { findByRole } = render(
+      <GqlMockedProvider<GetContactDonationsQuery>>
+        <ContactDonationsTab
+          accountListId={accountListId}
+          contactId={contactId}
+        />
+      </GqlMockedProvider>,
+    );
+    expect(await findByRole('alert')).toBeVisible();
   });
 });
