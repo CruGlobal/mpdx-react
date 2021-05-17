0610bd64a6f8eeb7c3ce9ecda486865be005ec92
Gyasi Story <gyasi.story@cru.org>
2021-05-17 17:10:47 -0400

    MPDX-6977  Add Grid for donations

--- src/components/Contacts/ContactDetails/ContactDontationsTab/ContactDonationsTab.tsx
+++ src/components/Contacts/ContactDetails/ContactDontationsTab/ContactDonationsTab.tsx
@@ -1,9 +1,22 @@
-/* eslint-disable eqeqeq */
-import { Box, styled, Tab } from '@material-ui/core';
+import {
+  Box,
+  CircularProgress,
+  styled,
+  Tab,
+  Typography,
+} from '@material-ui/core';
 import { Skeleton, TabContext, TabList, TabPanel } from '@material-ui/lab';
-import React from 'react';
+import MUIDataTable, {
+  MUIDataTableOptions,
+  MUIDataTableColumn,
+} from 'mui-datatables';
+import React, { ReactElement, useState } from 'react';
 import { useTranslation } from 'react-i18next/';
-import { useGetContactDonationsQuery } from './ContactDonationsTab.generated';
+import { currencyFormat } from '../../../../lib/intlFormat';
+import {
+  GetContactDonationsQueryVariables,
+  useGetContactDonationsQuery,
+} from './ContactDonationsTab.generated';
 import { DonationsGraph } from './DonationsGraph/DonationsGraph';
 
 const ContactDonationsContainer = styled(Box)(({ theme }) => ({
@@ -44,6 +57,11 @@ interface ContactDontationsProp {
   contactId: string;
 }
 
+export type ContactDonationsFilter = Omit<
+  GetContactDonationsQueryVariables,
+  'accountListId'
+>;
+
 enum DonationTabKey {
   Donations = 'Donations',
   PartnershipInfo = 'Partnership Info',
@@ -56,11 +74,85 @@ export const ContactDonationsTab: React.FC<ContactDontationsProp> = ({
   const { data, loading } = useGetContactDonationsQuery({
     variables: { accountListId: accountListId, contactId: contactId },
   });
+
   const { t } = useTranslation();
 
   const [selectedDonationTabKey, setSelectedDonationTabKey] = React.useState(
     DonationTabKey.Donations,
   );
+
+  const columns: MUIDataTableColumn[] = [
+    {
+      name: 'amount',
+      label: t('Amount'),
+      options: {
+        filter: false,
+        sort: true,
+        customBodyRender: (amount, { rowIndex }): ReactElement => {
+          const donation = data?.contact.donations.nodes[rowIndex];
+          if (!loading && donation) {
+            const {
+              amount: { amount, currency },
+            } = donation;
+            return (
+              <Typography variant="body1">
+                {currencyFormat(amount, currency)}
+              </Typography>
+            );
+          } else {
+            return <Skeleton variant="circle" width={40} height={40} />;
+          }
+        },
+      },
+    },
+    {
+      name: 'amount.convertedAmount',
+      label: t('Converted Amount'),
+      options: {
+        filter: false,
+        sort: false,
+        customBodyRender: (amount, { rowIndex }): ReactElement => {
+          const donation = data?.contact.donations.nodes[rowIndex];
+          if (!loading && donation) {
+            const {
+              amount: { convertedAmount, convertedCurrency },
+            } = donation;
+            return (
+              <Typography variant="body1">
+                {currencyFormat(convertedAmount, convertedCurrency)}
+              </Typography>
+            );
+          } else {
+            return <Skeleton variant="circle" width={40} height={40} />;
+          }
+        },
+      },
+    },
+    {
+      name: 'donationDate',
+      label: t('Date'),
+      options: { filter: false, sort: true },
+    },
+  ];
+
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const options: MUIDataTableOptions = {
+    serverSide: true,
+    onChangePage: (newPage) => {
+      setCurrentPage(newPage);
+    },
+    page: currentPage,
+    count: data?.contact.donations.totalCount || 0,
+    rowsPerPage: 25,
+    rowsPerPageOptions: [25],
+    fixedHeader: false,
+    fixedSelectColumn: false,
+    print: false,
+    download: false,
+    selectableRows: 'none',
+    search: false,
+  };
 
   const handleDonationTabChange = (
     _event: React.ChangeEvent<Record<string, unknown>>,
@@ -115,7 +207,16 @@ export const ContactDonationsTab: React.FC<ContactDontationsProp> = ({
               <ContactDonationsLoadingPlaceHolder />
             </>
           ) : (
-            `The list of all ${data?.contact.donations.nodes.length} Donations goes here`
+            <MUIDataTable
+              title={loading && <CircularProgress size={24} />}
+              data={
+                loading || !data
+                  ? [['', <Skeleton key={1} />]]
+                  : data?.contact.donations.nodes
+              }
+              columns={columns}
+              options={options}
+            />
           )}
         </TabPanel>
         <TabPanel value={DonationTabKey.PartnershipInfo}>
