23ba4e1d5248646f942a9df0677c2725cb6572d2
Gyasi Story <gyasi.story@cru.org>
2021-06-04 10:18:07 -0400

    MPDX-6977 add fist 10 list and load more button

--- src/components/Contacts/ContactDetails/ContactDontationsTab/ContactDonationsList/ContactDonationsList.tsx
+++ src/components/Contacts/ContactDetails/ContactDontationsTab/ContactDonationsList/ContactDonationsList.tsx
@@ -1,11 +1,16 @@
-import { CircularProgress, Typography } from '@material-ui/core';
+import {
+  Box,
+  Button,
+  styled,
+  Table,
+  TableCell,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@material-ui/core';
 import { Skeleton } from '@material-ui/lab';
 import { DateTime } from 'luxon';
-import MUIDataTable, {
-  MUIDataTableColumn,
-  MUIDataTableOptions,
-} from 'mui-datatables';
-import React, { ReactElement, useState } from 'react';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { currencyFormat } from '../../../../../lib/intlFormat/intlFormat';
 import { useContactDonationsListQuery } from './ContactDonationsList.generated';
@@ -14,6 +19,11 @@ interface ContactDonationsListProp {
   accountListId: string;
   contactId: string;
 }
+const DonationLoadingPlaceHolder = styled(Skeleton)(({ theme }) => ({
+  width: '100%',
+  height: '24px',
+  margin: theme.spacing(2, 0),
+}));
 
 export const ContactDonationsList: React.FC<ContactDonationsListProp> = ({
   accountListId,
@@ -38,179 +48,51 @@ export const ContactDonationsList: React.FC<ContactDonationsListProp> = ({
 
   const { t } = useTranslation();
 
-  const handleNextPage = (moveNext = true, newPage: number) => {
-    if (loading && !data?.contact) {
-      return;
-    }
-
-    const endCursor = data?.contact.donations.pageInfo.endCursor;
-    const startCursor = data?.contact.donations.pageInfo.startCursor;
-
-    if (moveNext && data?.contact.donations.pageInfo.hasNextPage) {
-      fetchMore({
-        variables: {
-          first: range,
-          after: endCursor,
-        },
-        updateQuery: (prev, { fetchMoreResult }) => {
-          if (!fetchMoreResult) {
-            return prev;
-          }
-          return {
-            contact: {
-              donations: fetchMoreResult.contact.donations,
-            },
-          };
-        },
-      }).finally(() => {
-        setCurrentPage(newPage);
-      });
-    }
-    if (!moveNext && data?.contact.donations.pageInfo.hasPreviousPage) {
-      fetchMore({
-        variables: {
-          last: range,
-          before: startCursor,
-        },
-        updateQuery: (prev, { fetchMoreResult }) => {
-          if (!fetchMoreResult) {
-            return prev;
-          }
-          return {
-            contact: {
-              donations: fetchMoreResult.contact.donations,
-            },
-          };
-        },
-      }).finally(() => {
-        setCurrentPage(newPage);
-      });
-    }
-  };
-
-  const columns: MUIDataTableColumn[] = [
-    {
-      name: 'donationDate',
-      label: t('Date'),
-      options: {
-        filter: false,
-        sort: false,
-        customBodyRender: (date, { rowIndex }): ReactElement => {
-          const donation = data?.contact.donations.nodes[rowIndex];
-          if (!loading && donation && networkStatus !== 3) {
-            return (
-              <>
-                <span role="textbox" />
-                <Typography variant="subtitle1">
-                  {DateTime.fromISO(donation.donationDate).toLocaleString()}
-                </Typography>
-              </>
-            );
-          } else {
-            return <Skeleton variant="circle" width={40} height={40} />;
-          }
-        },
-      },
-    },
-    {
-      name: 'amount',
-      label: t('Amount'),
-      options: {
-        filter: false,
-        sort: false,
-        customBodyRender: (amount, { rowIndex }): ReactElement => {
-          const donation = data?.contact.donations.nodes[rowIndex];
-          if (!loading && donation && networkStatus !== 3) {
-            const {
-              amount: { amount, currency },
-            } = donation;
-            return (
-              <Typography variant="subtitle1">
-                {currencyFormat(amount, currency)}
-              </Typography>
-            );
-          } else {
-            return <Skeleton variant="circle" width={40} height={40} />;
-          }
-        },
-      },
-    },
-    {
-      name: 'amount.convertedAmount',
-      label: t('Converted Amount'),
-      options: {
-        filter: false,
-        sort: false,
-        customBodyRender: (amount, { rowIndex }): ReactElement => {
-          const donation = data?.contact.donations.nodes[rowIndex];
-          if (!loading && donation && networkStatus !== 3) {
-            const {
-              amount: { convertedAmount, convertedCurrency },
-            } = donation;
-            return (
-              <Typography variant="subtitle1">
-                {currencyFormat(convertedAmount, convertedCurrency)}
-              </Typography>
-            );
-          } else {
-            return <Skeleton variant="circle" width={40} height={40} />;
-          }
-        },
-      },
-    },
-  ];
-
-  const options: MUIDataTableOptions = {
-    serverSide: true,
-    onChangePage: (newPage) => {
-      if (currentPage === newPage) {
-        return;
-      }
-      currentPage > newPage
-        ? handleNextPage(false, newPage)
-        : handleNextPage(true, newPage);
-    },
-    page: currentPage,
-    count: data?.contact.donations.totalCount || 0,
-    rowsPerPage: range,
-    rowsPerPageOptions: [10, 25, 50, 100],
-    onChangeRowsPerPage: (rowsPerPage) => {
-      data?.contact
-        ? fetchMore({
-            variables: { first: range },
-            updateQuery: (pv, { fetchMoreResult }) => {
-              if (!fetchMoreResult) {
-                return pv;
-              }
-              return {
-                contact: {
-                  donations: fetchMoreResult.contact.donations,
-                },
-              };
-            },
-          }).finally(() => {
-            setCurrentPage(0);
-            setRange(rowsPerPage);
-          })
-        : null;
-    },
-    fixedHeader: false,
-    fixedSelectColumn: false,
-    print: false,
-    download: false,
-    selectableRows: 'none',
-    search: false,
-  };
   return (
-    <MUIDataTable
-      title={loading && <CircularProgress size={24} />}
-      data={
-        loading || !data || networkStatus === 3
-          ? [['', <Skeleton key={1} />]]
-          : data?.contact.donations.nodes
-      }
-      columns={columns}
-      options={options}
-    />
+    <Box>
+      <Table>
+        <TableHead>
+          <TableCell>{t('Date')}</TableCell>
+          <TableCell>{t('Amount')}</TableCell>
+          <TableCell>{t('Converted Amount')}</TableCell>
+        </TableHead>
+        {loading ? (
+          <TableRow>
+            <TableCell>
+              <DonationLoadingPlaceHolder />
+            </TableCell>
+            <TableCell>
+              <DonationLoadingPlaceHolder />
+            </TableCell>
+            <TableCell>
+              <DonationLoadingPlaceHolder />
+            </TableCell>
+          </TableRow>
+        ) : (
+          data?.contact.donations.nodes.map((donation) => (
+            <TableRow key={donation.id}>
+              <TableCell>
+                {DateTime.fromISO(donation.donationDate).toLocaleString()}
+              </TableCell>
+              <TableCell>
+                {currencyFormat(
+                  donation.amount.amount,
+                  donation.amount.currency,
+                )}
+              </TableCell>
+              <TableCell>
+                {currencyFormat(
+                  donation.amount.convertedAmount,
+                  donation.amount.convertedCurrency,
+                )}
+              </TableCell>
+            </TableRow>
+          ))
+        )}
+      </Table>
+      {!loading && data?.contact.donations.pageInfo.hasNextPage ? (
+        <Button variant="outlined">{t('Load More')}</Button>
+      ) : null}
+    </Box>
   );
 };
