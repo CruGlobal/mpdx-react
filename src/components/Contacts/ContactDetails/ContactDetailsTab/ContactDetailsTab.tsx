66a83963d3c3c68ca5e40870417a662ee5615316
TheNoodleMoose <cjh605@gmail.com>
2021-06-08 11:10:59 -0400

    Create generic modal component

--- src/components/Contacts/ContactDetails/ContactDetailsTab/ContactDetailsTab.tsx
+++ src/components/Contacts/ContactDetails/ContactDetailsTab/ContactDetailsTab.tsx
@@ -3,18 +3,12 @@ import {
   Box,
   Button,
   CircularProgress,
-  Dialog,
-  DialogActions,
-  DialogContent,
   DialogContentText,
-  DialogTitle,
   Divider,
-  IconButton,
   styled,
   Typography,
 } from '@material-ui/core';
 import CreateIcon from '@material-ui/icons/Create';
-import CloseIcon from '@material-ui/icons/Close';
 import { Skeleton } from '@material-ui/lab';
 import { useSnackbar } from 'notistack';
 import { useTranslation } from 'react-i18next';
@@ -23,6 +17,7 @@ import {
   ContactsDocument,
   ContactsQuery,
 } from '../../../../../pages/accountLists/[accountListId]/contacts/Contacts.generated';
+import Modal from '../../../common/Modal/Modal';
 import {
   useContactDetailsTabQuery,
   useDeleteContactMutation,
@@ -83,21 +78,6 @@ const ContactDetailLoadingPlaceHolder = styled(Skeleton)(({ theme }) => ({
   width: '100%',
   height: '24px',
   margin: theme.spacing(2, 0),
-}));
-
-const DeleteContactTitle = styled(DialogTitle)(() => ({
-  textTransform: 'uppercase',
-  textAlign: 'center',
-}));
-
-const CloseButton = styled(IconButton)(({ theme }) => ({
-  position: 'absolute',
-  right: theme.spacing(1),
-  top: theme.spacing(1),
-  color: theme.palette.text.primary,
-  '&:hover': {
-    backgroundColor: theme.palette.cruGrayLight.main,
-  },
 }));
 
 const LoadingIndicator = styled(CircularProgress)(({ theme }) => ({
@@ -174,43 +154,38 @@ export const ContactDetailsTab: React.FC<ContactDetailTabProps> = ({
 
   const renderDeleteContactDialog = () => {
     return (
-      <Dialog
-        open={deleteDialogOpen}
-        aria-labelledby={t('Delete Contact Dialog')}
-        fullWidth
-        maxWidth="sm"
-      >
-        <DeleteContactTitle>
-          {t('Delete Contact')}
-          <CloseButton onClick={() => changeDeleteDialogOpen(false)}>
-            <CloseIcon titleAccess={t('Close')} />
-          </CloseButton>
-        </DeleteContactTitle>
-        <DialogContent dividers>
+      <Modal
+        isOpen={deleteDialogOpen}
+        title={t('Delete Contact')}
+        content={
           <DialogContentText>
             {t(
               'Are you sure you want to permanently delete this contact? Doing so will permanently delete this contacts information, as well as task history. This cannot be undone. If you wish to keep this information, you can try hiding this contact instead.',
             )}
           </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button
-            disabled={deleting}
-            onClick={() => changeDeleteDialogOpen(false)}
-          >
-            {t('Cancel')}
-          </Button>
-          <DialogDeleteButton
-            size="large"
-            variant="contained"
-            disabled={deleting}
-            onClick={handleDeleteContact}
-          >
-            {deleting && <LoadingIndicator size={20} />}
-            {t('delete contact')}
-          </DialogDeleteButton>
-        </DialogActions>
-      </Dialog>
+        }
+        customActionSection={
+          <>
+            <Button
+              disabled={deleting}
+              onClick={() => changeDeleteDialogOpen(false)}
+            >
+              {t('Cancel')}
+            </Button>
+            <DialogDeleteButton
+              size="large"
+              variant="contained"
+              disabled={deleting}
+              onClick={handleDeleteContact}
+            >
+              {deleting && <LoadingIndicator size={20} />}
+              {t('delete contact')}
+            </DialogDeleteButton>
+          </>
+        }
+        handleClose={() => changeDeleteDialogOpen(false)}
+        handleConfirm={handleDeleteContact}
+      />
     );
   };
 
