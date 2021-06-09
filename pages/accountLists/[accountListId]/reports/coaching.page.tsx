a87b28dbca919c5022354313608c0c57959e87c9
Robert Eldredge <robert.eldredge@cru.org>
2021-06-09 17:31:30 -0400

    Reports pages

---
+++ pages/accountLists/[accountListId]/reports/coaching.page.tsx
@@ -0,0 +1,39 @@
+import React, { ReactElement } from 'react';
+import Head from 'next/head';
+import { useTranslation } from 'react-i18next';
+import { useRouter } from 'next/router';
+import { Box } from '@material-ui/core';
+import { MonthlyActivity } from '../../../../src/components/Reports/DonationsReport/MonthlyActivity';
+import { DonationsList } from '../../../../src/components/Reports/DonationsReport/DonationsList';
+import Loading from '../../../../src/components/Loading';
+
+const CoachingReportPage = (): ReactElement => {
+  const { t } = useTranslation();
+  const { query, isReady } = useRouter();
+
+  const { accountListId } = query;
+
+  if (Array.isArray(accountListId)) {
+    throw new Error('accountListId should not be an array');
+  }
+
+  return (
+    <>
+      <Head>
+        <title>
+          MPDX | {t('Reports')} | {t('Coaching')}
+        </title>
+      </Head>
+      {isReady && accountListId ? (
+        <Box>
+          <MonthlyActivity accountListId={accountListId} />
+          <DonationsList accountListId={accountListId} />
+        </Box>
+      ) : (
+        <Loading loading />
+      )}
+    </>
+  );
+};
+
+export default CoachingReportPage;
