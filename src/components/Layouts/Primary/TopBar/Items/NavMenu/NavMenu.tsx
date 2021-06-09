e01eea5b46f903b161c3a83e82f023535edbf6a9
Robert Eldredge <robert.eldredge@cru.org>
2021-06-09 15:29:58 -0400

    Create Reports Dropdown on NavBar

--- src/components/Layouts/Primary/TopBar/Items/NavMenu/NavMenu.tsx
+++ src/components/Layouts/Primary/TopBar/Items/NavMenu/NavMenu.tsx
@@ -1,11 +1,15 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useState } from 'react';
 import {
   makeStyles,
   Grid,
   MenuItem,
   ListItemText,
   Theme,
-  Select,
+  Popper,
+  Grow,
+  ClickAwayListener,
+  MenuList,
+  Paper,
 } from '@material-ui/core';
 import NextLink from 'next/link';
 import { useTranslation } from 'react-i18next';
@@ -25,6 +29,24 @@ const NavMenu = (): ReactElement => {
   const classes = useStyles();
   const { state } = useApp();
   const { t } = useTranslation();
+
+  const [reportsMenuOpen, setReportsMenuOpen] = useState(false);
+  const anchorRef = React.useRef<HTMLLIElement>(null);
+
+  const handleReportsMenuToggle = () => {
+    setReportsMenuOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleReportsMenuClose = (event: React.MouseEvent<EventTarget>) => {
+    if (
+      anchorRef.current &&
+      anchorRef.current.contains(event.target as HTMLElement)
+    ) {
+      return;
+    }
+
+    setReportsMenuOpen(false);
+  };
 
   return (
     <>
@@ -64,21 +86,123 @@ const NavMenu = (): ReactElement => {
             </NextLink>
           </Grid>
           <Grid item className={classes.navListItem}>
-            <NextLink
-              href="/accountLists/[accountListId]/contacts"
-              as={`/accountLists/${state.accountListId}/contacts`}
-              scroll={false}
+            <MenuItem
+              ref={anchorRef}
+              aria-controls={reportsMenuOpen ? 'menu-list-grow' : undefined}
+              aria-haspopup="true"
+              onClick={handleReportsMenuToggle}
             >
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                value={'Report'}
-              >
-                <MenuItem>Ten</MenuItem>
-                <MenuItem>Twenty</MenuItem>
-                <MenuItem>Thirty</MenuItem>
-              </Select>
-            </NextLink>
+              <ListItemText primary={t('Reports')} />
+            </MenuItem>
+            <Popper
+              open={reportsMenuOpen}
+              anchorEl={anchorRef.current}
+              role={undefined}
+              transition
+              disablePortal
+            >
+              {({ TransitionProps, placement }) => (
+                <Grow
+                  {...TransitionProps}
+                  style={{
+                    transformOrigin:
+                      placement === 'bottom' ? 'center top' : 'center bottom',
+                  }}
+                >
+                  <Paper>
+                    <ClickAwayListener onClickAway={handleReportsMenuClose}>
+                      <MenuList
+                        autoFocusItem={reportsMenuOpen}
+                        id="menu-list-grow"
+                      >
+                        <NextLink
+                          href="/accountLists/[accountListId]/reports/dashboard"
+                          as={`/accountLists/${state.accountListId}/reports/dashboard`}
+                          scroll={false}
+                        >
+                          <MenuItem onClick={handleReportsMenuClose}>
+                            <ListItemText primary={t('Dashboard')} />
+                          </MenuItem>
+                        </NextLink>
+                        <NextLink
+                          href="/accountLists/[accountListId]/reports/partner"
+                          as={`/accountLists/${state.accountListId}/reports/partner`}
+                          scroll={false}
+                        >
+                          <MenuItem onClick={handleReportsMenuClose}>
+                            <ListItemText
+                              primary={t('14-Month Report (Partner)')}
+                            />
+                          </MenuItem>
+                        </NextLink>
+                        <NextLink
+                          href="/accountLists/[accountListId]/reports/salary"
+                          as={`/accountLists/${state.accountListId}/reports/salary`}
+                          scroll={false}
+                        >
+                          <MenuItem onClick={handleReportsMenuClose}>
+                            <ListItemText
+                              primary={t('14-Month Report (Salary)')}
+                            />
+                          </MenuItem>
+                        </NextLink>
+                        <NextLink
+                          href="/accountLists/[accountListId]/reports/designation_accounts"
+                          as={`/accountLists/${state.accountListId}/reports/designation_accounts`}
+                          scroll={false}
+                        >
+                          <MenuItem onClick={handleReportsMenuClose}>
+                            <ListItemText primary={t('Designation Accounts')} />
+                          </MenuItem>
+                        </NextLink>
+                        <NextLink
+                          href="/accountLists/[accountListId]/reports/financial_accounts"
+                          as={`/accountLists/${state.accountListId}/reports/financial_accounts`}
+                          scroll={false}
+                        >
+                          <MenuItem onClick={handleReportsMenuClose}>
+                            <ListItemText
+                              primary={t('Responsibility Centers')}
+                            />
+                          </MenuItem>
+                        </NextLink>
+                        <NextLink
+                          href="/accountLists/[accountListId]/reports/monthly"
+                          as={`/accountLists/${state.accountListId}/reports/monthly`}
+                          scroll={false}
+                        >
+                          <MenuItem onClick={handleReportsMenuClose}>
+                            <ListItemText
+                              primary={t('Expected Monthly Total')}
+                            />
+                          </MenuItem>
+                        </NextLink>
+                        <NextLink
+                          href="/accountLists/[accountListId]/reports/analysis"
+                          as={`/accountLists/${state.accountListId}/reports/analysis`}
+                          scroll={false}
+                        >
+                          <MenuItem onClick={handleReportsMenuClose}>
+                            <ListItemText
+                              primary={t('Partner Giving Analysis')}
+                            />
+                          </MenuItem>
+                        </NextLink>
+                        <NextLink
+                          href="/accountLists/[accountListId]/reports/coaching"
+                          as={`/accountLists/${state.accountListId}/reports/coaching`}
+                          scroll={false}
+                        >
+                          <MenuItem onClick={handleReportsMenuClose}>
+                            <ListItemText primary={t('Coaching')} />
+                          </MenuItem>
+                        </NextLink>
+                      </MenuList>
+                    </ClickAwayListener>
+                  </Paper>
+                </Grow>
+              )}
+            </Popper>
           </Grid>
           <Grid item className={classes.navListItem}>
             <HandoffLink path="/tools">
