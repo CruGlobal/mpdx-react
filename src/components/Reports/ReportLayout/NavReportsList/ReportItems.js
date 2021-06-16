838a7ca323b4a4cc4c4491cf71ab52ee480cf3f6
hotcakedev628 <hotcake.dev.628@hotmail.com>
2021-06-15 23:20:39 -0400

    add report items constant to reuse

---
+++ src/components/Reports/ReportLayout/NavReportsList/ReportItems.js
@@ -0,0 +1,37 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    id: 'donations',
+    title: 'Donations',
+  },
+  {
+    id: 'partnerCurrency',
+    title: 'Month Report',
+    subTitle: 'Partner Currency',
+  },
+  {
+    id: 'salaryCurrency',
+    title: 'Month Report',
+    subTitle: 'Salary Currency',
+  },
+  {
+    id: 'designationAccounts',
+    title: 'Designation Accounts',
+  },
+  {
+    id: 'responsibilityCenters',
+    title: 'Responsibility Centers',
+  },
+  {
+    id: 'expectedMonthlyTotal',
+    title: 'Expected Monthly Total',
+  },
+  {
+    id: 'partnerGivingAnalysis',
+    title: 'Partner Giving Analysis',
+  },
+  {
+    id: 'coaching',
+    title: 'Coaching',
+  },
+];
