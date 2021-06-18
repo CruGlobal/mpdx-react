74872ec2d5793d90bdb7e6878cc27bf4b9ee70aa
hotcakedev628 <hotcake.dev.628@hotmail.com>
2021-06-18 13:34:28 -0400

    update salary currency report page for layout

--- pages/accountLists/[accountListId]/reports/salaryCurrency.page.tsx
+++ pages/accountLists/[accountListId]/reports/salaryCurrency.page.tsx
@@ -1,14 +1,27 @@
-import React, { ReactElement } from 'react';
+import React, { ReactElement, useEffect } from 'react';
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
+import { useRouter } from 'next/router';
+import { getSession } from 'next-auth/client';
 import { useTranslation } from 'react-i18next';
+import { useApp } from 'src/components/App';
 import { SalaryReportTable } from 'src/components/Reports/SalaryReport/SalaryReportTable';
 import Loading from 'src/components/Loading';
 import { useAccountListId } from 'src/hooks/useAccountListId';
 import { ReportLayout } from 'src/components/Reports/ReportLayout/ReportLayout';
 
 const SalaryReportPage = (): ReactElement => {
+  const { dispatch } = useApp();
   const { t } = useTranslation();
   const accountListId = useAccountListId();
+  const router = useRouter();
+
+  useEffect(() => {
+    dispatch({
+      type: 'updateAccountListId',
+      accountListId: router.query.accountListId?.toString() ?? '',
+    });
+  }, []);
 
   return (
     <>
@@ -29,6 +42,20 @@ const SalaryReportPage = (): ReactElement => {
       )}
     </>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = await getSession({ req });
+
+  if (!session?.user['token']) {
+    res.writeHead(302, { Location: '/' });
+    res.end();
+    return { props: {} };
+  }
+
+  return {
+    props: {},
+  };
 };
 
 export default SalaryReportPage;
