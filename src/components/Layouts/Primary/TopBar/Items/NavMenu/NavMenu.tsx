0cd3a2da4fc525b9b7c9a3318e52553f17591b77
hotcakedev628 <hotcake.dev.628@hotmail.com>
2021-06-21 10:59:50 -0400

    update nav list constant url to fix chromatic error

--- src/components/Layouts/Primary/TopBar/Items/NavMenu/NavMenu.tsx
+++ src/components/Layouts/Primary/TopBar/Items/NavMenu/NavMenu.tsx
@@ -17,7 +17,7 @@ import NextLink from 'next/link';
 import { useTranslation } from 'react-i18next';
 import { useApp } from '../../../../../App';
 import HandoffLink from '../../../../../HandoffLink';
-import { ReportNavItems } from 'src/components/Reports/ReportLayout/NavReportsList/ReportNavItems';
+import { ReportNavItems } from '../../../../../Reports/ReportLayout/NavReportsList/ReportNavItems';
 
 const useStyles = makeStyles((theme: Theme) => ({
   navListItem: {
