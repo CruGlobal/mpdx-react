bcb50f0e79f0e21e36ab5a91615ea19efbc9c53a
Scotty Waggoner <ozzieorca@gmail.com>
2021-06-23 12:03:07 -0700

    MPDX-7048: Expected Monthly Total Report - REST to GraphQL

---
+++ pages/api/Schema/reports/expectedMonthlyTotal/datahandler.ts
@@ -0,0 +1,71 @@
+import { ExpectedMonthlyTotalReport } from '../../../graphql-rest.page.generated';
+
+export interface ExpectedMonthlyTotalResponse {
+  id: string;
+  type: string;
+  attributes: {
+    created_at: string;
+    expected_donations: {
+      contact_id: string;
+      contact_name: string;
+      contact_status: string;
+      converted_amount: number;
+      converted_currency: string;
+      converted_currency_symbol: string;
+      donation_amount: number;
+      donation_currency: string;
+      donation_currency_symbol: string;
+      pledge_amount: number;
+      pledge_currency: string;
+      pledge_currency_symbol: string;
+      pledge_frequency: string;
+      type: 'received' | 'likely' | 'unlikely';
+    }[];
+    total_currency: string;
+    total_currency_symbol: string;
+    updated_at: string | null;
+    updated_in_db_at: string | null;
+  };
+  relationships: {
+    account_list: {
+      data: {
+        id: string;
+        type: string;
+      };
+    };
+  };
+}
+
+const mapDonation = (
+  donation: ExpectedMonthlyTotalResponse['attributes']['expected_donations'][0],
+) => ({
+  contactId: donation.contact_id,
+  contactName: donation.contact_name,
+  contactStatus: donation.contact_status,
+  convertedAmount: donation.converted_amount,
+  convertedCurrency: donation.converted_currency,
+  convertedCurrencySymbol: donation.converted_currency_symbol,
+  donationAmount: donation.donation_amount,
+  donationCurrency: donation.donation_currency,
+  donationCurrencySymbol: donation.donation_currency_symbol,
+  pledgeAmount: donation.pledge_amount,
+  pledgeCurrency: donation.pledge_currency,
+  pledgeCurrencySymbol: donation.pledge_currency_symbol,
+  pledgeFrequency: donation.pledge_frequency,
+});
+
+export const mapExpectedMonthlyTotalReport = (
+  data: ExpectedMonthlyTotalResponse,
+): ExpectedMonthlyTotalReport => ({
+  received: data.attributes.expected_donations
+    .filter(({ type }) => type === 'received')
+    .map(mapDonation),
+  likely: data.attributes.expected_donations
+    .filter(({ type }) => type === 'likely')
+    .map(mapDonation),
+  unlikely: data.attributes.expected_donations
+    .filter(({ type }) => type === 'unlikely')
+    .map(mapDonation),
+  currency: data.attributes.total_currency,
+  currencySymbol: data.attributes.total_currency_symbol,
+});
