426a61ee58093d777eb38f14a5fffd2929154726
rathclayton1 <rathclayton1@gmail.com>
2021-06-25 10:31:02 -0400

    Clean up data grid
    -Use luxon with change month buttons
    -Display partners as links
    -Fixed table sorting logic
    -Added correct footer for totals

--- src/components/Reports/DonationsReport/DonationsReportTable.tsx
+++ src/components/Reports/DonationsReport/DonationsReportTable.tsx
@@ -3,35 +3,27 @@ import {
   Box,
   Typography,
   Button,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
   Link,
   Divider,
   makeStyles,
 } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import EditIcon from '@material-ui/icons/Edit';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import { useTranslation } from 'react-i18next';
-import {
-  DataGrid,
-  GridColDef,
-  GridValueGetterParams,
-} from '@material-ui/data-grid';
+import { DataGrid, GridColDef, GridCellParams } from '@material-ui/data-grid';
+import { DateTime } from 'luxon';
 import { ExpectedMonthlyTotalReportEmpty } from '../ExpectedMonthlyTotalReport/Empty/ExpectedMonthlyTotalReportEmpty';
-import theme from '../../../theme';
-import { GetDashboardQuery } from '../../../../pages/accountLists/GetDashboard.generated';
 
 interface Donation {
-  date: string;
+  date: Date;
   partnerId: string;
-  partner: [];
-  amount: string;
-  foreignAmount: string;
+  partner: string;
+  currency: string;
+  foreignCurrency: string;
+  convertedAmount: number;
+  foreignAmount: number;
   designation: string;
   method: string;
   id: string;
@@ -40,13 +32,11 @@ interface Donation {
 interface Props {
   data: Donation[];
   accountListId: string;
-  empty: boolean;
 }
 
 export const DonationsReportTable: React.FC<Props> = ({
   data,
   accountListId,
-  empty,
 }) => {
   const { t } = useTranslation();
 
@@ -55,63 +45,101 @@ export const DonationsReportTable: React.FC<Props> = ({
       '& .MuiDataGrid-row.Mui-even:not(:hover)': {
         backgroundColor:
           theme.palette.type === 'light'
-            ? 'rgba(0, 0, 0, 0.04)'
-            : 'rgba(255, 255, 255, 0.04)',
+            ? theme.palette.common.white
+            : theme.palette.cruGrayLight.main,
       },
     },
   }));
   const classes = useStyles();
+  const link = (params: GridCellParams) => (
+    <Typography>
+      <Link href="contact.html">{params.value}</Link>
+    </Typography>
+  );
+  const amount = (params: GridCellParams) => {
+    const row = params.row as Donation;
+
+    return <Typography>{row.convertedAmount + ' ' + row.currency}</Typography>;
+  };
+  const foreignAmount = (params: GridCellParams) => {
+    const row = params.row as Donation;
+
+    return (
+      <Typography>{row.foreignAmount + ' ' + row.foreignCurrency}</Typography>
+    );
+  };
+  const button = () => (
+    <IconButton color="primary">
+      <EditIcon />
+    </IconButton>
+  );
   const columns: GridColDef[] = [
-    { field: 'date', headerName: 'Date', type: 'date', width: 160 },
     {
-      field: 'partner',
-      headerName: 'Partner',
-      width: 220,
+      field: 'date',
+      headerName: t('Date'),
+      type: 'date',
+      width: 160,
     },
     {
-      field: 'amount',
-      headerName: 'Amount',
-
+      field: 'partner',
+      headerName: t('Partner'),
+      width: 220,
+      renderCell: link,
+    },
+    {
+      field: 'convertedAmount',
+      headerName: t('Amount'),
       width: 160,
+      renderCell: amount,
     },
     {
       field: 'foreignAmount',
-      headerName: 'Foreign Amount',
-
-      width: 160,
+      headerName: t('Foreign Amount'),
+      width: 180,
+      renderCell: foreignAmount,
     },
     {
       field: 'designation',
-      headerName: 'Designation',
+      headerName: t('Designation'),
       width: 220,
     },
     {
       field: 'method',
-      headerName: 'Method',
-      width: 200,
+      headerName: t('Method'),
+      width: 160,
     },
     {
       field: 'appeal',
-      headerName: 'Appeal',
-      width: 200,
+      headerName: t('Appeal'),
+      width: 130,
+      renderCell: button,
     },
   ];
 
-  const [month, setMonth] = useState('June 2021');
+  const [time, setTime] = useState(DateTime.now().startOf('month'));
 
-  const prevMonth = () => {
-    return 'disabled';
+  const title = time.monthLong + ' ' + time.year;
+
+  const hasNext = time.hasSame(DateTime.now().startOf('month'), 'month');
+
+  const setPrevMonth = () => {
+    setTime(time.minus({ months: 1 }));
   };
+
+  const setNextMonth = () => {
+    setTime(time.plus({ months: 1 }));
+  };
+
   return (
     <>
       <Box style={{ display: 'flex', margin: 8 }}>
-        <Typography variant="h6">{month}</Typography>
+        <Typography variant="h6">{title}</Typography>
         <Button
           style={{ marginLeft: 'auto' }}
           variant="contained"
           startIcon={<ChevronLeftIcon />}
           size="small"
-          onClick={() => setMonth('May 2021')}
+          onClick={() => setPrevMonth()}
         >
           Previous Month
         </Button>
@@ -119,70 +147,38 @@ export const DonationsReportTable: React.FC<Props> = ({
           variant="contained"
           endIcon={<ChevronRightIcon />}
           size="small"
-          onClick={() => setMonth('June 2021')}
+          onClick={() => setNextMonth()}
+          disabled={hasNext}
         >
           Next Month
         </Button>
       </Box>
       <Divider style={{ margin: 12 }} variant="middle"></Divider>
-      {!empty ? (
-        /*
-        <Box>
-          <TableContainer component={Paper}>
-            <Table aria-label="customized table">
-              <TableHead>
-                <TableRow>
-                  <TableCell align="left">{t('Date')}</TableCell>
-                  <TableCell align="left">{t('Partner')}</TableCell>
-                  <TableCell align="right">{t('Amount')}</TableCell>
-                  <TableCell align="right">{t('Foreign Amount')}</TableCell>
-                  <TableCell align="right">{t('Designation')}</TableCell>
-                  <TableCell align="right">{t('Method')}</TableCell>
-                  <TableCell align="left">{t('Appeal')}</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {data.map((row, index) => (
-                  <TableRow
-                    key={row.partnerId}
-                    style={{
-                      backgroundColor:
-                        index % 2
-                          ? theme.palette.common.white
-                          : theme.palette.cruGrayLight.main,
-                    }}
-                  >
-                    <TableCell align="left">{t(row.date)}</TableCell>
-                    <TableCell align="left">
-                      <Link href="../../../pages/accountLists/[accountListId]/contacts/[[...contactId]].page.tsx">
-                        {row.partner}
-                      </Link>
-                    </TableCell>
-                    <TableCell align="right">{t(row.amount)}</TableCell>
-                    <TableCell align="right">{row.foreignAmount}</TableCell>
-                    <TableCell align="right">{t(row.designation)}</TableCell>
-                    <TableCell align="right">{t(row.method)}</TableCell>
-                    <TableCell align="left"></TableCell>
-                  </TableRow>
-                ))}
-                <TableRow>
-                  <TableCell
-                    style={{ fontWeight: 'bold' }}
-                    colSpan={3}
-                    align="right"
-                  >
-                    {t('Total Donations: 1,234 CAD')}
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Box>
-      */ <Box
-          style={{ height: 400, width: '100%' }}
+      {data.length > 0 ? (
+        <Box
+          display="flex"
+          flexDirection="column"
           classes={{ root: classes.root }}
         >
-          <DataGrid rows={data} columns={columns} pageSize={50} />
+          <DataGrid
+            rows={data}
+            columns={columns}
+            autoPageSize
+            autoHeight
+            disableSelectionOnClick
+            hideFooter
+          />
+          <Typography
+            style={{ fontWeight: 'bold', marginLeft: 230, padding: 8 }}
+          >
+            Total CAD Donations:
+          </Typography>
+          <Divider variant="middle"></Divider>
+          <Typography
+            style={{ fontWeight: 'bold', marginLeft: 230, padding: 8 }}
+          >
+            Total Donations:
+          </Typography>
         </Box>
       ) : (
         <ExpectedMonthlyTotalReportEmpty />
