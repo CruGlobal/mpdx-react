af1a96ffd9f2599087a60e6a0fe5bce7477780ae
John Plastow <j2trumpet@gmail.com>
2021-07-13 08:57:09 -0700

    Adding translation

--- src/components/Contacts/ContactDetails/ContactNotesTab/ContactNotesTab.tsx
+++ src/components/Contacts/ContactDetails/ContactNotesTab/ContactNotesTab.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { TextField } from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
 
 export const ContactNotesTab: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <TextField
       fullWidth
       multiline
-      placeholder="Add contact notes"
+      placeholder={t('Add contact notes')}
       rows="10"
       variant="outlined"
     />
