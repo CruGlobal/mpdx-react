9672ad8ad9acf3849af9183e6462e6caa23069c2
hotcakedev628 <hotcake.dev.628@hotmail.com>
2021-07-23 21:14:00 -0400

    jest:update test case

--- src/components/Reports/FourteenMonthReports/FourteenMonthReport.test.tsx
+++ src/components/Reports/FourteenMonthReports/FourteenMonthReport.test.tsx
@@ -8,19 +8,153 @@ import theme from 'src/theme';
 
 const accountListId = '111';
 const title = 'test title';
-const currencyType = 'salary';
 const onNavListToggle = jest.fn();
 
-//TODO: Need test coverage for error state
+const mocks = {
+  FourteenMonthReport: {
+    fourteenMonthReport: {
+      currencyGroups: [
+        {
+          contacts: [
+            {
+              accountNumbers: ['10182'],
+              average: 86,
+              id: 'contact-1',
+              lateBy30Days: false,
+              lateBy60Days: false,
+              minimum: 85,
+              months: [
+                {
+                  donations: [
+                    {
+                      amount: 85,
+                      currency: 'CAD',
+                      date: '2020-07-15',
+                      paymentMethod: 'BANK_TRANS',
+                    },
+                  ],
+                  month: '2020-10-01',
+                  salaryCurrencyTotal: 85,
+                  total: 35,
+                },
+                {
+                  donations: [
+                    {
+                      amount: 85,
+                      currency: 'CAD',
+                      date: '2020-11-15',
+                      paymentMethod: 'BANK_TRANS',
+                    },
+                  ],
+                  month: '2020-11-01',
+                  salaryCurrencyTotal: 85,
+                  total: 35,
+                },
+                {
+                  donations: [
+                    {
+                      amount: 85,
+                      currency: 'CAD',
+                      date: '2020-12-15',
+                      paymentMethod: 'BANK_TRANS',
+                    },
+                  ],
+                  month: '2020-12-01',
+                  salaryCurrencyTotal: 85,
+                  total: 35,
+                },
+                {
+                  donations: [
+                    {
+                      amount: 85,
+                      currency: 'CAD',
+                      date: '2021-1-15',
+                      paymentMethod: 'BANK_TRANS',
+                    },
+                  ],
+                  month: '2021-1-01',
+                  salaryCurrencyTotal: 85,
+                  total: 35,
+                },
+              ],
+              name: 'test name',
+              pledgeAmount: null,
+              pledgeCurrency: 'CAD',
+              pledgeFrequency: null,
+              status: null,
+              total: 1290,
+            },
+          ],
+          currency: 'cad',
+          totals: {
+            months: [
+              {
+                month: '2020-10-01',
+                total: 1836.32,
+              },
+              {
+                month: '2020-11-01',
+                total: 1836.32,
+              },
+              {
+                month: '2020-12-01',
+                total: 1836.32,
+              },
+              {
+                month: '2021-1-01',
+                total: 1836.32,
+              },
+            ],
+          },
+        },
+      ],
+      salaryCurrency: 'CAD',
+    },
+  },
+};
+
+const errorMocks = {
+  FourteenMonthReport: {
+    currencyGroups: [
+      {
+        currency: 'cad',
+        totals: {
+          year: 0,
+          average: 0,
+          minimum: 0,
+          months: [
+            {
+              month: '2020-10-01',
+              total: 0,
+            },
+            {
+              month: '2020-11-01',
+              total: 0,
+            },
+            {
+              month: '2020-12-01',
+              total: 0,
+            },
+            {
+              month: '2021-1-01',
+              total: 0,
+            },
+          ],
+        },
+      },
+    ],
+    salaryCurrency: 'CAD',
+  },
+};
 
 describe('FourteenMonthReport', () => {
-  it('loading', async () => {
+  it('salary report loading', async () => {
     const { queryByTestId, queryByText } = render(
       <ThemeProvider theme={theme}>
         <GqlMockedProvider<FourteenMonthReportQuery>>
           <FourteenMonthReport
             accountListId={accountListId}
-            currencyType={currencyType}
+            currencyType="salary"
             isNavListOpen={true}
             title={title}
             onNavListToggle={onNavListToggle}
@@ -35,115 +169,12 @@ describe('FourteenMonthReport', () => {
   });
 
   it('salary report loaded', async () => {
-    const mocks = {
-      FourteenMonthReport: {
-        fourteenMonthReport: {
-          currencyGroups: [
-            {
-              contacts: [
-                {
-                  accountNumbers: ['10182'],
-                  average: 86,
-                  id: 'contact-1',
-                  lateBy30Days: false,
-                  lateBy60Days: false,
-                  minimum: 85,
-                  months: [
-                    {
-                      donations: [
-                        {
-                          amount: 85,
-                          currency: 'CAD',
-                          date: '2020-07-15',
-                          paymentMethod: 'BANK_TRANS',
-                        },
-                      ],
-                      month: '2020-10-01',
-                      salaryCurrencyTotal: 85,
-                      total: 35,
-                    },
-                    {
-                      donations: [
-                        {
-                          amount: 85,
-                          currency: 'CAD',
-                          date: '2020-11-15',
-                          paymentMethod: 'BANK_TRANS',
-                        },
-                      ],
-                      month: '2020-11-01',
-                      salaryCurrencyTotal: 85,
-                      total: 35,
-                    },
-                    {
-                      donations: [
-                        {
-                          amount: 85,
-                          currency: 'CAD',
-                          date: '2020-12-15',
-                          paymentMethod: 'BANK_TRANS',
-                        },
-                      ],
-                      month: '2020-12-01',
-                      salaryCurrencyTotal: 85,
-                      total: 35,
-                    },
-                    {
-                      donations: [
-                        {
-                          amount: 85,
-                          currency: 'CAD',
-                          date: '2021-1-15',
-                          paymentMethod: 'BANK_TRANS',
-                        },
-                      ],
-                      month: '2021-1-01',
-                      salaryCurrencyTotal: 85,
-                      total: 35,
-                    },
-                  ],
-                  name: 'test name',
-                  pledgeAmount: null,
-                  pledgeCurrency: 'CAD',
-                  pledgeFrequency: null,
-                  status: null,
-                  total: 1290,
-                },
-              ],
-              currency: 'cad',
-              totals: {
-                months: [
-                  {
-                    month: '2020-10-01',
-                    total: 1836.32,
-                  },
-                  {
-                    month: '2020-11-01',
-                    total: 1836.32,
-                  },
-                  {
-                    month: '2020-12-01',
-                    total: 1836.32,
-                  },
-                  {
-                    month: '2021-1-01',
-                    total: 1836.32,
-                  },
-                ],
-              },
-            },
-          ],
-          salaryCurrency: 'CAD',
-        },
-      },
-    };
-
     const { getByTestId, queryByTestId, queryByText } = render(
       <ThemeProvider theme={theme}>
         <GqlMockedProvider<FourteenMonthReportQuery> mocks={mocks}>
           <FourteenMonthReport
             accountListId={accountListId}
-            currencyType={currencyType}
+            currencyType="salary"
             isNavListOpen={true}
             title={title}
             onNavListToggle={onNavListToggle}
@@ -162,47 +193,83 @@ describe('FourteenMonthReport', () => {
     expect(getByTestId('FourteenMonthReport')).toBeInTheDocument();
   });
 
-  it('error', async () => {
-    const mocks = {
-      FourteenMonthReport: {
-        currencyGroups: [
-          {
-            currency: 'cad',
-            totals: {
-              year: 0,
-              average: 0,
-              minimum: 0,
-              months: [
-                {
-                  month: '2020-10-01',
-                  total: 0,
-                },
-                {
-                  month: '2020-11-01',
-                  total: 0,
-                },
-                {
-                  month: '2020-12-01',
-                  total: 0,
-                },
-                {
-                  month: '2021-1-01',
-                  total: 0,
-                },
-              ],
-            },
-          },
-        ],
-        salaryCurrency: 'CAD',
-      },
-    };
+  it('partner report loading', async () => {
+    const { queryByTestId, queryByText } = render(
+      <ThemeProvider theme={theme}>
+        <GqlMockedProvider<FourteenMonthReportQuery>>
+          <FourteenMonthReport
+            accountListId={accountListId}
+            currencyType="donor"
+            isNavListOpen={true}
+            title={title}
+            onNavListToggle={onNavListToggle}
+          />
+        </GqlMockedProvider>
+      </ThemeProvider>,
+    );
 
-    const { queryByTestId, getByTestId, queryByText } = render(
+    expect(queryByText(title)).toBeInTheDocument();
+    expect(queryByTestId('LoadingFourteenMonthReport')).toBeInTheDocument();
+    expect(queryByTestId('Notification')).toBeNull();
+  });
+
+  it('partner report loaded', async () => {
+    const { getByTestId, queryByTestId, queryByText } = render(
       <ThemeProvider theme={theme}>
         <GqlMockedProvider<FourteenMonthReportQuery> mocks={mocks}>
           <FourteenMonthReport
             accountListId={accountListId}
-            currencyType={currencyType}
+            currencyType="donor"
+            isNavListOpen={true}
+            title={title}
+            onNavListToggle={onNavListToggle}
+          />
+        </GqlMockedProvider>
+      </ThemeProvider>,
+    );
+
+    await waitFor(() => {
+      expect(
+        queryByTestId('LoadingFourteenMonthReport'),
+      ).not.toBeInTheDocument();
+    });
+
+    expect(queryByText(title)).toBeInTheDocument();
+    expect(getByTestId('FourteenMonthReport')).toBeInTheDocument();
+  });
+
+  it('salary report error', async () => {
+    const { queryByTestId, getByTestId, queryByText } = render(
+      <ThemeProvider theme={theme}>
+        <GqlMockedProvider<FourteenMonthReportQuery> mocks={errorMocks}>
+          <FourteenMonthReport
+            accountListId={accountListId}
+            currencyType="salary"
+            isNavListOpen={true}
+            title={title}
+            onNavListToggle={onNavListToggle}
+          />
+        </GqlMockedProvider>
+      </ThemeProvider>,
+    );
+
+    await waitFor(() => {
+      expect(
+        queryByTestId('LoadingFourteenMonthReport'),
+      ).not.toBeInTheDocument();
+    });
+
+    expect(queryByText(title)).toBeInTheDocument();
+    expect(getByTestId('Notification')).toBeInTheDocument();
+  });
+
+  it('partner report error', async () => {
+    const { queryByTestId, getByTestId, queryByText } = render(
+      <ThemeProvider theme={theme}>
+        <GqlMockedProvider<FourteenMonthReportQuery> mocks={errorMocks}>
+          <FourteenMonthReport
+            accountListId={accountListId}
+            currencyType="donor"
             isNavListOpen={true}
             title={title}
             onNavListToggle={onNavListToggle}
