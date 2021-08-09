180cce94e94c625814be5df25571936516fa6ec3
hotcakedev628 <hotcake.dev.628@hotmail.com>
2021-08-09 14:02:25 -0400

    jest: add case if total balance is undefined

--- src/components/Reports/DesignationAccountsReport/Layout/Header/Header.test.tsx
+++ src/components/Reports/DesignationAccountsReport/Layout/Header/Header.test.tsx
@@ -26,4 +26,19 @@ describe('DesignationAccountsReportHeader', () => {
     expect(getByText(`Balance: ${totalBalance}`)).toBeInTheDocument();
     userEvent.click(getByRole('button', { name: 'Toggle Filter Panel' }));
   });
+
+  it('should not render total balance if undefined', async () => {
+    const { queryByText } = render(
+      <ThemeProvider theme={theme}>
+        <Header
+          isNavListOpen={true}
+          title={title}
+          onNavListToggle={onNavListToggle}
+          totalBalance={undefined}
+        />
+      </ThemeProvider>,
+    );
+
+    expect(queryByText(`Balance: ${totalBalance}`)).toBeNull();
+  });
 });
