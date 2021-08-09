b5c928f34fce1563ebebaa95cbad0b8e28ab8220
CSimbulan <c.simbulan@hotmail.com>
2021-08-09 11:03:02 -0400

    card fo if you have no appeals

---
+++ src/components/Tool/Appeal/NoAppeals.tsx
@@ -0,0 +1,28 @@
+import { Box, CardContent, Typography } from '@material-ui/core';
+import React, { ReactElement } from 'react';
+import Icon from '@mdi/react';
+import { mdiAlertCircleOutline } from '@mdi/js';
+import AnimatedCard from '../../../../src/components/AnimatedCard';
+
+const NoAppeals = (): ReactElement => {
+  return (
+    <Box m={1}>
+      <AnimatedCard>
+        <CardContent>
+          <Box
+            display="flex"
+            justifyContent="center"
+            textAlign="center"
+            flexDirection="column"
+            alignItems="center"
+          >
+            <Icon path={mdiAlertCircleOutline} size={3} />
+            <Typography variant="h6">You do not have any appeals.</Typography>
+          </Box>
+        </CardContent>
+      </AnimatedCard>
+    </Box>
+  );
+};
+
+export default NoAppeals;
