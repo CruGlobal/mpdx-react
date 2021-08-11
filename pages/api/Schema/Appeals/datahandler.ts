95fe5786daa0b69f7d46688becaab7ad865871c6
CSimbulan <c.simbulan@hotmail.com>
2021-08-11 11:13:59 -0400

    switched from forEach to map

--- pages/api/Schema/Appeals/datahandler.ts
+++ pages/api/Schema/Appeals/datahandler.ts
@@ -1,4 +1,3 @@
-import { Contact } from '../../../../graphql/types.generated';
 import { Appeal } from '../../graphql-rest.page.generated';
 
 export interface AppealsResponse {
@@ -19,48 +18,23 @@ export interface AppealsResponse {
     updated_at: string;
     updated_in_db_at: string;
   };
-  relationships: {
-    account_list: {
-      data: {
-        id: string;
-        type: string;
-      };
-    };
-    contacts: {
-      data: Contact[];
-    };
-    donations: {
-      data: [
-        {
-          id: string;
-          type: string;
-        },
-      ];
-    };
-  };
 }
 
 const getAppeals = (data: AppealsResponse[]): Appeal[] => {
-  const response: Appeal[] = [];
-  data.forEach((item) => {
-    const appeal: Appeal = {
-      id: item.id,
-      name: item.attributes.name,
-      amount: parseFloat(item.attributes.amount) || 0,
-      pledgesAmountNotReceivedNotProcessed:
-        item.attributes.pledges_amount_not_received_not_processed,
-      pledgesAmountProcessed: item.attributes.pledges_amount_processed,
-      pledgesAmountReceivedNotProcessed:
-        item.attributes.pledges_amount_received_not_processed,
-      pledgesAmountTotal: item.attributes.pledges_amount_total,
-      amountCurrency: item.attributes.total_currency,
-      updatedAt: item.attributes.updated_at,
-      createdAt: item.attributes.created_at,
-    };
-
-    response.push(appeal);
-  });
-  return response;
+  return data.map((item) => ({
+    id: item.id,
+    name: item.attributes.name,
+    amount: parseFloat(item.attributes.amount) || 0,
+    pledgesAmountNotReceivedNotProcessed:
+      item.attributes.pledges_amount_not_received_not_processed,
+    pledgesAmountProcessed: item.attributes.pledges_amount_processed,
+    pledgesAmountReceivedNotProcessed:
+      item.attributes.pledges_amount_received_not_processed,
+    pledgesAmountTotal: item.attributes.pledges_amount_total,
+    amountCurrency: item.attributes.total_currency,
+    updatedAt: item.attributes.updated_at,
+    createdAt: item.attributes.created_at,
+  }));
 };
 
 export { getAppeals };
