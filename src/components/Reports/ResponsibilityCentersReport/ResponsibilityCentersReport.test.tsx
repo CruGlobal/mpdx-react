caf7233ca866f729f235a4004ea621b8087d80a8
hotcakedev628 <hotcake.dev.628@hotmail.com>
2021-08-18 15:17:24 -0400

    update test for responsibility centers report

--- src/components/Reports/ResponsibilityCentersReport/ResponsibilityCentersReport.test.tsx
+++ src/components/Reports/ResponsibilityCentersReport/ResponsibilityCentersReport.test.tsx
@@ -1,63 +1,94 @@
 import React from 'react';
 import { render, waitFor } from '@testing-library/react';
-import { MockedProvider } from '@apollo/client/testing';
 import { ThemeProvider } from '@material-ui/core';
+import { FinancialAccountsQuery } from './GetFinancialAccounts.generated';
 import { ResponsibilityCentersReport } from './ResponsibilityCentersReport';
-import {
-  financialAccountsEmptyMock,
-  financialAccountsErrorMock,
-  financialAccountsLoadingMock,
-  financialAccountsMock,
-} from './ResponsibilityCentersReport.mock';
+import { GqlMockedProvider } from '__tests__/util/graphqlMocking';
 import theme from 'src/theme';
 
 const accountListId = '111';
 const title = 'test title';
 const onNavListToggle = jest.fn();
 
-describe('FinancialAccounts', () => {
+const mocks = {
+  FinancialAccounts: {
+    financialAccounts: {
+      edges: [
+        {
+          node: {
+            active: false,
+            balance: {
+              conversionDate: '2/2/2021',
+              convertedAmount: 3500,
+              convertedCurrency: 'CAD',
+            },
+            code: '13212',
+            id: 'test-id-111',
+            name: 'Test Account',
+            organization: {
+              id: '111-2222-3333',
+              name: 'test org 01',
+            },
+            updatedAt: '2/2/2021',
+          },
+        },
+      ],
+    },
+  },
+};
+
+const errorMocks = {
+  FinancialAccounts: {},
+  error: { name: 'error', message: 'Error loading data.  Try again.' },
+};
+
+const emptyMocks = {
+  FinancialAccounts: {
+    financialAccounts: {
+      edges: [],
+    },
+  },
+};
+
+describe('ResponsibilityCentersReport', () => {
   it('default', async () => {
-    const { queryByTestId, getByTestId, getByText } = render(
+    const { getByText, getByTestId, queryByTestId } = render(
       <ThemeProvider theme={theme}>
-        <MockedProvider
-          mocks={[financialAccountsMock(accountListId)]}
-          addTypename={false}
-        >
+        <GqlMockedProvider<FinancialAccountsQuery> mocks={mocks}>
           <ResponsibilityCentersReport
             accountListId={accountListId}
             isNavListOpen={true}
             title={title}
             onNavListToggle={onNavListToggle}
           />
-        </MockedProvider>
+        </GqlMockedProvider>
       </ThemeProvider>,
     );
 
     await waitFor(() => {
-      expect(queryByTestId('LoadingFinancialAccounts')).not.toBeInTheDocument();
+      expect(
+        queryByTestId('LoadingResponsibilityCenters'),
+      ).not.toBeInTheDocument();
     });
 
     expect(getByText(title)).toBeInTheDocument();
     expect(getByText('CA$3,500')).toBeInTheDocument();
     expect(queryByTestId('Notification')).not.toBeInTheDocument();
     expect(getByTestId('AccountsGroupList')).toBeInTheDocument();
-    expect(getByTestId('ResponsibilityCentersScrollBox')).toBeInTheDocument();
+    expect(getByTestId('DesignationAccountsScrollBox')).toBeInTheDocument();
   });
 
   it('loading', async () => {
     const { queryByTestId, getByText } = render(
       <ThemeProvider theme={theme}>
-        <MockedProvider
-          mocks={[financialAccountsLoadingMock(accountListId)]}
-          addTypename={false}
-        >
+        <GqlMockedProvider<FinancialAccountsQuery>>
           <ResponsibilityCentersReport
             accountListId={accountListId}
             isNavListOpen={true}
             title={title}
             onNavListToggle={onNavListToggle}
           />
-        </MockedProvider>
+        </GqlMockedProvider>
       </ThemeProvider>,
     );
 
@@ -66,20 +97,40 @@ describe('FinancialAccounts', () => {
     expect(queryByTestId('Notification')).not.toBeInTheDocument();
   });
 
-  it('empty', async () => {
-    const { queryByTestId, getByText } = render(
+  it('error', async () => {
+    const { queryByTestId } = render(
       <ThemeProvider theme={theme}>
-        <MockedProvider
-          mocks={[financialAccountsEmptyMock(accountListId)]}
-          addTypename={false}
-        >
+        <GqlMockedProvider<FinancialAccountsQuery> mocks={errorMocks}>
           <ResponsibilityCentersReport
             accountListId={accountListId}
             isNavListOpen={true}
             title={title}
             onNavListToggle={onNavListToggle}
           />
-        </MockedProvider>
+        </GqlMockedProvider>
+      </ThemeProvider>,
+    );
+
+    await waitFor(() => {
+      expect(
+        queryByTestId('LoadingResponsibilityCenters'),
+      ).not.toBeInTheDocument();
+    });
+
+    expect(queryByTestId('Notification')).toBeInTheDocument();
+  });
+
+  it('empty', async () => {
+    const { queryByTestId, getByText } = render(
+      <ThemeProvider theme={theme}>
+        <GqlMockedProvider<FinancialAccountsQuery> mocks={emptyMocks}>
+          <ResponsibilityCentersReport
+            accountListId={accountListId}
+            isNavListOpen={true}
+            title={title}
+            onNavListToggle={onNavListToggle}
+          />
+        </GqlMockedProvider>
       </ThemeProvider>,
     );
 
@@ -91,31 +142,5 @@ describe('FinancialAccounts', () => {
 
     expect(getByText(title)).toBeInTheDocument();
     expect(queryByTestId('EmptyReport')).toBeInTheDocument();
-  });
-
-  it('error', async () => {
-    const { queryByTestId } = render(
-      <ThemeProvider theme={theme}>
-        <MockedProvider
-          mocks={[financialAccountsErrorMock(accountListId)]}
-          addTypename={false}
-        >
-          <ResponsibilityCentersReport
-            accountListId={accountListId}
-            isNavListOpen={true}
-            title={title}
-            onNavListToggle={onNavListToggle}
-          />
-        </MockedProvider>
-      </ThemeProvider>,
-    );
-
-    await waitFor(() => {
-      expect(
-        queryByTestId('LoadingResponsibilityCenters'),
-      ).not.toBeInTheDocument();
-    });
-
-    expect(queryByTestId('Notification')).toBeInTheDocument();
   });
 });
