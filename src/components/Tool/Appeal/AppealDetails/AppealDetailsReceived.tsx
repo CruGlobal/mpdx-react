69080fb7bd2a5e68139f1214428645f17250de6b
CSimbulan <c.simbulan@hotmail.com>
2021-08-19 21:24:34 -0400

    added styles to datagrid

--- src/components/Tool/Appeal/AppealDetails/AppealDetailsReceived.tsx
+++ src/components/Tool/Appeal/AppealDetails/AppealDetailsReceived.tsx
@@ -1,15 +1,103 @@
 import React, { ReactElement } from 'react';
-import { Box } from '@material-ui/core';
+import { Box, IconButton, makeStyles } from '@material-ui/core';
 import { DataGrid } from '@material-ui/data-grid';
+import Icon from '@mdi/react';
+import { mdiSquareEditOutline, mdiDelete } from '@mdi/js';
+
+import theme from '../../../../theme';
+
+const useStyles = makeStyles(() => ({
+  container: {
+    marginTop: 20,
+    height: '60vh',
+    '& .MuiDataGrid-row.Mui-odd': {
+      backgroundColor: theme.palette.cruGrayLight.main,
+    },
+    '& .MuiDataGrid-columnHeader': {
+      backgroundColor: theme.palette.mpdxBlue.main,
+      color: 'white',
+      height: '100%',
+      '& .MuiCheckbox-colorPrimary.Mui-checked': {
+        color: 'white',
+      },
+      '& .MuiDataGrid-columnHeaderTitle': {
+        fontWeight: 600,
+      },
+    },
+    '& .MuiDataGrid-columnHeaderWrapper': {
+      '& .MuiDataGrid-cell': {
+        backgroundColor: theme.palette.mpdxBlue.main,
+      },
+    },
+    '& .MuiDataGrid-row:hover': {
+      backgroundColor: theme.palette.cruGrayMedium.main,
+      '& .MuiCheckbox-colorPrimary': {
+        color: 'white',
+      },
+    },
+    '& .MuiDataGrid-sortIcon': {
+      color: 'white',
+    },
+    '& .MuiDataGrid-row.Mui-selected': {
+      backgroundColor: `${theme.palette.mpdxYellow.main} !important`,
+      '& .MuiCheckbox-colorPrimary': {
+        color: `${theme.palette.mpdxBlue.main} !important`,
+      },
+    },
+    '& .MuiSvgIcon-fontSizeSmall': {
+      color: 'white',
+    },
+  },
+  actionIconButton: {
+    color: theme.palette.cruGrayDark.main,
+    '&:hover': { color: theme.palette.mpdxBlue.main, cursor: 'pointer' },
+  },
+}));
 
 const columns = [
-  { field: 'contact', headerName: 'Contact', flex: 1 },
-  { field: 'amount', headerName: 'Amount Received', flex: 1 },
-  { field: 'date', headerName: 'Date Received', flex: 1 },
+  {
+    field: 'contact',
+    headerName: 'Contact',
+    flex: 1,
+  },
+  {
+    field: 'amount',
+    headerName: 'Amount Received',
+    flex: 1,
+  },
+  {
+    field: 'date',
+    headerName: 'Date Received',
+    flex: 1,
+  },
+  {
+    field: 'actions',
+    headerName: 'Actions',
+    flex: 0.3,
+    sortable: false,
+    renderCell: function renderActions() {
+      const classes = useStyles();
+      return (
+        <>
+          <IconButton className={classes.actionIconButton}>
+            <Icon path={mdiSquareEditOutline} size={1} />
+          </IconButton>
+          <IconButton className={classes.actionIconButton}>
+            <Icon path={mdiDelete} size={1} />
+          </IconButton>
+        </>
+      );
+    },
+  },
 ];
 
 const rows = [
-  { id: 1, contact: 'Test 123', amount: '500.00 CAD', date: '06/14/2021' },
+  {
+    id: 1,
+    contact: 'Test 123',
+    amount: '500.00 CAD',
+    date: '06/14/2021',
+  },
   { id: 2, contact: 'vvvv', amount: '5000.00 CAD', date: '06/22/2020' },
   { id: 3, contact: 'asdasd', amount: '212.00 CAD', date: '06/13/2021' },
   { id: 4, contact: 'test 1234', amount: '1.00 CAD', date: '03/14/2021' },
@@ -17,20 +105,17 @@ const rows = [
 ];
 
 const AppealDetailsReceived = (): ReactElement => {
+  const classes = useStyles();
+
   return (
-    <Box
-      component="div"
-      style={{
-        marginTop: 20,
-        height: '60vh',
-      }}
-    >
+    <Box component="div" className={classes.container}>
       <DataGrid
         rows={rows}
         columns={columns}
         checkboxSelection
         pageSize={25}
         rowsPerPageOptions={[10, 25, 50, 100]}
+        disableSelectionOnClick
       />
     </Box>
   );
