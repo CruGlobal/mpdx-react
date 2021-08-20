596290c2844335cd027ec03630afefe24be01b45
CSimbulan <c.simbulan@hotmail.com>
2021-08-20 11:01:51 -0400

    moved both appeal queries into same component

--- src/components/Tool/Appeal/Appeals.tsx
+++ src/components/Tool/Appeal/Appeals.tsx
@@ -7,6 +7,7 @@ import {
   styled,
 } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
+import { useGetPrimaryAppealQuery } from '../../../../pages/accountLists/[accountListId]/tools/GetPrimaryAppeal.generated';
 import { useGetAppealsQuery } from '../../../../pages/accountLists/[accountListId]/tools/GetAppeals.generated';
 import { useAccountListId } from '../../../../src/hooks/useAccountListId';
 import Appeal from '../../../../src/components/Tool/Appeal/Appeal';
@@ -16,66 +17,106 @@ const LoadingIndicator = styled(CircularProgress)(({ theme }) => ({
   margin: theme.spacing(0, 1, 0, 0),
 }));
 
-export interface Props {
-  primaryId: string;
-}
-
-const Appeals = ({ primaryId }: Props): ReactElement => {
+const Appeals = (): ReactElement => {
   const accountListId = useAccountListId();
   const { t } = useTranslation();
-  const { data, loading } = useGetAppealsQuery({
+  const primaryAppealResponse = useGetPrimaryAppealQuery({
+    variables: { accountListId: accountListId || '' },
+  });
+  const regularAppealsResponse = useGetAppealsQuery({
     variables: { accountListId: accountListId || '' },
   });
 
   return (
     <Box mb={1}>
       <Box m={1}>
-        <Typography variant="h6">{t('Appeals')}</Typography>
+        <Typography variant="h6">{t('Primary Appeal')}</Typography>
       </Box>
       <Divider />
-      {loading ? (
+      {primaryAppealResponse.loading ? (
         <Box display="flex" justifyContent="center" mt={10}>
           <LoadingIndicator color="primary" size={40} />
         </Box>
-      ) : data && data.appeals ? (
+      ) : primaryAppealResponse.data && primaryAppealResponse.data.appeals ? (
+        <Appeal
+          name={primaryAppealResponse.data.appeals.nodes[0].name || ''}
+          id={primaryAppealResponse.data.appeals.nodes[0].id || ''}
+          primary
+          amount={primaryAppealResponse.data.appeals.nodes[0].amount || 0}
+          amountCurrency={
+            primaryAppealResponse.data.appeals.nodes[0].amountCurrency
+          }
+          given={
+            primaryAppealResponse.data.appeals.nodes[0]
+              .pledgesAmountProcessed || 0
+          }
+          received={
+            primaryAppealResponse.data.appeals.nodes[0]
+              .pledgesAmountReceivedNotProcessed || 0
+          }
+          commited={
+            primaryAppealResponse.data.appeals.nodes[0]
+              .pledgesAmountNotReceivedNotProcessed || 0
+          }
+          total={
+            primaryAppealResponse.data.appeals.nodes[0].pledgesAmountTotal || 0
+          }
+        />
+      ) : (
+        <NoAppeals primary />
+      )}
+      <Box m={1}>
+        <Typography variant="h6">{t('Appeals')}</Typography>
+      </Box>
+      <Divider />
+      {regularAppealsResponse.loading ? (
+        <Box display="flex" justifyContent="center" mt={10}>
+          <LoadingIndicator color="primary" size={40} />
+        </Box>
+      ) : regularAppealsResponse.data && regularAppealsResponse.data.appeals ? (
         <>
-          {data.appeals
-            .filter((appeal) => appeal.id !== primaryId)
-            .map((appeal) => (
-              <Box key={appeal.name} mb={3}>
-                <Appeal
-                  name={appeal.name || ''}
-                  primary={false}
-                  amount={appeal.amount || 0}
-                  amountCurrency={appeal.amountCurrency}
-                  given={appeal.pledgesAmountProcessed}
-                  received={appeal.pledgesAmountReceivedNotProcessed}
-                  commited={appeal.pledgesAmountNotReceivedNotProcessed}
-                  total={appeal.pledgesAmountTotal}
-                />
-              </Box>
-            ))}
-          <Box display="flex" justifyContent="center">
-            <Typography variant="h6">
-              Showing{' '}
-              <strong>
-                {
-                  data.appeals.filter((appeal) => appeal.id !== primaryId)
-                    .length // Incase there is no primary appeal.
-                }
-              </strong>{' '}
-              of{' '}
-              <strong>
-                {
-                  data.appeals.filter((appeal) => appeal.id !== primaryId)
-                    .length
-                }
-              </strong>
-            </Typography>
-          </Box>
+          {regularAppealsResponse.data.appeals.nodes.map((appeal) => (
+            <Box key={appeal.name} mb={3}>
+              <Appeal
+                name={appeal.name || ''}
+                id={appeal.id || ''}
+                primary={false}
+                amount={appeal.amount || 0}
+                amountCurrency={appeal.amountCurrency}
+                given={appeal.pledgesAmountProcessed}
+                received={appeal.pledgesAmountReceivedNotProcessed}
+                commited={appeal.pledgesAmountNotReceivedNotProcessed}
+                total={appeal.pledgesAmountTotal}
+              />
+            </Box>
+          ))}
         </>
       ) : (
         <NoAppeals />
+      )}
+      {!primaryAppealResponse.loading && !regularAppealsResponse.loading && (
+        <Box display="flex" justifyContent="center">
+          <Typography variant="h6">
+            Showing{' '}
+            <strong>
+              {(primaryAppealResponse.data
+                ? primaryAppealResponse.data.appeals.nodes.length
+                : 0) +
+                (regularAppealsResponse.data
+                  ? regularAppealsResponse.data.appeals.nodes.length
+                  : 0)}
+            </strong>{' '}
+            of{' '}
+            <strong>
+              {(primaryAppealResponse.data
+                ? primaryAppealResponse.data.appeals.nodes.length
+                : 0) +
+                (regularAppealsResponse.data
+                  ? regularAppealsResponse.data.appeals.nodes.length
+                  : 0)}
+            </strong>
+          </Typography>
+        </Box>
       )}
     </Box>
   );
