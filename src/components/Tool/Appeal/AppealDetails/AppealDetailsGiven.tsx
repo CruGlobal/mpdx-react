e6e20ad11553877ca16004d6463bcbd8ad50692f
CSimbulan <c.simbulan@hotmail.com>
2021-08-23 18:13:35 -0400

    given view

---
+++ src/components/Tool/Appeal/AppealDetails/AppealDetailsGiven.tsx
@@ -0,0 +1,140 @@
+import React, { ReactElement } from 'react';
+import { Box, IconButton, makeStyles } from '@material-ui/core';
+import { DataGrid, GridSelectionModel } from '@material-ui/data-grid';
+import Icon from '@mdi/react';
+import { mdiSquareEditOutline, mdiDelete } from '@mdi/js';
+import i18n from 'i18next';
+import { useAppealContext } from '../AppealContextProvider/AppealContextProvider';
+import theme from '../../../../theme';
+import { TestAppeal } from '../../../../../pages/accountLists/[accountListId]/tools/appeals/testAppeal';
+import AppealDetailsNoData from './AppealDetailsNoData';
+
+const useStyles = makeStyles(() => ({
+  container: {
+    marginTop: 20,
+    height: '60vh',
+    '& .MuiDataGrid-row.Mui-odd': {
+      backgroundColor: theme.palette.cruGrayLight.main,
+    },
+    '& .MuiDataGrid-columnHeader': {
+      backgroundColor: theme.palette.mpdxBlue.main,
+      color: 'white',
+      height: '100%',
+      '& .MuiCheckbox-colorPrimary.Mui-checked': {
+        color: 'white',
+      },
+      '& .MuiDataGrid-columnHeaderTitle': {
+        fontWeight: 600,
+      },
+    },
+    '& .MuiDataGrid-columnHeaderWrapper': {
+      '& .MuiDataGrid-cell': {
+        backgroundColor: theme.palette.mpdxBlue.main,
+      },
+    },
+    '& .MuiDataGrid-row:hover': {
+      backgroundColor: theme.palette.cruGrayMedium.main,
+      '& .MuiCheckbox-colorPrimary': {
+        color: 'white',
+      },
+    },
+    '& .MuiDataGrid-sortIcon': {
+      color: 'white',
+    },
+    '& .MuiDataGrid-row.Mui-selected': {
+      backgroundColor: `${theme.palette.mpdxYellow.main} !important`,
+      '& .MuiCheckbox-colorPrimary': {
+        color: `${theme.palette.mpdxBlue.main} !important`,
+      },
+    },
+    '& .MuiSvgIcon-fontSizeSmall': {
+      color: 'white',
+    },
+  },
+  actionIconButton: {
+    color: theme.palette.cruGrayDark.main,
+    '&:hover': { color: theme.palette.mpdxBlue.main, cursor: 'pointer' },
+  },
+}));
+
+const columns = [
+  {
+    field: 'contact',
+    headerName: i18n.t('Contact'),
+    flex: 1,
+  },
+  {
+    field: 'donation',
+    headerName: i18n.t('Donation(s)'),
+    flex: 1,
+  },
+  {
+    field: 'date',
+    headerName: i18n.t('Date'),
+    flex: 1,
+  },
+  {
+    field: 'actions',
+    headerName: i18n.t('Actions'),
+    flex: 0.3,
+    sortable: false,
+    renderCell: function renderActions() {
+      const classes = useStyles();
+      return (
+        <>
+          <IconButton className={classes.actionIconButton}>
+            <Icon path={mdiSquareEditOutline} size={1} />
+          </IconButton>
+          <IconButton className={classes.actionIconButton}>
+            <Icon path={mdiDelete} size={1} />
+          </IconButton>
+        </>
+      );
+    },
+  },
+];
+
+export interface Props {
+  appeal: TestAppeal;
+}
+
+const AppealDetailsGiven = ({ appeal }: Props): ReactElement => {
+  const classes = useStyles();
+  const { appealState, setAppealState } = useAppealContext();
+
+  const rows = appeal.given.map((donation, index) => ({
+    id: index,
+    contact: donation.name,
+    donation: `${donation.amount.toFixed(2)} ${donation.currency}`,
+    date: donation.date,
+  }));
+
+  const updateSelected = (e: GridSelectionModel): void => {
+    const temp: string[] = [];
+    for (const x of e) {
+      temp.push(rows[parseInt(x.toString())].contact);
+    }
+    setAppealState({ ...appealState, selected: [...temp] });
+  };
+  return (
+    <>
+      {appeal.given.length > 0 ? (
+        <Box component="div" className={classes.container}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            checkboxSelection
+            pageSize={25}
+            rowsPerPageOptions={[10, 25, 50, 100]}
+            disableSelectionOnClick
+            onSelectionModelChange={updateSelected}
+          />
+        </Box>
+      ) : (
+        <AppealDetailsNoData />
+      )}
+    </>
+  );
+};
+
+export default AppealDetailsGiven;
