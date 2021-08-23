232e8a671d750434b3955521d782ec82513b6383
CSimbulan <c.simbulan@hotmail.com>
2021-08-23 13:31:12 -0400

    removed wrapper and console log

--- src/components/Tool/Appeal/Appeals.tsx
+++ src/components/Tool/Appeal/Appeals.tsx
@@ -33,26 +33,23 @@ const Appeals = (): ReactElement => {
         <Box display="flex" justifyContent="center" mt={10}>
           <LoadingIndicator color="primary" size={40} />
         </Box>
-      ) : data?.primaryAppeal && data.primaryAppeal.nodes.length > 0 ? (
-        <>
-          {console.log(data)}
-          <Appeal
-            name={data.primaryAppeal.nodes[0].name || ''}
-            id={data.primaryAppeal.nodes[0].id || ''}
-            primary
-            amount={data.primaryAppeal.nodes[0].amount || 0}
-            amountCurrency={data.primaryAppeal.nodes[0].amountCurrency}
-            given={data.primaryAppeal.nodes[0].pledgesAmountProcessed || 0}
-            received={
-              data.primaryAppeal.nodes[0].pledgesAmountReceivedNotProcessed || 0
-            }
-            commited={
-              data.primaryAppeal.nodes[0]
-                .pledgesAmountNotReceivedNotProcessed || 0
-            }
-            total={data.primaryAppeal.nodes[0].pledgesAmountTotal || 0}
-          />
-        </>
+      ) : data?.primaryAppeal ? (
+        <Appeal
+          name={data.primaryAppeal.nodes[0].name || ''}
+          id={data.primaryAppeal.nodes[0].id || ''}
+          primary
+          amount={data.primaryAppeal.nodes[0].amount || 0}
+          amountCurrency={data.primaryAppeal.nodes[0].amountCurrency}
+          given={data.primaryAppeal.nodes[0].pledgesAmountProcessed || 0}
+          received={
+            data.primaryAppeal.nodes[0].pledgesAmountReceivedNotProcessed || 0
+          }
+          commited={
+            data.primaryAppeal.nodes[0].pledgesAmountNotReceivedNotProcessed ||
+            0
+          }
+          total={data.primaryAppeal.nodes[0].pledgesAmountTotal || 0}
+        />
       ) : (
         <NoAppeals primary />
       )}
@@ -64,7 +61,7 @@ const Appeals = (): ReactElement => {
         <Box display="flex" justifyContent="center" mt={10}>
           <LoadingIndicator color="primary" size={40} />
         </Box>
-      ) : data?.regularAppeals && data.regularAppeals.nodes.length > 0 ? (
+      ) : data?.regularAppeals ? (
         <>
           {data.regularAppeals.nodes.map((appeal) => (
             <Box key={appeal.name} mb={3}>
