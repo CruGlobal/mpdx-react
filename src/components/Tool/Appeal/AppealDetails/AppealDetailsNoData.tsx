7f5e4b62ae09bd1e145f7e182e7e892dcadce723
CSimbulan <c.simbulan@hotmail.com>
2021-08-23 18:11:27 -0400

    card if appeal has no data for that view

---
+++ src/components/Tool/Appeal/AppealDetails/AppealDetailsNoData.tsx
@@ -0,0 +1,47 @@
+import { Box, styled, Typography } from '@material-ui/core';
+import React, { ReactElement } from 'react';
+import { useAppealContext } from '../AppealContextProvider/AppealContextProvider';
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  width: '100%',
+  border: '1px solid',
+  borderColor: theme.palette.cruGrayMedium.main,
+  marginTop: 30,
+  backgroundColor: theme.palette.cruGrayLight.main,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: theme.spacing(12),
+  borderRadius: 5,
+}));
+
+export interface Props {
+  dataType: string;
+}
+
+const renderCase = (dataType: string): string => {
+  switch (dataType) {
+    case 'given':
+      return 'No donations yet towards this appeal';
+    case 'received':
+      return 'No gifts have been received and not yet processed to this appeal';
+    case 'commited':
+      return 'No contacts with commitments have committed to this appeal';
+    case 'asked':
+      return 'No contacts have been asked for this appeal';
+    case 'excluded':
+      return 'No contacts have been excluded from this appeal';
+  }
+};
+
+const NoData = (): ReactElement => {
+  const { appealState } = useAppealContext();
+
+  return (
+    <StyledBox>
+      <Typography>{renderCase(appealState.subDisplay)}</Typography>
+    </StyledBox>
+  );
+};
+
+export default NoData;
