2688df14954b6f541ef116b1eecfd4a55b748413
Chris Simbulan <simbulan.c@gmail.com>
2021-08-25 14:34:00 -0400

    component for page with 0 statuses to fix

---
+++ src/components/Tool/FixCommitmentInfo/NoContacts.tsx
@@ -0,0 +1,37 @@
+import React, { ReactElement } from 'react';
+import { Box, Typography, styled } from '@material-ui/core';
+import { mdiCurrencyUsd } from '@mdi/js';
+import { useTranslation } from 'react-i18next';
+import Icon from '@mdi/react';
+
+const StyledBox = styled(Box)(({ theme }) => ({
+  width: '100%',
+  border: '1px solid',
+  borderColor: theme.palette.cruGrayMedium.main,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexDirection: 'column',
+  backgroundColor: theme.palette.cruGrayLight.main,
+  paddingTop: theme.spacing(7),
+  paddingBottom: theme.spacing(7),
+}));
+
+const NoContacts = (): ReactElement => {
+  const { t } = useTranslation();
+  return (
+    <StyledBox>
+      <Icon path={mdiCurrencyUsd} size={1.5} />
+      <Typography variant="h5">
+        {t('No contacts with commitment info need attention')}
+      </Typography>
+      <Typography>
+        {t(
+          'Contacts with possibly incorrect commitment info will appear here.',
+        )}
+      </Typography>
+    </StyledBox>
+  );
+};
+
+export default NoContacts;
