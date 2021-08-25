badec5a47f6abc856a0a2db2271d8092c84e7eb4
Chris Simbulan <simbulan.c@gmail.com>
2021-08-25 14:33:29 -0400

    options for select input

---
+++ src/components/Tool/FixCommitmentInfo/InputOptions/Frequencies.ts
@@ -0,0 +1,40 @@
+import i18n from 'i18next';
+
+export const frequencies = [
+  {
+    title: i18n.t('Weekly'),
+    value: 'weekly',
+  },
+  {
+    title: i18n.t('Every 2 Weeks'),
+    value: 'every-2-weeks',
+  },
+  {
+    title: i18n.t('Monthly'),
+    value: 'monthly',
+  },
+  {
+    title: i18n.t('Every 2 Months'),
+    value: 'every-2-months',
+  },
+  {
+    title: i18n.t('Quarterly'),
+    value: 'quarterly',
+  },
+  {
+    title: i18n.t('Every 4 Months'),
+    value: 'every-4-months',
+  },
+  {
+    title: i18n.t('Every 6 Months'),
+    value: 'eveyr-6-months',
+  },
+  {
+    title: i18n.t('Annually'),
+    value: 'annually',
+  },
+  {
+    title: i18n.t('Every 2 Years'),
+    value: 'every-2-years',
+  },
+];
