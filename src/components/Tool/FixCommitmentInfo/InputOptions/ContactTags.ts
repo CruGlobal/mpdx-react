cb4f291fce9f7fcf6070dcd51bdb670518c8014d
Chris Simbulan <simbulan.c@gmail.com>
2021-08-25 14:33:29 -0400

    options for select input

---
+++ src/components/Tool/FixCommitmentInfo/InputOptions/ContactTags.ts
@@ -0,0 +1,60 @@
+import i18n from 'i18next';
+
+export const contactTags = [
+  {
+    title: i18n.t('Never Contacted'),
+    value: 'never-contacted',
+  },
+  {
+    title: i18n.t('Ask In Future'),
+    value: 'ask-in-future',
+  },
+  {
+    title: i18n.t('Cultivate Relationship'),
+    value: 'cultivate-relationship',
+  },
+  {
+    title: i18n.t('Contact for Appointment'),
+    value: 'contact-for-appointment',
+  },
+  {
+    title: i18n.t('Appointment Scheduled'),
+    value: 'appointment-scheduled',
+  },
+  {
+    title: i18n.t('Call for Decision'),
+    value: 'call-for-decision',
+  },
+  {
+    title: i18n.t('Partner - Financial'),
+    value: 'partner-financial',
+  },
+  {
+    title: i18n.t('Partner - Special'),
+    value: 'partner-special',
+  },
+  {
+    title: i18n.t('Partner - Pray'),
+    value: 'parter-pray',
+  },
+  {
+    title: i18n.t('Not Interested'),
+    value: 'not-interested',
+  },
+  {
+    title: i18n.t('Unresponsive'),
+    value: 'unresponsive',
+  },
+  {
+    title: i18n.t('Never Ask'),
+    value: 'never-ask',
+  },
+  {
+    title: i18n.t('Research Abandoned'),
+    value: 'research-abandoned',
+  },
+  {
+    title: i18n.t('Expired Referral'),
+    value: 'expired-referral',
+  },
+];
