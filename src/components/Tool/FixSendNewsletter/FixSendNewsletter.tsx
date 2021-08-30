86413569924f25dd04e72da99503bfcadd82a9b2
Chris Simbulan <simbulan.c@gmail.com>
2021-08-30 12:05:33 -0400

    added button to toggle between test data

--- src/components/Tool/FixSendNewsletter/FixSendNewsletter.tsx
+++ src/components/Tool/FixSendNewsletter/FixSendNewsletter.tsx
@@ -6,16 +6,18 @@ import {
   Typography,
   Grid,
   Divider,
+  Button,
 } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
 import NavToolDrawer from '../NavToolList/NavToolDrawer';
 import theme from '../../../theme';
 import Contact from './Contact';
+import NoContacts from './NoContacts';
 
 const useStyles = makeStyles(() => ({
   container: {
     padding: theme.spacing(3),
-    marginRight: theme.spacing(2),
+    marginRight: theme.spacing(1),
     display: 'flex',
     [theme.breakpoints.down('lg')]: {
       paddingLeft: theme.spacing(5),
@@ -50,29 +52,40 @@ const useStyles = makeStyles(() => ({
     width: '100%',
     display: 'flex',
     justifyContent: 'center',
-    marginTop: theme.spacing(2),
   },
 }));
+
+const testData = [
+  {
+    title: 'Test Contact and Friends',
+    name: 'Test Contact',
+    tag: 'Partner - Financial',
+    address: {
+      street: '70 Test Ave',
+      city: 'Vancouver BC V5Z 2V7',
+    },
+    source: 'Source: DonorHub (12/16/2014)',
+    newsletterType: 'physical',
+    email: 'test@test.com',
+  },
+  {
+    title: 'Anonymous Test',
+    newsletterType: 'both',
+  },
+];
 
 const FixSendNewsletter = (): ReactElement => {
   const classes = useStyles();
   const [isNavListOpen, setNavListOpen] = useState<boolean>(true);
+  const [test, setTest] = useState(testData);
   const { t } = useTranslation();
   const handleNavListToggle = () => {
     setNavListOpen(!isNavListOpen);
   };
 
-  const testData = [
-    {
-      name: 'Test test',
-      tagTitle: 'Partner - Financial',
-      tagValue: 'partner-financial',
-      frequencyTitle: 'Monthly',
-      frequencyValue: 'monthly',
-      amount: 50,
-      amountCurrency: 'CAD',
-    },
-  ];
+  const toggleData = (): void => {
+    test.length > 0 ? setTest([]) : setTest(testData);
+  };
 
   //TODO: Make navbar selectId = "fixSendNewsletter" when other branch gets merged
 
@@ -82,7 +95,7 @@ const FixSendNewsletter = (): ReactElement => {
         <NavToolDrawer
           open={isNavListOpen}
           toggle={handleNavListToggle}
-          selectedId="fixCommitmentInfo"
+          selectedId="fixSendNewsletter"
         />
         <Container
           className={classes.container}
@@ -106,33 +119,37 @@ const FixSendNewsletter = (): ReactElement => {
                     'Contacts that appear here have an empty Newsletter Status and Partner Status set to Financial, Special, or Pray. Choose a newsletter status for contacts below.',
                   )}
                 </Typography>
+                <Button size="small" variant="outlined" onClick={toggleData}>
+                  Change Test
+                </Button>
               </Box>
             </Grid>
-            {testData.length > 0 ? (
+            {test.length > 0 ? (
               <>
                 <Grid item xs={12}>
-                  <Contact
-                    title={'Test User'}
-                    source={'Donor Hub (12/12/2021)'}
-                    newsletterType={'Physical'}
-                    tag={'Partner - Financial'}
-                  />
+                  {test.map((contact) => (
+                    <Contact
+                      title={contact.title}
+                      tag={contact.tag}
+                      name={contact.name || ''}
+                      key={contact.title}
+                      address={contact.address || { street: '', city: '' }}
+                      email={contact.email || ''}
+                      newsletterType={contact.newsletterType}
+                    />
+                  ))}
                 </Grid>
                 <Grid item xs={12}>
                   <Box className={classes.footer}>
                     <Typography>
-                      Showing <strong>{testData.length}</strong> of{' '}
-                      <strong>{testData.length}</strong>
+                      Showing <strong>{test.length}</strong> of{' '}
+                      <strong>{test.length}</strong>
                     </Typography>
                   </Box>
                 </Grid>
               </>
             ) : (
-              <Contact
-                title={'Test User'}
-                source={'Donor Hub (12/12/2021)'}
-                newsletterType={'Physical'}
-              />
+              <NoContacts />
             )}
           </Grid>
         </Container>
