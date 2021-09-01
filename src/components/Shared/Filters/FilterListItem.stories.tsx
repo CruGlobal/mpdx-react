854c3fab4476a3281ddff40751e1488c58ec278b
Robert Eldredge <robert.eldredge@cru.org>
2021-09-01 18:42:52 -0400

    update stories

---
+++ src/components/Shared/Filters/FilterListItem.stories.tsx
@@ -0,0 +1,184 @@
+import React, { ReactElement } from 'react';
+import {
+  CheckboxFilter,
+  DaterangeFilter,
+  DateRangeInput,
+  DateRangeOption,
+  MultiselectFilter,
+  NumericRangeFilter,
+  NumericRangeInput,
+  TextFilter,
+} from '../../../../graphql/types.generated';
+import { FilterListItem } from './FilterListItem';
+
+export default {
+  title: 'Shared/FilterListItem',
+};
+
+const checkboxFilter: CheckboxFilter = {
+  __typename: 'CheckboxFilter',
+  featured: false,
+  filterKey: 'checkboxFilter',
+  title: 'Checkbox Filter',
+};
+
+const daterangeFilter: DaterangeFilter = {
+  __typename: 'DaterangeFilter',
+  featured: false,
+  filterKey: 'dateRangeFilter',
+  options: undefined,
+  title: 'Date Range Filter',
+};
+
+const multiselectFilter: MultiselectFilter = {
+  __typename: 'MultiselectFilter',
+  defaultSelection: undefined,
+  featured: false,
+  filterKey: 'multiselectFilter',
+  options: [
+    { name: 'Option 1', value: '1' },
+    { name: 'Option 2', value: '2' },
+    { name: 'Option 3', value: '3' },
+  ],
+  title: 'Multiselect Filter',
+};
+
+const numericRangeFilter: NumericRangeFilter = {
+  __typename: 'NumericRangeFilter',
+  featured: false,
+  filterKey: 'numericRangeFilter',
+  max: 1.0,
+  maxLabel: 'max',
+  min: 0.0,
+  minLabel: 'min',
+  title: 'Numeric Range Filter',
+};
+
+const textFieldFilter: TextFilter = {
+  __typename: 'TextFilter',
+  featured: false,
+  filterKey: 'textFieldFilter',
+  options: undefined,
+  title: 'Text Field Filter',
+};
+
+export const CheckboxBlank = (): ReactElement => {
+  return (
+    <FilterListItem
+      filter={checkboxFilter}
+      value={undefined}
+      onUpdate={() => {}}
+    />
+  );
+};
+
+export const CheckboxFilled = (): ReactElement => {
+  return (
+    <FilterListItem filter={checkboxFilter} value={true} onUpdate={() => {}} />
+  );
+};
+
+export const DateRangeBlank = (): ReactElement => {
+  return (
+    <FilterListItem
+      filter={daterangeFilter}
+      value={undefined}
+      onUpdate={() => {}}
+    />
+  );
+};
+
+export const DateRangeFilled = (): ReactElement => {
+  const dateRange: DateRangeInput = { min: '2021-08-01', max: '2021-08-30' };
+
+  return (
+    <FilterListItem
+      filter={daterangeFilter}
+      value={dateRange}
+      onUpdate={() => {}}
+    />
+  );
+};
+
+export const DateRangeOptions = (): ReactElement => {
+  const dateRangeOptions: DateRangeOption[] = [
+    { name: '1', rangeStart: '2021-06-01', rangeEnd: '2021-06-31' },
+    { name: '1', rangeStart: '2021-07-01', rangeEnd: '2021-07-31' },
+    { name: '1', rangeStart: '2021-08-01', rangeEnd: '2021-08-31' },
+  ];
+
+  const daterangeFilterWithOptions: DaterangeFilter = {
+    ...daterangeFilter,
+    options: dateRangeOptions,
+  };
+
+  return (
+    <FilterListItem
+      filter={daterangeFilterWithOptions}
+      value={undefined}
+      onUpdate={() => {}}
+    />
+  );
+};
+
+export const MultiselectBlank = (): ReactElement => {
+  return (
+    <FilterListItem
+      filter={multiselectFilter}
+      value={undefined}
+      onUpdate={() => {}}
+    />
+  );
+};
+
+export const MultiselectFilled = (): ReactElement => {
+  return (
+    <FilterListItem
+      filter={multiselectFilter}
+      value={['1', '2']}
+      onUpdate={() => {}}
+    />
+  );
+};
+
+export const NumericRangeBlank = (): ReactElement => {
+  return (
+    <FilterListItem
+      filter={numericRangeFilter}
+      value={undefined}
+      onUpdate={() => {}}
+    />
+  );
+};
+
+export const NumericRangeFilled = (): ReactElement => {
+  const numericRange: NumericRangeInput = { min: 0.0, max: 1.0 };
+
+  return (
+    <FilterListItem
+      filter={numericRangeFilter}
+      value={numericRange}
+      onUpdate={() => {}}
+    />
+  );
+};
+
+export const TextFieldBlank = (): ReactElement => {
+  return (
+    <FilterListItem
+      filter={textFieldFilter}
+      value={undefined}
+      onUpdate={() => {}}
+    />
+  );
+};
+
+export const TextFieldFilled = (): ReactElement => {
+  return (
+    <FilterListItem
+      filter={textFieldFilter}
+      value={'Text'}
+      onUpdate={() => {}}
+    />
+  );
+};
