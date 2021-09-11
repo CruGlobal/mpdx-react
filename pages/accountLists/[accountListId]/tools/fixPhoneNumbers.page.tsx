f2f337f7679c1180a2dda2b854e16a881b9ed463
Chris Simbulan <simbulan.c@gmail.com>
2021-09-10 20:16:21 -0400

    fixed translations

--- pages/accountLists/[accountListId]/tools/fixPhoneNumbers.page.tsx
+++ pages/accountLists/[accountListId]/tools/fixPhoneNumbers.page.tsx
@@ -1,19 +1,19 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
 import { useTranslation } from 'react-i18next';
 import Head from 'next/head';
 import FixPhoneNumbers from '../../../../src/components/Tool/FixPhoneNumbers/FixPhoneNumbers';
 
-const fixPhoneNumbers = (): ReactElement => {
+const FixPhoneNumbersPage: React.FC = () => {
   const { t } = useTranslation();
 
   return (
     <>
       <Head>
-        <title>MPDX | {t('MPDX | Fix Phone Numbers')}</title>
+        <title>MPDX | {t('Fix Phone Numbers')}</title>
       </Head>
       <FixPhoneNumbers />
     </>
   );
 };
 
-export default fixPhoneNumbers;
+export default FixPhoneNumbersPage;
