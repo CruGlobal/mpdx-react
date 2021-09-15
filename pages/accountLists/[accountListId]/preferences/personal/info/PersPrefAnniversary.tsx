c85c37a86af8055c40ba14a68ee7d8aa73b45a24
John Plastow <j2trumpet@gmail.com>
2021-09-15 14:32:13 -0700

    Continuing TSX conversion
    PersPrefSocials.tsx and PersPrefInfo.tsx have errors

--- pages/accountLists/[accountListId]/preferences/personal/info/PersPrefAnniversary.tsx
+++ pages/accountLists/[accountListId]/preferences/personal/info/PersPrefAnniversary.tsx
@@ -1,22 +1,29 @@
-import { Typography } from "@material-ui/core";
+import React from 'react';
+import { Typography } from '@material-ui/core';
 
-export const PersPrefAnniversary = ({
+interface AnniversaryProps {
+  marital_status: string;
+  anniversary_day: number;
+  anniversary_month: string;
+}
+
+const PersPrefAnniversary: React.FC<AnniversaryProps> = ({
   marital_status,
   anniversary_day,
   anniversary_month,
 }) => {
   const anniversary = anniversary_month || anniversary_day ? true : false;
 
-  let output = "";
+  let output = '';
 
   if (marital_status) {
     output += marital_status;
   } else if (anniversary) {
-    output += "Anniversary";
+    output += 'Anniversary';
   }
 
   if (anniversary) {
-    output += ": ";
+    output += ': ';
     if (anniversary_month) {
       output += `${anniversary_month} `;
     }
@@ -25,9 +32,11 @@ export const PersPrefAnniversary = ({
     }
   }
 
-  if (output !== "") {
+  if (output !== '') {
     return <Typography gutterBottom>{output}</Typography>;
   }
 
   return null;
 };
+
+export default PersPrefAnniversary;
