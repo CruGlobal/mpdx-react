b6414418134009977e1fcafc984aefd52fb3873a
Chris Simbulan <simbulan.c@gmail.com>
2021-09-14 20:53:29 -0400

    fixed render issue

--- src/components/Tool/FixCommitmentInfo/FixCommitmentInfo.tsx
+++ src/components/Tool/FixCommitmentInfo/FixCommitmentInfo.tsx
@@ -6,8 +6,11 @@ import {
   Typography,
   Grid,
   Divider,
+  CircularProgress,
 } from '@material-ui/core';
 import { useTranslation } from 'react-i18next';
+import { useAccountListId } from '../../../../src/hooks/useAccountListId';
+import { useGetInvalidStatusesQuery } from './GetInvalidStatuses.generated';
 import Contact from './Contact';
 import NoContacts from './NoContacts';
 
@@ -42,83 +45,78 @@ const useStyles = makeStyles((theme: Theme) => ({
     width: '100%',
     display: 'flex',
     justifyContent: 'center',
-    marginTop: theme.spacing(2),
   },
 }));
 
 const FixCommitmentInfo: React.FC = () => {
   const classes = useStyles();
   const { t } = useTranslation();
-
-  const testData = [
-    {
-      name: 'Test test',
-      tagTitle: 'Partner - Financial',
-      tagValue: 'partner-financial',
-      frequencyTitle: 'Monthly',
-      frequencyValue: 'monthly',
-      amount: 50,
-      amountCurrency: 'CAD',
-    },
-  ];
-
-  //TODO: Make navbar selectId = "fixCommitmentInfo" when other branch gets merged
+  const accountListId = useAccountListId();
+  const { data, loading } = useGetInvalidStatusesQuery({
+    variables: { accountListId: accountListId || '' },
+  });
 
   return (
     <>
       <Box className={classes.outer} data-testid="Home">
-        <Grid container className={classes.container}>
-          <Grid item xs={12}>
-            <Typography variant="h4">{t('Fix Commitment Info')}</Typography>
-            <Divider className={classes.divider} />
-            <Box className={classes.descriptionBox}>
-              <Typography>
-                <strong>
-                  {t('You have {{amount}} partner statuses to confirm.', {
-                    amount: testData.length,
-                  })}
-                </strong>
-              </Typography>
-              <Typography>
-                {t(
-                  'MPDX has assigned partnership statuses and giving frequencies ' +
-                    'based on your partner’s giving history. MPDX has made its best ' +
-                    'attempt at matching the appropriate statuses for you. However, ' +
-                    'you will need to confirm them to be sure MPDX’s matching was ' +
-                    'accurate.',
-                )}
-              </Typography>
-            </Box>
+        {!loading && data ? (
+          <Grid container className={classes.container}>
+            <Grid item xs={12}>
+              <Typography variant="h4">{t('Fix Commitment Info')}</Typography>
+              <Divider className={classes.divider} />
+              <Box className={classes.descriptionBox}>
+                <Typography>
+                  <strong>
+                    {t('You have {{amount}} partner statuses to confirm.', {
+                      amount: data?.contacts.nodes.length,
+                    })}
+                  </strong>
+                </Typography>
+                <Typography>
+                  {t(
+                    'MPDX has assigned partnership statuses and giving frequencies ' +
+                      'based on your partner’s giving history. MPDX has made its best ' +
+                      'attempt at matching the appropriate statuses for you. However, ' +
+                      'you will need to confirm them to be sure MPDX’s matching was ' +
+                      'accurate.',
+                  )}
+                </Typography>
+              </Box>
+            </Grid>
+            {data.contacts?.nodes.length > 0 ? (
+              <>
+                <Grid item xs={12}>
+                  <Box>
+                    {data.contacts.nodes.map((contact) => (
+                      <Contact
+                        name={contact.name}
+                        key={contact.name}
+                        tagTitle={contact.status || ''}
+                        tagValue={contact.status || ''}
+                        amount={contact.pledgeAmount || 0}
+                        amountCurrency={contact.pledgeCurrency || ''}
+                        frequencyTitle={contact.pledgeFrequency || ''}
+                        frequencyValue={contact.pledgeFrequency || ''}
+                      />
+                    ))}
+                  </Box>
+                </Grid>
+                <Grid item xs={12}>
+                  <Box className={classes.footer}>
+                    <Typography>
+                      Showing <strong>{data.contacts.nodes.length}</strong> of{' '}
+                      <strong>{data.contacts.nodes.length}</strong>
+                    </Typography>
+                  </Box>
+                </Grid>
+              </>
+            ) : (
+              <NoContacts />
+            )}
           </Grid>
-          {testData.length > 0 ? (
-            <>
-              <Grid item xs={12}>
-                {testData.map((contact) => (
-                  <Contact
-                    name={contact.name}
-                    key={contact.name}
-                    tagTitle={contact.tagTitle}
-                    tagValue={contact.tagValue}
-                    amount={contact.amount}
-                    amountCurrency={contact.amountCurrency}
-                    frequencyTitle={contact.frequencyTitle}
-                    frequencyValue={contact.frequencyValue}
-                  />
-                ))}
-              </Grid>
-              <Grid item xs={12}>
-                <Box className={classes.footer}>
-                  <Typography>
-                    Showing <strong>{testData.length}</strong> of{' '}
-                    <strong>{testData.length}</strong>
-                  </Typography>
-                </Box>
-              </Grid>
-            </>
-          ) : (
-            <NoContacts />
-          )}
-        </Grid>
+        ) : (
+          <CircularProgress />
+        )}
       </Box>
     </>
   );
