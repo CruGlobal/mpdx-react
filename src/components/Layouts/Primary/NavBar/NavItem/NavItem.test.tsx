031f3e50ecf8057ba7385a498ee8ad9e2aab0bb2
hotcakedev628 <hotcake.dev.628@hotmail.com>
2021-09-17 14:51:48 -0400

    update test case

--- src/components/Layouts/Primary/NavBar/NavItem/NavItem.test.tsx
+++ src/components/Layouts/Primary/NavBar/NavItem/NavItem.test.tsx
@@ -23,46 +23,4 @@ describe('NavItem', () => {
 
     expect(getByText(title)).toBeInTheDocument();
   });
-
-  it('closed nav item with children', async () => {
-    const { getByRole, getByText } = render(
-      <ThemeProvider theme={theme}>
-        <NavItem
-          depth={0}
-          href="/test/[accountListId]"
-          as={`/test/${accountListId}`}
-          open={false}
-          title={title}
-        >
-          <span />
-        </NavItem>
-      </ThemeProvider>,
-    );
-
-    expect(getByText(title)).toBeInTheDocument();
-    expect(
-      getByRole('img', { hidden: true, name: 'Collapse' }),
-    ).toBeInTheDocument();
-  });
-
-  it('opened nav item with children', async () => {
-    const { getByRole, getByText } = render(
-      <ThemeProvider theme={theme}>
-        <NavItem
-          depth={0}
-          href="/test/[accountListId]"
-          as={`/test/${accountListId}`}
-          open={true}
-          title={title}
-        >
-          <span />
-        </NavItem>
-      </ThemeProvider>,
-    );
-
-    expect(getByText(title)).toBeInTheDocument();
-    expect(
-      getByRole('img', { hidden: true, name: 'Expand' }),
-    ).toBeInTheDocument();
-  });
 });
