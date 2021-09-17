008d796c13114fc58cef75d6aaf7d241132ee584
Gyasi Story <gyasi.story@cru.org>
2021-09-17 09:34:03 -0400

    MPDX-7132 Add Coaching to Menu

---
+++ pages/accountLists/[accountListId]/coaches.page.tsx
@@ -0,0 +1,19 @@
+import React, { ReactElement } from 'react';
+import Head from 'next/head';
+import { useTranslation } from 'react-i18next';
+import { CoachingList } from 'src/components/Coaching/CoachingList';
+
+const CoachesPage = (): ReactElement => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <Head>
+        <title>MPDX | {t('Coaches')}</title>
+      </Head>
+      <CoachingList />
+    </>
+  );
+};
+
+export default CoachesPage;
