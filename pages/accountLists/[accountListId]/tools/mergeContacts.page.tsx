a8690c237a7915c9805b19ed19f5770a54824848
Chris Simbulan <simbulan.c@gmail.com>
2021-09-20 15:08:14 -0400

    fixed import and syntax

--- pages/accountLists/[accountListId]/tools/mergeContacts.page.tsx
+++ pages/accountLists/[accountListId]/tools/mergeContacts.page.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import Head from 'next/head';
 import MergeContacts from '../../../../src/components/Tool/MergeContacts/MergeContacts';
-import { useAccountListId } from 'src/hooks/useAccountListId';
-import Loading from 'src/components/Loading';
+import { useAccountListId } from '../../../../src/hooks/useAccountListId';
+import Loading from '../../../../src/components/Loading';
 
 const MergeContactsPage: React.FC = () => {
   const { t } = useTranslation();
