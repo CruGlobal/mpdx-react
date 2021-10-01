45e3febdeb74d17d02c232929c2a67d3ee250caf
Chris Simbulan <simbulan.c@gmail.com>
2021-10-01 08:12:49 -0400

    fixed test

--- src/components/Tool/FixCommitmentInfo/Contact.test.tsx
+++ src/components/Tool/FixCommitmentInfo/Contact.test.tsx
@@ -4,9 +4,10 @@ import TestWrapper from '../../../../__tests__/util/TestWrapper';
 import Contact from './Contact';
 
 const testData = {
+  id: 'test123',
   name: 'Test test',
-  tagTitle: 'Partner - Financial',
-  tagValue: 'partner-financial',
+  statusTitle: 'Partner - Financial',
+  statusValue: 'partner-financial',
   frequencyTitle: 'Monthly',
   frequencyValue: 'monthly',
   amount: 50,
@@ -15,26 +16,31 @@ const testData = {
 
 describe('FixCommitmentContact', () => {
   it('default', () => {
+    const hideFunction = jest.fn();
+    const updateFunction = jest.fn();
     const { getByText } = render(
       <TestWrapper>
         <Contact
+          id={testData.id}
           name={testData.name}
           key={testData.name}
-          tagTitle={testData.tagTitle}
-          tagValue={testData.tagValue}
+          statusTitle={testData.statusTitle}
+          statusValue={testData.statusValue}
           amount={testData.amount}
           amountCurrency={testData.amountCurrency}
           frequencyTitle={testData.frequencyTitle}
           frequencyValue={testData.frequencyValue}
+          hideFunction={hideFunction}
+          updateFunction={updateFunction}
         />
       </TestWrapper>,
     );
     expect(getByText(testData.name)).toBeInTheDocument();
-    expect(getByText(testData.tagTitle)).toBeInTheDocument();
+    expect(getByText(testData.statusTitle)).toBeInTheDocument();
     expect(getByText(testData.frequencyTitle)).toBeInTheDocument();
     expect(
       getByText(
-        `Current: ${testData.tagTitle} ${testData.amount.toFixed(2)} ${
+        `Current: ${testData.statusTitle} ${testData.amount.toFixed(2)} ${
           testData.amountCurrency
         } ${testData.frequencyTitle}`,
       ),
