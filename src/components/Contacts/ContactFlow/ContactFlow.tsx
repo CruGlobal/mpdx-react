7bb012c6e38dfcd535b0799955c2b5c560a9f3f0
Chris Simbulan <simbulan.c@gmail.com>
2021-10-06 09:16:06 -0400

    Create ContactFlow.tsx

---
+++ src/components/Contacts/ContactFlow/ContactFlow.tsx
@@ -0,0 +1,71 @@
+import { Box, CircularProgress } from '@material-ui/core';
+import React from 'react';
+import { ContactFlowColumn } from './ContactFlowColumn';
+import { useContactsFlowQuery } from './ContactsFlow.generated';
+import theme from 'src/theme';
+
+interface Props {
+  x?: string;
+  accountListId: string;
+}
+
+export const ContactFlow: React.FC<Props> = ({ accountListId }: Props) => {
+  const { data, loading } = useContactsFlowQuery({
+    variables: {
+      accountListId: accountListId ?? '',
+    },
+    skip: !accountListId,
+  });
+
+  return (
+    <>
+      {loading ? (
+        <CircularProgress />
+      ) : (
+        <Box
+          display="grid"
+          width="100%"
+          height="100%"
+          gridTemplateColumns="repeat(5, 1fr)"
+          gridGap={theme.spacing(1)}
+        >
+          <Box width="100%" p={2}>
+            <ContactFlowColumn
+              title="Contact for Appointment"
+              color={'red'}
+              data={data?.contactForAppointment.nodes || []}
+            />
+          </Box>
+          <Box width="100%" p={2}>
+            <ContactFlowColumn
+              title="Potential Partners"
+              color={theme.palette.progressBarYellow.main}
+              data={data?.potentialPartners.nodes || []}
+            />
+          </Box>
+          <Box width="100%" p={2}>
+            <ContactFlowColumn
+              title="Monthly Partners"
+              color={theme.palette.mpdxGreen.main}
+              data={data?.monthlyPartners.nodes || []}
+            />
+          </Box>
+          <Box width="100%" p={2}>
+            <ContactFlowColumn
+              title="Special Partners"
+              color={theme.palette.mpdxBlue.main}
+              data={data?.specialPartners.nodes || []}
+            />
+          </Box>
+          <Box width="100%" p={2}>
+            <ContactFlowColumn
+              title="Prayer Partners"
+              color={theme.palette.cruGrayDark.main}
+              data={data?.prayerPartners.nodes || []}
+            />
+          </Box>
+        </Box>
+      )}
+    </>
+  );
+};
