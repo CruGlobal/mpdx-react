6444c187394e46e3f3180e4dad29f2e0668891de
hotcakedev628 <hotcake.dev.628@hotmail.com>
2021-10-07 15:06:56 -0400

    fix giftstatus adding theme provider

--- src/components/Contacts/GiftStatus/GiftStatus.test.tsx
+++ src/components/Contacts/GiftStatus/GiftStatus.test.tsx
@@ -1,23 +1,33 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { DateTime } from 'luxon';
+import { ThemeProvider } from '@material-ui/core';
+import theme from '../../../theme';
 import { GiftStatus } from './GiftStatus';
 
 describe('GiftStatus', () => {
   it('is Late', () => {
     const { getByTitle } = render(
-      <GiftStatus lateAt={DateTime.now().minus({ day: 1 }).toISO()} />,
+      <ThemeProvider theme={theme}>
+        <GiftStatus lateAt={DateTime.now().minus({ day: 1 }).toISO()} />
+      </ThemeProvider>,
     );
     expect(getByTitle('Late')).toBeVisible();
   });
   it('is On Time', () => {
     const { getByTitle } = render(
-      <GiftStatus lateAt={DateTime.now().toISO()} />,
+      <ThemeProvider theme={theme}>
+        <GiftStatus lateAt={DateTime.now().toISO()} />
+      </ThemeProvider>,
     );
     expect(getByTitle('On Time')).toBeVisible();
   });
   it('is hidden', () => {
-    const { queryByTitle } = render(<GiftStatus lateAt={undefined} />);
+    const { queryByTitle } = render(
+      <ThemeProvider theme={theme}>
+        <GiftStatus lateAt={undefined} />
+      </ThemeProvider>,
+    );
     expect(queryByTitle('On Time')).toBeNull();
     expect(queryByTitle('Late')).toBeNull();
   });
