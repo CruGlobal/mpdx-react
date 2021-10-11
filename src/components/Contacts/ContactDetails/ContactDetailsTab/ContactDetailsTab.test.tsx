4161d989b042f0d1a809fcf2b0ee5c599081cd40
hotcakedev628 <hotcake.dev.628@hotmail.com>
2021-10-11 15:36:12 -0400

    return test case for contact details tab

--- src/components/Contacts/ContactDetails/ContactDetailsTab/ContactDetailsTab.test.tsx
+++ src/components/Contacts/ContactDetails/ContactDetailsTab/ContactDetailsTab.test.tsx
@@ -3,11 +3,16 @@ import { render, waitFor } from '@testing-library/react';
 import { ThemeProvider } from '@material-ui/core';
 import { SnackbarProvider } from 'notistack';
 import userEvent from '@testing-library/user-event';
+import { InMemoryCache } from '@apollo/client';
 import { MuiPickersUtilsProvider } from '@material-ui/pickers';
 import LuxonUtils from '@date-io/luxon';
 import TestRouter from '../../../../../__tests__/util/TestRouter';
 import theme from '../../../../theme';
 import { GqlMockedProvider } from '../../../../../__tests__/util/graphqlMocking';
+import {
+  ContactsDocument,
+  ContactsQuery,
+} from '../../../../../pages/accountLists/[accountListId]/contacts/Contacts.generated';
 import { ContactDetailsTab } from './ContactDetailsTab';
 import { ContactDetailsTabQuery } from './ContactDetailsTab.generated';
 
@@ -549,5 +554,136 @@ describe('ContactDetailTab', () => {
     await waitFor(() =>
       expect(queryByText('Edit Contact Other Details')).not.toBeInTheDocument(),
     );
+  });
+
+  it('handles deleting contact', async () => {
+    const cache = new InMemoryCache();
+    jest.spyOn(cache, 'writeQuery');
+    const mocks = {
+      ContactDetailsTab: {
+        contact: {
+          id: contactId,
+          name: 'Person, Test',
+          addresses: {
+            nodes: [
+              {
+                street: '123 Sesame Street',
+                city: 'New York',
+                state: 'NY',
+                postalCode: '10001',
+                country: 'USA',
+                primaryMailingAddress: true,
+              },
+            ],
+          },
+          tagList: ['tag1', 'tag2', 'tag3'],
+          people: {
+            nodes: [
+              {
+                id: contactId,
+                firstName: 'Test',
+                lastName: 'Person',
+                primaryPhoneNumber: { number: '555-555-5555' },
+                primaryEmailAddress: {
+                  email: 'testperson@fake.com',
+                },
+              },
+            ],
+          },
+          website: 'testperson.com',
+        },
+      },
+      DeleteContact: {
+        deleteContact: {
+          id: contactId,
+        },
+      },
+    };
+
+    const data: ContactsQuery = {
+      contacts: {
+        nodes: [
+          {
+            id: contactId,
+            name: 'Person, Test',
+            starred: false,
+            people: {
+              nodes: [
+                {
+                  anniversaryDay: null,
+                  anniversaryMonth: null,
+                  birthdayDay: null,
+                  birthdayMonth: null,
+                },
+              ],
+            },
+            uncompletedTasksCount: 0,
+          },
+        ],
+        pageInfo: { endCursor: 'Mg', hasNextPage: false },
+        totalCount: 1,
+      },
+    };
+    cache.writeQuery({
+      query: ContactsDocument,
+      variables: {
+        accountListId,
+        searchTerm: undefined,
+      },
+      data,
+    });
+
+    const { queryAllByText, queryByText } = render(
+      <SnackbarProvider>
+        <TestRouter router={router}>
+          <ThemeProvider theme={theme}>
+            <GqlMockedProvider<ContactDetailsTabQuery>
+              mocks={mocks}
+              cache={cache}
+            >
+              <ContactDetailsTab
+                accountListId={accountListId}
+                contactId={contactId}
+                onClose={onClose}
+              />
+            </GqlMockedProvider>
+          </ThemeProvider>
+        </TestRouter>
+      </SnackbarProvider>,
+    );
+    await waitFor(() => expect(queryByText('Loading')).not.toBeInTheDocument());
+    expect(queryAllByText('Person, Test')[0]).toBeInTheDocument();
+    userEvent.click(queryAllByText('delete contact')[0]);
+    userEvent.click(queryAllByText('delete contact')[1]);
+    await waitFor(() =>
+      expect(cache.writeQuery).toHaveBeenCalledWith({
+        query: ContactsDocument,
+        variables: {
+          accountListId,
+          searchTerm: undefined,
+          after: undefined,
+        },
+        data: {
+          contacts: {
+            nodes: [],
+            pageInfo: { endCursor: 'Mg', hasNextPage: false },
+            totalCount: 0,
+          },
+        },
+      }),
+    );
+    expect(onClose).toHaveBeenCalled();
+    await waitFor(() =>
+      expect(router.push).toHaveBeenCalledWith({
+        pathname: '/accountLists/[accountListId]/contacts',
+        query: {
+          accountListId,
+          searchTerm: undefined,
+        },
+      }),
+    );
+    expect(mockEnqueue).toHaveBeenCalledWith('Contact successfully deleted', {
+      variant: 'success',
+    });
   });
 });
