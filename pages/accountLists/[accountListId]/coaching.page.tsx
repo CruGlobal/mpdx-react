2c8c1d56dfd80d15bd3b0a8eddc4f4445b11ce8d
Gyasi Story <gyasi.story@cru.org>
2021-10-11 11:56:33 -0400

    MPDX-7156---Lint Clean

--- pages/accountLists/[accountListId]/coaching.page.tsx
+++ pages/accountLists/[accountListId]/coaching.page.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
 import { useTranslation } from 'react-i18next';
-import { useRouter } from 'next/router';
 import { CoachingList } from 'src/components/Coaching/CoachingList';
 import { useAccountListId } from 'src/hooks/useAccountListId';
 import Loading from 'src/components/Loading';
@@ -9,7 +8,6 @@ import Loading from 'src/components/Loading';
 const CoachingPage: React.FC = () => {
   const { t } = useTranslation();
   const accountListId = useAccountListId();
-  const { query, push, replace, isReady, pathname } = useRouter();
 
   return (
     <>
