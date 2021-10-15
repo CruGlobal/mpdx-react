2adf30710c1b920281cd7ef83f203fda14a8b0fa
hotcakedev628 <hotcake.dev.628@hotmail.com>
2021-10-15 13:26:08 -0400

    add test case for actions dropdown in contact header

---
+++ src/components/Contacts/ContactsHeader/ActionsDropdown/ActionsDropdown.test.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { ThemeProvider } from '@material-ui/core';
+import theme from '../../../../theme';
+import { ActionsDropDown } from './ActionsDropdown';
+
+const onChange = jest.fn();
+
+describe('ActionsDropDown', () => {
+  it('default', () => {
+    const { getByRole } = render(
+      <ThemeProvider theme={theme}>
+        <ActionsDropDown disabled={true} onChange={onChange} value={''} />
+      </ThemeProvider>,
+    );
+
+    const actionsDropdown = getByRole('textbox');
+    expect(actionsDropdown).toBeInTheDocument();
+    expect(actionsDropdown).toBeDisabled();
+  });
+});
