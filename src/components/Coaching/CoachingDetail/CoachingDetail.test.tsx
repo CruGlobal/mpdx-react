875c9eb285b0338de02f7a101a5358376d2481d2
Gyasi Story <gyasi.story@cru.org>
2021-10-21 16:51:28 -0400

    MPDX-7156 Add test for Title

--- src/components/Coaching/CoachingDetail/CoachingDetail.test.tsx
+++ src/components/Coaching/CoachingDetail/CoachingDetail.test.tsx
@@ -1,6 +1,12 @@
 import { renderHook } from '@testing-library/react-hooks';
-import { useLoadCoachingDetailQuery } from './LoadCoachingDetail.generated';
+import React from 'react';
+import {
+  LoadCoachingDetailQuery,
+  useLoadCoachingDetailQuery,
+} from './LoadCoachingDetail.generated';
+import { CoachingDetail } from './CoachingDetail';
 import { GqlMockedProvider } from '__tests__/util/graphqlMocking';
+import { render } from '__tests__/util/testingLibraryReactMock';
 
 const coachingId = 'coaching-id';
 describe('LoadCoachingDetail', () => {
@@ -16,5 +22,22 @@ describe('LoadCoachingDetail', () => {
     expect(result.current.data?.coachingAccountList.name).toMatchInlineSnapshot(
       `"Jet fighter Vacuum Maze"`,
     );
+  });
+  it('view', async () => {
+    const { findByText } = render(
+      <GqlMockedProvider<LoadCoachingDetailQuery>
+        mocks={{
+          LoadCoachingDetail: {
+            coachingAccountList: {
+              id: 'coaching-id',
+              name: 'John Doe',
+            },
+          },
+        }}
+      >
+        <CoachingDetail coachingId="coaching-id" />
+      </GqlMockedProvider>,
+    );
+    expect(await findByText('John Doe')).toBeVisible();
   });
 });
