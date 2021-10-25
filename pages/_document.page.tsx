7a012f33142837a13d126e3e3cf54bcc087d74bf
Scotty Waggoner <ozzieorca@gmail.com>
2021-10-25 20:26:50 +0000

    Fix Next imports and types
    - Upgrade Next packages further

--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -4,10 +4,11 @@ import Document, {
   Head,
   Main,
   NextScript,
+  DocumentContext,
   DocumentInitialProps,
 } from 'next/document';
 import { ServerStyleSheets } from '@material-ui/core/styles';
-import { RenderPageResult } from 'next/dist/next-server/lib/utils';
+import { RenderPageResult } from 'next/dist/shared/lib/utils';
 import theme from '../src/theme';
 
 class MyDocument extends Document {
@@ -24,51 +25,52 @@ class MyDocument extends Document {
       </Html>
     );
   }
+  static async getInitialProps(
+    ctx: DocumentContext,
+  ): Promise<DocumentInitialProps> {
+    // Resolution order
+    //
+    // On the server:
+    // 1. app.getInitialProps
+    // 2. page.getInitialProps
+    // 3. document.getInitialProps
+    // 4. app.render
+    // 5. page.render
+    // 6. document.render
+    //
+    // On the server with error:
+    // 1. document.getInitialProps
+    // 2. app.render
+    // 3. page.render
+    // 4. document.render
+    //
+    // On the client
+    // 1. app.getInitialProps
+    // 2. page.getInitialProps
+    // 3. app.render
+    // 4. page.render
+
+    // Render app and page and get the context of the page with collected side effects.
+    const sheets = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    ctx.renderPage = (): RenderPageResult | Promise<RenderPageResult> =>
+      originalRenderPage({
+        enhanceApp: (App) => (props): ReactElement =>
+          sheets.collect(<App {...props} />),
+      });
+
+    const initialProps = await Document.getInitialProps(ctx);
+
+    return {
+      ...initialProps,
+      // Styles fragment is rendered after the app and page rendering finish.
+      styles: [
+        ...React.Children.toArray(initialProps.styles),
+        sheets.getStyleElement(),
+      ],
+    };
+  }
 }
-
-MyDocument.getInitialProps = async (ctx): Promise<DocumentInitialProps> => {
-  // Resolution order
-  //
-  // On the server:
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. document.getInitialProps
-  // 4. app.render
-  // 5. page.render
-  // 6. document.render
-  //
-  // On the server with error:
-  // 1. document.getInitialProps
-  // 2. app.render
-  // 3. page.render
-  // 4. document.render
-  //
-  // On the client
-  // 1. app.getInitialProps
-  // 2. page.getInitialProps
-  // 3. app.render
-  // 4. page.render
-
-  // Render app and page and get the context of the page with collected side effects.
-  const sheets = new ServerStyleSheets();
-  const originalRenderPage = ctx.renderPage;
-
-  ctx.renderPage = (): RenderPageResult | Promise<RenderPageResult> =>
-    originalRenderPage({
-      enhanceApp: (App) => (props): ReactElement =>
-        sheets.collect(<App {...props} />),
-    });
-
-  const initialProps = await Document.getInitialProps(ctx);
-
-  return {
-    ...initialProps,
-    // Styles fragment is rendered after the app and page rendering finish.
-    styles: [
-      ...React.Children.toArray(initialProps.styles),
-      sheets.getStyleElement(),
-    ],
-  };
-};
 
 export default MyDocument;
