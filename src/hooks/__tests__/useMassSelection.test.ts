72e331a1a873fd28abffc1cc6554711f98e33832
TheNoodleMoose <cjh605@gmail.com>
2021-11-02 14:43:10 -0400

    Add tests for useMassSelection hook

---
+++ src/hooks/__tests__/useMassSelection.test.ts
@@ -0,0 +1,161 @@
+import { renderHook } from '@testing-library/react-hooks';
+import { ListHeaderCheckBoxState } from '../../components/Shared/Header/ListHeader';
+import { useMassSelection } from '../useMassSelection';
+
+const id = '123';
+describe('useMassSelection', () => {
+  it('should return selectionType', () => {
+    const { result } = renderHook(() => useMassSelection(10), {});
+
+    expect(result.current.selectionType).toEqual(
+      ListHeaderCheckBoxState.unchecked,
+    );
+  });
+
+  //#region ToggleSelectionById Tests
+  it('toggleSelectionById | should toggle a row as selected', () => {
+    const { result } = renderHook(() => useMassSelection(10), {});
+
+    expect(result.current.selectionType).toEqual(
+      ListHeaderCheckBoxState.unchecked,
+    );
+
+    result.current.toggleSelectionById(id);
+
+    expect(result.current.isRowChecked(id)).toBe(true);
+    expect(result.current.selectionType).toEqual(
+      ListHeaderCheckBoxState.partial,
+    );
+  });
+
+  it('toggleSelectionById | should unselect a selected row', () => {
+    const { result, rerender } = renderHook(() => useMassSelection(10), {});
+
+    expect(result.current.selectionType).toEqual(
+      ListHeaderCheckBoxState.unchecked,
+    );
+    // Toggle to selected
+    result.current.toggleSelectionById(id);
+
+    expect(result.current.isRowChecked(id)).toBe(true);
+    expect(result.current.selectionType).toEqual(
+      ListHeaderCheckBoxState.partial,
+    );
+
+    // Toggle to unselected
+    result.current.toggleSelectionById(id);
+
+    expect(result.current.isRowChecked(id)).toBe(false);
+
+    rerender();
+
+    expect(result.current.selectionType).toEqual(
+      ListHeaderCheckBoxState.unchecked,
+    );
+  });
+
+  it('toggleSelectionById | should switch to All if all ids selected', () => {
+    const { result, rerender } = renderHook(() => useMassSelection(3), {});
+
+    expect(result.current.selectionType).toEqual(
+      ListHeaderCheckBoxState.unchecked,
+    );
+    // Toggle to selected
+    result.current.toggleSelectionById(id);
+    result.current.toggleSelectionById('321');
+    result.current.toggleSelectionById('231');
+
+    expect(result.current.isRowChecked(id)).toBe(true);
+    expect(result.current.isRowChecked('321')).toBe(true);
+    expect(result.current.isRowChecked('231')).toBe(true);
+
+    rerender();
+
+    expect(result.current.selectionType).toEqual(
+      ListHeaderCheckBoxState.checked,
+    );
+  });
+
+  it('toggleSelectionById | should switch to None if all ids are unselected', () => {
+    const { result, rerender } = renderHook(() => useMassSelection(3), {});
+
+    expect(result.current.selectionType).toEqual(
+      ListHeaderCheckBoxState.unchecked,
+    );
+
+    result.current.toggleSelectAll();
+
+    expect(result.current.selectionType).toEqual(
+      ListHeaderCheckBoxState.checked,
+    );
+
+    // Toggle to unselected
+    result.current.toggleSelectionById(id);
+    result.current.toggleSelectionById('321');
+    result.current.toggleSelectionById('231');
+
+    expect(result.current.isRowChecked(id)).toBe(false);
+    expect(result.current.isRowChecked('321')).toBe(false);
+    expect(result.current.isRowChecked('231')).toBe(false);
+
+    rerender();
+
+    expect(result.current.selectionType).toEqual(
+      ListHeaderCheckBoxState.unchecked,
+    );
+  });
+  //#endregion
+
+  it('toggleSelectAll | Partial', () => {
+    const { result } = renderHook(() => useMassSelection(10), {});
+
+    expect(result.current.selectionType).toEqual(
+      ListHeaderCheckBoxState.unchecked,
+    );
+    // Toggle to selected
+    result.current.toggleSelectionById(id);
+
+    expect(result.current.selectionType).toEqual(
+      ListHeaderCheckBoxState.partial,
+    );
+
+    result.current.toggleSelectAll();
+
+    expect(result.current.selectionType).toEqual(
+      ListHeaderCheckBoxState.checked,
+    );
+  });
+  it('toggleSelectAll | None', () => {
+    const { result } = renderHook(() => useMassSelection(10), {});
+
+    expect(result.current.selectionType).toEqual(
+      ListHeaderCheckBoxState.unchecked,
+    );
+
+    result.current.toggleSelectAll();
+
+    expect(result.current.selectionType).toEqual(
+      ListHeaderCheckBoxState.checked,
+    );
+  });
+
+  it('toggleSelectAll | All', () => {
+    const { result } = renderHook(() => useMassSelection(10), {});
+
+    expect(result.current.selectionType).toEqual(
+      ListHeaderCheckBoxState.unchecked,
+    );
+    // Toggle to all
+    result.current.toggleSelectAll();
+
+    expect(result.current.selectionType).toEqual(
+      ListHeaderCheckBoxState.checked,
+    );
+    // Toggle back to none
+    result.current.toggleSelectAll();
+
+    expect(result.current.selectionType).toEqual(
+      ListHeaderCheckBoxState.unchecked,
+    );
+  });
+});
