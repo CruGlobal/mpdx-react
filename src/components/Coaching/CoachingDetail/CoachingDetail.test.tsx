c900713bd142df9e0d430bc109987ba1b9894d39
Gyasi Story <gyasi.story@cru.org>
2021-11-03 15:38:24 -0400

    MPDX-7182 update Coaching Detail with AppealProgress

--- src/components/Coaching/CoachingDetail/CoachingDetail.test.tsx
+++ src/components/Coaching/CoachingDetail/CoachingDetail.test.tsx
@@ -14,6 +14,7 @@ describe('LoadCoachingDetail', () => {
             coachingAccountList: {
               id: coachingId,
               name: 'John Doe',
+              currency: 'USD',
             },
           },
         }}
