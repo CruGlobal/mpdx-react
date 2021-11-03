46839afa6d3e79f5987db383679bd2d3936c6e34
Chris Simbulan <simbulan.c@gmail.com>
2021-11-03 15:57:19 -0400

    added header for contact flow settings page

---
+++ src/components/Contacts/ContactFlow/ContactFlowSetup/Header/ContactFlowSetupHeader.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { styled, Box, Button } from '@material-ui/core';
+import { ChevronLeft } from '@material-ui/icons';
+import { useTranslation } from 'react-i18next';
+
+const HeaderWrap = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  padding: theme.spacing(2),
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  borderBottom: `1px solid ${theme.palette.cruGrayMedium.main}`,
+}));
+
+const BackButton = styled(Button)(() => ({
+  color: '#2196F3',
+  fontWeight: 600,
+}));
+
+const AddColumnButton = styled(Button)(({ theme }) => ({
+  backgroundColor: theme.palette.mpdxBlue.main,
+  color: theme.palette.common.white,
+}));
+
+interface Props {
+  addColumn: () => void;
+}
+
+export const ContactFlowSetupHeader: React.FC<Props> = ({
+  addColumn,
+}: Props) => {
+  const { t } = useTranslation();
+  return (
+    <HeaderWrap>
+      <BackButton>
+        <ChevronLeft />
+        {t('Contacts')}
+      </BackButton>
+      <AddColumnButton onClick={addColumn}>{t('Add Column')}</AddColumnButton>
+    </HeaderWrap>
+  );
+};
