cd538497df1e55a1bd426b805ca0a0c6517d994f
Chris Simbulan <simbulan.c@gmail.com>
2021-11-09 16:01:15 -0500

    fixed test

--- src/components/Contacts/ContactFlow/ContactFlowRow/ContactFlowRow.test.tsx
+++ src/components/Contacts/ContactFlow/ContactFlowRow/ContactFlowRow.test.tsx
@@ -3,50 +3,58 @@ import { render } from '@testing-library/react';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import userEvent from '@testing-library/user-event';
+import { ThemeProvider } from '@material-ui/core';
 import TestWrapper from '../../../../../__tests__/util/TestWrapper';
 import { StatusEnum } from '../../../../../graphql/types.generated';
+import theme from '../../../../../src/theme';
 import { ContactFlowRow } from './ContactFlowRow';
 
 const accountListId = 'abc';
 const id = '123';
 const name = 'Test Name';
-const status = StatusEnum.PartnerFinancial;
+const status = {
+  id: StatusEnum.PartnerFinancial,
+  value: 'Partner - Financial',
+};
 const onContactSelected = jest.fn();
 
 describe('ContactFlowRow', () => {
   it('should display contact name and status', () => {
     const { getByText, getByTitle } = render(
       <DndProvider backend={HTML5Backend}>
-        <TestWrapper>
-          <ContactFlowRow
-            accountListId={accountListId}
-            id={id}
-            name={name}
-            status={status}
-            starred
-            onContactSelected={onContactSelected}
-          />
-        </TestWrapper>
+        <ThemeProvider theme={theme}>
+          <TestWrapper>
+            <ContactFlowRow
+              accountListId={accountListId}
+              id={id}
+              name={name}
+              status={status}
+              starred
+              onContactSelected={onContactSelected}
+            />
+          </TestWrapper>
+        </ThemeProvider>
       </DndProvider>,
     );
     expect(getByText('Test Name')).toBeInTheDocument();
-    expect(getByText('PARTNER_FINANCIAL')).toBeInTheDocument();
     expect(getByTitle('Filled Star Icon')).toBeInTheDocument();
   });
 
   it('should call contact selected function', () => {
     const { getByText } = render(
       <DndProvider backend={HTML5Backend}>
-        <TestWrapper>
-          <ContactFlowRow
-            accountListId={accountListId}
-            id={id}
-            name={name}
-            status={status}
-            starred
-            onContactSelected={onContactSelected}
-          />
-        </TestWrapper>
+        <ThemeProvider theme={theme}>
+          <TestWrapper>
+            <ContactFlowRow
+              accountListId={accountListId}
+              id={id}
+              name={name}
+              status={status}
+              starred
+              onContactSelected={onContactSelected}
+            />
+          </TestWrapper>
+        </ThemeProvider>
       </DndProvider>,
     );
     userEvent.click(getByText('Test Name'));
