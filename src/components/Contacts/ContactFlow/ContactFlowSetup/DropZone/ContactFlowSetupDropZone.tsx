abe66ca380cdaecc3dcc03f1bb94f4c0d9e12531
Chris Simbulan <simbulan.c@gmail.com>
2021-11-23 16:34:41 -0500

    removed unused variables

--- src/components/Contacts/ContactFlow/ContactFlowSetup/DropZone/ContactFlowSetupDropZone.tsx
+++ src/components/Contacts/ContactFlow/ContactFlowSetup/DropZone/ContactFlowSetupDropZone.tsx
@@ -1,8 +1,6 @@
 import { Box } from '@material-ui/core';
-import { useSnackbar } from 'notistack';
 import React from 'react';
 import { useDrop } from 'react-dnd';
-import { useTranslation } from 'react-i18next';
 import theme from '../../../../../../src/theme';
 import { ContactFlowSetupItemDrag } from '../Row/ContactFlowSetupStatusRow';
 
@@ -18,23 +16,17 @@ interface Props {
 export const ContactFlowSetupDropZone: React.FC<Props> = ({
   columnIndex,
   moveStatus,
-}: // eslint-disable-next-line @typescript-eslint/no-unused-vars
-Props) => {
+}: Props) => {
   const [{ isOver, canDrop }, drop] = useDrop(() => ({
     accept: 'status',
     canDrop: (item: ContactFlowSetupItemDrag) =>
       item.originIndex !== columnIndex,
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     drop: (item) => moveStatus(item.originIndex, columnIndex, item.status),
     collect: (monitor) => ({
       isOver: !!monitor.isOver(),
       canDrop: !!monitor.canDrop(),
     }),
   }));
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { enqueueSnackbar } = useSnackbar();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const { t } = useTranslation();
 
   return (
     <Box
