49922f5f899103adb1efeffe47e80388bfc82fbc
Gyasi Story <gyasi.story@cru.org>
2021-12-02 16:40:48 -0500

    MPDX-7050 Fix Pretty Errors

--- pages/api/Schema/AccountListAnalytics/resolvers.ts
+++ pages/api/Schema/AccountListAnalytics/resolvers.ts
@@ -1,11 +1,18 @@
-import { Resolvers } from "pages/api/graphql-rest.page.generated";
+import { Resolvers } from 'pages/api/graphql-rest.page.generated';
 
 const AccountListAnalyticsResolvers: Resolvers = {
-    Query: {
-        accountListAnalytics: async (_source, { accountListId, dateRange }, { dataSources }) => {
-            return dataSources.mpdxRestApi.getAccountListAnalytics(accountListId, dateRange);
-        }
-    }
-}
+  Query: {
+    accountListAnalytics: async (
+      _source,
+      { accountListId, dateRange },
+      { dataSources },
+    ) => {
+      return dataSources.mpdxRestApi.getAccountListAnalytics(
+        accountListId,
+        dateRange,
+      );
+    },
+  },
+};
 
 export { AccountListAnalyticsResolvers };
