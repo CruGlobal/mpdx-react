c64fc105dbc47e5e3a373cd14a268c6ed0dace40
Gyasi Story <gyasistory@gmail.com>
2021-12-14 11:33:43 -0500

    MPDX-7187 Create isMonthly state

--- src/components/Coaching/CoachingDetail/CoachingDetail.tsx
+++ src/components/Coaching/CoachingDetail/CoachingDetail.tsx
@@ -95,6 +95,7 @@ export const CoachingDetail: React.FC<CoachingDetailProps> = ({
     ? accountListData?.accountList
     : coachingData?.coachingAccountList;
 
+  const { isMonthly, setIsMonthly } = React.useState(true);
   return (
     <CoachingDetailContainer>
       <CoachingSideContainer bgcolor={theme.palette.progressBarGray.main}>
