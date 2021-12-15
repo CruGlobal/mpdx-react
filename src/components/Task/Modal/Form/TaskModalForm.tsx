9633b27d231db77ce2f0795f230a568ba115f2d4
Chris Simbulan <simbulan.c@gmail.com>
2021-12-15 03:56:48 -0500

    refetch queries after updating task

--- src/components/Task/Modal/Form/TaskModalForm.tsx
+++ src/components/Task/Modal/Form/TaskModalForm.tsx
@@ -51,6 +51,7 @@ import {
 } from '../../Drawer/Form/TaskDrawer.generated';
 import theme from '../../../../../src/theme';
 import { useCreateTaskCommentMutation } from '../../Drawer/CommentList/Form/CreateTaskComment.generated';
+import { TasksDocument } from 'pages/accountLists/[accountListId]/tasks/Tasks.generated';
 
 export const ActionButton = styled(Button)(() => ({
   color: theme.palette.info.main,
@@ -148,6 +149,12 @@ const TaskModalForm = ({
     if (isUpdate(attributes)) {
       await updateTask({
         variables: { accountListId, attributes },
+        refetchQueries: [
+          {
+            query: TasksDocument,
+            variables: { accountListId },
+          },
+        ],
       });
     } else {
       await createTask({
@@ -165,6 +172,16 @@ const TaskModalForm = ({
             });
           }
         },
+        refetchQueries: [
+          {
+            query: GetTasksForTaskListDocument,
+            variables: { accountListId, first: rowsPerPage, ...filter },
+          },
+          {
+            query: TasksDocument,
+            variables: { accountListId },
+          },
+        ],
       });
     }
     enqueueSnackbar(t('Task saved successfully'), { variant: 'success' });
