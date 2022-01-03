0d7efe0e0f4b60766c06a8e31f226e0a0d1dd239
TheNoodleMoose <cjh605@gmail.com>
2022-01-03 13:52:13 -0500

    Build out search menu with autocomplete, add tests

--- src/components/Layouts/Primary/TopBar/Items/SearchMenu/SearchMenu.test.tsx
+++ src/components/Layouts/Primary/TopBar/Items/SearchMenu/SearchMenu.test.tsx
@@ -1,17 +1,121 @@
 import React from 'react';
 import { render, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import TestWrapper from '../../../../../../../__tests__/util/TestWrapper';
+import { GqlMockedProvider } from '../../../../../../../__tests__/util/graphqlMocking';
+import { StatusEnum } from '../../../../../../../graphql/types.generated';
+import TestRouter from '../../../../../../../__tests__/util/TestRouter';
 import SearchMenu from './SearchMenu';
+import { GetSearchMenuContactsQuery } from './SearchMenu.generated';
+
+const router = {
+  pathname: '/accountLists/[accountListId]',
+  query: { accountListId: '1' },
+  push: jest.fn(),
+  isReady: true,
+};
 
 describe('SearchMenu', () => {
   it('default', async () => {
-    const { getByRole, getByText } = render(
-      <TestWrapper>
-        <SearchMenu />
-      </TestWrapper>,
+    const { getByRole, getByPlaceholderText } = render(
+      <GqlMockedProvider<GetSearchMenuContactsQuery>>
+        <TestRouter router={router}>
+          <SearchMenu />
+        </TestRouter>
+      </GqlMockedProvider>,
     );
     userEvent.click(getByRole('button'));
-    await waitFor(() => expect(getByText('Search Menu')).toBeInTheDocument());
+    await waitFor(() =>
+      expect(
+        getByPlaceholderText('Type something to start searching'),
+      ).toBeInTheDocument(),
+    );
+  });
+
+  it('handles searching', async () => {
+    const { getByRole, getByPlaceholderText, getByText } = render(
+      <GqlMockedProvider<GetSearchMenuContactsQuery>
+        mocks={{
+          GetSearchMenuContacts: {
+            contacts: {
+              nodes: [
+                {
+                  name: 'Cool, Guy',
+                  status: StatusEnum.AskInFuture,
+                  id: '123',
+                },
+                {
+                  name: 'Cool, Dude',
+                  status: StatusEnum.CallForDecision,
+                  id: '1234',
+                },
+              ],
+            },
+          },
+        }}
+      >
+        <TestRouter router={router}>
+          <SearchMenu />
+        </TestRouter>
+      </GqlMockedProvider>,
+    );
+
+    userEvent.click(getByRole('button'));
+    await waitFor(() =>
+      expect(
+        getByPlaceholderText('Type something to start searching'),
+      ).toBeInTheDocument(),
+    );
+    userEvent.type(
+      getByPlaceholderText('Type something to start searching'),
+      'Cool',
+    );
+    await waitFor(() => expect(getByText('Cool, Guy')).toBeVisible());
+    await waitFor(() => expect(getByText('Cool, Dude')).toBeVisible());
+    expect(getByText('Create a new contact for "Cool"')).toBeVisible();
+  });
+
+  it('handles clicking search result', async () => {
+    const { getByRole, getByPlaceholderText, getByText } = render(
+      <GqlMockedProvider<GetSearchMenuContactsQuery>
+        mocks={{
+          GetSearchMenuContacts: {
+            contacts: {
+              nodes: [
+                {
+                  name: 'Cool, Guy',
+                  status: StatusEnum.AskInFuture,
+                  id: '123',
+                },
+                {
+                  name: 'Cool, Dude',
+                  status: StatusEnum.CallForDecision,
+                  id: '1234',
+                },
+              ],
+            },
+          },
+        }}
+      >
+        <TestRouter router={router}>
+          <SearchMenu />
+        </TestRouter>
+      </GqlMockedProvider>,
+    );
+
+    userEvent.click(getByRole('button'));
+    await waitFor(() =>
+      expect(
+        getByPlaceholderText('Type something to start searching'),
+      ).toBeInTheDocument(),
+    );
+    userEvent.type(
+      getByPlaceholderText('Type something to start searching'),
+      'Cool',
+    );
+    await waitFor(() => expect(getByText('Cool, Guy')).toBeVisible());
+    await waitFor(() => expect(getByText('Cool, Dude')).toBeVisible());
+    expect(getByText('Create a new contact for "Cool"')).toBeVisible();
+    userEvent.click(getByText('Cool, Guy'));
+    await waitFor(() => expect(router.push).toHaveBeenCalled());
   });
 });
--- src/components/Layouts/Primary/TopBar/Items/SearchMenu/SearchMenu.tsx
+++ src/components/Layouts/Primary/TopBar/Items/SearchMenu/SearchMenu.tsx
@@ -1,66 +1,316 @@
-import React, { ReactElement, useState } from 'react';
-import { Box, IconButton, makeStyles, Theme, Menu } from '@material-ui/core';
+import React, { ReactElement, useCallback, useState } from 'react';
+import {
+  Box,
+  IconButton,
+  styled,
+  Dialog,
+  TextField,
+  Typography,
+} from '@material-ui/core';
 import SearchIcon from '@material-ui/icons/Search';
 import { useTranslation } from 'react-i18next';
+import CompassIcon from '@material-ui/icons/Explore';
+import PersonIcon from '@material-ui/icons/Person';
+import PeopleIcon from '@material-ui/icons/People';
+import AddIcon from '@material-ui/icons/Add';
+import { Autocomplete, createFilterOptions } from '@material-ui/lab';
+import debounce from 'lodash/fp/debounce';
+import NextLink from 'next/link';
+import { useAccountListId } from '../../../../../../hooks/useAccountListId';
+import { StatusEnum } from '../../../../../../../graphql/types.generated';
+import { useGetSearchMenuContactsQuery } from './SearchMenu.generated';
 
-const useStyles = makeStyles((theme: Theme) => ({
-  link: {
-    textTransform: 'none',
-    color: 'rgba(255,255,255,0.75)',
-    transition: 'color 0.2s ease-in-out',
-    '&:hover': {
-      color: 'rgba(255,255,255,1)',
-    },
-  },
-  menuPaper: {
-    width: '50ch',
-    [theme.breakpoints.down('xs')]: {
-      width: '100%',
-    },
+const SearchDialog = styled(Dialog)(() => ({
+  '& .MuiPaper-root': {
+    position: 'absolute',
+    top: 50,
   },
 }));
 
+const SearchButton = styled(IconButton)(() => ({
+  textTransform: 'none',
+  color: 'rgba(255,255,255,0.75)',
+  transition: 'color 0.2s ease-in-out',
+  '&:hover': {
+    color: 'rgba(255,255,255,1)',
+  },
+}));
+
+interface Option {
+  name: string;
+  status?: StatusEnum | null;
+  icon: ReactElement;
+  link: string;
+}
+
 const SearchMenu = (): ReactElement => {
-  const classes = useStyles();
   const { t } = useTranslation();
+  const accountListId = useAccountListId();
 
-  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement>();
+  const [isOpen, setIsOpen] = useState(false);
 
-  const handleClick = (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-  ) => {
-    setAnchorEl(event.currentTarget);
+  //#region Search
+  const [search, setSearch] = useState('');
+  const [wildcardSearch, setWildcardSearch] = useState('');
+
+  const { data, loading } = useGetSearchMenuContactsQuery({
+    variables: {
+      accountListId: accountListId ?? '',
+      contactsFilter: {
+        wildcardSearch,
+      },
+    },
+    skip: !accountListId,
+  });
+
+  const handleUpdateWildcardSearch = useCallback(
+    debounce(1000, (searchTerm: string) => {
+      setWildcardSearch(searchTerm);
+    }),
+    [],
+  );
+
+  const handleSearch = (searchTerm: string) => {
+    // Update input state via non debounced function
+    setSearch(searchTerm);
+    // Update wildcard search via debounced function
+    handleUpdateWildcardSearch(searchTerm);
   };
-
   const handleClose = () => {
-    setAnchorEl(undefined);
+    setIsOpen(false);
+    setSearch('');
   };
 
+  const filter = createFilterOptions<Option>({ limit: 5 });
+
+  const defaultOptions: Option[] = [
+    {
+      name: t('Contacts'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/contacts`,
+    },
+    {
+      name: t('Tasks'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/tasks`,
+    },
+    {
+      name: t('Preferences - Manage Accounts'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/preferences/manageAccounts`,
+    },
+    {
+      name: t('Preferences - Manage Coaches'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/preferences/coaching`,
+    },
+    {
+      name: t('Preferences - Connect Services'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/preferences/connectServices`,
+    },
+    {
+      name: t('Reports - Donations'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/reports/PartnerGivingAnalysis`,
+    },
+    {
+      name: t('Reports - Monthly Report (Partner Currency)'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/reports/partnerCurrency`,
+    },
+    {
+      name: t('Reports - Monthly Report (Salary Currency)'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/reports/salaryCurrency`,
+    },
+    {
+      name: t('Reports - Designation Accounts'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/reports/designationAccounts`,
+    },
+    {
+      name: t('Reports - Responsibility Centers'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/reports/responsibilityCenters`,
+    },
+    {
+      name: t('Reports - Expected Monthly Total'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/reports/expectedMonthlyTotal`,
+    },
+    {
+      name: t('Reports - Partner Giving Analysis'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/reports/PartnerGivingAnalysis`,
+    },
+    {
+      name: t('Reports - Coaching'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/reports/coaching`,
+    },
+    {
+      name: t('Tools'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/tools`,
+    },
+    {
+      name: t('Coaching'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/coaching`,
+    },
+    {
+      name: t('Tools - Fix - Commitment Info'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/tools/fixCommitmentInfo`,
+    },
+    {
+      name: t('Tools - Fix - Mailing Addresses'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/tools/fixMailingAddresses`,
+    },
+    {
+      name: t('Tools - Fix - Send Newsletter'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/tools/fixSendNewsletter`,
+    },
+    {
+      name: t('Tools - Fix - Merge Contacts'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/tools/fixMergeContacts`,
+    },
+    {
+      name: t('Tools - Fix - Email Addresses'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/tools/fixEmailAddresses`,
+    },
+    {
+      name: t('Tools - Fix - Phone Numbers'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/tools/fixPhoneNumbers`,
+    },
+    {
+      name: t('Tools - Fix - Merge People'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/tools/fixMergePeople`,
+    },
+    {
+      name: t('Tools - Import - Google'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/tools/google`,
+    },
+    {
+      name: t('Tools - Import - TntConnect'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/tools/tntConnect`,
+    },
+    {
+      name: t('Tools - Import - CSV'),
+      icon: <CompassIcon />,
+      link: `/accountLists/${accountListId}/tools/csv`,
+    },
+  ];
+
+  const options: Option[] = [
+    data?.contacts.nodes.map((contact) => ({
+      name: contact.name,
+      status: contact.status,
+      icon: <PersonIcon />,
+      link: `/accountLists/${accountListId}/contacts/${contact.id}`,
+    })) ?? [],
+    defaultOptions,
+  ].flat();
+  //#endregion
+
+  //#region JSX
   return (
     <>
-      <IconButton
-        className={classes.link}
+      <SearchButton
         aria-controls="search-menu"
         aria-haspopup="true"
-        onClick={handleClick}
+        onClick={() => setIsOpen(true)}
       >
         <SearchIcon />
-      </IconButton>
-      <Menu
-        id="search-menu"
-        anchorEl={anchorEl}
-        keepMounted
-        open={Boolean(anchorEl)}
-        onClose={handleClose}
-        classes={{ paper: classes.menuPaper }}
-      >
-        <Box display="flex" flexDirection="row" justifyContent="center">
-          <SearchIcon />
-          <Box flexGrow={1}>{t('Search Menu')}</Box>
+      </SearchButton>
+
+      <SearchDialog fullWidth maxWidth="lg" open={isOpen} onClose={handleClose}>
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <Autocomplete
+            fullWidth
+            freeSolo
+            loading={loading}
+            filterSelectedOptions
+            onChange={handleClose}
+            getOptionLabel={(option) => {
+              if (typeof option === 'string') {
+                return option;
+              }
+
+              return option.name;
+            }}
+            renderOption={(option) => (
+              <NextLink href={option.link} passHref>
+                <Box display="flex">
+                  <Box display="flex" marginRight={1}>
+                    {option.icon}
+                  </Box>
+                  <Box display="flex" flexDirection="column">
+                    <Typography>{option.name}</Typography>
+                    <Typography variant="subtitle2">
+                      {option.status && t(option.status)}
+                    </Typography>
+                  </Box>
+                </Box>
+              </NextLink>
+            )}
+            options={search !== '' ? options : []}
+            filterOptions={(options, params) => {
+              const filtered = filter(options, params);
+              if (params.inputValue !== '') {
+                if (
+                  data?.contacts.totalCount &&
+                  data?.contacts.totalCount > 5
+                ) {
+                  filtered.splice(5, 0, {
+                    name: t(`And ${data?.contacts.totalCount - 5} more`),
+                    icon: <PeopleIcon />,
+                    link: `/accountLists/${accountListId}/contacts?searchTerm=${search}`,
+                  });
+                }
+                filtered.push({
+                  name: t(`Create a new contact for "${params.inputValue}"`),
+                  icon: <AddIcon />,
+                  link: `/accountLists/${accountListId}/contacts`, //temp
+                });
+              }
+
+              return filtered;
+            }}
+            renderInput={(params): ReactElement => (
+              <TextField
+                {...params}
+                fullWidth
+                placeholder={t('Type something to start searching')}
+                InputProps={{
+                  ...params.InputProps,
+                  type: 'search',
+                  startAdornment: (
+                    <IconButton>
+                      <SearchIcon />
+                    </IconButton>
+                  ),
+                  endAdornment: null,
+                }}
+                onChange={(e) => handleSearch(e.target.value)}
+              />
+            )}
+            inputValue={search}
+          />
         </Box>
-      </Menu>
+      </SearchDialog>
     </>
   );
+  //#endregion
 };
 
 export default SearchMenu;
