c136a34c55147e3d82e71526d8460f2ccb3b2cd0
Chris Simbulan <simbulan.c@gmail.com>
2022-03-16 13:18:43 -0400

    added test for rendering with data and clicking the name

--- src/components/Contacts/ContactDetails/ContactReferralTab/ContactReferralTab.test.tsx
+++ src/components/Contacts/ContactDetails/ContactReferralTab/ContactReferralTab.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { renderHook } from '@testing-library/react-hooks';
+import userEvent from '@testing-library/user-event';
 import { GqlMockedProvider } from '../../../../../__tests__/util/graphqlMocking';
 import { render } from '../../../../../__tests__/util/testingLibraryReactMock';
 import { ContactReferralTab } from './ContactReferralTab';
@@ -62,5 +63,42 @@ describe('ContactReferralTab', () => {
       </GqlMockedProvider>,
     );
     expect(await findByText('No Referrals')).toBeVisible();
+  });
+
+  it('tests render with data and click event', async () => {
+    const { findByText, getByText } = render(
+      <GqlMockedProvider<ContactReferralTabQuery>
+        mocks={{
+          ContactReferralTab: {
+            contact: {
+              id: 'contact-id',
+              name: 'name',
+              contactReferralsByMe: {
+                nodes: [
+                  {
+                    id: 'referral-id',
+                    createdAt: '2021-04-29T07:48:28+0000',
+                    referredTo: {
+                      id: 'contact-id-2',
+                      name: 'name-2',
+                    },
+                  },
+                ],
+              },
+            },
+          },
+        }}
+      >
+        <ContactReferralTab
+          accountListId="accountList-id"
+          contactId="contact-id"
+          onContactSelected={onContactSelected}
+        />
+      </GqlMockedProvider>,
+    );
+    expect(await findByText('name-2')).toBeVisible();
+    expect(await findByText('2021-04-29')).toBeVisible();
+    userEvent.click(getByText('name-2'));
+    expect(onContactSelected).toHaveBeenCalledWith('contact-id-2');
   });
 });
