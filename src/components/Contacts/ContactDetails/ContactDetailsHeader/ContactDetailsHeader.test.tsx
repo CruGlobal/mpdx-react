91be23f562a9416ea81b226ed59859611eff6b4b
Chris Simbulan <simbulan.c@gmail.com>
2022-03-31 16:40:43 -0400

    added wrappers to give test properties
    added wrappers to give test properties

--- src/components/Contacts/ContactDetails/ContactDetailsHeader/ContactDetailsHeader.test.tsx
+++ src/components/Contacts/ContactDetails/ContactDetailsHeader/ContactDetailsHeader.test.tsx
@@ -1,22 +1,36 @@
+import { ThemeProvider } from '@material-ui/core';
 import { render } from '@testing-library/react';
 import React from 'react';
+import { SnackbarProvider } from 'notistack';
 import { GqlMockedProvider } from '../../../../../__tests__/util/graphqlMocking';
 import { ContactDetailsHeader } from './ContactDetailsHeader';
 import { GetContactDetailsHeaderQuery } from './ContactDetailsHeader.generated';
+import TestRouter from '__tests__/util/TestRouter';
+import theme from 'src/theme';
 
 const accountListId = 'abc';
 const contactId = 'contact-1';
 
+const router = {
+  query: { accountListId },
+};
+
 describe('ContactDetails', () => {
   it('should show loading state', async () => {
     const { queryByTestId } = render(
-      <GqlMockedProvider<GetContactDetailsHeaderQuery>>
-        <ContactDetailsHeader
-          accountListId={accountListId}
-          contactId={contactId}
-          onClose={() => {}}
-        />
-      </GqlMockedProvider>,
+      <SnackbarProvider>
+        <TestRouter router={router}>
+          <ThemeProvider theme={theme}>
+            <GqlMockedProvider<GetContactDetailsHeaderQuery>>
+              <ContactDetailsHeader
+                accountListId={accountListId}
+                contactId={contactId}
+                onClose={() => {}}
+              />
+            </GqlMockedProvider>
+          </ThemeProvider>
+        </TestRouter>
+      </SnackbarProvider>,
     );
 
     expect(queryByTestId('Skeleton')).toBeInTheDocument();
@@ -24,23 +38,29 @@ describe('ContactDetails', () => {
 
   it('should render with contact details', async () => {
     const { findByText, queryByTestId } = render(
-      <GqlMockedProvider<GetContactDetailsHeaderQuery>
-        mocks={{
-          GetContactDetailsHeader: {
-            contact: {
-              primaryPerson: { firstName: 'Fname', lastName: 'Lname' },
-              lastDonation: null,
-              pledgeCurrency: 'USD',
-            },
-          },
-        }}
-      >
-        <ContactDetailsHeader
-          accountListId={accountListId}
-          contactId={contactId}
-          onClose={() => {}}
-        />
-      </GqlMockedProvider>,
+      <SnackbarProvider>
+        <TestRouter router={router}>
+          <ThemeProvider theme={theme}>
+            <GqlMockedProvider<GetContactDetailsHeaderQuery>
+              mocks={{
+                GetContactDetailsHeader: {
+                  contact: {
+                    primaryPerson: { firstName: 'Fname', lastName: 'Lname' },
+                    lastDonation: null,
+                    pledgeCurrency: 'USD',
+                  },
+                },
+              }}
+            >
+              <ContactDetailsHeader
+                accountListId={accountListId}
+                contactId={contactId}
+                onClose={() => {}}
+              />
+            </GqlMockedProvider>
+          </ThemeProvider>
+        </TestRouter>
+      </SnackbarProvider>,
     );
 
     expect(await findByText('Fname Lname')).toBeVisible();
@@ -50,24 +70,30 @@ describe('ContactDetails', () => {
 
   it('should render without primaryPerson', async () => {
     const { findByText, queryByTestId } = render(
-      <GqlMockedProvider<GetContactDetailsHeaderQuery>
-        mocks={{
-          GetContactDetailsHeader: {
-            contact: {
-              name: 'Lname, Fname',
-              primaryPerson: null,
-              pledgeCurrency: 'USD',
-              lastDonation: null,
-            },
-          },
-        }}
-      >
-        <ContactDetailsHeader
-          accountListId={accountListId}
-          contactId={contactId}
-          onClose={() => {}}
-        />
-      </GqlMockedProvider>,
+      <SnackbarProvider>
+        <TestRouter router={router}>
+          <ThemeProvider theme={theme}>
+            <GqlMockedProvider<GetContactDetailsHeaderQuery>
+              mocks={{
+                GetContactDetailsHeader: {
+                  contact: {
+                    name: 'Lname, Fname',
+                    primaryPerson: null,
+                    pledgeCurrency: 'USD',
+                    lastDonation: null,
+                  },
+                },
+              }}
+            >
+              <ContactDetailsHeader
+                accountListId={accountListId}
+                contactId={contactId}
+                onClose={() => {}}
+              />
+            </GqlMockedProvider>
+          </ThemeProvider>
+        </TestRouter>
+      </SnackbarProvider>,
     );
 
     expect(await findByText('Lname, Fname')).toBeVisible();
