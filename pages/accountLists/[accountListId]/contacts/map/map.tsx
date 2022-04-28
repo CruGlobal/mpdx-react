0d4309311a43baca6d925c9031011615c48f9605
Chris Simbulan <simbulan.c@gmail.com>
2022-04-28 18:53:10 -0400

    render data points on map

--- pages/accountLists/[accountListId]/contacts/map/map.tsx
+++ pages/accountLists/[accountListId]/contacts/map/map.tsx
@@ -1,4 +1,8 @@
+import React, { useEffect, useState } from 'react';
 import GoogleMapReact from 'google-map-react';
+import Geocode from 'react-geocode';
+import { useContactsQuery } from '../Contacts.generated';
+import { useAccountListId } from 'src/hooks/useAccountListId';
 
 interface ContactsMapProps {
   selectedIds: string[];
@@ -7,9 +11,20 @@ interface ContactsMapProps {
 interface ContactCardProps {
   contactId: string;
   contactName: string;
-  contactAddress: object;
+  contactAddress: {
+    id: string;
+    street: string;
+  };
 }
 
+interface Coordinates {
+  lat: number;
+  lng: number;
+}
+
+Geocode.setApiKey(process.env.GOOGLE_GEOCODE_API_KEY);
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const ContactCard: React.FC<ContactCardProps> = () => {
   return (
     <div>
@@ -23,14 +38,16 @@ const ContactCard: React.FC<ContactCardProps> = () => {
         Country
         <br />
       </p>
-      <a className="btn btn-primary">Show Contact</a>
+      <a className="btn btn-primary" href="/">
+        Show Contact
+      </a>
     </div>
   );
 };
 
 interface MapMarkerProps {
-  latitude: number;
-  longitude: number;
+  lat: number;
+  lng: number;
 }
 
 const MapMarker: React.FC<MapMarkerProps> = () => {
@@ -42,6 +59,83 @@ const MapMarker: React.FC<MapMarkerProps> = () => {
 };
 
 export const ContactsMap: React.FC<ContactsMapProps> = () => {
+  const accountListId = useAccountListId();
+  const [ccc, setCcc] = useState<Coordinates[]>([]);
+  const { data, loading, fetchMore } = useContactsQuery({
+    variables: {
+      accountListId: accountListId ?? '',
+    },
+    skip: !accountListId,
+  });
+
+  // const getCoords = async (address: string) => {
+  //   let x = {};
+  //   await Geocode.fromAddress(`${address}`).then(
+  //     (response: {
+  //       results: { geometry: { location: { lat: any; lng: any } } }[];
+  //     }) => {
+  //       const { lat, lng } = response.results[0].geometry.location;
+  //       x = { lat, lng };
+  //     },
+  //     (error: any) => {
+  //       console.error(error);
+  //     },
+  //   );
+  //   return x;
+  // };
+
+  // const filterCoords = () => {
+  //   const v = [];
+  //   const coords = data?.contacts.nodes.map((contact) => {
+  //     // console.log(contact.name);
+  //     // console.log(contact.primaryAddress?.street);
+  //     if (!contact.primaryAddress?.street) {
+  //       return;
+  //     }
+  //     const z = { lat: 0, lng: 0 };
+  //     getCoords(contact.primaryAddress?.street).then((x) => {
+  //       //console.log(`${contact.name} ${x.lat}`);
+  //       if (x.lat) {
+  //         setCcc([...ccc, { lat: x.lat, lng: x.lng }]);
+  //       }
+  //     });
+  //     return z;
+  //   });
+  //   const filtered = coords?.filter((coord) => coord !== undefined);
+  //   //console.log(filtered);
+  //   //setCcc(filtered);
+  // };
+
+  const getCoords = async () => {
+    const coords: Coordinates[] = [];
+    if (!data) {
+      return;
+    }
+    for (const contact of data?.contacts.nodes) {
+      if (!contact.primaryAddress?.street) {
+        continue;
+      }
+      await Geocode.fromAddress(`${contact.primaryAddress.street}`).then(
+        (response: {
+          results: { geometry: { location: { lat: any; lng: any } } }[];
+        }) => {
+          const { lat, lng } = response.results[0].geometry.location;
+          coords.push({ lat, lng });
+        },
+        (error: any) => {
+          console.error(error);
+        },
+      );
+    }
+    setCcc(coords);
+  };
+
+  useEffect(() => {
+    if (!loading) {
+      getCoords();
+    }
+  }, [loading]);
+
   return (
     // Important! Always set the container height explicitly
     <div style={{ height: '100vh', width: '100%' }}>
@@ -50,7 +144,9 @@ export const ContactsMap: React.FC<ContactsMapProps> = () => {
         defaultCenter={{ lat: 59.95, lng: 30.33 }}
         defaultZoom={11}
       >
-        <MapMarker latitude={59.955413} longitude={30.337844} />
+        {ccc.map((coord, index) => (
+          <MapMarker lat={coord.lat} lng={coord.lng} key={index} />
+        ))}
       </GoogleMapReact>
     </div>
   );
