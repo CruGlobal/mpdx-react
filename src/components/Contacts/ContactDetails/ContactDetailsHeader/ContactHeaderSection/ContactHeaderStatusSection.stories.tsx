b2a45afef167d28914a26409191e25bde48d39df
Gyasi Story <gyasistory@gmail.com>
2022-04-29 11:45:52 -0400

    MPDX-7311 - Add status and late data  Storybook

--- src/components/Contacts/ContactDetails/ContactDetailsHeader/ContactHeaderSection/ContactHeaderStatusSection.stories.tsx
+++ src/components/Contacts/ContactDetails/ContactDetailsHeader/ContactHeaderSection/ContactHeaderStatusSection.stories.tsx
@@ -1,3 +1,4 @@
+import { StatusEnum } from 'graphql/types.generated';
 import { DateTime } from 'luxon';
 import React, { ReactElement } from 'react';
 import { gqlMock } from '../../../../../../__tests__/util/graphqlMocking';
@@ -17,9 +18,12 @@ export const Default = (): ReactElement => {
     ContactDetailsHeaderFragmentDoc,
     {
       mocks: {
+        status: StatusEnum.PartnerFinancial,
+        pledgeCurrency: 'USD',
         lastDonation: {
           donationDate: DateTime.now().plus({ month: 5 }).toISO(),
         },
+        lateAt: DateTime.now().minus({ month: 5 }).toISO(),
       },
     },
   );
