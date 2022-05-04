ee5189d7da3bbb0ab6c66c40cc6d5850e30c22a0
Gyasi Story <gyasistory@gmail.com>
2022-05-04 11:26:49 -0400

    MPDX-7311 Create Storybook for Partner section

---
+++ src/components/Contacts/ContactDetails/ContactDetailsHeader/ContactHeaderSection/ContactHeaderPartnerSection.stories.tsx
@@ -0,0 +1,31 @@
+import { DateTime } from 'luxon';
+import React, { ReactElement } from 'react';
+import { gqlMock } from '__tests__/util/graphqlMocking';
+import { ContactDetailsHeaderFragmentDoc } from '../ContactDetailsHeader.generated';
+import { ContactHeaderPartnerSection } from './ContactHeaderPartnerSection';
+import { ContactHeaderStatusFragment } from './ContactHeaderStatus.generated';
+
+export default {
+  title: 'Contacts/ContactDetails/Header/PartnerSection',
+};
+
+const contact = gqlMock<ContactHeaderStatusFragment>(
+  ContactDetailsHeaderFragmentDoc,
+  {
+    mocks: {
+      pledgeCurrency: 'USD',
+      lastDonation: {
+        donationDate: DateTime.now().plus({ month: 5 }).toISO(),
+      },
+      lateAt: DateTime.now().minus({ month: 5 }).toISO(),
+    },
+  },
+);
+
+export const Default = (): ReactElement => {
+  return <ContactHeaderPartnerSection loading={false} contact={contact} />;
+};
+
+export const Loading = (): ReactElement => {
+  return <ContactHeaderPartnerSection loading={true} contact={contact} />;
+};
