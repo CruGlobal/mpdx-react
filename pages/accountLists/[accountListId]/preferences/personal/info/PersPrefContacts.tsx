e1f855db71186a0f12b285c0256919450f333695
John Plastow <j2trumpet@gmail.com>
2022-05-17 14:55:09 -0400

    Removing old commented-out code

--- pages/accountLists/[accountListId]/preferences/personal/info/PersPrefContacts.tsx
+++ pages/accountLists/[accountListId]/preferences/personal/info/PersPrefContacts.tsx
@@ -10,7 +10,6 @@ import {
 } from '@material-ui/core';
 import { ExpandMore } from '@material-ui/icons';
 import { accordionShared } from '../shared/PersPrefShared';
-// import PersPrefContact from './PersPrefContact';
 
 const StyledAccordion = styled(Accordion)({
   backgroundColor: 'transparent',
