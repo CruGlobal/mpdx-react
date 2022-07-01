1fea566d231ad05ec95f0ca014b51ac58224ebb8
John Plastow <j2trumpet@gmail.com>
2022-07-01 11:51:21 -0700

    Opens correct modal

--- src/components/Contacts/ContactDetails/ContactTasksTab/DeleteTaskIconButton/DeleteTaskIconButton.tsx
+++ src/components/Contacts/ContactDetails/ContactTasksTab/DeleteTaskIconButton/DeleteTaskIconButton.tsx
@@ -1,7 +1,7 @@
 import { DialogActions, IconButton, styled } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import { DeletedItemIcon } from '../../../../common/DeleteItemIcon/DeleteItemIcon';
-import useTaskModal from '../../../../../hooks/useTaskModal';
+import { DeleteConfirmation } from '../../../../common/Modal/DeleteConfirmation/DeleteConfirmation';
 
 const DeleteButton = styled(IconButton)(({ theme }) => ({
   margin: theme.spacing(1),
@@ -12,11 +12,22 @@ export const StyledDialogActions = styled(DialogActions)(({ theme }) => ({
 }));
 
 export const DeleteTaskIconButton: React.FC = () => {
-  const { openTaskModal } = useTaskModal();
+  const [removeDialogOpen, handleRemoveDialog] = useState(false);
 
   return (
-    <DeleteButton onClick={() => openTaskModal({ view: 'delete' })}>
-      <DeletedItemIcon />
-    </DeleteButton>
+    <>
+      <DeleteButton onClick={() => handleRemoveDialog(true)}>
+        <DeletedItemIcon />
+      </DeleteButton>
+      <DeleteConfirmation
+        deleteType="task"
+        open={removeDialogOpen}
+        deleting={false} // deleting
+        onClickConfirm={() => {
+          return true;
+        }} // onDeleteTask
+        onClickDecline={handleRemoveDialog}
+      />
+    </>
   );
 };
