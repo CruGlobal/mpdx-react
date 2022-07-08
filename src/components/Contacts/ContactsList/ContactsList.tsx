59ee427db37ee84cf8aec6a3d38d00354b5ce967
Chris Simbulan <simbulan.c@gmail.com>
2022-07-08 06:29:15 -0400

    remove extra contact page provider

--- src/components/Contacts/ContactsList/ContactsList.tsx
+++ src/components/Contacts/ContactsList/ContactsList.tsx
@@ -6,7 +6,6 @@ import { InfiniteList } from 'src/components/InfiniteList/InfiniteList';
 import NullState from 'src/components/Shared/Filters/NullState/NullState';
 import {
   ContactsPageContext,
-  ContactsPageProvider,
   ContactsPageType,
 } from 'pages/accountLists/[accountListId]/contacts/ContactsPageContext';
 import { TableViewModeEnum } from 'src/components/Shared/Header/ListHeader';
@@ -42,39 +41,37 @@ export const ContactsList: React.FC = () => {
   });
 
   return (
-    <ContactsPageProvider>
-      <InfiniteList
-        loading={loading}
-        data={data?.contacts?.nodes ?? []}
-        totalCount={data?.contacts?.totalCount ?? 0}
-        style={{ height: 'calc(100vh - 160px)' }}
-        itemContent={(index, contact) => (
-          <ContactRow
-            key={contact.id}
-            contact={contact}
-            useTopMargin={index === 0}
+    <InfiniteList
+      loading={loading}
+      data={data?.contacts?.nodes ?? []}
+      totalCount={data?.contacts?.totalCount ?? 0}
+      style={{ height: 'calc(100vh - 160px)' }}
+      itemContent={(index, contact) => (
+        <ContactRow
+          key={contact.id}
+          contact={contact}
+          useTopMargin={index === 0}
+        />
+      )}
+      groupBy={(item) => item.name[0].toUpperCase()}
+      endReached={() =>
+        data?.contacts?.pageInfo.hasNextPage &&
+        fetchMore({
+          variables: {
+            after: data.contacts?.pageInfo.endCursor,
+          },
+        })
+      }
+      EmptyPlaceholder={
+        <Box width="75%" margin="auto" mt={2}>
+          <NullState
+            page="contact"
+            totalCount={data?.allContacts.totalCount || 0}
+            filtered={isFiltered}
+            changeFilters={setActiveFilters}
           />
-        )}
-        groupBy={(item) => item.name[0].toUpperCase()}
-        endReached={() =>
-          data?.contacts?.pageInfo.hasNextPage &&
-          fetchMore({
-            variables: {
-              after: data.contacts?.pageInfo.endCursor,
-            },
-          })
-        }
-        EmptyPlaceholder={
-          <Box width="75%" margin="auto" mt={2}>
-            <NullState
-              page="contact"
-              totalCount={data?.allContacts.totalCount || 0}
-              filtered={isFiltered}
-              changeFilters={setActiveFilters}
-            />
-          </Box>
-        }
-      />
-    </ContactsPageProvider>
+        </Box>
+      }
+    />
   );
 };
