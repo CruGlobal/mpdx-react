f7c962f8698efc47d11df8d8527bcc2c70dbbb85
Chris Simbulan <simbulan.c@gmail.com>
2022-07-14 08:27:40 -0400

    Create MassActionsAddTagsModal.tsx

---
+++ src/components/Contacts/MassActions/AddTags/MassActionsAddTagsModal.tsx
@@ -0,0 +1,188 @@
+import {
+  Button,
+  CircularProgress,
+  DialogActions,
+  DialogContent,
+  FormControl,
+  styled,
+  Typography,
+} from '@material-ui/core';
+import * as yup from 'yup';
+import { Autocomplete } from '@material-ui/lab';
+import { Formik } from 'formik';
+import React, { ReactElement } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useSnackbar } from 'notistack';
+import { Add } from '@material-ui/icons';
+import { ContactsUpdateInput } from '../../../../../graphql/types.generated';
+import Modal from '../../../common/Modal/Modal';
+import {
+  ContactTagIcon,
+  ContactTagInput,
+} from '../../ContactDetails/ContactDetailsTab/Tags/ContactTags';
+import { useGetContactTagListQuery } from '../../ContactDetails/ContactDetailsTab/Tags/ContactTags.generated';
+import { useContactsAddTagsMutation } from './ContactsAddTags.generated';
+import theme from 'src/theme';
+import { ContactsDocument } from 'pages/accountLists/[accountListId]/contacts/Contacts.generated';
+
+interface MassActionsAddTagsModalProps {
+  ids: string[];
+  accountListId: string;
+  handleClose: () => void;
+}
+
+const NewTagInstructions = styled(Typography)(() => ({
+  marginTop: theme.spacing(2),
+  fontWeight: 550,
+}));
+
+const ExistingTagButton = styled(Button)(() => ({
+  textTransform: 'none',
+  width: 'fit-content',
+}));
+
+const AddTagIcon = styled(Add)(() => ({
+  color: theme.palette.info.main,
+}));
+
+const tagSchema = yup.object({
+  tagList: yup.array().of(yup.string()).default([]).nullable(),
+  ids: yup.array().of(yup.string()).default([]),
+});
+
+export const MassActionsAddTagsModal: React.FC<MassActionsAddTagsModalProps> = ({
+  handleClose,
+  accountListId,
+  ids,
+}) => {
+  const { t } = useTranslation();
+
+  const { enqueueSnackbar } = useSnackbar();
+
+  const [contactsAddTags, { loading: updating }] = useContactsAddTagsMutation();
+
+  const onSubmit = async (attributes: ContactsUpdateInput) => {
+    await contactsAddTags({
+      variables: {
+        accountListId,
+        attributes,
+      },
+      refetchQueries: [
+        {
+          query: ContactsDocument,
+          variables: { accountListId },
+        },
+      ],
+    });
+    enqueueSnackbar(t('Tags added to contacts!'), {
+      variant: 'success',
+    });
+    handleClose();
+  };
+
+  //   const {
+  //     data: appeals,
+  //     loading: loadingAppeals,
+  //   } = useGetAppealsForMassActionQuery({
+  //     variables: {
+  //       accountListId,
+  //     },
+  //   });
+
+  const { data: contactTagsList, loading } = useGetContactTagListQuery({
+    variables: {
+      accountListId,
+    },
+  });
+
+  return (
+    <Modal title={t('Add Tags')} isOpen={true} handleClose={handleClose}>
+      <Formik
+        initialValues={{
+          tagList: [],
+          ids: ids,
+        }}
+        onSubmit={onSubmit}
+        validationSchema={tagSchema}
+      >
+        {({
+          values: { tagList },
+          setFieldValue,
+          handleSubmit,
+          isSubmitting,
+          isValid,
+        }): ReactElement => (
+          <form onSubmit={handleSubmit} noValidate>
+            <DialogContent dividers>
+              <FormControl fullWidth>
+                {contactTagsList?.accountList.contactTagList && tagList ? (
+                  <>
+                    <Typography>{t('Choose an existing tag:')}</Typography>
+                    {contactTagsList?.accountList.contactTagList
+                      .filter((tag) => !tagList.includes(tag))
+                      .map((tag) => (
+                        <ExistingTagButton
+                          key={tag}
+                          onClick={() =>
+                            setFieldValue('tagList', [...tagList, tag])
+                          }
+                        >
+                          <AddTagIcon />
+                          {tag}
+                        </ExistingTagButton>
+                      ))}
+                  </>
+                ) : null}
+              </FormControl>
+              <FormControl fullWidth>
+                <NewTagInstructions>
+                  {t(
+                    'Create New Tags (separate multiple tags with Enter key) *',
+                  )}
+                </NewTagInstructions>
+                <Autocomplete
+                  multiple
+                  freeSolo
+                  fullWidth
+                  loading={loading}
+                  popupIcon={<ContactTagIcon />}
+                  filterSelectedOptions
+                  value={tagList ?? []}
+                  options={[]}
+                  renderInput={(params): ReactElement => (
+                    <ContactTagInput
+                      {...params}
+                      placeholder={t('add tag')}
+                      //   disabled={isSubmitting || updating}
+                    />
+                  )}
+                  onChange={(_, tagList): void =>
+                    setFieldValue('tagList', tagList)
+                  }
+                />
+              </FormControl>
+            </DialogContent>
+            <DialogActions>
+              <Button
+                onClick={handleClose}
+                disabled={isSubmitting}
+                variant="text"
+              >
+                {t('Cancel')}
+              </Button>
+              <Button
+                color="primary"
+                type="submit"
+                variant="contained"
+                disabled={!isValid || isSubmitting || tagList?.length === 0}
+              >
+                {updating && <CircularProgress color="primary" size={20} />}
+                {t('Save')}
+              </Button>
+            </DialogActions>
+          </form>
+        )}
+      </Formik>
+    </Modal>
+  );
+};
