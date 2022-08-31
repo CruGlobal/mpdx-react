b8577a41ca2440f47eb9aa13f98532e581d27471
Gyasi Story <gyasi.story@cru.org>
2022-08-31 13:42:49 -0400

    Contact Details Header  Fix

--- src/components/Contacts/ContactDetails/ContactDetailsHeader/ContactDetailsHeader.stories.tsx
+++ src/components/Contacts/ContactDetails/ContactDetailsHeader/ContactDetailsHeader.stories.tsx
@@ -2,54 +2,65 @@ import { MockedProvider } from '@apollo/client/testing';
 import React, { ReactElement } from 'react';
 
 import { GqlMockedProvider } from '../../../../../__tests__/util/graphqlMocking';
+import { ContactDetailProvider } from '../ContactDetailContext';
 
 import { ContactDetailsHeader } from './ContactDetailsHeader';
 import {
   GetContactDetailsHeaderDocument,
   GetContactDetailsHeaderQuery,
 } from './ContactDetailsHeader.generated';
+import { ContactsPageProvider } from 'pages/accountLists/[accountListId]/contacts/ContactsPageContext';
+
+const accountListId = 'accountList-1';
+const contactId = 'contact-1';
 
 export default {
   title: 'Contacts/ContactDetails/Header',
+  component: ContactDetailsHeader,
 };
-
-const accountListId = 'abc';
-const contactId = 'contact-1';
 
 export const Default = (): ReactElement => {
   return (
-    <GqlMockedProvider<GetContactDetailsHeaderQuery>>
-      <ContactDetailsHeader
-        accountListId={accountListId}
-        contactId={contactId}
-        onClose={() => {}}
-      />
-    </GqlMockedProvider>
+    <ContactsPageProvider>
+      <ContactDetailProvider>
+        <GqlMockedProvider<GetContactDetailsHeaderQuery>>
+          <ContactDetailsHeader
+            accountListId={accountListId}
+            contactId={contactId}
+            onClose={() => {}}
+          />
+        </GqlMockedProvider>
+      </ContactDetailProvider>
+    </ContactsPageProvider>
   );
 };
 
 export const Loading = (): ReactElement => {
   return (
-    <MockedProvider
-      mocks={[
-        {
-          request: {
-            query: GetContactDetailsHeaderDocument,
-            variables: {
-              accountListId,
-              contactId,
+    <ContactsPageProvider>
+      <ContactDetailProvider>
+        <MockedProvider
+          mocks={[
+            {
+              request: {
+                query: GetContactDetailsHeaderDocument,
+                variables: {
+                  accountListId: accountListId,
+                  contactId: contactId,
+                },
+              },
+              result: {},
+              delay: 100931731455,
             },
-          },
-          result: {},
-          delay: 100931731455,
-        },
-      ]}
-    >
-      <ContactDetailsHeader
-        accountListId={accountListId}
-        contactId={contactId}
-        onClose={() => {}}
-      />
-    </MockedProvider>
+          ]}
+        >
+          <ContactDetailsHeader
+            accountListId={accountListId}
+            contactId={contactId}
+            onClose={() => {}}
+          />
+        </MockedProvider>
+      </ContactDetailProvider>
+    </ContactsPageProvider>
   );
 };
