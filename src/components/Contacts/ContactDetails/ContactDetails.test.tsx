bf53857b95b397cb79fce23b81e16ebef375f672
Gyasi Story <gyasi.story@cru.org>
2022-09-02 15:21:34 -0400

    Remove unneeded test

--- src/components/Contacts/ContactDetails/ContactDetails.test.tsx
+++ src/components/Contacts/ContactDetails/ContactDetails.test.tsx
@@ -3,14 +3,9 @@ import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { SnackbarProvider } from 'notistack';
 import React from 'react';
-import {
-  PledgeFrequencyEnum,
-  StatusEnum,
-} from '../../../../graphql/types.generated';
 import { GqlMockedProvider } from '../../../../__tests__/util/graphqlMocking';
 import theme from '../../../theme';
 import { ContactDetails } from './ContactDetails';
-import { GetContactDetailsHeaderQuery } from './ContactDetailsHeader/ContactDetailsHeader.generated';
 import { ContactDetailProvider } from './ContactDetailContext';
 import TestRouter from '__tests__/util/TestRouter';
 import { ContactsPageProvider } from 'pages/accountLists/[accountListId]/contacts/ContactsPageContext';
@@ -23,70 +18,6 @@ const router = {
 };
 
 describe('ContactDetails', () => {
-  it('should show loading state', async () => {
-    const { getByTestId } = render(
-      <SnackbarProvider>
-        <TestRouter router={router}>
-          <GqlMockedProvider<GetContactDetailsHeaderQuery>
-            mocks={{
-              contact: {
-                id: contactId,
-                status: StatusEnum.PartnerFinancial,
-                pledgeCurrency: 'USD',
-                pledgeAmount: 500,
-                pledgeFrequency: PledgeFrequencyEnum.Monthly,
-                pledgeReceived: true,
-                lastDonation: null,
-              },
-            }}
-          >
-            <MuiThemeProvider theme={theme}>
-              <ContactsPageProvider>
-                <ContactDetailProvider>
-                  <ContactDetails onClose={() => {}} />
-                </ContactDetailProvider>
-              </ContactsPageProvider>
-            </MuiThemeProvider>
-          </GqlMockedProvider>
-        </TestRouter>
-      </SnackbarProvider>,
-    );
-
-    expect(getByTestId('Skeleton')).toBeVisible();
-  });
-
-  it('should render with contact details', async () => {
-    const { findByText, queryByTestId } = render(
-      <SnackbarProvider>
-        <TestRouter router={router}>
-          <GqlMockedProvider<GetContactDetailsHeaderQuery>
-            mocks={{
-              GetContactDetailsHeader: {
-                contact: {
-                  pledgeCurrency: 'USD',
-                  lastDonation: null,
-                  primaryPerson: { firstName: 'Fname', lastName: 'Lname' },
-                },
-              },
-            }}
-          >
-            <MuiThemeProvider theme={theme}>
-              <ContactsPageProvider>
-                <ContactDetailProvider>
-                  <ContactDetails onClose={() => {}} />
-                </ContactDetailProvider>
-              </ContactsPageProvider>
-            </MuiThemeProvider>
-          </GqlMockedProvider>
-        </TestRouter>
-      </SnackbarProvider>,
-    );
-
-    expect(await findByText('Fname Lname')).toBeVisible();
-
-    expect(queryByTestId('Skeleton')).toBeNull();
-  });
-
   it('should change tab', async () => {
     const { getAllByRole } = render(
       <SnackbarProvider>
