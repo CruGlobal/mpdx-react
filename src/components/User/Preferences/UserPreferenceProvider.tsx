411bc0e60da4946ff528c23433534f27fa17367d
fevega1 <7077599+fevega1@users.noreply.github.com>
2022-09-08 17:03:00 -0400

    Fix "cannot update a component while rendering a different component"

--- src/components/User/Preferences/UserPreferenceProvider.tsx
+++ src/components/User/Preferences/UserPreferenceProvider.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, ReactNode, useContext } from 'react';
+import React, { createContext, ReactNode, useContext, useEffect } from 'react';
 import { useSession } from 'next-auth/react';
 import { useGetUserQuery } from '../GetUser.generated';
 import i18next from 'src/lib/i18n';
@@ -16,9 +16,11 @@ export const UserPreferenceProvider: React.FC<ReactNode> = ({ children }) => {
   const { data: session } = useSession();
   const { data: user, loading } = useGetUserQuery({ skip: !session });
 
-  if (!loading) {
-    i18next.changeLanguage(user?.user.preferences?.locale ?? 'en');
-  }
+  useEffect(() => {
+    if (!loading) {
+      i18next.changeLanguage(user?.user.preferences?.locale ?? 'en');
+    }
+  });
 
   return (
     <UserPreferenceContext.Provider value={{}}>
