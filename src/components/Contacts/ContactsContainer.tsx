cf8b5d18732249088b431c325539499f65ae88e4
John Plastow II <j2trumpet@users.noreply.github.com>
2022-10-19 10:48:35 -0700

    Reset ContactsContainer to state on 7/7/22

--- src/components/Contacts/ContactsContainer.tsx
+++ src/components/Contacts/ContactsContainer.tsx
@@ -5,7 +5,7 @@ import { useTranslation } from 'react-i18next';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import _ from 'lodash';
-import clsx from 'clsx';
+import { SidePanelsLayout } from '../Layouts/SidePanelsLayout';
 import {
   ContactsPageContext,
   ContactsPageType,
@@ -19,54 +19,6 @@ import { ContactFlowDragLayer } from './ContactFlow/ContactFlowDragLayer/Contact
 
 const WhiteBackground = styled(Box)(({ theme }) => ({
   backgroundColor: theme.palette.common.white,
-}));
-
-const OuterWrapper = styled(Box)(({ theme }) => ({
-  position: 'relative',
-  overflowX: 'hidden',
-  '& > div': {
-    height: '100%',
-  },
-  '& .left, & .right': {
-    position: 'absolute',
-    top: 0,
-    transition: 'left ease-in-out 225ms',
-    backgroundColor: theme.palette.common.white,
-    borderColor: '#EBECEC',
-    borderStyle: 'solid',
-  },
-  '&.leftOpen .left, &.rightOpen .right': {
-    left: 0,
-  },
-  '&.rightOpen .right': {
-    [theme.breakpoints.up('md')]: {
-      left: 'calc(100% - 900px)',
-    },
-  },
-}));
-
-const LeftWrapper = styled(Box)(() => ({
-  zIndex: 20,
-  left: '-290px',
-  width: '290px',
-  borderWidth: '0 1px 0 0',
-  overflowY: 'auto',
-}));
-
-const CenterWrapper = styled(Box)(() => ({
-  zIndex: 10,
-  position: 'relative',
-}));
-
-const RightWrapper = styled(Box)(({ theme }) => ({
-  zIndex: 30,
-  width: '100%',
-  left: '100%',
-  borderWidth: 0,
-  [theme.breakpoints.up('md')]: {
-    width: '900px',
-    borderWidth: '0 0 0 1px',
-  },
 }));
 
 export const ContactsContainer: React.FC = ({}) => {
@@ -95,19 +47,12 @@ export const ContactsContainer: React.FC = ({}) => {
         <DndProvider backend={HTML5Backend}>
           <ContactFlowDragLayer />
           <WhiteBackground>
-            <OuterWrapper
-              className={clsx(
-                filterPanelOpen && 'leftOpen',
-                contactDetailsOpen && 'rightOpen',
-              )}
-            >
-              <LeftWrapper className="left">
-                <ContactsLeftPanel />
-              </LeftWrapper>
-              <CenterWrapper className="center">
-                <ContactsMainPanel />
-              </CenterWrapper>
-              <RightWrapper className="right">
+            <SidePanelsLayout
+              leftPanel={<ContactsLeftPanel />}
+              leftOpen={filterPanelOpen}
+              leftWidth="290px"
+              mainContent={<ContactsMainPanel />}
+              rightPanel={
                 <ContactsRightPanel
                   onClose={() =>
                     setContactFocus(
@@ -118,8 +63,10 @@ export const ContactsContainer: React.FC = ({}) => {
                     )
                   }
                 />
-              </RightWrapper>
-            </OuterWrapper>
+              }
+              rightOpen={contactDetailsOpen}
+              rightWidth="60%"
+            />
           </WhiteBackground>
         </DndProvider>
       ) : (
