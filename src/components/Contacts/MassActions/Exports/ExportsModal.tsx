904e4aba95f7a6d6eb9fdf65a2c3283b12607a80
Chris Simbulan <simbulan.c@gmail.com>
2022-10-20 15:00:17 -0400

    add functionality for exporting csv and xlsx

--- src/components/Contacts/MassActions/Exports/ExportsModal.tsx
+++ src/components/Contacts/MassActions/Exports/ExportsModal.tsx
@@ -1,12 +1,17 @@
 import { Button, Grid, Typography, Box, styled } from '@material-ui/core';
-import React from 'react';
+import { useSession } from 'next-auth/react';
+import { useSnackbar } from 'notistack';
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { exportRest } from './exportRest';
+import { MailMergedLabelModal } from './MailMergedLabelModal/MailMergedLabelModal';
 import Modal from 'src/components/common/Modal/Modal';
 
 interface ExportsModalProps {
   ids: string[];
   accountListId: string;
   handleClose: () => void;
+  openMailMergedLabelModal: () => void;
 }
 
 const ExportActionButton = styled(Button)(({ theme }) => ({
@@ -19,14 +24,39 @@ const ExportActionButton = styled(Button)(({ theme }) => ({
   },
 }));
 
-export const ExportsModal: React.FC<ExportsModalProps> = ({ handleClose }) => {
+export const ExportsModal: React.FC<ExportsModalProps> = ({
+  accountListId,
+  ids,
+  handleClose,
+  openMailMergedLabelModal,
+}) => {
   const { t } = useTranslation();
+  const { data: sessionData } = useSession();
+  const token = sessionData?.user?.apiToken ?? '';
+  const { enqueueSnackbar } = useSnackbar();
+
+  const restHandler = async (fileType: 'csv' | 'xlsx', mailing = false) => {
+    try {
+      exportRest(accountListId, ids, token, fileType, mailing);
+    } catch (err) {
+      enqueueSnackbar(JSON.stringify(err), {
+        variant: 'error',
+      });
+    }
+    handleClose();
+  };
+
   return (
     <Modal title={t('Export Contacts')} isOpen={true} handleClose={handleClose}>
       <Grid container>
         <Grid item xs={12} md={6}>
           <Box display="flex" flexDirection="column" alignItems="center" p={2}>
-            <ExportActionButton>
+            <ExportActionButton
+              onClick={() => {
+                openMailMergedLabelModal();
+                handleClose();
+              }}
+            >
               {t('PDF of Mail Merged Labels')}
             </ExportActionButton>
             <Typography>
@@ -38,7 +68,9 @@ export const ExportsModal: React.FC<ExportsModalProps> = ({ handleClose }) => {
         </Grid>
         <Grid item xs={12} md={6}>
           <Box display="flex" flexDirection="column" alignItems="center" p={2}>
-            <ExportActionButton>{t('CSV for Mail Merge')}</ExportActionButton>
+            <ExportActionButton onClick={() => restHandler('csv', true)}>
+              {t('CSV for Mail Merge')}
+            </ExportActionButton>
             <Typography>
               {t(
                 'Best for making mailing labels. Addresses will be formatted based on country.',
@@ -48,7 +80,9 @@ export const ExportsModal: React.FC<ExportsModalProps> = ({ handleClose }) => {
         </Grid>
         <Grid item xs={12} md={6}>
           <Box display="flex" flexDirection="column" alignItems="center" p={2}>
-            <ExportActionButton>{t('Advanced CSV')}</ExportActionButton>
+            <ExportActionButton onClick={() => restHandler('csv')}>
+              {t('Advanced CSV')}
+            </ExportActionButton>
             <Typography>
               {t(
                 'All of the information for your contacts, best for advanced sorting/filtering and importing into other software.',
@@ -58,7 +92,9 @@ export const ExportsModal: React.FC<ExportsModalProps> = ({ handleClose }) => {
         </Grid>
         <Grid item xs={12} md={6}>
           <Box display="flex" flexDirection="column" alignItems="center" p={2}>
-            <ExportActionButton>{t('Advanced Excel (XLSX')}</ExportActionButton>
+            <ExportActionButton onClick={() => restHandler('xlsx')}>
+              {t('Advanced Excel (XLSX)')}
+            </ExportActionButton>
             <Typography>
               {t(
                 `All of the information for your contacts in Excel's default XLSX format.`,
