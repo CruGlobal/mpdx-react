8caa4112212e21cdbd1c74288ebeba370e16024b
John Plastow II <j2trumpet@users.noreply.github.com>
2022-10-20 08:51:13 -0700

    Adding autoFocus prop

--- src/components/Layouts/Primary/TopBar/Items/SearchMenu/SearchMenu.tsx
+++ src/components/Layouts/Primary/TopBar/Items/SearchMenu/SearchMenu.tsx
@@ -366,6 +366,8 @@ const SearchMenu = (): ReactElement => {
                   setWildcardSearch(e.target.value);
                   handleUpdateWildcardSearch(e.target.value);
                 }}
+                // eslint-disable-next-line
+                autoFocus
               />
             )}
           />
