ebc9835f8e244cbf231e6a4e170c460f2ae44b9e
Gyasi Story <gyasi.story@cru.org>
2022-10-27 15:36:52 -0400

    Add Missing Zero

--- src/components/Task/Modal/Form/LogForm/TaskModalLogForm.test.tsx
+++ src/components/Task/Modal/Form/LogForm/TaskModalLogForm.test.tsx
@@ -144,7 +144,7 @@ describe('TaskModalLogForm', () => {
     );
     expect(
       getAllByRole('textbox').find(
-        (item) => (item as HTMLInputElement).value === 'Jan 5, 2016',
+        (item) => (item as HTMLInputElement).value === 'Jan 05, 2016',
       ),
     ).toBeInTheDocument();
     userEvent.click(getByLabelText('Action'));
