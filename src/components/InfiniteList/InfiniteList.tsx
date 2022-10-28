9f51431cbc7cee79c865f4fb39f6e25fb96b7266
Chris Simbulan <simbulan.c@gmail.com>
2022-10-28 06:46:20 -0400

    make groups collapsible

--- src/components/InfiniteList/InfiniteList.tsx
+++ src/components/InfiniteList/InfiniteList.tsx
@@ -1,5 +1,5 @@
-import React, { ReactElement } from 'react';
-import { List, ListItem, Typography } from '@mui/material';
+import React, { ReactElement, useEffect, useState } from 'react';
+import { Box, List, ListItem, Typography } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import theme from 'src/theme';
 import {
@@ -10,6 +10,8 @@ import {
   ItemContent,
 } from 'react-virtuoso';
 import Skeleton from '@mui/material/Skeleton';
+import ExpandMore from '@mui/icons-material/ExpandMore';
+import ExpandLess from '@mui/icons-material/ExpandLess';
 
 const height = 72;
 const padding = 0;
@@ -59,9 +61,13 @@ const Loading: React.FC = () => (
   </div>
 );
 
-const GroupLabel = styled(Typography)(({ theme }) => ({
+const GroupWrapper = styled(Box)(() => ({
+  cursor: 'pointer',
   borderBottom: `1px solid ${theme.palette.grey[200]}`,
   background: theme.palette.common.white,
+}));
+
+const GroupLabel = styled(Typography)(({ theme }) => ({
   marginTop: theme.spacing(2),
   padding: theme.spacing(0, 2),
 }));
@@ -86,30 +92,52 @@ export const InfiniteList = <T, C>({
   ...props
 }: Omit<GroupedVirtuosoProps<T, C>, 'groupCounts' | 'itemContent'> &
   InfiniteListProps<T, C>): ReactElement => {
-  const groups =
-    data?.length > 0
+  const [groups, setGroups] = useState({});
+
+  useEffect(()=>{
+    setGroups(data?.length > 0
       ? groupBy
-        ? data.reduce<{ [groupLabel: string]: number }>((acc, item) => {
+        ? data.reduce<{ [groupLabel: string]: {
+          value: number,
+          collapse: boolean
+        } }>((acc, item) => {
             const label = groupBy(item);
             return {
               ...acc,
-              [label]: acc[label] ? acc[label] + 1 : 1,
+              [label]: {
+                value: acc[label]?.value ? acc[label].value + 1 : 1,
+                collapse: false
+              },
             };
           }, {})
         : { '': totalCount }
-      : {};
+      : {})
+  },[data])
+
+  const collapseGroup = (groupLabel:string) => {
+    setGroups({...groups, [groupLabel]: {...groups[groupLabel], collapse: !groups[groupLabel].collapse}})
+  }
+
+  const getItemVisibility = (groupIndex: number) => {
+    const key = Object.keys(groups)[groupIndex];
+    return !groups[key].collapse;
+  }
 
   return (
     <GroupedVirtuoso
-      groupCounts={Object.values(groups)}
+      groupCounts={Object.values(groups).map((group) => group.value)}
       {...props}
-      itemContent={(index) =>
-        data[index] && props.itemContent(index, data[index], context)
+      itemContent={(index, groupIndex) => {
+        return getItemVisibility(groupIndex) ? data[index] && props.itemContent(index, data[index], context) : null
+      }
       }
       groupContent={(index) => {
         const groupLabel = Object.keys(groups)[index];
         return groupLabel ? (
-          <GroupLabel variant="h5">{groupLabel}</GroupLabel>
+          <GroupWrapper display="flex" alignItems="center" width="100%" justifyContent="space-between" onClick={()=>collapseGroup(groupLabel)}>
+            <GroupLabel variant="h5" >{groupLabel}</GroupLabel>
+            {groups[groupLabel].collapse ? <ExpandMore /> : <ExpandLess />}
+          </GroupWrapper>
         ) : null;
       }}
       components={{
