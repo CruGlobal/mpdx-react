dc32e2386ebad95d22fe271aace5aaa5d77d2ac9
John Plastow II <j2trumpet@users.noreply.github.com>
2022-10-28 14:46:32 -0700

    Removed optional second argument

--- src/components/Welcome/Welcome.test.tsx
+++ src/components/Welcome/Welcome.test.tsx
@@ -15,10 +15,7 @@ describe('Welcome', () => {
     );
     expect(getByTestId('welcomeTitle')).toHaveTextContent('test title');
     expect(getByTestId('welcomeSubtitle')).toHaveTextContent('test subtitle');
-    expect(getByTestId('welcomeImg')).toHaveAttribute(
-      'src',
-      'drawkit-grape-pack-illustration-2.svg',
-    );
+    expect(getByTestId('welcomeImg')).toHaveAttribute('src');
     expect(getByTestId('children')).toHaveTextContent('children');
   });
 
@@ -36,9 +33,6 @@ describe('Welcome', () => {
     expect(() => getByTestId('welcomeSubtitle')).toThrow();
     expect(getByTestId('testTitle')).toHaveTextContent('test title');
     expect(getByTestId('testSubtitle')).toHaveTextContent('test subtitle');
-    expect(getByTestId('welcomeImg')).toHaveAttribute(
-      'src',
-      'drawkit-grape-pack-illustration-1.svg',
-    );
+    expect(getByTestId('welcomeImg')).toHaveAttribute('src');
   });
 });
