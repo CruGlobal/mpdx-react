02dc5e01656116066f20023783a9b5394bebbd89
Chris Simbulan <simbulan.c@gmail.com>
2022-11-01 16:57:47 -0400

    change correct bg color and remove providers

--- src/components/Shared/Header/ListHeader.test.tsx
+++ src/components/Shared/Header/ListHeader.test.tsx
@@ -11,13 +11,7 @@ import {
   ListHeaderCheckBoxState,
   TableViewModeEnum,
 } from './ListHeader';
-import {
-  ContactsPageContext,
-  ContactsPageProvider,
-  ContactsPageType,
-} from 'pages/accountLists/[accountListId]/contacts/ContactsPageContext';
 import TestRouter from '__tests__/util/TestRouter';
-import { GqlMockedProvider } from '__tests__/util/graphqlMocking';
 
 const toggleFilterPanel = jest.fn();
 const onSearchTermChanged = jest.fn();
@@ -55,18 +49,19 @@ const router = {
 };
 
 const ButtonGroup: React.FC = () => {
-  const { handleViewModeChange } = React.useContext(
-    ContactsPageContext,
-  ) as ContactsPageType;
   return (
     <>
       <Button
         data-testid="list-button"
-        onClick={(event) => handleViewModeChange(event, TableViewModeEnum.List)}
+        onClick={() =>
+          push({ pathname: '/accountLists/123/contacts/', query: {} })
+        }
       />
       <Button
         data-testid="map-button"
-        onClick={(event) => handleViewModeChange(event, TableViewModeEnum.Map)}
+        onClick={() =>
+          push({ pathname: '/accountLists/123/contacts/map', query: {} })
+        }
       />
     </>
   );
@@ -137,32 +132,28 @@ describe('ListHeader', () => {
       expect(queryByTestId('star-filter-button')).toBeInTheDocument();
     });
 
-    it.skip('renders a button group and switches views', async () => {
+    it('renders a button group and switches views', async () => {
       const { getByTestId } = render(
-        <ThemeProvider theme={theme}>
-          <TestRouter router={router}>
-            <GqlMockedProvider>
-              <ContactsPageProvider>
-                <ListHeader
-                  selectedIds={selectedIds}
-                  page="contact"
-                  activeFilters={false}
-                  starredFilter={{}}
-                  contactsView={TableViewModeEnum.List}
-                  toggleStarredFilter={toggleStarredFilter}
-                  headerCheckboxState={ListHeaderCheckBoxState.unchecked}
-                  filterPanelOpen={true}
-                  contactDetailsOpen={true}
-                  toggleFilterPanel={toggleFilterPanel}
-                  onCheckAllItems={onCheckAllItems}
-                  onSearchTermChanged={onSearchTermChanged}
-                  openEditFieldsModal={openEditFieldsModal}
-                  buttonGroup={<ButtonGroup />}
-                />
-              </ContactsPageProvider>
-            </GqlMockedProvider>
-          </TestRouter>
-        </ThemeProvider>,
+        <TestRouter router={router}>
+          <ThemeProvider theme={theme}>
+            <ListHeader
+              selectedIds={selectedIds}
+              page="contact"
+              activeFilters={false}
+              starredFilter={{}}
+              contactsView={TableViewModeEnum.List}
+              toggleStarredFilter={toggleStarredFilter}
+              headerCheckboxState={ListHeaderCheckBoxState.unchecked}
+              filterPanelOpen={true}
+              contactDetailsOpen={true}
+              toggleFilterPanel={toggleFilterPanel}
+              onCheckAllItems={onCheckAllItems}
+              onSearchTermChanged={onSearchTermChanged}
+              openEditFieldsModal={openEditFieldsModal}
+              buttonGroup={<ButtonGroup />}
+            />
+          </ThemeProvider>
+        </TestRouter>,
       );
 
       expect(getByTestId('list-button')).toBeInTheDocument();
@@ -171,6 +162,15 @@ describe('ListHeader', () => {
       await waitFor(() =>
         expect(router.push).toHaveBeenCalledWith({
           pathname: '/accountLists/123/contacts/',
+          query: {},
+        }),
+      );
+
+      userEvent.click(getByTestId('map-button'));
+
+      await waitFor(() =>
+        expect(router.push).toHaveBeenCalledWith({
+          pathname: '/accountLists/123/contacts/map',
           query: {},
         }),
       );
@@ -596,7 +596,7 @@ describe('ListHeader', () => {
     expect(onSearchTermChanged).not.toHaveBeenCalled();
   });
 
-  it.skip('filters button displays for open filter panel', async () => {
+  it('filters button displays for open filter panel', async () => {
     const { getByRole } = render(
       <ThemeProvider theme={theme}>
         <ListHeader
@@ -626,13 +626,13 @@ describe('ListHeader', () => {
     });
 
     expect(filterButton).toHaveStyle({
-      backgroundColor: theme.palette.secondary.dark,
+      backgroundColor: 'transparent',
     });
     expect(toggleFilterPanel).not.toHaveBeenCalled();
     expect(onSearchTermChanged).not.toHaveBeenCalled();
   });
 
-  it.skip('filters button displays for active filters', async () => {
+  it('filters button displays for active filters', async () => {
     const { getByRole } = render(
       <ThemeProvider theme={theme}>
         <ListHeader
@@ -668,7 +668,7 @@ describe('ListHeader', () => {
     expect(onSearchTermChanged).not.toHaveBeenCalled();
   });
 
-  it.skip('filters button displays for active filters and filter panel open', async () => {
+  it('filters button displays for active filters and filter panel open', async () => {
     const { getByRole } = render(
       <ThemeProvider theme={theme}>
         <ListHeader
