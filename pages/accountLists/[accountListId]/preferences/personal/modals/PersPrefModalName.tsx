11b84126aaaf771b9c96cb107e5c55addbc3ab60
John Plastow II <j2trumpet@users.noreply.github.com>
2022-11-11 14:15:00 -0800

    Using new PersPrefInput for name fields

--- pages/accountLists/[accountListId]/preferences/personal/modals/PersPrefModalName.tsx
+++ pages/accountLists/[accountListId]/preferences/personal/modals/PersPrefModalName.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { OutlinedInput, Unstable_Grid2 as Grid } from '@mui/material';
-import { PersPrefFieldWrapper } from '../shared/PersPrefForms';
+import { Unstable_Grid2 as Grid } from '@mui/material';
+import { PersPrefInput } from '../forms/PersPrefInput';
 import { info } from '../DemoContent';
 
 export const PersPrefModalName: React.FC = () => {
@@ -11,30 +11,26 @@ export const PersPrefModalName: React.FC = () => {
     <Grid container spacing={2} marginBottom={2}>
       {/* Title */}
       <Grid xs={12} sm={2}>
-        <PersPrefFieldWrapper labelText={t('Title')}>
-          <OutlinedInput value={info.title} />
-        </PersPrefFieldWrapper>
+        <PersPrefInput label={t('Title')} value={info.title} />
       </Grid>
 
       {/* First name */}
       <Grid xs={12} sm={4}>
-        <PersPrefFieldWrapper labelText={t('First Name')} required>
-          <OutlinedInput value={info.first_name} />
-        </PersPrefFieldWrapper>
+        <PersPrefInput
+          label={t('First Name')}
+          value={info.first_name}
+          required
+        />
       </Grid>
 
       {/* Last name */}
       <Grid xs={12} sm={4}>
-        <PersPrefFieldWrapper labelText={t('Last Name')} required>
-          <OutlinedInput value={info.last_name} />
-        </PersPrefFieldWrapper>
+        <PersPrefInput label={t('Last Name')} value={info.last_name} required />
       </Grid>
 
       {/* Suffix  */}
       <Grid xs={12} sm={2}>
-        <PersPrefFieldWrapper labelText={t('Suffix')}>
-          <OutlinedInput value={info.suffix} />
-        </PersPrefFieldWrapper>
+        <PersPrefInput label={t('Suffix')} value={info.suffix} />
       </Grid>
     </Grid>
   );
