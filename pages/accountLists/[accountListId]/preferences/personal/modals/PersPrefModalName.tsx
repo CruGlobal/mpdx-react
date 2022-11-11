4335382ba1dbcf16fd59e2a2f11c9aec923cedb5
John Plastow II <j2trumpet@users.noreply.github.com>
2022-11-11 10:21:06 -0800

    Getting rid of unnecessary styled components

--- pages/accountLists/[accountListId]/preferences/personal/modals/PersPrefModalName.tsx
+++ pages/accountLists/[accountListId]/preferences/personal/modals/PersPrefModalName.tsx
@@ -1,54 +1,41 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { OutlinedInput, Unstable_Grid2 as Grid } from '@mui/material';
-import { styled } from '@mui/material/styles';
 import { PersPrefFieldWrapper } from '../shared/PersPrefForms';
 import { info } from '../DemoContent';
-
-const StyledGridItem = styled(Grid)(({ theme }) => ({
-  [theme.breakpoints.down('xs')]: {
-    '&:not(:last-child) .MuiFormControl-root': {
-      marginBottom: 0,
-    },
-  },
-}));
-
-const StyledGrid = styled(Grid)(({ theme }) => ({
-  marginBottom: theme.spacing(2),
-}));
 
 export const PersPrefModalName: React.FC = () => {
   const { t } = useTranslation();
 
   return (
-    <StyledGrid container spacing={2}>
+    <Grid container spacing={2} marginBottom={2}>
       {/* Title */}
-      <StyledGridItem xs={12} sm={2}>
+      <Grid xs={12} sm={2}>
         <PersPrefFieldWrapper labelText={t('Title')}>
           <OutlinedInput value={info.title} />
         </PersPrefFieldWrapper>
-      </StyledGridItem>
+      </Grid>
 
       {/* First name */}
-      <StyledGridItem xs={12} sm={4}>
+      <Grid xs={12} sm={4}>
         <PersPrefFieldWrapper labelText={t('First Name')} required>
           <OutlinedInput value={info.first_name} />
         </PersPrefFieldWrapper>
-      </StyledGridItem>
+      </Grid>
 
       {/* Last name */}
-      <StyledGridItem xs={12} sm={4}>
+      <Grid xs={12} sm={4}>
         <PersPrefFieldWrapper labelText={t('Last Name')} required>
           <OutlinedInput value={info.last_name} />
         </PersPrefFieldWrapper>
-      </StyledGridItem>
+      </Grid>
 
       {/* Suffix  */}
-      <StyledGridItem xs={12} sm={2}>
+      <Grid xs={12} sm={2}>
         <PersPrefFieldWrapper labelText={t('Suffix')}>
           <OutlinedInput value={info.suffix} />
         </PersPrefFieldWrapper>
-      </StyledGridItem>
-    </StyledGrid>
+      </Grid>
+    </Grid>
   );
 };
