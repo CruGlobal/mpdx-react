bded132abb6e5775e73d921d79d443254a90ce29
Caleb Cox <canac@users.noreply.github.com>
2023-01-06 11:29:38 -0600

    Add currency column show/hide tests

--- src/components/Reports/DonationsReport/Table/DonationsReportTable.test.tsx
+++ src/components/Reports/DonationsReport/Table/DonationsReportTable.test.tsx
@@ -3,6 +3,7 @@ import { render, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { ThemeProvider } from '@mui/material/styles';
 import { DateTime } from 'luxon';
+import { cloneDeep } from 'lodash';
 import theme from '../../../../theme';
 import { GetDonationsTableQuery } from '../GetDonationsTable.generated';
 import { GqlMockedProvider } from '../../../../../__tests__/util/graphqlMocking';
@@ -206,5 +207,46 @@ describe('DonationsReportTable', () => {
 
     userEvent.click(getByText('John'));
     expect(onSelectContact).not.toHaveBeenCalled();
+  });
+
+  it('hides currency column if all currencies match the account currency', async () => {
+    const { getByLabelText, queryByLabelText } = render(
+      <ThemeProvider theme={theme}>
+        <GqlMockedProvider<GetDonationsTableQuery> mocks={mocks}>
+          <DonationsReportTable
+            accountListId={'abc'}
+            onSelectContact={onSelectContact}
+            time={time}
+            setTime={setTime}
+          />
+        </GqlMockedProvider>
+      </ThemeProvider>,
+    );
+
+    await waitFor(() => expect(getByLabelText('Partner')).toBeInTheDocument());
+    expect(queryByLabelText('Foreign Amount')).not.toBeInTheDocument();
+  });
+
+  it('shows currency column if a currency does not match the account currency', async () => {
+    const mocksWithMultipleCurrencies = cloneDeep(mocks);
+    mocksWithMultipleCurrencies.GetDonationsTable.donations.nodes[0].amount.currency =
+      'EUR';
+    const { getByLabelText } = render(
+      <ThemeProvider theme={theme}>
+        <GqlMockedProvider<GetDonationsTableQuery>
+          mocks={mocksWithMultipleCurrencies}
+        >
+          <DonationsReportTable
+            accountListId={'abc'}
+            onSelectContact={onSelectContact}
+            time={time}
+            setTime={setTime}
+          />
+        </GqlMockedProvider>
+      </ThemeProvider>,
+    );
+
+    await waitFor(() => expect(getByLabelText('Partner')).toBeInTheDocument());
+    expect(getByLabelText('Foreign Amount')).toBeInTheDocument();
   });
 });
