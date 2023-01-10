5882d955b73f4281c27e989a2fe6491ca5bf67ba
Caleb Cox <canac@users.noreply.github.com>
2023-01-10 16:28:43 -0600

    Revert config change

--- apollo.config.js
+++ apollo.config.js
@@ -2,7 +2,7 @@ module.exports = {
   client: {
     service: {
       name: 'MPDX',
-      url: 'http://localhost:3000/api/graphql',
+      url: 'https://api.stage.mpdx.org/graphql',
     },
     // Added generated files to avoid duplicate operation name errors https://www.apollographql.com/docs/devtools/apollo-config/#clientexcludes
     excludes: ['**/node_modules', '**/__tests__', '**/*.generated.ts'],
