754989ee94d85c97360f2486ca0f6b1554619b7b
Daniel Bisgrove <daniel@bizz-websites.com>
2023-01-13 09:54:00 -0500

    Added test for App settings provider

---
+++ src/components/common/AppSettings/AppSettingsProvider.test.tsx
@@ -0,0 +1,23 @@
+import { useContext } from 'react';
+import { render } from '@testing-library/react';
+import { AppSettingsContext, AppSettingsProvider } from './AppSettingsProvider';
+
+const TestingComponent = () => {
+  const { appName } = useContext(AppSettingsContext);
+  return (
+    <>
+      <p data-testid="app-name">{appName}</p>
+    </>
+  );
+};
+
+describe('<AuthProvider />', () => {
+  test('provides expected AuthContext obj to child elements', () => {
+    const { getByTestId } = render(
+      <AppSettingsProvider>
+        <TestingComponent />
+      </AppSettingsProvider>,
+    );
+    expect(getByTestId('app-name').textContent).toEqual(process.env.APP_NAME);
+  });
+});
