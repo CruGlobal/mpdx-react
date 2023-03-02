a435ef3e560608fa04de394ab772f9027d141d8c
Daniel Bisgrove <daniel@bizz-websites.com>
2023-03-02 16:57:27 -0500

    wrapping getUser hook with getSession hook to prevent auth_error on graphQl

--- src/components/User/useUser.tsx
+++ src/components/User/useUser.tsx
@@ -1,10 +1,21 @@
 import { GetUserQuery, useGetUserQuery } from './GetUser.generated';
+import { getSession } from 'next-auth/react';
+import { useState, useEffect } from 'react';
+import { Session } from 'next-auth';
 
 // Use this call to grab user data
 export const useUser = (): GetUserQuery['user'] | undefined => {
+  const [session, setSession] = useState<Session | null>(null);
+
+  useEffect(() => {
+    getSession().then((session) => {
+      setSession(session);
+    });
+  }, []);
+
   const { data } = useGetUserQuery({
     fetchPolicy: 'cache-first',
+    skip: !session,
   });
-
   return data?.user;
 };
