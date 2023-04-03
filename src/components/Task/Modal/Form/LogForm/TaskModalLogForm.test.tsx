fac44640bd651ef5c3ea8270f934806322a0241d
Caleb Cox <canac@users.noreply.github.com>
2023-04-03 10:08:03 -0500

    Fix tests

--- src/components/Task/Modal/Form/LogForm/TaskModalLogForm.test.tsx
+++ src/components/Task/Modal/Form/LogForm/TaskModalLogForm.test.tsx
@@ -190,7 +190,7 @@ describe('TaskModalLogForm', () => {
     );
     expect(
       getAllByRole('textbox').find(
-        (item) => (item as HTMLInputElement).value === '1/05/2016',
+        (item) => (item as HTMLInputElement).value === '1/5/2016',
       ),
     ).toBeInTheDocument();
     userEvent.click(getByLabelText('Action'));
