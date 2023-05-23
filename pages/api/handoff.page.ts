46d7c62192c59d27e2e168c02bd6077d3fd3c286
Daniel Bisgrove <daniel@bizz-websites.com>
2023-05-23 11:32:44 -0400

    Adding import

--- pages/api/handoff.page.ts
+++ pages/api/handoff.page.ts
@@ -6,6 +6,7 @@ import {
   GetDefaultAccountQueryVariables,
   GetDefaultAccountDocument,
 } from './getDefaultAccount.generated';
+import { reportError } from './utils/RollBar';
 
 if (!process.env.JWT_SECRET) {
   throw new Error('JWT_SECRET env var is not set');
