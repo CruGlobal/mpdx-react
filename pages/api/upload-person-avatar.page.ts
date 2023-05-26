3f27c613e1ee99639e747e49c60044bb1b73ab84
Caleb Cox <canac@users.noreply.github.com>
2023-05-26 11:30:06 -0500

    Add error handling to lambda

--- pages/api/upload-person-avatar.page.ts
+++ pages/api/upload-person-avatar.page.ts
@@ -30,62 +30,66 @@ const uploadPersonAvatar = async (
   req: NextApiRequest,
   res: NextApiResponse,
 ): Promise<void> => {
-  if (req.method !== 'POST') {
-    res.status(405).send('Method Not Found');
-    return;
-  }
+  try {
+    if (req.method !== 'POST') {
+      res.status(405).send('Method Not Found');
+      return;
+    }
 
-  const jwt = await getToken({
-    req,
-    secret: process.env.JWT_SECRET,
-  });
-  const apiToken = (jwt as { apiToken: string } | null)?.apiToken;
-  if (!apiToken) {
-    res.status(401).send('Unauthorized');
-    return;
-  }
+    const jwt = await getToken({
+      req,
+      secret: process.env.JWT_SECRET,
+    });
+    const apiToken = (jwt as { apiToken: string } | null)?.apiToken;
+    if (!apiToken) {
+      res.status(401).send('Unauthorized');
+      return;
+    }
 
-  const {
-    fields: { personId },
-    files: { avatar },
-  } = await parseBody(req);
-  if (typeof personId !== 'string') {
-    res.status(400).send('Missing personId');
-    return;
-  }
-  if (!avatar || Array.isArray(avatar)) {
-    res.status(400).send('Missing avatar');
-    return;
-  }
+    const {
+      fields: { personId },
+      files: { avatar },
+    } = await parseBody(req);
+    if (typeof personId !== 'string') {
+      res.status(400).send('Missing personId');
+      return;
+    }
+    if (!avatar || Array.isArray(avatar)) {
+      res.status(400).send('Missing avatar');
+      return;
+    }
 
-  const pictureId = uuidv4();
-  const file = new File(
-    [await readFile(avatar.filepath)],
-    avatar.originalFilename ?? 'avatar',
-  );
+    const pictureId = uuidv4();
+    const file = new File(
+      [await readFile(avatar.filepath)],
+      avatar.originalFilename ?? 'avatar',
+    );
 
-  const form = new FormData();
-  form.append('data[id]', personId);
-  form.append('data[type]', 'people');
-  form.append('data[attributes][overwrite]', 'true');
-  form.append('data[relationships][pictures][data][][id]', pictureId);
-  form.append('data[relationships][pictures][data][][type]', 'pictures');
-  form.append('included[][id]', pictureId);
-  form.append('included[][type]', 'pictures');
-  form.append('included[][attributes][image]', file);
-  form.append('included[][attributes][primary]', 'true');
+    const form = new FormData();
+    form.append('data[id]', personId);
+    form.append('data[type]', 'people');
+    form.append('data[attributes][overwrite]', 'true');
+    form.append('data[relationships][pictures][data][][id]', pictureId);
+    form.append('data[relationships][pictures][data][][type]', 'pictures');
+    form.append('included[][id]', pictureId);
+    form.append('included[][type]', 'pictures');
+    form.append('included[][attributes][image]', file);
+    form.append('included[][attributes][primary]', 'true');
 
-  const fetchRes = await fetch(
-    `${process.env.REST_API_URL}contacts/people/${personId}`,
-    {
-      method: 'PUT',
-      headers: {
-        authorization: `Bearer ${apiToken}`,
+    const fetchRes = await fetch(
+      `${process.env.REST_API_URL}contacts/people/${personId}`,
+      {
+        method: 'PUT',
+        headers: {
+          authorization: `Bearer ${apiToken}`,
+        },
+        body: form,
       },
-      body: form,
-    },
-  );
-  res.status(fetchRes.status).json({ success: fetchRes.status === 200 });
+    );
+    res.status(fetchRes.status).json({ success: fetchRes.status === 200 });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err });
+  }
 };
 
 export default uploadPersonAvatar;
