aa9671ab81e9ee50aeb0d087132f5f86f3c0866f
Bizz <56281168+dr-bizz@users.noreply.github.com>
2023-08-07 15:04:32 -0400

    Allow users to delete saved filters (#784)
    * Allow users to delete saved filters. Adding delete filter modal, which handles the deleting of the saved filter and written tests to cover all functionality.

--- src/components/Shared/Filters/FilterPanel.test.tsx
+++ src/components/Shared/Filters/FilterPanel.test.tsx
@@ -560,6 +560,43 @@ describe('FilterPanel', () => {
       expect(getByText('Filter')).toBeVisible();
     });
 
+    it('deletes saved filter', async () => {
+      const mutationSpy = jest.fn();
+
+      const { getByText, getByTestId, queryByTestId, queryAllByTestId } =
+        render(
+          <LocalizationProvider dateAdapter={AdapterLuxon}>
+            <ThemeProvider theme={theme}>
+              <GqlMockedProvider onCall={mutationSpy}>
+                <FilterPanel
+                  filters={[filterPanelDefaultMock, filterPanelFeaturedMock]}
+                  savedFilters={[savedFiltersMockThree]}
+                  selectedFilters={{}}
+                  onClose={onClose}
+                  onSelectedFiltersChanged={onSelectedFiltersChanged}
+                />
+              </GqlMockedProvider>
+            </ThemeProvider>
+          </LocalizationProvider>,
+        );
+
+      await waitFor(() => expect(queryByTestId('LoadingState')).toBeNull());
+
+      expect(queryAllByTestId('FilterGroup').length).toEqual(2);
+      expect(getByTestId('FilterListItemShowAll')).toBeVisible();
+      userEvent.click(getByText('Saved Filters'));
+      expect(getByText('My Cool Filter')).toBeVisible();
+
+      expect(getByTestId('deleteSavedFilter')).toBeVisible();
+      userEvent.click(getByTestId('deleteSavedFilter'));
+
+      await waitFor(() =>
+        expect(getByText('Delete Saved filter')).toBeVisible(),
+      );
+      userEvent.click(getByText('No'));
+      expect(getByText('Delete Saved filter')).not.toBeVisible();
+    });
+
     it('closes panel', async () => {
       const { queryByTestId, getByLabelText } = render(
         <GqlMockedProvider>
