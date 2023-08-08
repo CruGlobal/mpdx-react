79c25540ed6279ee824cbd58bf2ea18ad65ccaad
Daniel Bisgrove <daniel@bizz-websites.com>
2023-08-08 09:58:11 -0400

    Ensuring MPDX.org isn't hardcoded into handoff page. Also converted main code into an exportable function so I can use it on stop-impersonating page

--- pages/api/handoff.page.ts
+++ pages/api/handoff.page.ts
@@ -11,52 +11,73 @@ if (!process.env.JWT_SECRET) {
   throw new Error('JWT_SECRET env var is not set');
 }
 
+export const returnRedirectUrl = async (req: NextApiRequest) => {
+  try {
+    const jwtToken = (await getToken({
+      req,
+      secret: process.env.JWT_SECRET as string,
+    })) as { apiToken: string; userID: string } | null;
+
+    const path = req.query.path ?? '';
+
+    if (jwtToken && req.query.auth !== 'true') {
+      const client = await ssrClient(jwtToken.apiToken);
+      const response = await client.query<
+        GetDefaultAccountQuery,
+        GetDefaultAccountQueryVariables
+      >({
+        query: GetDefaultAccountDocument,
+      });
+      let defaultAccountID = req.query?.accountListId;
+      if (!defaultAccountID) {
+        const {
+          data: { user, accountLists },
+        } = response;
+        defaultAccountID =
+          user?.defaultAccountList || accountLists?.nodes[0]?.id;
+      }
+      const userId = req.query.userId || jwtToken.userID;
+
+      const url = new URL(
+        process.env.NODE_ENV === 'production'
+          ? `http://${process.env.REWRITE_DOMAIN}/handoff`
+          : `http://stage.${process.env.REWRITE_DOMAIN}/handoff`,
+      );
+
+      url.searchParams.append('accessToken', jwtToken.apiToken);
+      url.searchParams.append('accountListId', defaultAccountID.toString());
+      url.searchParams.append('userId', userId.toString());
+      url.searchParams.append('path', path.toString());
+      return url.href;
+    } else if (jwtToken && req.query.auth === 'true') {
+      const url = new URL(
+        process.env.NODE_ENV === 'production'
+          ? `http://auth.${process.env.REWRITE_DOMAIN}/${path
+              .toString()
+              .replace(/^\/+/, '')}`
+          : `http://auth.stage.${process.env.REWRITE_DOMAIN}/${path
+              .toString()
+              .replace(/^\/+/, '')}`,
+      );
+      url.searchParams.append('access_token', jwtToken.apiToken);
+      return url.href;
+    } else {
+      throw new Error('Something went wrong. jwtToken or auth are undefined');
+    }
+  } catch (error) {
+    throw new Error((error as Error).message);
+  }
+};
+
 const handoff = async (
   req: NextApiRequest,
   res: NextApiResponse,
 ): Promise<void> => {
-  const jwtToken = (await getToken({
-    req,
-    secret: process.env.JWT_SECRET as string,
-  })) as { apiToken: string; userID: string } | null;
-  if (jwtToken && req.query.path && req.query.auth !== 'true') {
-    const client = await ssrClient(jwtToken.apiToken);
-    const response = await client.query<
-      GetDefaultAccountQuery,
-      GetDefaultAccountQueryVariables
-    >({
-      query: GetDefaultAccountDocument,
-    });
-    let defaultAccountID = req.query?.accountListId;
-    if (!defaultAccountID) {
-      const {
-        data: { user, accountLists },
-      } = response;
-      defaultAccountID = user?.defaultAccountList || accountLists?.nodes[0]?.id;
-    }
-    const userId = req.query.userId || jwtToken.userID;
-    const url = new URL(
-      `https://${
-        process.env.SITE_URL === 'https://next.mpdx.org' ? '' : 'stage.'
-      }mpdx.org/handoff`,
-    );
-
-    url.searchParams.append('accessToken', jwtToken.apiToken);
-    url.searchParams.append('accountListId', defaultAccountID.toString());
-    url.searchParams.append('userId', userId.toString());
-    url.searchParams.append('path', req.query.path.toString());
-    res.redirect(url.href);
-  } else if (jwtToken && req.query.path && req.query.auth === 'true') {
-    const url = new URL(
-      `https://auth.${
-        process.env.SITE_URL === 'https://next.mpdx.org' ? '' : 'stage.'
-      }mpdx.org/${req.query.path.toString().replace(/^\/+/, '')}`,
-    );
-
-    url.searchParams.append('access_token', jwtToken.apiToken);
-    res.redirect(url.href);
-  } else {
-    res.status(422);
+  try {
+    const redirectUrl = await returnRedirectUrl(req);
+    res.redirect(redirectUrl);
+  } catch (err) {
+    res.redirect('/');
   }
 };
 
