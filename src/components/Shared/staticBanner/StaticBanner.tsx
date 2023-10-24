92c0b20cddde7b5d478d1fad4047186339f72163
Caleb Alldrin <caleb.alldrin@cru.org>
2023-10-24 13:27:50 -0700

    Fix test and add useeffect

--- src/components/Shared/staticBanner/StaticBanner.tsx
+++ src/components/Shared/staticBanner/StaticBanner.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import Alert from '@mui/material/Alert';
 import { Link } from '@mui/material';
@@ -11,22 +11,23 @@ export const StaticBanner: React.FC<StaticBannerProps> = ({
   severity = 'warning',
 }) => {
   const { t } = useTranslation();
-  let nonCruOrg = false;
+  const [nonCruOrg, setNonCruOrg] = useState(false);
 
-  const checkIfCruOrg = () => {
-    return new Promise((resolve) => {
-      resolve((nonCruOrg = true));
-    });
-  };
-  checkIfCruOrg();
+  useEffect(() => {
+    const checkIfCruOrg = () => {
+      return new Promise((resolve) => {
+        resolve(setNonCruOrg(true));
+      });
+    };
+    checkIfCruOrg();
+  }, []);
 
   return nonCruOrg ? (
-    <Alert severity={severity}>
+    <Alert severity={severity} data-testid="staticBanner">
       {t(
-        "Due to data privacy regulations and costs, Cru will no longer be able to host MPDX data for non-Cru/non-CCCI ministries. This means that MPDX will no longer be available for use outside of Cru/CCCI.  Your data in MPDX will be deleted if you don't export it from MPDX by January 31, 2024 or let us know why you might need an extension. For more information and to take action, ",
+        `Due to data privacy regulations and costs, Cru will no longer be able to host MPDX data for non-Cru/non-CCCI ministries. This means that MPDX will no longer be available for use outside of Cru/CCCI.  Your data in MPDX will be deleted if you don't export it from MPDX by January 31, 2024 or let us know why you might need an extension. For more information and to take action, `,
       )}
       <Link
-        data-testid="staticBanner"
         href="https://docs.google.com/document/d/18TnQGmshg71l3J9Gd-4ltjIjhK2PLtuG_Vc94bt6xzE/"
         target="_blank"
         rel="noreferrer"
