898a1c542d87049b36325ba8200472edb40dfb0d
Daniel Bisgrove <daniel@bizz-websites.com>
2023-11-13 09:55:54 -0500

    Adding wrapper and notification page

---
+++ pages/accountLists/[accountListId]/settings/notifications.page.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { Box } from '@mui/material';
+import { SettingsWrapper } from './wrapper';
+import { NotificationsTable } from 'src/components/Settings/notifications/NotificationsTable';
+
+const Notifications: React.FC = () => {
+  const { t } = useTranslation();
+
+  return (
+    <SettingsWrapper
+      pageTitle={t('Notifications')}
+      pageHeading={t('Notifications')}
+    >
+      <Box component="section" marginTop={3}>
+        <p>
+          Based on an analysis of a partner&apos;s giving history, MPDX can
+          notify you of events that you will probably want to follow up on. The
+          detection logic is based on a set of rules that are right most of the
+          time, but you will still want to verify an event manually before
+          contacting the partner.
+        </p>
+      </Box>
+      <Box component="section" marginTop={1}>
+        <p>
+          For each event MPDX can notify you via email and also create a task
+          entry reminding you to do something about it. The options below allow
+          you to control that behavior.
+        </p>
+      </Box>
+      <NotificationsTable />
+    </SettingsWrapper>
+  );
+};
+
+export default Notifications;
