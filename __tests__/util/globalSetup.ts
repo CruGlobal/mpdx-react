f39f9b8034ea5eec0d8a27fffb9a76b9badc07a2
Daniel Bisgrove <daniel@bizz-websites.com>
2023-11-20 14:43:32 -0500

    Adding tests for helpscout and env var to globalSetup for test

--- __tests__/util/globalSetup.ts
+++ __tests__/util/globalSetup.ts
@@ -1,6 +1,7 @@
 const globalSetup = (): void => {
   process.env.TZ = 'UTC';
   process.env.JWT_SECRET = 'test-environment-key';
+  process.env.HS_CONTACTS_CONTACT_SUGGESTIONS = 'ContactArticleId';
 };
 
 export default globalSetup;
--- src/lib/helpScout.test.ts
+++ src/lib/helpScout.test.ts
@@ -1,4 +1,10 @@
-import { BeaconFn, identifyUser, initBeacon } from './helpScout';
+import {
+  BeaconFn,
+  identifyUser,
+  initBeacon,
+  suggestArticles,
+  showArticle,
+} from './helpScout';
 
 describe('HelpScout', () => {
   const beacon = jest.fn() as BeaconFn;
@@ -33,6 +39,32 @@ describe('HelpScout', () => {
         name: 'name',
         email: 'email',
       });
+    });
+  });
+
+  describe('suggestArticles', () => {
+    it('calls beacon with no envVar defined', () => {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      suggestArticles('');
+      expect(beacon).toHaveBeenCalledWith('suggest', []);
+    });
+
+    it('calls beacon wiht one envVar defined', () => {
+      suggestArticles('HS_CONTACTS_CONTACT_SUGGESTIONS');
+      expect(beacon).toHaveBeenCalledWith('suggest', ['ContactArticleId']);
+    });
+  });
+
+  describe('showArticle', () => {
+    it('articleId not defined', () => {
+      showArticle('');
+      expect(beacon).toHaveBeenCalledWith('open', undefined);
+    });
+
+    it('articleId is defined', () => {
+      showArticle('TestArticleId');
+      expect(beacon).toHaveBeenCalledWith('article', 'TestArticleId');
     });
   });
 });
