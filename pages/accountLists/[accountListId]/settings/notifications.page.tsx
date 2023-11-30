55e6038aacfa6a6b92c1e7c6ea15fa1342e2e9d2
Daniel Bisgrove <daniel@bizz-websites.com>
2023-11-30 10:14:17 -0500

    Typography changes

--- pages/accountLists/[accountListId]/settings/notifications.page.tsx
+++ pages/accountLists/[accountListId]/settings/notifications.page.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Box } from '@mui/material';
-import { SettingsWrapper } from './wrapper';
+import useGetAppSettings from 'src/hooks/useGetAppSettings';
 import { NotificationsTable } from 'src/components/Settings/notifications/NotificationsTable';
+import { SettingsWrapper } from './wrapper';
 
 const Notifications: React.FC = () => {
   const { t } = useTranslation();
+  const { appName } = useGetAppSettings();
 
   return (
     <SettingsWrapper
@@ -14,18 +16,24 @@ const Notifications: React.FC = () => {
     >
       <Box component="section" marginTop={3}>
         <p>
-          {t(`Based on an analysis of a partner&apos;s giving history, MPDX can
+          {t(
+            `Based on an analysis of a partner's giving history, {{appName}} can
           notify you of events that you will probably want to follow up on. The
           detection logic is based on a set of rules that are right most of the
           time, but you will still want to verify an event manually before
-          contacting the partner.`)}
+          contacting the partner.`,
+            { appName },
+          )}
         </p>
       </Box>
       <Box component="section" marginTop={1}>
         <p>
-          {t(`For each event MPDX can notify you via email and also create a task
+          {t(
+            `For each event {{appName}} can notify you via email and also create a task
           entry reminding you to do something about it. The options below allow
-          you to control that behavior.`)}
+          you to control that behavior.`,
+            { appName },
+          )}
         </p>
       </Box>
       <NotificationsTable />
