bf5d68b78e077dee486aaa67098665590ed1f4f7
Caleb Cox <canac@users.noreply.github.com>
2024-01-10 11:58:30 -0600

    Create reusable form inputs

---
+++ src/components/Task/Modal/Form/Inputs/TagsAutocomplete/TagsAutocomplete.tsx
@@ -0,0 +1,80 @@
+import { Autocomplete, Chip, CircularProgress, TextField } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import { useTagOptionsQuery } from './TagsAutocomplete.generated';
+
+export enum TagTypeEnum {
+  Contact = 'Contact',
+  Tag = 'Tag',
+}
+
+interface TagsAutocompleteProps {
+  accountListId: string;
+  type: TagTypeEnum;
+  value: string[];
+  onChange: (tagList: string[]) => void;
+}
+
+export const TagsAutocomplete: React.FC<TagsAutocompleteProps> = ({
+  accountListId,
+  type,
+  value,
+  onChange,
+}) => {
+  const { t } = useTranslation();
+
+  const { data, loading } = useTagOptionsQuery({
+    variables: {
+      accountListId,
+      contact: type === TagTypeEnum.Contact,
+    },
+  });
+  // Because of the @skip and @include directives, only contactTagList or taskTagList will be populated, but not both
+  const options =
+    data?.accountList.contactTagList ?? data?.accountList.taskTagList ?? [];
+
+  return (
+    <Autocomplete
+      multiple
+      openOnFocus
+      autoSelect
+      freeSolo
+      renderTags={(value, getTagProps) =>
+        value.map((option, index) => (
+          <Chip
+            {...getTagProps({ index })}
+            color="default"
+            size="small"
+            key={index}
+            label={option}
+          />
+        ))
+      }
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label={t('Tags')}
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <>
+                {loading && <CircularProgress color="primary" size={20} />}
+                {params.InputProps.endAdornment}
+              </>
+            ),
+          }}
+        />
+      )}
+      onChange={(event, newValue) => {
+        if (event.type === 'blur' && newValue.length < value.length) {
+          // Prevent tabbing out of the field from deselecting an item, while still
+          // allowing tab to select the highlighted item
+          return;
+        }
+
+        onChange(newValue);
+      }}
+      value={value}
+      options={options}
+    />
+  );
+};
