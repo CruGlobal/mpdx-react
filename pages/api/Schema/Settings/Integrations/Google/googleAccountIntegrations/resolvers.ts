9afc276e795c2838de4c1cddd849eaf93b16a663
Caleb Alldrin <caleb.alldrin@cru.org>
2024-01-24 07:25:08 -0800

    Move and rename integrations rest API data

---
+++ pages/api/Schema/Settings/Integrations/Google/googleAccountIntegrations/resolvers.ts
@@ -0,0 +1,18 @@
+import { Resolvers } from '../../../../../graphql-rest.page.generated';
+
+const GoogleAccountIntegrationsResolvers: Resolvers = {
+  Query: {
+    googleAccountIntegrations: async (
+      _source,
+      { input: { googleAccountId, accountListId } },
+      { dataSources },
+    ) => {
+      return dataSources.mpdxRestApi.googleAccountIntegrations(
+        googleAccountId,
+        accountListId,
+      );
+    },
+  },
+};
+
+export { GoogleAccountIntegrationsResolvers };
