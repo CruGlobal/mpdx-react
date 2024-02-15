b1e0f7298635b6423c302406ac3b2ef44430711b
Bizz <56281168+dr-bizz@users.noreply.github.com>
2024-02-15 09:20:58 -0500

    MPDX-7772 Preferences - Admin (#866)
    * Admin preferences.
    - Impersonate user
    - Reset an account
    - Phone TS error fix
    - Adding tests
    
    * Adding extractCookie helper
    
    * Moved userUser to hooks folder. Updated files and made ImpersonateUserAccordion use useUser.
    
    * Adding admin page tests
    
    * renaming for functions and types
    
    * Login page tests
    
    * Fixing test issues, using getByRole rather than getByTestId. cleaning up code.
    
    * Okta username input label wording correction
    
    * ensuring impersonate user shows on load
    
    * Changing wrapper to Wrapper as it's a Component
    
    * Changing wrapper to Wrapper as it's a Component
    
    * Updating accordion test as impersonate user defaults to be open

--- pages/accountLists/[accountListId]/settings/integrations/index.page.tsx
+++ pages/accountLists/[accountListId]/settings/integrations/index.page.tsx
@@ -11,7 +11,7 @@ import { OrganizationAccordion } from 'src/components/Settings/integrations/Orga
 import { PrayerlettersAccordion } from 'src/components/Settings/integrations/Prayerletters/PrayerlettersAccordion';
 import { AccordionGroup } from 'src/components/Shared/Forms/Accordions/AccordionGroup';
 import { suggestArticles } from 'src/lib/helpScout';
-import { SettingsWrapper } from '../wrapper';
+import { SettingsWrapper } from '../Wrapper';
 import { IntegrationsContextProvider } from './IntegrationsContext';
 
 interface Props {
@@ -38,6 +38,7 @@ const Integrations = ({ apiToken }: Props): ReactElement => {
     <SettingsWrapper
       pageTitle={t('Connect Services')}
       pageHeading={t('Connect Services')}
+      selectedMenuId="integrations"
     >
       <IntegrationsContextProvider apiToken={apiToken}>
         <AccordionGroup title="">
---
+++ pages/login-apioauth.page.test.tsx
@@ -0,0 +1,124 @@
+import { GetServerSidePropsContext } from 'next';
+import { ThemeProvider } from '@mui/material/styles';
+import { render, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { getSession, signIn } from 'next-auth/react';
+import { I18nextProvider } from 'react-i18next';
+import TestRouter from '__tests__/util/TestRouter';
+import { GqlMockedProvider } from '__tests__/util/graphqlMocking';
+import i18n from 'src/lib/i18n';
+import theme from 'src/theme';
+import Login, { LoginProps, getServerSideProps } from './login.page';
+
+jest.mock('next-auth/react', () => ({
+  getSession: jest.fn(),
+  signIn: jest.fn(),
+}));
+
+interface getServerSidePropsReturn {
+  props: LoginProps;
+  redirect: unknown;
+}
+
+interface ComponentsProps {
+  props: LoginProps;
+  mutationSpy?: () => void;
+}
+
+const Components = ({ props, mutationSpy }: ComponentsProps) => (
+  <ThemeProvider theme={theme}>
+    <TestRouter>
+      <GqlMockedProvider onCall={mutationSpy}>
+        <I18nextProvider i18n={i18n}>
+          <Login {...props} />
+        </I18nextProvider>
+      </GqlMockedProvider>
+    </TestRouter>
+  </ThemeProvider>
+);
+
+describe('Login - API_OAUTH', () => {
+  const setHeaderMock = jest.fn();
+  const context = {
+    query: {
+      accountListId: 'accountListId',
+    },
+    res: {
+      setHeader: setHeaderMock,
+    },
+    req: {
+      headers: {
+        cookie: '',
+      },
+    },
+  } as unknown as GetServerSidePropsContext;
+
+  beforeEach(() => {
+    setHeaderMock.mockClear();
+  });
+
+  process.env.AUTH_PROVIDER = 'API_OAUTH';
+  process.env.API_OAUTH_VISIBLE_NAME = 'Third-party oAuth';
+
+  it('should pass down props and render login', async () => {
+    (getSession as jest.Mock).mockReturnValue(null);
+    const { props, redirect } = (await getServerSideProps(
+      context,
+    )) as getServerSidePropsReturn;
+
+    expect(props).toEqual({
+      signInButtonText: 'Sign In with Third-party oAuth',
+      signInAuthProviderId: 'apioauth',
+      immediateSignIn: false,
+    });
+    expect(redirect).toBeUndefined();
+  });
+
+  it('should immediate Sign In if cookie present', async () => {
+    context.req.headers.cookie =
+      'cookieOne=valueOne;mpdx-handoff.redirect-url=http://URL.org;cookieTwo=valueTwo;';
+    const { props, redirect } = (await getServerSideProps(
+      context,
+    )) as getServerSidePropsReturn;
+
+    expect(props).toEqual({
+      signInButtonText: 'Sign In with Third-party oAuth',
+      signInAuthProviderId: 'apioauth',
+      immediateSignIn: true,
+    });
+    expect(redirect).toBeUndefined();
+    expect(setHeaderMock).toHaveBeenCalledWith(
+      'Set-Cookie',
+      `mpdx-handoff.redirect-url=; HttpOnly; path=/; Max-Age=0`,
+    );
+
+    const { getByTestId } = render(<Components props={props} />);
+
+    await waitFor(() => expect(getByTestId('Loading')).toBeInTheDocument());
+    expect(signIn).toHaveBeenCalledWith('apioauth');
+  });
+
+  it('calls signin on button click', async () => {
+    context.req.headers.cookie = '';
+    const { props } = (await getServerSideProps(
+      context,
+    )) as getServerSidePropsReturn;
+
+    const { queryByTestId, getByRole } = render(<Components props={props} />);
+
+    await waitFor(() =>
+      expect(queryByTestId('Loading')).not.toBeInTheDocument(),
+    );
+
+    const loginButton = getByRole('button', {
+      name: /sign in/i,
+    });
+    await waitFor(() => expect(loginButton).toBeInTheDocument());
+    userEvent.click(loginButton);
+
+    await waitFor(() => {
+      expect(signIn).toHaveBeenCalledTimes(1);
+      expect(signIn).toHaveBeenCalledWith('apioauth');
+    });
+  });
+});
