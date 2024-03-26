ee849fe64727109459218d9b9d81b876f2088ac0
Daniel Bisgrove <daniel@bizz-websites.com>
2024-03-26 14:59:57 -0400

    Speed improvements to third-party scripts and fonts.

--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -19,16 +19,19 @@ class MyDocument extends Document {
         <Head>
           <meta name="theme-color" content={theme.palette.mpdxBlue.main} />
           <link
-            href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;700&display=swap"
-            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;700&display=swap&text=1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz%20%21%22%23%24%25%26%27"
+            rel="preload"
+            as="font"
+            type="font/woff2"
+            crossOrigin="true"
           />
           {process.env.DATADOG_CONFIGURED === 'true' && (
-            <Script id="datadog-rum" strategy="afterInteractive">
+            <Script id="datadog-rum" strategy="lazyOnload">
               {`!function(a,e,t,n,s){a=a[s]=a[s]||{q:[],onReady:function(e){a.q.push(e)}},(s=e.createElement(t)).async=1,s.src=n,(n=e.getElementsByTagName(t)[0]).parentNode.insertBefore(s,n)}(window,document,"script","https://www.datadoghq-browser-agent.com/datadog-rum-v4.js","DD_RUM"),DD_RUM.onReady(function(){DD_RUM.init({clientToken:"${process.env.DATADOG_CLIENT_TOKEN}",applicationId:"${process.env.DATADOG_APP_ID}",site:"datadoghq.com",service:"mpdx-web-react",sessionSampleRate:100,sessionReplaySampleRate:20,trackUserInteractions:!0,trackResources:!0,trackLongTasks:!0,defaultPrivacyLevel:"mask-user-input"}),DD_RUM.startSessionReplayRecording()});`}
             </Script>
           )}
           {process.env.GOOGLE_TAG_MANAGER_CONTAINER_ID && (
-            <Script id="google-analytics" strategy="afterInteractive">
+            <Script id="google-analytics" strategy="lazyOnload">
               {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${process.env.GOOGLE_TAG_MANAGER_CONTAINER_ID}');`}
             </Script>
           )}
