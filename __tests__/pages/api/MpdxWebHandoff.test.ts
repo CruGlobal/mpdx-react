3de20573700fb982624a2fa11f5828f3097f3c02
Daniel Bisgrove <daniel@bizz-websites.com>
2024-04-16 11:03:29 -0400

    ensuring devs don't need to add SITE_URL to their .env files

--- __tests__/pages/api/MpdxWebHandoff.test.ts
+++ __tests__/pages/api/MpdxWebHandoff.test.ts
@@ -5,8 +5,8 @@ import { taskFiltersTabs } from '../../../src/utils/tasks/taskFilterTabs';
 
 jest.mock('next-auth/jwt', () => ({ getToken: jest.fn() }));
 
-const siteUrl = `${process.env.SITE_URL}`;
-const accountListsUrl = `${process.env.SITE_URL}/accountLists`;
+const siteUrl = process.env.SITE_URL;
+const accountListsUrl = `${siteUrl}/accountLists`;
 // User one
 const userOneId = 'userId_1';
 const userOneToken = 'userOne.token';
@@ -41,7 +41,7 @@ describe('/api/mpdx-web-handoff', () => {
     expect(res._getRedirectUrl()).toBe(`${siteUrl}/`);
   });
 
-  const redirectUrl = `${process.env.SITE_URL}/accountLists/accountListId/contacts`;
+  const redirectUrl = `${siteUrl}/accountLists/accountListId/contacts`;
 
   describe('No prev session', () => {
     beforeEach(() => {
--- __tests__/util/globalSetup.ts
+++ __tests__/util/globalSetup.ts
@@ -1,6 +1,7 @@
 const globalSetup = (): void => {
   process.env.TZ = 'UTC';
   process.env.JWT_SECRET = 'test-environment-key';
+  process.env.SITE_URL = 'https://next.mpdx.org';
 };
 
 export default globalSetup;
