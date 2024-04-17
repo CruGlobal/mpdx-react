3ba859ac40ebae6052751e66019d253d573676ae
Daniel Bisgrove <daniel@bizz-websites.com>
2024-04-17 10:00:57 -0400

    Moving APP_NAME to globalSetup

--- __tests__/util/globalSetup.ts
+++ __tests__/util/globalSetup.ts
@@ -2,6 +2,7 @@ const globalSetup = (): void => {
   process.env.TZ = 'UTC';
   process.env.JWT_SECRET = 'test-environment-key';
   process.env.SITE_URL = 'https://next.mpdx.org';
+  process.env.APP_NAME = 'MPDX';
 };
 
 export default globalSetup;
