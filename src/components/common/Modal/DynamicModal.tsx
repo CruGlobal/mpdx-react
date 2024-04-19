9f404964f31b045aa33934de7ce5c7af41cf174a
Caleb Cox <canac@users.noreply.github.com>
2024-04-19 14:26:11 -0500

    Lazy load EmptyReport add donation modal contents

--- src/components/common/Modal/DynamicModal.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic';
-import { DynamicModalPlaceholder } from 'src/components/DynamicPlaceholders/DynamicModalPlaceholder';
-
-export const preloadModal = () =>
-  import(/* webpackChunkName: "Modal" */ './Modal');
-
-export const DynamicModal = dynamic(preloadModal, {
-  loading: DynamicModalPlaceholder,
-});
