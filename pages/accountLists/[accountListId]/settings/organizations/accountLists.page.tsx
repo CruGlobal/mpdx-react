e3dcecee900e82b7006b676cebff716d5ad8ade6
Daniel Bisgrove <daniel@bizz-websites.com>
2024-04-25 16:53:39 -0400

    Removing enforing admin on organizations pages as user doesn't have to be admin, could just have organizations to manage.

--- pages/accountLists/[accountListId]/settings/organizations/accountLists.page.tsx
+++ pages/accountLists/[accountListId]/settings/organizations/accountLists.page.tsx
@@ -10,7 +10,7 @@ import {
 import { styled } from '@mui/material/styles';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import { useTranslation } from 'react-i18next';
-import { enforceAdmin } from 'pages/api/utils/pagePropsHelpers';
+import { loadSession } from 'pages/api/utils/pagePropsHelpers';
 import { AccountLists } from 'src/components/Settings/Organization/AccountLists/AccountLists';
 import { useDebouncedValue } from 'src/hooks/useDebounce';
 import { SettingsWrapper } from '../Wrapper';
@@ -123,6 +123,6 @@ const AccountListsOrganizations = (): ReactElement => {
   );
 };
 
-export const getServerSideProps = enforceAdmin;
+export const getServerSideProps = loadSession;
 
 export default AccountListsOrganizations;
