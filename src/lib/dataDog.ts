194383982a3d36fca2ef574e886aef3c3ae6e2c4
Caleb Cox <canac@users.noreply.github.com>
2024-05-10 09:07:18 -0500

    Unconditionally set the DataDog user with the new attributes

--- src/lib/dataDog.ts
+++ src/lib/dataDog.ts
@@ -1,7 +1,6 @@
 declare global {
   interface Window {
     DD_RUM: {
-      getUser: () => Record<string, string | undefined>;
       setUser: (user: Record<string, string>) => void;
       clearUser: () => void;
     };
@@ -12,7 +11,7 @@ export const isDataDogConfigured = (): boolean => {
   if (typeof window === 'undefined') return false;
   return !!(
     process.env.DATADOG_CONFIGURED === 'true' &&
-    window.DD_RUM?.hasOwnProperty('getUser')
+    window.DD_RUM?.hasOwnProperty('setUser')
   );
 };
 
@@ -30,18 +29,11 @@ export const setDataDogUser = ({
   accountListId,
 }: SetDataDogUserProps): void => {
   if (!isDataDogConfigured()) return;
-  if (
-    window.DD_RUM.getUser()?.accountListId &&
-    window.DD_RUM.getUser()?.accountListId === accountListId
-  )
-    return;
-  if (window.DD_RUM.getUser()?.accountListId !== accountListId)
-    clearDataDogUser();
   window.DD_RUM.setUser({
     id: accountListId,
     name,
     email,
-    userId: userId,
+    userId,
   });
 };
 
