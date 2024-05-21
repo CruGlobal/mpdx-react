2c46c345e65df0e9950bb14b99743ef87c7c510a
Caleb Alldrin <caleb.alldrin@cru.org>
2024-05-21 15:19:21 -0700

    Fix activity name error when blank

--- src/components/Task/Modal/Form/TaskModalHelper.ts
+++ src/components/Task/Modal/Form/TaskModalHelper.ts
@@ -80,11 +80,13 @@ export const handleTaskActionChange = ({
     setFieldValue(
       'subject',
       activity?.name
-        .split(' ')
-        .map((word) => {
-          return word[0].toUpperCase() + word.substring(1);
-        })
-        .join(' '),
+        ? activity.name
+            .split(' ')
+            .map((word) => {
+              return word[0].toUpperCase() + word.substring(1);
+            })
+            .join(' ')
+        : '',
     );
   }
 };
