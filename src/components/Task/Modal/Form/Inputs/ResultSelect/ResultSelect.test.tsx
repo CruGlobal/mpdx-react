9a796ce56061c42cf2117490f29daa3aa64c921b
Daniel Bisgrove <daniel@bizz-websites.com>
2024-06-04 15:40:33 -0400

    Tasks - Created/Edited inputs used on all Task Modals

---
+++ src/components/Task/Modal/Form/Inputs/ResultSelect/ResultSelect.test.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { ThemeProvider } from '@emotion/react';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import TestWrapper from '__tests__/util/TestWrapper';
+import { Phase, ResultEnum } from 'src/graphql/types.generated';
+import theme from 'src/theme';
+import { SetResultSelected, handleResultChange } from '../../TaskModalHelper';
+import { ResultSelect, ResultSelectProps } from './ResultSelect';
+
+jest.mock('../../TaskModalHelper');
+
+const setFieldValue = jest.fn();
+const setResultSelected = jest.fn();
+
+beforeEach(() => {
+  handleResultChange as jest.Mock;
+  setFieldValue.mockClear();
+  setResultSelected.mockClear();
+});
+
+beforeEach(() => {});
+
+const phaseData = {
+  id: 'CONNECTION',
+  name: 'Connection',
+} as Phase;
+
+type ComponentsProps = {
+  availableResults: ResultSelectProps['availableResults'];
+};
+
+const Components = ({ availableResults }: ComponentsProps) => (
+  <ThemeProvider theme={theme}>
+    <TestWrapper>
+      <ResultSelect
+        availableResults={availableResults}
+        setFieldValue={setFieldValue}
+        setResultSelected={setResultSelected as unknown as SetResultSelected}
+        phaseData={phaseData}
+      />
+    </TestWrapper>
+  </ThemeProvider>
+);
+
+const standardAvailableResults = [
+  ResultEnum.None,
+  ResultEnum.Attempted,
+  ResultEnum.AttemptedLeftMessage,
+  ResultEnum.Completed,
+  ResultEnum.Done,
+  ResultEnum.Received,
+];
+
+describe('ResultSelect', () => {
+  it('does not render when no availableResults', () => {
+    const { queryByRole } = render(<Components availableResults={[]} />);
+    expect(queryByRole('combobox', { name: 'Result' })).not.toBeInTheDocument();
+  });
+
+  it('renders when availableResults', () => {
+    const { getByRole } = render(
+      <Components availableResults={standardAvailableResults} />,
+    );
+    expect(getByRole('combobox', { name: 'Result' })).toBeInTheDocument();
+  });
+
+  it('fires setFieldValue & setResultSelected on select of an option', () => {
+    const { getByRole } = render(
+      <Components availableResults={standardAvailableResults} />,
+    );
+
+    userEvent.click(getByRole('combobox', { name: 'Result' }));
+    expect(getByRole('option', { name: 'Completed' })).toBeInTheDocument();
+    userEvent.click(getByRole('option', { name: 'Completed' }));
+
+    expect(handleResultChange).toHaveBeenCalledWith({
+      result: ResultEnum.Completed,
+      setFieldValue,
+      setResultSelected,
+      phaseData,
+    });
+  });
+});
