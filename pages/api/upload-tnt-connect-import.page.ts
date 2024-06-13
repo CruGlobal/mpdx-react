bb73f1d9fdf511bbac866413c8ff129cf013a9d3
Caleb Alldrin <caleb.alldrin@cru.org>
2024-06-13 14:36:54 -0700

    Add Rest API upload

---
+++ pages/api/upload-tnt-connect-import.page.ts
@@ -0,0 +1,82 @@
+import { readFile } from 'fs/promises';
+import { NextApiRequest, NextApiResponse } from 'next';
+import formidable, { IncomingForm } from 'formidable';
+import fetch, { File, FormData } from 'node-fetch';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const parseBody = async (
+  req: NextApiRequest,
+): Promise<{ fields: formidable.Fields; files: formidable.Files }> => {
+  return new Promise((resolve, reject) => {
+    const form = new IncomingForm();
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({ fields, files });
+      }
+    });
+  });
+};
+
+const uploadTntConnect = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+): Promise<void> => {
+  try {
+    if (req.method !== 'POST') {
+      res.status(405).send('Method Not Found');
+      return;
+    }
+
+    const {
+      fields: { override, selectedTags, accountListId, apiToken },
+      files: { file },
+    } = await parseBody(req);
+    if (typeof override !== 'string') {
+      res.status(400).send('Missing override');
+      return;
+    }
+    if (!apiToken) {
+      res.status(401).send('Unauthorized');
+      return;
+    }
+    if (!file || Array.isArray(file)) {
+      res.status(400).send('Missing file');
+      return;
+    }
+
+    const fileUpload = new File(
+      [await readFile(file.filepath)],
+      file.originalFilename ?? 'avatar',
+    );
+    const form = new FormData();
+    form.append('data[type]', 'imports');
+    form.append('data[attributes][override]', override);
+    form.append(
+      'data[attributes][tag_list]',
+      Array.isArray(selectedTags) ? selectedTags.join(',') : selectedTags,
+    );
+    form.append('data[attributes][file]', fileUpload);
+    const fetchRes = await fetch(
+      `${process.env.REST_API_URL}account_lists/${accountListId}/imports/tnt`,
+      {
+        method: 'POST',
+        headers: {
+          authorization: `Bearer ${apiToken}`,
+        },
+        body: form,
+      },
+    );
+    res.status(fetchRes.status).json({ success: fetchRes.status === 201 });
+  } catch (err) {
+    res.status(500).json({ success: false, error: err });
+  }
+};
+
+export default uploadTntConnect;
---
+++ src/components/Tool/TntConnect/uploadTntConnect.ts
@@ -0,0 +1,66 @@
+import { TFunction } from 'react-i18next';
+
+export const validateTnt = ({
+  file,
+  t,
+}: {
+  file: File;
+  t: TFunction;
+}): { success: true } | { success: false; message: string } => {
+  if (!file.type.includes('xml')) {
+    return {
+      success: false,
+      message: t('Cannot upload file: file must be a xml file'),
+    };
+  }
+  // The /api lambda appears to truncate the source body at 2^20 bytes
+  // Conservatively set the limit at 1MB, which is a little lower than 1MiB because of the
+  // overhead of encoding multipart/form-data and the other fields in the POST body
+  if (file.size > 1_000_000) {
+    return {
+      success: false,
+      message: t('Cannot upload file: file size cannot exceed 1MB'),
+    };
+  }
+
+  return { success: true };
+};
+
+export const uploadTnt = async ({
+  selectedTags,
+  override,
+  file,
+  t,
+  apiToken,
+  accountListId,
+}: {
+  selectedTags: string[];
+  override: string;
+  file: File;
+  t: TFunction;
+  apiToken: string;
+  accountListId: string;
+}): Promise<void> => {
+  const validationResult = validateTnt({ file, t });
+  if (!validationResult.success) {
+    throw new Error(validationResult.message);
+  }
+
+  const form = new FormData();
+  form.append('override', override);
+  form.append('tag_list', selectedTags.join(','));
+  form.append('file', file);
+  form.append('apiToken', apiToken);
+  form.append('accountListId', accountListId);
+
+  const res = await fetch(`/api/upload-tnt-connect-import`, {
+    method: 'POST',
+    body: form,
+  }).catch((err) => {
+    throw new Error(t('Cannot upload file: server error') + err);
+  });
+  const data: { success: boolean } = await res.json();
+  if (!data.success) {
+    throw new Error(t('Cannot upload file: server error') + data);
+  }
+};
