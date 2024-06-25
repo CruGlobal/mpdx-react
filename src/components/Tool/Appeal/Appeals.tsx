b5036476509574d2c70a62c03d99560e0945f334
Daniel Bisgrove <daniel@bizz-websites.com>
2024-06-25 16:51:15 -0400

    Simplifying code and improvements to the left side of the Appeals initial page

--- src/components/Tool/Appeal/Appeals.tsx
+++ src/components/Tool/Appeal/Appeals.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
-import { Box, CircularProgress, Divider, Typography } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import React, { useMemo } from 'react';
+import { Box, Divider, Skeleton, Typography } from '@mui/material';
 import { useSnackbar } from 'notistack';
 import { useTranslation } from 'react-i18next';
 import {
@@ -12,15 +11,11 @@ import Appeal from './Appeal';
 import { useChangePrimaryAppealMutation } from './ChangePrimaryAppeal.generated';
 import NoAppeals from './NoAppeals';
 
-const LoadingIndicator = styled(CircularProgress)(({ theme }) => ({
-  margin: theme.spacing(0, 1, 0, 0),
-}));
-
-interface Props {
+interface AppealsProps {
   accountListId: string;
 }
 
-const Appeals: React.FC<Props> = ({ accountListId }: Props) => {
+const Appeals: React.FC<AppealsProps> = ({ accountListId }) => {
   const { t } = useTranslation();
   const { enqueueSnackbar } = useSnackbar();
   const [changePrimaryAppeal, { loading: updating }] =
@@ -34,7 +29,7 @@ const Appeals: React.FC<Props> = ({ accountListId }: Props) => {
     pageInfo: data?.regularAppeals.pageInfo,
   });
 
-  const changePrimary = async (newPrimaryId: string): Promise<void> => {
+  const handleChangePrimary = async (newPrimaryId: string): Promise<void> => {
     await changePrimaryAppeal({
       variables: {
         accountListId,
@@ -52,6 +47,14 @@ const Appeals: React.FC<Props> = ({ accountListId }: Props) => {
     });
   };
 
+  const primaryAppeal = useMemo(() => {
+    if (data?.primaryAppeal) {
+      return data.primaryAppeal.nodes[0];
+    } else {
+      return null;
+    }
+  }, [data]);
+
   return (
     <Box mb={1}>
       <Box m={1}>
@@ -59,26 +62,12 @@ const Appeals: React.FC<Props> = ({ accountListId }: Props) => {
       </Box>
       <Divider />
       {loading || updating ? (
-        <Box display="flex" justifyContent="center" mt={10}>
-          <LoadingIndicator color="primary" size={40} />
-        </Box>
-      ) : data?.primaryAppeal && data.primaryAppeal.nodes.length > 0 ? (
+        <Skeleton height={'170px'} />
+      ) : primaryAppeal ? (
         <Appeal
-          name={data.primaryAppeal.nodes[0].name || ''}
-          id={data.primaryAppeal.nodes[0].id || ''}
-          primary
-          amount={data.primaryAppeal.nodes[0].amount || 0}
-          amountCurrency={data.primaryAppeal.nodes[0].amountCurrency}
-          given={data.primaryAppeal.nodes[0].pledgesAmountProcessed || 0}
-          received={
-            data.primaryAppeal.nodes[0].pledgesAmountReceivedNotProcessed || 0
-          }
-          commited={
-            data.primaryAppeal.nodes[0].pledgesAmountNotReceivedNotProcessed ||
-            0
-          }
-          total={data.primaryAppeal.nodes[0].pledgesAmountTotal || 0}
-          changePrimary={changePrimary}
+          appeal={primaryAppeal}
+          primary={true}
+          changePrimary={handleChangePrimary}
         />
       ) : (
         <NoAppeals primary />
@@ -88,25 +77,18 @@ const Appeals: React.FC<Props> = ({ accountListId }: Props) => {
       </Box>
       <Divider />
       {loading || updating ? (
-        <Box display="flex" justifyContent="center" mt={10}>
-          <LoadingIndicator color="primary" size={40} />
-        </Box>
-      ) : data?.regularAppeals && data.regularAppeals.nodes.length > 0 ? (
+        <>
+          {Array(5)
+            .fill('')
+            .map((_, idx) => (
+              <Skeleton height={140} key={`appeal-skeleton-${idx}`} />
+            ))}
+        </>
+      ) : data?.regularAppeals && data.regularAppeals.nodes.length ? (
         <>
           {data.regularAppeals.nodes.map((appeal) => (
             <Box key={appeal.name} mb={3}>
-              <Appeal
-                name={appeal.name || ''}
-                id={appeal.id || ''}
-                primary={false}
-                amount={appeal.amount || 0}
-                amountCurrency={appeal.amountCurrency}
-                given={appeal.pledgesAmountProcessed}
-                received={appeal.pledgesAmountReceivedNotProcessed}
-                commited={appeal.pledgesAmountNotReceivedNotProcessed}
-                total={appeal.pledgesAmountTotal}
-                changePrimary={changePrimary}
-              />
+              <Appeal appeal={appeal} changePrimary={handleChangePrimary} />
             </Box>
           ))}
         </>
