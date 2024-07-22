a81ca675e0e1f5db59668df658f514eb782272e5
Daniel Bisgrove <daniel@bizz-websites.com>
2024-07-22 11:16:59 -0400

    Exporting contacts components so we don't have to duplicate code when creating the Tools contact components.

--- src/components/Contacts/ContactFlow/ContactFlowColumn/ContactFlowColumn.tsx
+++ src/components/Contacts/ContactFlow/ContactFlowColumn/ContactFlowColumn.tsx
@@ -6,6 +6,7 @@ import {
   CircularProgress,
   Typography,
 } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import { useDrop } from 'react-dnd';
 import { useContactsQuery } from 'pages/accountLists/[accountListId]/contacts/Contacts.generated';
 import {
@@ -17,14 +18,49 @@ import {
   ContactFilterStatusEnum,
   IdValue,
 } from 'src/graphql/types.generated';
-import theme from '../../../../theme';
+import theme from 'src/theme';
 import { useLoadConstantsQuery } from '../../../Constants/LoadConstants.generated';
 import { InfiniteList } from '../../../InfiniteList/InfiniteList';
 import { ContactRowFragment } from '../../ContactRow/ContactRow.generated';
 import { ContactFlowDropZone } from '../ContactFlowDropZone/ContactFlowDropZone';
 import { ContactFlowRow } from '../ContactFlowRow/ContactFlowRow';
 
-interface Props {
+export const ContainerBox = styled(Box, {
+  shouldForwardProp: (prop) => prop !== 'color',
+})(({ color }: { color: string }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  borderBottom: `5px solid ${color}`,
+  height: theme.spacing(7),
+}));
+
+export const ColumnTitle = styled(Typography)(() => ({
+  fontWeight: 600,
+  whiteSpace: 'nowrap',
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+}));
+
+export const StyledCardContent = styled(CardContent)(() => ({
+  position: 'relative',
+  height: 'calc(100vh - 260px)',
+  padding: 0,
+  background: theme.palette.cruGrayLight.main,
+}));
+
+export const CardContentInner = styled(Box, {
+  shouldForwardProp: (prop) => prop !== 'canDrop',
+})(({ canDrop }: { canDrop: boolean }) => ({
+  position: 'absolute',
+  width: '100%',
+  height: '100%',
+  top: '0',
+  right: '0',
+  display: canDrop ? 'grid' : 'none',
+}));
+
+export interface ContactFlowColumnProps {
   data?: ContactRowFragment[];
   statuses: ContactFilterStatusEnum[];
   selectedFilters: ContactFilterSetInput;
@@ -44,7 +80,6 @@ interface Props {
     } & Pick<IdValue, 'id' | 'value'>,
   ) => Promise<void>;
 }
-
 export interface StatusStructure {
   id: string | undefined;
   value: string | undefined;
@@ -52,7 +87,7 @@ export interface StatusStructure {
 
 const nullStatus = { id: 'NULL', value: '' };
 
-export const ContactFlowColumn: React.FC<Props> = ({
+export const ContactFlowColumn: React.FC<ContactFlowColumnProps> = ({
   statuses,
   title,
   color,
@@ -60,7 +95,7 @@ export const ContactFlowColumn: React.FC<Props> = ({
   searchTerm,
   onContactSelected,
   changeContactStatus,
-}: Props) => {
+}) => {
   const { sanitizedFilters } = React.useContext(
     ContactsContext,
   ) as ContactsType;
@@ -95,49 +130,19 @@ export const ContactFlowColumn: React.FC<Props> = ({
     <CircularProgress />
   ) : (
     <Card>
-      <Box
-        p={2}
-        display="flex"
-        alignItems="center"
-        justifyContent="space-between"
-        data-testid="column-header"
-        borderBottom={`5px solid ${color}`}
-        height={theme.spacing(7)}
-      >
+      <ContainerBox p={2} data-testid="column-header" color={color}>
         <Box width="80%">
-          <Typography
-            variant="h6"
-            style={{
-              fontWeight: 600,
-              whiteSpace: 'nowrap',
-              overflow: 'hidden',
-              textOverflow: 'ellipsis',
-            }}
-          >
-            {title}
-          </Typography>
+          <ColumnTitle variant="h6">{title}</ColumnTitle>
         </Box>
         <Box display="flex" alignItems="center">
           <Typography>{data?.contacts.totalCount || 0}</Typography>
         </Box>
-      </Box>
-      <CardContent
-        style={{
-          position: 'relative',
-          height: 'calc(100vh - 260px)',
-          padding: 0,
-          background: theme.palette.cruGrayLight.main,
-        }}
-      >
-        <Box
-          {...{ ref: drop }}
-          position="absolute"
-          width="100%"
-          height="100%"
-          top={0}
-          right={0}
-          display={canDrop ? 'grid' : 'none'}
+      </ContainerBox>
+      <StyledCardContent>
+        <CardContentInner
+          canDrop={canDrop}
           gridTemplateRows={`repeat(${statuses.length},auto)`}
+          {...{ ref: drop }}
         >
           {statusesStructured.map((status) => (
             <ContactFlowDropZone
@@ -146,7 +151,7 @@ export const ContactFlowColumn: React.FC<Props> = ({
               changeContactStatus={changeContactStatus}
             />
           ))}
-        </Box>
+        </CardContentInner>
         <Box ref={cardContentRef} width="100%" height="100%">
           <InfiniteList
             loading={loading}
@@ -154,17 +159,14 @@ export const ContactFlowColumn: React.FC<Props> = ({
             itemContent={(_index, contact) => (
               <ContactFlowRow
                 accountListId={accountListId}
-                id={contact.id}
-                name={contact.name}
+                contact={contact}
                 status={
                   constants?.constant.statuses?.find(
                     (constant) => constant.id === contact.status,
                   ) || nullStatus
                 }
-                starred={contact.starred}
                 onContactSelected={onContactSelected}
                 columnWidth={cardContentRef.current?.offsetWidth}
-                avatar={contact.avatar}
               />
             )}
             endReached={() =>
@@ -176,7 +178,7 @@ export const ContactFlowColumn: React.FC<Props> = ({
             EmptyPlaceholder={undefined}
           />
         </Box>
-      </CardContent>
+      </StyledCardContent>
     </Card>
   );
 };
