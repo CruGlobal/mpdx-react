bc96f423a811fed122426552fa50006c616b6e75
Daniel Bisgrove <daniel@bizz-websites.com>
2024-07-22 11:25:24 -0400

    Adding Appeal contact flow components. Importing certain components from contacts to reduce code maintenance.

--- src/components/Tool/Appeal/AppealDetails/AppealsMainPanel/AppealsMainPanel.tsx
+++ src/components/Tool/Appeal/AppealDetails/AppealsMainPanel/AppealsMainPanel.tsx
@@ -1,41 +1,55 @@
 import React from 'react';
 import { TableViewModeEnum } from 'src/components/Shared/Header/ListHeader';
-// import { DynamicContactFlow } from '../ContactFlow/DynamicContactFlow';
 import {
   AppealsContext,
   AppealsType,
 } from '../../AppealsContext/AppealsContext';
+import { DynamicContactFlow } from '../../Flow/DynamicContactFlow';
 import { DynamicContactsList } from '../../List/ContactsList/DynamicContactsList';
 import { AppealsMainPanelHeader } from './AppealsMainPanelHeader';
+import { useAppealQuery } from './appealInfo.generated';
 
 export const AppealsMainPanel: React.FC = () => {
   const {
-    // accountListId,
-    // activeFilters,
-    // starredFilter,
-    // searchTerm,
-    // setContactFocus,
+    accountListId,
+    appealId,
+    activeFilters,
+    starredFilter,
+    searchTerm,
+    setContactFocus,
     viewMode,
     userOptionsLoading,
   } = React.useContext(AppealsContext) as AppealsType;
+
+  const { data: appealInfo, loading: appealInfoLoading } = useAppealQuery({
+    variables: {
+      accountListId: accountListId ?? '',
+      appealId: appealId ?? '',
+    },
+    skip: !accountListId || !appealId,
+  });
 
   return (
     <>
       <AppealsMainPanelHeader />
       {!userOptionsLoading &&
         (viewMode === TableViewModeEnum.List ? (
-          <DynamicContactsList />
+          <DynamicContactsList
+            appealInfo={appealInfo}
+            appealInfoLoading={appealInfoLoading}
+          />
         ) : (
-          <h3>Flows</h3>
-          // <DynamicContactFlow
-          //   accountListId={accountListId ?? ''}
-          //   selectedFilters={{
-          //     ...activeFilters,
-          //     ...starredFilter,
-          //   }}
-          //   searchTerm={searchTerm}
-          //   onContactSelected={setContactFocus}
-          // />
+          <DynamicContactFlow
+            accountListId={accountListId ?? ''}
+            selectedFilters={{
+              ...activeFilters,
+              ...starredFilter,
+            }}
+            searchTerm={searchTerm}
+            onContactSelected={setContactFocus}
+            appealInfo={appealInfo}
+            appealInfoLoading={appealInfoLoading}
+          />
         ))}
     </>
   );
---
+++ src/components/Tool/Appeal/Flow/ContactFlowRow/ContactFlowRow.test.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import TestWrapper from '__tests__/util/TestWrapper';
+import theme from 'src/theme';
+import { AppealStatusEnum } from '../../AppealsContext/AppealsContext';
+import { ContactFlowRow } from './ContactFlowRow';
+
+const accountListId = 'abc';
+const id = '123';
+const name = 'Test Name';
+const status = AppealStatusEnum.Processed;
+const onContactSelected = jest.fn();
+
+describe('ContactFlowRow', () => {
+  it('should display contact name and status', () => {
+    const { getByText, getByTitle } = render(
+      <DndProvider backend={HTML5Backend}>
+        <ThemeProvider theme={theme}>
+          <TestWrapper>
+            <ContactFlowRow
+              accountListId={accountListId}
+              id={id}
+              name={name}
+              status={status}
+              starred
+              onContactSelected={onContactSelected}
+            />
+          </TestWrapper>
+        </ThemeProvider>
+      </DndProvider>,
+    );
+    expect(getByText('Test Name')).toBeInTheDocument();
+    expect(getByTitle('Filled Star Icon')).toBeInTheDocument();
+  });
+
+  it('should call contact selected function', () => {
+    const { getByText } = render(
+      <DndProvider backend={HTML5Backend}>
+        <ThemeProvider theme={theme}>
+          <TestWrapper>
+            <ContactFlowRow
+              accountListId={accountListId}
+              id={id}
+              name={name}
+              status={status}
+              starred
+              onContactSelected={onContactSelected}
+            />
+          </TestWrapper>
+        </ThemeProvider>
+      </DndProvider>,
+    );
+    userEvent.click(getByText('Test Name'));
+    expect(getByText('Test Name')).toBeInTheDocument();
+    expect(onContactSelected).toHaveBeenCalledWith('123', true, true);
+  });
+});
