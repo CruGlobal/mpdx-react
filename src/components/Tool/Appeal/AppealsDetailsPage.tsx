5441d11d3a1d89046d20a9de82a445cadb5bb26c
Daniel Bisgrove <daniel@bizz-websites.com>
2024-07-30 16:27:54 -0400

    3. Appeal Details Frame - Building main frame.

--- src/components/Tool/Appeal/AppealsDetailsPage.tsx
+++ src/components/Tool/Appeal/AppealsDetailsPage.tsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { SidePanelsLayout } from 'src/components/Layouts/SidePanelsLayout';
+import { headerHeight } from 'src/components/Shared/Header/ListHeader';
+import { AppealsLeftPanel } from './AppealDetails/AppealLeftPanel/AppealsLeftPanel';
+import { AppealsMainPanel } from './AppealDetails/AppealsMainPanel/AppealsMainPanel';
+import { AppealsContext, AppealsType } from './AppealsContext/AppealsContext';
 
 const AppealsDetailsPage: React.FC = () => {
-  return <>Details Page</>;
+  const { filterPanelOpen, contactDetailsOpen } = useContext(
+    AppealsContext,
+  ) as AppealsType;
+
+  return (
+    <SidePanelsLayout
+      leftPanel={<AppealsLeftPanel />}
+      leftOpen={filterPanelOpen}
+      leftWidth="290px"
+      mainContent={<AppealsMainPanel />}
+      rightPanel={<p>Contact</p>}
+      rightOpen={contactDetailsOpen}
+      rightWidth="60%"
+      headerHeight={headerHeight}
+    />
+  );
 };
 
 export default AppealsDetailsPage;
