47072b1428b97037a6c6a6a3dd580059e04c4656
wjames111 <william.james@cru.org>
2024-08-01 17:35:43 -0400

    Adds test for FixCommitmentInfo page.

---
+++ pages/accountLists/[accountListId]/tools/fixCommitmentInfo/[[...contactId]].page.test.tsx
@@ -0,0 +1,114 @@
+import { useRouter } from 'next/router';
+import { ThemeProvider } from '@mui/material/styles';
+import { render, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { ErgonoMockShape } from 'graphql-ergonomock';
+import { getSession } from 'next-auth/react';
+import { SnackbarProvider } from 'notistack';
+import { I18nextProvider } from 'react-i18next';
+import { VirtuosoMockContext } from 'react-virtuoso';
+import TestRouter from '__tests__/util/TestRouter';
+import { GqlMockedProvider } from '__tests__/util/graphqlMocking';
+import { mockInvalidStatusesResponse } from 'src/components/Tool/FixCommitmentInfo/FixCommitmentInfoMocks';
+import { InvalidStatusesQuery } from 'src/components/Tool/FixCommitmentInfo/GetInvalidStatuses.generated';
+import i18n from 'src/lib/i18n';
+import theme from 'src/theme';
+import FixCommitmentInfoPage from './[[...contactId]].page';
+
+jest.mock('next-auth/react');
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(),
+}));
+jest.mock('src/lib/helpScout', () => ({
+  suggestArticles: jest.fn(),
+}));
+jest.mock('notistack', () => ({
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  ...jest.requireActual('notistack'),
+  useSnackbar: () => {
+    return {
+      enqueueSnackbar: jest.fn(),
+    };
+  },
+}));
+
+const pushFn = jest.fn();
+const accountListId = 'account-list-1';
+const session = {
+  expires: '2021-10-28T14:48:20.897Z',
+  user: {
+    email: 'Chair Library Bed',
+    image: null,
+    name: 'Dung Tapestry',
+    token: 'superLongJwtString',
+  },
+};
+const Components = ({
+  mockNodes = mockInvalidStatusesResponse,
+}: {
+  mockNodes?: ErgonoMockShape[];
+}) => (
+  <ThemeProvider theme={theme}>
+    <TestRouter>
+      <GqlMockedProvider<{
+        InvalidStatuses: InvalidStatusesQuery;
+      }>
+        mocks={{
+          InvalidStatuses: {
+            contacts: {
+              nodes: mockNodes,
+            },
+          },
+        }}
+      >
+        <I18nextProvider i18n={i18n}>
+          <SnackbarProvider>
+            <VirtuosoMockContext.Provider
+              value={{ viewportHeight: 1000, itemHeight: 100 }}
+            >
+              <FixCommitmentInfoPage />
+            </VirtuosoMockContext.Provider>
+          </SnackbarProvider>
+        </I18nextProvider>
+      </GqlMockedProvider>
+    </TestRouter>
+  </ThemeProvider>
+);
+
+describe('FixCommitmentInfoPage', () => {
+  beforeEach(() => {
+    (getSession as jest.Mock).mockResolvedValue(session);
+    (useRouter as jest.Mock).mockReturnValue({
+      query: {
+        accountListId,
+      },
+      isReady: true,
+      push: pushFn,
+    });
+  });
+
+  it('should open up contact details', async () => {
+    const { getByText, queryByTestId, getAllByTestId } = render(<Components />);
+    await waitFor(() =>
+      expect(queryByTestId('loading')).not.toBeInTheDocument(),
+    );
+
+    await waitFor(() => {
+      userEvent.click(getAllByTestId('confirmButton')[0]);
+    });
+
+    const contactName = getByText('Tester 2');
+
+    expect(contactName).toBeInTheDocument();
+    userEvent.click(contactName);
+
+    await waitFor(() => {
+      expect(pushFn).toHaveBeenCalledWith({
+        pathname:
+          '/accountLists/account-list-1/tools/fixCommitmentInfo/tester-2',
+        query: { tabKey: 'Donations' },
+      });
+    });
+  });
+});
