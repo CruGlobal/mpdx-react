7c696f1e4994a7f6a7077d8471989d94cb57922f
Daniel Bisgrove <daniel@bizz-websites.com>
2024-08-26 16:27:05 -0400

    6. Appeal Form - Add Appeal Modal

--- src/components/Tool/Appeal/InitialPage/AppealsInitialPage.tsx
+++ src/components/Tool/Appeal/InitialPage/AppealsInitialPage.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Box, Divider, Grid, Theme, Typography } from '@mui/material';
+import {
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  Divider,
+  Grid,
+  Theme,
+  Typography,
+} from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { makeStyles } from 'tss-react/mui';
-import AddAppealForm from 'src/components/Tool/Appeal/InitialPage/AddAppealForm';
+import AddAppealForm from 'src/components/Tool/Appeal/InitialPage/AddAppealForm/AddAppealForm';
 import Appeals from 'src/components/Tool/Appeal/InitialPage/Appeals';
 import { useAccountListId } from 'src/hooks/useAccountListId';
+import theme from 'src/theme';
 
 const useStyles = makeStyles()((theme: Theme) => ({
   container: {
@@ -53,7 +63,19 @@ const AppealsInitialPage: React.FC = () => {
         </Grid>
         <Grid item xs={12} sm={12} md={6}>
           <Box width="100%" display="flex" justifyContent="center">
-            <AddAppealForm accountListId={accountListId || ''} />
+            <Box m={1}>
+              <Card>
+                <CardHeader
+                  title="Add Appeal"
+                  style={{
+                    backgroundColor: theme.palette.cruGrayLight.main,
+                  }}
+                />
+                <CardContent>
+                  <AddAppealForm accountListId={accountListId || ''} />
+                </CardContent>
+              </Card>
+            </Box>
           </Box>
         </Grid>
       </Grid>
---
+++ src/components/Tool/Appeal/Modals/AddAppealModal/AddAppealModal.tsx
@@ -0,0 +1,95 @@
+import React, { useMemo, useRef } from 'react';
+import { DialogActions, DialogContent } from '@mui/material';
+import { FormikValues } from 'formik';
+import { DateTime } from 'luxon';
+import { useTranslation } from 'react-i18next';
+import {
+  CancelButton,
+  SubmitButton,
+} from 'src/components/common/Modal/ActionButtons/ActionButtons';
+import Modal from 'src/components/common/Modal/Modal';
+import { FilterOption } from 'src/graphql/types.generated';
+import { useAccountListId } from 'src/hooks/useAccountListId';
+import AddAppealForm, {
+  ContactExclusion,
+  contactExclusions,
+} from '../../InitialPage/AddAppealForm/AddAppealForm';
+
+interface AddAppealModalProps {
+  appealName: string;
+  appealGoal: number;
+  appealStatuses: FilterOption[];
+  appealExcludes: ContactExclusion[];
+  appealIncludes: object;
+  isEndOfYearAsk: boolean;
+  handleClose: () => void;
+}
+
+export const AddAppealModal: React.FC<AddAppealModalProps> = ({
+  appealName,
+  appealGoal,
+  appealStatuses,
+  appealExcludes = [],
+  appealIncludes = {},
+  isEndOfYearAsk,
+  handleClose,
+}) => {
+  const { t } = useTranslation();
+  const accountListId = useAccountListId();
+  const formRef = useRef<FormikValues>();
+
+  const inclusionFilter = useMemo(() => {
+    if (!isEndOfYearAsk) {
+      return { ...appealIncludes };
+    }
+    const now = DateTime.local();
+    const fiveYears = now.minus({ years: 5 }).toFormat('yyyy-MM-dd');
+    const nowFormatted = now.toFormat('yyyy-MM-dd');
+
+    const includes = {
+      donation: 'one',
+      donation_date: `${fiveYears}..${nowFormatted}`,
+    };
+
+    return {
+      ...appealIncludes,
+      ...includes,
+    };
+  }, [appealIncludes]);
+
+  const exclusionFilter = useMemo(() => {
+    if (!isEndOfYearAsk) {
+      return appealExcludes;
+    }
+    return contactExclusions;
+  }, [appealIncludes]);
+
+  const handleSubmit = async () => {
+    if (formRef.current) {
+      await formRef.current.handleSubmit();
+      handleClose();
+    }
+  };
+
+  return (
+    <Modal isOpen={true} title={t('Add Appeal')} handleClose={handleClose}>
+      <DialogContent dividers>
+        <AddAppealForm
+          accountListId={accountListId ?? ''}
+          appealName={appealName}
+          appealGoal={appealGoal}
+          appealStatuses={appealStatuses}
+          appealExcludes={exclusionFilter}
+          appealIncludes={inclusionFilter}
+          formRef={formRef}
+        />
+      </DialogContent>
+      <DialogActions>
+        <CancelButton onClick={handleClose}>{t('No')}</CancelButton>
+        <SubmitButton type="button" onClick={handleSubmit}>
+          {t('Yes')}
+        </SubmitButton>
+      </DialogActions>
+    </Modal>
+  );
+};
