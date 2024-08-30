0cd13e796526f0446a39c3e78c9ea28f86d03743
Caleb Cox <canac@users.noreply.github.com>
2024-08-30 14:00:40 -0500

    Add makeGetServerSideProps helper

--- pages/api/utils/pagePropsHelpers.ts
+++ pages/api/utils/pagePropsHelpers.ts
@@ -1,4 +1,8 @@
-import { GetServerSideProps } from 'next';
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from 'next';
 import { Session } from 'next-auth';
 import { getSession } from 'next-auth/react';
 
@@ -43,5 +47,58 @@ export const loadSession: GetServerSideProps<PagePropsWithSession> = async (
     props: {
       session,
     },
+  };
+};
+
+// It is a common pattern in getServerSideProps to need to first extract the API
+// token from the session, redirect to the login page if the API token is
+// missing, then use the API token to do custom logic and eventually return the
+// page props. This helper eliminates some of that boilerplate. Use it like this:
+//
+// export const getServerSideProps = makeGetServerSideProps<PageProps>(async (session) => {
+//   const ssrClient = makeSsrClient(session.user.apiToken);
+//   // Use the ssrClient
+//   return {
+//     props: {
+//       pageData,
+//     },
+//   };
+// })
+export const makeGetServerSideProps = <PageProps = Record<string, unknown>>(
+  getServerSidePropsFromSession: (
+    session: Session,
+    context: GetServerSidePropsContext,
+  ) => Promise<GetServerSidePropsResult<PageProps>>,
+): GetServerSideProps<PageProps & PagePropsWithSession> => {
+  // Return a getServerSideProps method
+  return async (context) => {
+    // Start by loading the session and redirecting if it is missing
+    const session = await getSession(context);
+    if (!session) {
+      return {
+        redirect: {
+          destination: '/login',
+          permanent: false,
+        },
+      };
+    }
+
+    // Pass the session off to the page's custom logic to build out the page props
+    const result = await getServerSidePropsFromSession(session, context);
+    if ('props' in result) {
+      // Add the session to the returned props
+      return {
+        props: {
+          session,
+          // Props can either be a promise or the actual props
+          ...(result.props instanceof Promise
+            ? await result.props
+            : result.props),
+        },
+      };
+    } else {
+      // Pass redirects and not found results through without modification
+      return result;
+    }
   };
 };
