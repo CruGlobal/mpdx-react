d4710dafd06edb3e6b05c56a046fb32f6685b9ea
Caleb Cox <canac@users.noreply.github.com>
2024-09-26 13:14:46 -0500

    Remove useless key

--- src/components/Contacts/ContactFlow/ContactFlowSetup/DropZone/ContactFlowSetupDropZone.tsx
+++ src/components/Contacts/ContactFlow/ContactFlowSetup/DropZone/ContactFlowSetupDropZone.tsx
@@ -41,7 +41,6 @@ export const ContactFlowSetupDropZone: React.FC<Props> = ({
 
   return (
     <Box
-      key={status}
       ref={drop}
       display={canDrop ? 'flex' : 'none'}
       justifyContent="start"
