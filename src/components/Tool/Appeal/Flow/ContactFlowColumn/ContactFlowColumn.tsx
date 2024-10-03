df129f0985568aa716a1745244a50d64dd245e9f
Caleb Cox <canac@users.noreply.github.com>
2024-10-03 11:46:50 -0500

    Localize appeal status when dragging

--- src/components/Tool/Appeal/Flow/ContactFlowColumn/ContactFlowColumn.tsx
+++ src/components/Tool/Appeal/Flow/ContactFlowColumn/ContactFlowColumn.tsx
@@ -214,6 +214,7 @@ export const ContactFlowColumn: React.FC<Props> = ({
           ref={drop}
         >
           <ContactFlowDropZone
+            title={title}
             status={appealStatus}
             changeContactStatus={changeContactStatus}
           />
