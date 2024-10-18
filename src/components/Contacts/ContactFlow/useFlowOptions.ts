f34357d3b3c12f5f85df60ff983d4656a5099f46
Caleb Alldrin <caleb.alldrin@cru.org>
2024-10-18 10:44:20 -0700

    Consolidate converting old Cru statuses

--- src/components/Contacts/ContactFlow/useFlowOptions.ts
+++ src/components/Contacts/ContactFlow/useFlowOptions.ts
@@ -1,76 +1,7 @@
 import { useMemo } from 'react';
 import { StatusEnum } from 'src/graphql/types.generated';
+import { convertStatus } from 'src/utils/functions/convertContactStatus';
 import { useGetUserOptionsQuery } from './GetUserOptions.generated';
-
-// Convert a status string from flow options into a StatusEnum
-const convertFlowOptionStatus = (status: string): StatusEnum | null => {
-  // Flow options statuses are StatusEnum values (i.e. "NEVER_CONTACTED") after task phases
-  // Flow options statuses were in sentence case with spaces (i.e. "Never Contacted") before task phases
-  switch (status) {
-    case 'NEVER_CONTACTED':
-    case 'Never Contacted':
-      return StatusEnum.NeverContacted;
-
-    case 'ASK_IN_FUTURE':
-    case 'Ask in Future':
-      return StatusEnum.AskInFuture;
-
-    case 'CULTIVATE_RELATIONSHIP':
-    case 'Cultivate Relationship':
-      return StatusEnum.CultivateRelationship;
-
-    case 'CONTACT_FOR_APPOINTMENT':
-    case 'Contact for Appointment':
-      return StatusEnum.ContactForAppointment;
-
-    case 'APPOINTMENT_SCHEDULED':
-    case 'Appointment Scheduled':
-      return StatusEnum.AppointmentScheduled;
-
-    case 'CALL_FOR_DECISION':
-    case 'Call for Decision':
-      return StatusEnum.CallForDecision;
-
-    case 'PARTNER_FINANCIAL':
-    case 'Partner - Financial':
-      return StatusEnum.PartnerFinancial;
-
-    case 'PARTNER_SPECIAL':
-    case 'Partner - Special':
-      return StatusEnum.PartnerSpecial;
-
-    case 'PARTNER_PRAY':
-    case 'Partner - Pray':
-      return StatusEnum.PartnerPray;
-
-    case 'NOT_INTERESTED':
-    case 'Not Interested':
-      return StatusEnum.NotInterested;
-
-    case 'UNRESPONSIVE':
-    case 'UNRESPONSIVE':
-      return StatusEnum.Unresponsive;
-
-    case 'NEVER_ASK':
-    case 'Never Ask':
-      return StatusEnum.NeverAsk;
-
-    case 'RESEARCH_ABANDONED':
-    case 'Research Abandoned':
-      return StatusEnum.ResearchAbandoned;
-
-    case 'RESEARCH_CONTACT_INFO':
-    case 'Research Contact Info':
-      return StatusEnum.ResearchContactInfo;
-
-    case 'EXPIRED_REFERRAL':
-    case 'Expired Referral':
-      return StatusEnum.ExpiredReferral;
-
-    default:
-      return null;
-  }
-};
 
 const isTruthy = <T>(value: T): value is NonNullable<T> => Boolean(value);
 
@@ -100,7 +31,7 @@ export const useFlowOptions = (): UseFlowOptionReturn => {
     return rawOptions.map((option) => ({
       ...option,
       statuses: option.statuses
-        .map((status) => convertFlowOptionStatus(status))
+        .map((status) => convertStatus(status))
         // Ignore null values that didn't match a valid status
         .filter(isTruthy),
     }));
