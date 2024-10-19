0bfc3ad2b99aa03c372b4d16955b8a4bf4698333
Caleb Alldrin <caleb.alldrin@cru.org>
2024-10-18 20:19:15 -0700

    Add tests

--- src/utils/functions/convertContactStatus.test.ts
+++ src/utils/functions/convertContactStatus.test.ts
@@ -3,10 +3,6 @@ import { convertStatus } from './convertContactStatus';
 
 describe('convertContactStatus', () => {
   it('converts to StatusEnum or null', () => {
-    // old status
-    expect(convertStatus('Partner - Financial')).toEqual(
-      StatusEnum.PartnerFinancial,
-    );
     // new lowercase status
     expect(convertStatus('partner_financial')).toEqual(
       StatusEnum.PartnerFinancial,
@@ -19,5 +15,29 @@ describe('convertContactStatus', () => {
     expect(convertStatus('hidden')).toEqual(null);
     expect(convertStatus(undefined)).toEqual(null);
     expect(convertStatus('')).toEqual(null);
+  });
+
+  it('converts old status format to StatusEnum or null', () => {
+    expect(convertStatus('Partner - Financial')).toEqual(
+      StatusEnum.PartnerFinancial,
+    );
+    expect(convertStatus('Partner - Special')).toEqual(
+      StatusEnum.PartnerSpecial,
+    );
+    expect(convertStatus('Call for Decision')).toEqual(
+      StatusEnum.CallForDecision,
+    );
+    expect(convertStatus('Expired Referral')).toEqual(
+      StatusEnum.ExpiredReferral,
+    );
+    expect(convertStatus('Cultivate Relationship')).toEqual(
+      StatusEnum.CultivateRelationship,
+    );
+    expect(convertStatus('Partner - Pray')).toEqual(StatusEnum.PartnerPray);
+    expect(convertStatus('Ask in Future')).toEqual(StatusEnum.AskInFuture);
+    expect(convertStatus('Not Interested')).toEqual(StatusEnum.NotInterested);
+    expect(convertStatus('Never Contacted')).toEqual(StatusEnum.NeverContacted);
+    expect(convertStatus('Unresponsive')).toEqual(StatusEnum.Unresponsive);
+    expect(convertStatus('Never Ask')).toEqual(StatusEnum.NeverAsk);
   });
 });
