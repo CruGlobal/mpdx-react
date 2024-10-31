9627353b83adfca4d1c116ec2dffc76f58f2f99e
Caleb Cox <canac@users.noreply.github.com>
2024-10-31 13:29:38 -0500

    Add the current URL to server-side login redirects

--- pages/organizations/[orgId]/accept_invite/[inviteId].page.tsx
+++ pages/organizations/[orgId]/accept_invite/[inviteId].page.tsx
@@ -1,32 +1,27 @@
 import { GetServerSideProps } from 'next';
 import { ReactNode } from 'react';
 import { getSession } from 'next-auth/react';
+import { loginRedirect } from 'pages/api/utils/pagePropsHelpers';
 
 // This page redirect old email invite links to the new page that handles invites
 const OrgInvitePage = (): ReactNode => null;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getSession(context);
-  const { orgId, inviteId, code } = context.query;
+  if (!session) {
+    return loginRedirect(context);
+  }
 
+  const { orgId, inviteId, code } = context.query;
   const redirectURL =
     orgId && inviteId && code
       ? `/acceptInvite?orgId=${orgId}&orgInviteId=${inviteId}&inviteCode=${code}`
       : // Intentionally redirect to invalid accountListId since we don't have the current user's accountListId
         '/accountLists/_/';
-
   return {
-    redirect: session
-      ? {
-          destination: redirectURL,
-          permanent: true,
-        }
-      : {
-          destination: '/login',
-          permanent: false,
-        },
-    props: {
-      session,
+    redirect: {
+      destination: redirectURL,
+      permanent: true,
     },
   };
 };
