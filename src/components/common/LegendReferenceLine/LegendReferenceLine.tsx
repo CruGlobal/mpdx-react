e3940ad49c82d7783de2f089e86973e95f980dcf
Caleb Cox <canac@users.noreply.github.com>
2024-12-09 14:34:01 -0600

    fixup! Create reusable LegendReferenceLine component

--- src/components/common/LegendReferenceLine/LegendReferenceLine.tsx
+++ src/components/common/LegendReferenceLine/LegendReferenceLine.tsx
@@ -15,6 +15,13 @@ const Container = styled(Box)(({ theme }) => ({
   gap: theme.spacing(1),
 }));
 
+const Line = styled(Box)({
+  display: 'inline-block',
+  height: '5px',
+  width: '20px',
+  borderRadius: '5px',
+});
+
 // This component shows the color, name, and value of a reference line in a recharts graph. It
 // should be put in the header of the card containing the graph.
 export const LegendReferenceLine: React.FC<LegendReferenceLineProps> = ({
@@ -23,15 +30,7 @@ export const LegendReferenceLine: React.FC<LegendReferenceLineProps> = ({
   color,
 }) => (
   <Container>
-    <Box
-      sx={{
-        display: 'inline-block',
-        height: '5px',
-        width: '20px',
-        borderRadius: '5px',
-        backgroundColor: color,
-      }}
-    />
+    <Line sx={{ backgroundColor: color }} />
     <Typography variant="body1" component="span">
       <strong>{name}</strong> {value}
     </Typography>
