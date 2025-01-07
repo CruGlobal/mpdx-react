4c58065d2383b6884c4a649bd25aa80197f0423a
Daniel Bisgrove <daniel@bizz-websites.com>
2025-01-07 10:46:28 -0500

    Show relationship code on the partnershipInfo

--- src/components/Contacts/ContactDetails/ContactDonationsTab/PartnershipInfo/PartnershipInfo.tsx
+++ src/components/Contacts/ContactDetails/ContactDonationsTab/PartnershipInfo/PartnershipInfo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import CheckCircleOutline from '@mui/icons-material/CheckCircleOutline';
 import Clear from '@mui/icons-material/Clear';
 import CreateIcon from '@mui/icons-material/Create';
@@ -16,6 +16,9 @@ import { currencyFormat } from '../../../../../lib/intlFormat';
 import { HandshakeIcon } from '../../ContactDetailsHeader/ContactHeaderSection/HandshakeIcon';
 import { ContactDonorAccountsFragment } from '../ContactDonationsTab.generated';
 import { EditPartnershipInfoModal } from './EditPartnershipInfoModal/EditPartnershipInfoModal';
+import { useUserOrganizationAccountsQuery } from './PartnershipInfo.generated';
+
+export const SwitzerlandOrganizationName = 'Campus fuer Christus Switzerland';
 
 const IconAndTextContainer = styled(Box)(({ theme }) => ({
   margin: theme.spacing(0, 4),
@@ -73,6 +76,18 @@ export const PartnershipInfo: React.FC<PartnershipInfoProp> = ({ contact }) => {
 
   const [editPartnershipModalOpen, setEditPartnershipModalOpen] =
     useState(false);
+
+  const { data } = useUserOrganizationAccountsQuery();
+  const userOrganizationAccounts = data?.userOrganizationAccounts;
+
+  const showRelationshipCode = useMemo(
+    () =>
+      userOrganizationAccounts?.some(
+        (organizationAccount) =>
+          organizationAccount.organization.name === SwitzerlandOrganizationName,
+      ) ?? false,
+    [userOrganizationAccounts],
+  );
 
   return (
     <PartnershipInfoContainer>
@@ -196,6 +211,21 @@ export const PartnershipInfo: React.FC<PartnershipInfoProp> = ({ contact }) => {
           )}
         </LabelsAndText>
       </IconAndTextContainerCenter>
+
+      {showRelationshipCode && (
+        <IconAndTextContainerCenter>
+          <IconContainer>
+            <ClearIcon />
+          </IconContainer>
+          <LabelsAndText variant="subtitle1" color="textSecondary">
+            {t('Relationship Code')}
+          </LabelsAndText>
+          <LabelsAndText variant="subtitle1">
+            {contact?.relationshipCode}
+          </LabelsAndText>
+        </IconAndTextContainerCenter>
+      )}
+
       <IconAndTextContainerCenter>
         <IconContainer>
           <ClearIcon />
@@ -239,6 +269,7 @@ export const PartnershipInfo: React.FC<PartnershipInfoProp> = ({ contact }) => {
       {contact && editPartnershipModalOpen ? (
         <EditPartnershipInfoModal
           handleClose={() => setEditPartnershipModalOpen(false)}
+          showRelationshipCode={showRelationshipCode}
           contact={contact}
         />
       ) : null}
