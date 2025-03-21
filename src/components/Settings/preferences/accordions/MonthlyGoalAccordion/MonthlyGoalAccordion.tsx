4239c6702e3848abe12bd77719cc1ef64caf2b28
Caleb Cox <canac@users.noreply.github.com>
2025-03-21 16:26:12 -0500

    Use getHealthIndicatorInfo in MonthlyGoalAccordion

--- src/components/Settings/preferences/accordions/MonthlyGoalAccordion/MonthlyGoalAccordion.tsx
+++ src/components/Settings/preferences/accordions/MonthlyGoalAccordion/MonthlyGoalAccordion.tsx
@@ -2,7 +2,6 @@ import React, { ReactElement, useMemo } from 'react';
 import WarningIcon from '@mui/icons-material/Warning';
 import { Box, Button, TextField, Tooltip, Typography } from '@mui/material';
 import { Formik } from 'formik';
-import { DateTime } from 'luxon';
 import { useSnackbar } from 'notistack';
 import { useTranslation } from 'react-i18next';
 import * as yup from 'yup';
@@ -11,6 +10,7 @@ import { AccordionItem } from 'src/components/Shared/Forms/Accordions/AccordionI
 import { FieldWrapper } from 'src/components/Shared/Forms/FieldWrapper';
 import { AccountListSettingsInput } from 'src/graphql/types.generated';
 import { useLocale } from 'src/hooks/useLocale';
+import { GoalSource, getHealthIndicatorInfo } from 'src/lib/healthIndicator';
 import { currencyFormat, dateFormat, numberFormat } from 'src/lib/intlFormat';
 import { AccordionProps } from '../../../accordionHelper';
 import { useUpdateAccountPreferencesMutation } from '../UpdateAccountPreferences.generated';
@@ -26,9 +26,9 @@ const formatMonthlyGoal = (
   goal: number | null,
   currency: string | null,
   locale: string,
-): string => {
+): string | null => {
   if (goal === null) {
-    return '';
+    return null;
   }
 
   if (currency) {
@@ -68,59 +68,54 @@ export const MonthlyGoalAccordion: React.FC<MonthlyGoalAccordionProps> = ({
       accountListId,
     },
   });
+
+  const accountList = currency
+    ? { currency, monthlyGoal: initialMonthlyGoal, monthlyGoalUpdatedAt }
+    : null;
+  const healthIndicatorData = data?.healthIndicatorData.at(-1);
   const {
-    machineCalculatedGoal: calculatedGoal,
-    machineCalculatedGoalCurrency: calculatedCurrency,
-  } = data?.healthIndicatorData.at(-1) ?? {};
+    goalSource,
+    machineCalculatedGoalCurrency,
+    unsafeMachineCalculatedGoal,
+    preferencesGoalLow,
+    preferencesGoalUpdatedAt,
+  } = getHealthIndicatorInfo(accountList, healthIndicatorData);
+
   const formattedCalculatedGoal = useMemo(
     () =>
       formatMonthlyGoal(
-        calculatedGoal ?? null,
-        calculatedCurrency ?? null,
+        unsafeMachineCalculatedGoal,
+        machineCalculatedGoalCurrency,
         locale,
       ),
-    [calculatedGoal, calculatedCurrency, locale],
+    [unsafeMachineCalculatedGoal, machineCalculatedGoalCurrency, locale],
   );
 
   const accordionValue = useMemo(() => {
     const goal = formatMonthlyGoal(initialMonthlyGoal, currency, locale);
 
-    if (initialMonthlyGoal === null) {
-      if (typeof calculatedGoal === 'number') {
-        // There is no preferences goal, but there is a machine-calculated goal
-        return t('{{goal}} (estimated)', { goal: formattedCalculatedGoal });
+    if (goalSource === GoalSource.Preferences) {
+      if (preferencesGoalLow) {
+        return (
+          <Typography component="span" color="statusWarning.main">
+            {t('{{goal}} (below machine-calculated support goal)', { goal })}
+          </Typography>
+        );
+      } else if (preferencesGoalUpdatedAt) {
+        return t('{{goal}} (last updated {{updated}})', {
+          goal,
+          updated: dateFormat(preferencesGoalUpdatedAt, locale),
+        });
       } else {
-        // There is no preferences goal or machine-calculated goal
-        return null;
+        return goal;
       }
-    } else if (
-      typeof currency === 'string' &&
-      currency === calculatedCurrency &&
-      typeof calculatedGoal === 'number' &&
-      initialMonthlyGoal < calculatedGoal
-    ) {
-      // Staff-entered goal is below machine-calculated goal
-      return (
-        <Typography component="span" color="statusWarning.main">
-          {t('{{goal}} (below machine-calculated support goal)', { goal })}
-        </Typography>
-      );
-    } else if (monthlyGoalUpdatedAt) {
-      // Staff-entered goal has an updated date
-      const date = DateTime.fromISO(monthlyGoalUpdatedAt);
-      return t('{{goal}} (last updated {{updated}})', {
-        goal,
-        updated: dateFormat(date, locale),
-      });
-    } else {
-      // Staff-entered goal does not have an updated date
-      return goal;
+    } else if (formattedCalculatedGoal !== null) {
+      return t('{{goal}} (estimated)', { goal: formattedCalculatedGoal });
     }
   }, [
     initialMonthlyGoal,
-    calculatedGoal,
     formattedCalculatedGoal,
-    monthlyGoalUpdatedAt,
+    preferencesGoalUpdatedAt,
     currency,
     locale,
   ]);
@@ -154,13 +149,13 @@ export const MonthlyGoalAccordion: React.FC<MonthlyGoalAccordionProps> = ({
   };
 
   const getInstructions = () => {
-    if (typeof calculatedGoal !== 'number') {
+    if (unsafeMachineCalculatedGoal === null) {
       return t(
         'This amount should be set to the amount your organization has determined is your target monthly goal. If you do not know, make your best guess for now. You can change it at any time.',
       );
     }
 
-    if (initialMonthlyGoal) {
+    if (goalSource === GoalSource.MachineCalculated) {
       return t(
         'Based on the past year, NetSuite estimates that you need at least {{goal}} of monthly support. You can choose your own target monthly goal or leave it blank to use the estimate.',
         { goal: formattedCalculatedGoal },
@@ -175,9 +170,12 @@ export const MonthlyGoalAccordion: React.FC<MonthlyGoalAccordionProps> = ({
 
   const getWarning = (currentGoal: number | null) => {
     if (
-      typeof currentGoal === 'number' &&
-      typeof calculatedGoal === 'number' &&
-      currentGoal < calculatedGoal
+      currentGoal &&
+      accountList &&
+      getHealthIndicatorInfo(
+        { ...accountList, monthlyGoal: currentGoal },
+        healthIndicatorData,
+      ).preferencesGoalLow
     ) {
       return (
         <Typography
@@ -258,26 +256,27 @@ export const MonthlyGoalAccordion: React.FC<MonthlyGoalAccordionProps> = ({
               >
                 {t('Save')}
               </Button>
-              {calculatedGoal && initialMonthlyGoal !== null && (
-                <Tooltip
-                  title={t(
-                    'Reset to NetSuite estimated goal of {{calculatedGoal}}',
-                    {
-                      calculatedGoal: formattedCalculatedGoal,
-                    },
-                  )}
-                >
-                  <Button
-                    variant="outlined"
-                    type="button"
-                    onClick={() => {
-                      onSubmit({ monthlyGoal: null });
-                    }}
+              {unsafeMachineCalculatedGoal !== null &&
+                goalSource === GoalSource.Preferences && (
+                  <Tooltip
+                    title={t(
+                      'Reset to NetSuite estimated goal of {{calculatedGoal}}',
+                      {
+                        calculatedGoal: formattedCalculatedGoal,
+                      },
+                    )}
                   >
-                    {t('Reset to Calculated Goal')}
-                  </Button>
-                </Tooltip>
-              )}
+                    <Button
+                      variant="outlined"
+                      type="button"
+                      onClick={() => {
+                        onSubmit({ monthlyGoal: null });
+                      }}
+                    >
+                      {t('Reset to Calculated Goal')}
+                    </Button>
+                  </Tooltip>
+                )}
             </Box>
           </form>
         )}
