d84b3bd5b6bb54484f96a4fe1cea3eb6d273667f
Caleb Cox <canac@users.noreply.github.com>
2025-03-26 16:30:29 -0500

    Extrapolate missing HI periods

--- src/components/Reports/HealthIndicatorReport/HealthIndicatorGraph/useGraphData.test.tsx
+++ src/components/Reports/HealthIndicatorReport/HealthIndicatorGraph/useGraphData.test.tsx
@@ -1,8 +1,34 @@
 import { ReactElement } from 'react';
 import { renderHook } from '@testing-library/react-hooks';
-import { GqlMockedProvider } from '__tests__/util/graphqlMocking';
-import { HealthIndicatorGraphQuery } from './HealthIndicatorGraph.generated';
-import { useGraphData } from './useGraphData';
+import { GqlMockedProvider, gqlMock } from '__tests__/util/graphqlMocking';
+import { HealthIndicatorQueryVariables } from 'src/components/Dashboard/MonthlyGoal/HealthIndicator.generated';
+import {
+  HealthIndicatorGraphDocument,
+  HealthIndicatorGraphQuery,
+} from './HealthIndicatorGraph.generated';
+import {
+  calculatePeriodSpans,
+  uniqueMonths,
+  useGraphData,
+  weightedAverage,
+} from './useGraphData';
+
+const AverageWrapper = ({ children }: { children: ReactElement }) => (
+  <GqlMockedProvider<{ HealthIndicatorGraph: HealthIndicatorGraphQuery }>
+    mocks={{
+      HealthIndicatorGraph: {
+        healthIndicatorData: [
+          { indicationPeriodBegin: '2024-01-10', overallHi: 10 },
+          { indicationPeriodBegin: '2024-02-15', overallHi: null },
+          { indicationPeriodBegin: '2024-02-25', overallHi: 80 },
+          { indicationPeriodBegin: '2024-02-26', overallHi: 90 },
+        ],
+      },
+    }}
+  >
+    {children}
+  </GqlMockedProvider>
+);
 
 const Wrapper = ({ children }: { children: ReactElement }) => (
   <GqlMockedProvider<{ HealthIndicatorGraph: HealthIndicatorGraphQuery }>
@@ -10,28 +36,46 @@ const Wrapper = ({ children }: { children: ReactElement }) => (
       HealthIndicatorGraph: {
         healthIndicatorData: [
           {
-            indicationPeriodBegin: '2024-01-01',
-            overallHi: 33,
-            consistencyHi: 7,
-            depthHi: 21,
-            ownershipHi: 35,
-            successHi: 49,
+            indicationPeriodBegin: '2024-01-10',
+            consistencyHi: 10,
+            depthHi: 10,
+            ownershipHi: 10,
+            successHi: 10,
           },
           {
-            indicationPeriodBegin: '2024-02-01',
-            overallHi: null,
+            indicationPeriodBegin: '2024-01-31',
+            consistencyHi: 40,
+            depthHi: 40,
+            ownershipHi: 40,
+            successHi: 40,
+          },
+          {
+            indicationPeriodBegin: '2024-02-15',
             consistencyHi: null,
             depthHi: null,
             ownershipHi: null,
             successHi: null,
           },
           {
-            indicationPeriodBegin: '2024-03-01',
-            overallHi: 40,
-            consistencyHi: 14,
-            depthHi: 28,
-            ownershipHi: 42,
-            successHi: 56,
+            indicationPeriodBegin: '2024-03-04',
+            consistencyHi: 40,
+            depthHi: 40,
+            ownershipHi: 40,
+            successHi: 40,
+          },
+          {
+            indicationPeriodBegin: '2024-03-06',
+            consistencyHi: null,
+            depthHi: null,
+            ownershipHi: null,
+            successHi: null,
+          },
+          {
+            indicationPeriodBegin: '2024-03-10',
+            consistencyHi: 50,
+            depthHi: 50,
+            ownershipHi: 50,
+            successHi: 50,
           },
         ],
       },
@@ -42,6 +86,141 @@ const Wrapper = ({ children }: { children: ReactElement }) => (
 );
 
 const accountListId = 'account-list-1';
+
+describe('weightedAverage', () => {
+  it('returns null when items is empty', () => {
+    expect(weightedAverage([], 'field', [])).toBeNull();
+  });
+
+  it('returns null when items contains only missing values', () => {
+    expect(
+      weightedAverage([{ field: null }, { field: undefined }], 'field', [1, 1]),
+    ).toBeNull();
+  });
+
+  it('calculates the weighted average of the field', () => {
+    expect(
+      weightedAverage(
+        [{ field: 1 }, { field: 3 }, { field: 5 }],
+        'field',
+        [1, 2, 1],
+      ),
+    ).toBe(3);
+  });
+
+  it('ignores missing values', () => {
+    expect(
+      weightedAverage(
+        [
+          { field: 1 },
+          { field: null },
+          { field: 2 },
+          { field: undefined },
+          { field: 3 },
+        ],
+        'field',
+        [1, 1, 1, 1, 1],
+      ),
+    ).toBe(2);
+  });
+});
+
+describe('uniqueMonths', () => {
+  it('returns an empty set when data is undefined', () => {
+    expect(uniqueMonths(undefined)).toEqual(new Set());
+  });
+
+  it('returns the unique months', () => {
+    const data = gqlMock<
+      HealthIndicatorGraphQuery,
+      HealthIndicatorQueryVariables
+    >(HealthIndicatorGraphDocument, {
+      variables: { accountListId },
+      mocks: {
+        healthIndicatorData: [
+          { indicationPeriodBegin: '2024-01-01' },
+          { indicationPeriodBegin: '2024-01-02' },
+          { indicationPeriodBegin: '2024-01-03' },
+          { indicationPeriodBegin: '2024-02-04' },
+          { indicationPeriodBegin: '2024-04-05' },
+        ],
+      },
+    });
+    expect(uniqueMonths(data)).toEqual(
+      new Set(['2024-01', '2024-02', '2024-04']),
+    );
+  });
+});
+
+const makePeriod = (
+  indicationPeriodBegin: string,
+): HealthIndicatorGraphQuery['healthIndicatorData'][number] => ({
+  id: '',
+  indicationPeriodBegin,
+});
+
+describe('calculatePeriodSpans', () => {
+  it('returns all 1s when no periods are missing', () => {
+    expect(
+      calculatePeriodSpans([
+        makePeriod('2024-01-05'),
+        makePeriod('2024-01-06'),
+        makePeriod('2024-01-07'),
+        makePeriod('2024-01-08'),
+        makePeriod('2024-01-09'),
+        makePeriod('2024-01-10'),
+      ]),
+    ).toEqual([1, 1, 1, 1, 1, 1]);
+  });
+
+  it('returns an empty array when there are no periods', () => {
+    expect(calculatePeriodSpans([])).toEqual([]);
+  });
+
+  it('extrapolates missing days', () => {
+    expect(
+      calculatePeriodSpans([
+        makePeriod('2024-01-05'),
+        makePeriod('2024-01-15'),
+        makePeriod('2024-01-28'),
+      ]),
+    ).toEqual([
+      10, // January 5-14
+      13, // January 15-27
+      1, // January 28
+    ]);
+  });
+
+  it('handles spanning multiple months', () => {
+    expect(
+      calculatePeriodSpans([
+        makePeriod('2024-01-30'),
+        makePeriod('2024-02-03'),
+        makePeriod('2024-03-03'),
+        makePeriod('2024-03-04'),
+        makePeriod('2024-03-06'),
+      ]),
+    ).toEqual([
+      4, // January 30-February 2
+      29, // February 2-March 2
+      1, // March 3
+      2, // March 4-5
+      1, // March 6
+    ]);
+  });
+
+  it('handles spanning daylight savings start', () => {
+    expect(
+      calculatePeriodSpans([
+        makePeriod('2024-03-09'),
+        makePeriod('2024-03-10'),
+      ]),
+    ).toEqual([
+      1, // March 9
+      1, // March 10
+    ]);
+  });
+});
 
 describe('useGraphData', () => {
   it('loading is true while the data is loading', async () => {
@@ -61,16 +240,21 @@ describe('useGraphData', () => {
     const { result, waitForNextUpdate } = renderHook(
       () => useGraphData(accountListId),
       {
-        wrapper: Wrapper,
+        wrapper: AverageWrapper,
       },
     );
 
     expect(result.current.average).toBe(null);
     await waitForNextUpdate();
-    expect(result.current.average).toBe(37); // (33 + 44) / 2, rounded
+    // Jan 10 - Feb 14 = 36 span * 10 HI
+    // Feb 15 - Feb 24 = 10 span * null HI
+    // Feb 25          = 1 span  * 80 HI
+    // Feb 26          = 1 span  * 90 HI
+    // Average = (36*10 + 1*80 + 1*90) / 38 = ~13.95, rounds to 14
+    expect(result.current.average).toBe(14);
   });
 
-  it('calculates the periods', async () => {
+  it('extrapolates missing periods and averages periods in the same month', async () => {
     const { result, waitForNextUpdate } = renderHook(
       () => useGraphData(accountListId),
       {
@@ -82,17 +266,21 @@ describe('useGraphData', () => {
     await waitForNextUpdate();
     expect(result.current.periods).toEqual([
       {
+        // Jan 10 - Jan 30 = 21 span * 10 HI
+        // Jan 31          = 1 span  * 40 HI
+        // Average = (21*10 + 1*40) / 22 = ~11.36, rounds to 11
         month: 'Jan 2024',
-        consistency: 7,
-        depth: 21,
-        ownership: 35,
-        success: 49,
-        consistencyScaled: 1,
-        depthScaled: 3,
-        ownershipScaled: 15,
-        successScaled: 14,
+        consistency: 11,
+        depth: 11,
+        ownership: 11,
+        success: 11,
+        consistencyScaled: 2,
+        depthScaled: 2,
+        ownershipScaled: 5,
+        successScaled: 3,
       },
       {
+        // No February periods, so all indicators are null
         month: 'Feb 2024',
         consistency: null,
         depth: null,
@@ -104,15 +292,19 @@ describe('useGraphData', () => {
         successScaled: null,
       },
       {
+        // Mar 4 - Mar 5 = 2 span * 40 HI
+        // Mar 6 - Mar 9 = 4 span * null HI
+        // Mar 10        = 1 span * 50 HI
+        // Average = (2*40 + 1*50) / 3 = ~43.33, rounds to 43
         month: 'Mar 2024',
-        consistency: 14,
-        consistencyScaled: 2,
-        depth: 28,
-        depthScaled: 4,
-        ownership: 42,
-        ownershipScaled: 18,
-        success: 56,
-        successScaled: 16,
+        consistency: 43,
+        depth: 43,
+        ownership: 43,
+        success: 43,
+        consistencyScaled: 6,
+        depthScaled: 6,
+        ownershipScaled: 19,
+        successScaled: 12,
       },
     ]);
   });
--- src/components/Reports/HealthIndicatorReport/HealthIndicatorGraph/useGraphData.ts
+++ src/components/Reports/HealthIndicatorReport/HealthIndicatorGraph/useGraphData.ts
@@ -1,8 +1,11 @@
+import { useMemo } from 'react';
 import { DateTime } from 'luxon';
 import { useLocale } from 'src/hooks/useLocale';
 import { monthYearFormat } from 'src/lib/intlFormat';
-import { isNotNullish } from 'src/lib/typeGuards';
-import { useHealthIndicatorGraphQuery } from './HealthIndicatorGraph.generated';
+import {
+  HealthIndicatorGraphQuery,
+  useHealthIndicatorGraphQuery,
+} from './HealthIndicatorGraph.generated';
 
 export interface Period {
   month: string;
@@ -23,11 +26,94 @@ interface UseGraphDataResult {
 }
 
 // Scale a health indicator value by its weight in the overall calculation
-const scale = (
-  value: number | null | undefined,
-  weight = 1,
-): number | null | undefined => {
-  return typeof value === 'number' ? (value * weight) / 7 : value;
+const scale = (value: number | null, weight = 1): number | null => {
+  return value === null ? null : (value * weight) / 7;
+};
+
+// Round a health indicator value if it is set
+const round = (value: number | null): number | null => {
+  return value === null ? null : Math.round(value);
+};
+
+/**
+ * Calculate the weighted average value of a particular field in an array of items, ignoring
+ * missing values.
+ *
+ * @param items An array of records with a property {@link field} that is `number | null | undefined`
+ * @param field The field in {@link items} to be averaged
+ * @param weights An array of each item's weight. It must have the same length as {@link items}.
+ * @returns `null` if no records had the field, or the average otherwise
+ */
+export const weightedAverage = <
+  Item extends { [_ in Field]?: number | null | undefined },
+  Field extends keyof Item,
+>(
+  items: Array<Item>,
+  field: Field,
+  weights: number[],
+): number | null => {
+  const { total, denominator } = items.reduce(
+    ({ total, denominator }, item, index) => {
+      const value = item[field];
+      if (typeof value !== 'number') {
+        // Ignore missing values
+        return { total, denominator };
+      }
+
+      const weight = weights[index];
+      return {
+        total: total + value * weight,
+        denominator: denominator + weight,
+      };
+    },
+    { total: 0, denominator: 0 },
+  );
+
+  return denominator === 0 ? null : total / denominator;
+};
+
+/**
+ * Calculate the unique months represented in the health indicator periods.
+ */
+export const uniqueMonths = (
+  data: HealthIndicatorGraphQuery | undefined,
+): Set<string> => {
+  return new Set(
+    data?.healthIndicatorData.map((period) =>
+      // Extract the year and month from the ISO timestamp
+      period.indicationPeriodBegin.slice(0, 7),
+    ),
+  );
+};
+
+/**
+ * If there are missing periods, we need to extrapolate the existing periods to fill in the missing
+ * ones. When there are missing periods, the periods around them will need to span multiple days
+ * instead of just a single day. This code calculates how many days each period spans.
+ *
+ * For example:
+ * - In a month without missing periods, this will be an array of 1s, i.e. [1, 1, 1, 1, 1, ...].
+ * - In January with periods for the 5th, 15th, and 25th, this will be [10, 10, 1] because the
+ *   first period spans from January 5-14, the second spans from January 15-24, and the third
+ *   only covers January 25.
+ *
+ * See the test cases for more examples of the expected outputs of various inputs.
+ */
+export const calculatePeriodSpans = (
+  periods: HealthIndicatorGraphQuery['healthIndicatorData'],
+): number[] => {
+  return periods.map((period, index) => {
+    // The last period always has a span of 1
+    if (index === periods.length - 1) {
+      return 1;
+    }
+
+    const start = DateTime.fromISO(period.indicationPeriodBegin);
+    // Periods end at the start of the next period
+    const end = DateTime.fromISO(periods[index + 1].indicationPeriodBegin);
+    // Calculate how many days the period spans
+    return end.diff(start, 'days').days;
+  });
 };
 
 export const useGraphData = (accountListId: string): UseGraphDataResult => {
@@ -39,37 +125,52 @@ export const useGraphData = (accountListId: string): UseGraphDataResult => {
     },
   });
 
-  const average = data
-    ? Math.round(
-        data.healthIndicatorData
-          .map((month) => month.overallHi)
-          .filter(isNotNullish)
-          .reduce(
-            (total, overall, _index, months) => total + overall / months.length,
-            0,
-          ),
-      )
-    : null;
+  const averageOverallHi = useMemo(() => {
+    if (!data) {
+      return null;
+    }
 
-  const periods =
-    data?.healthIndicatorData.map((month) => ({
-      month: monthYearFormat(
-        DateTime.fromISO(month.indicationPeriodBegin),
-        locale,
-      ),
-      consistency: month.consistencyHi,
-      depth: month.depthHi,
-      ownership: month.ownershipHi,
-      success: month.successHi,
-      consistencyScaled: scale(month.consistencyHi),
-      depthScaled: scale(month.depthHi),
-      ownershipScaled: scale(month.ownershipHi, 3),
-      successScaled: scale(month.successHi, 2),
-    })) ?? null;
+    const periodSpans = calculatePeriodSpans(data.healthIndicatorData);
+    return weightedAverage(data.healthIndicatorData, 'overallHi', periodSpans);
+  }, [data]);
+
+  const periods = useMemo(() => {
+    if (!data) {
+      return null;
+    }
+
+    return Array.from(uniqueMonths(data).values()).map((isoMonth) => {
+      const periods = data.healthIndicatorData.filter((period) =>
+        period.indicationPeriodBegin.startsWith(isoMonth),
+      );
+
+      const periodSpans = calculatePeriodSpans(periods);
+      const consistency = weightedAverage(
+        periods,
+        'consistencyHi',
+        periodSpans,
+      );
+      const depth = weightedAverage(periods, 'depthHi', periodSpans);
+      const ownership = weightedAverage(periods, 'ownershipHi', periodSpans);
+      const success = weightedAverage(periods, 'successHi', periodSpans);
+
+      return {
+        month: monthYearFormat(DateTime.fromISO(isoMonth), locale),
+        consistency: round(consistency),
+        depth: round(depth),
+        ownership: round(ownership),
+        success: round(success),
+        consistencyScaled: round(scale(consistency)),
+        depthScaled: round(scale(depth)),
+        ownershipScaled: round(scale(ownership, 3)),
+        successScaled: round(scale(success, 2)),
+      };
+    });
+  }, [data]);
 
   return {
     loading,
-    average,
+    average: round(averageOverallHi),
     periods,
   };
 };
