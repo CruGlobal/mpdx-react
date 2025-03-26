da0893b7d3baceecf7c3057c70ef4d56713261ac
Caleb Cox <canac@users.noreply.github.com>
2025-03-26 16:45:07 -0500

    Pick the latest available HI period in DonationHistories

--- src/components/Dashboard/DonationHistories/graphData.test.ts
+++ src/components/Dashboard/DonationHistories/graphData.test.ts
@@ -38,6 +38,7 @@ describe('calculateGraphData', () => {
               {
                 convertedTotal: 30,
                 startDate: '2020-11-01',
+                endDate: '2020-11-30',
                 totals: [
                   { currency: 'USD', convertedAmount: 10 },
                   { currency: 'EUR', convertedAmount: 20 },
@@ -46,6 +47,7 @@ describe('calculateGraphData', () => {
               {
                 convertedTotal: 40,
                 startDate: '2020-12-01',
+                endDate: '2020-12-31',
                 totals: [
                   { currency: 'EUR', convertedAmount: 15 },
                   { currency: 'CAD', convertedAmount: 15 },
@@ -91,11 +93,11 @@ describe('calculateGraphData', () => {
           },
           reportsDonationHistories: {
             periods: [
-              { startDate: '2020-08-01' },
-              { startDate: '2020-09-01' },
-              { startDate: '2020-10-01' },
-              { startDate: '2020-11-01' },
-              { startDate: '2020-12-01' },
+              { startDate: '2020-08-01', endDate: '2020-08-31' },
+              { startDate: '2020-09-01', endDate: '2020-09-30' },
+              { startDate: '2020-10-01', endDate: '2020-10-31' },
+              { startDate: '2020-11-01', endDate: '2020-11-30' },
+              { startDate: '2020-12-01', endDate: '2020-12-31' },
             ],
           },
           healthIndicatorData: [
@@ -150,15 +152,16 @@ describe('calculateGraphData', () => {
           },
           reportsDonationHistories: {
             periods: [
-              { startDate: '2020-07-01' },
-              { startDate: '2020-08-01' },
-              { startDate: '2020-09-01' },
-              { startDate: '2020-10-01' },
-              { startDate: '2020-11-01' },
-              { startDate: '2020-12-01' },
+              { startDate: '2020-07-01', endDate: '2020-07-31' },
+              { startDate: '2020-08-01', endDate: '2020-08-31' },
+              { startDate: '2020-09-01', endDate: '2020-09-30' },
+              { startDate: '2020-10-01', endDate: '2020-10-31' },
+              { startDate: '2020-11-01', endDate: '2020-11-30' },
+              { startDate: '2020-12-01', endDate: '2020-12-31' },
             ],
           },
           // August, September, and November are missing
+          // December has two periods
           healthIndicatorData: [
             {
               indicationPeriodBegin: '2020-07-01',
@@ -177,6 +180,12 @@ describe('calculateGraphData', () => {
               machineCalculatedGoal: 120,
               machineCalculatedGoalCurrency: 'USD',
             },
+            {
+              indicationPeriodBegin: '2020-12-02',
+              staffEnteredGoal: 240,
+              machineCalculatedGoal: 120,
+              machineCalculatedGoalCurrency: 'USD',
+            },
           ],
         },
         variables,
@@ -190,7 +199,7 @@ describe('calculateGraphData', () => {
         { goal: 200, startDate: 'Sep 20' }, // extrapolated from July
         { goal: 110, startDate: 'Oct 20' },
         { goal: 110, startDate: 'Nov 20' }, // extrapolated from October
-        { goal: 220, startDate: 'Dec 20' },
+        { goal: 240, startDate: 'Dec 20' }, // uses latest December period
       ]);
     });
   });
@@ -249,120 +258,64 @@ describe('calculateGraphData', () => {
   });
 
   describe('domainMax', () => {
+    const data = gqlMock<GetDonationGraphQuery, GetDonationGraphQueryVariables>(
+      GetDonationGraphDocument,
+      {
+        mocks: {
+          accountList: {
+            monthlyGoal: 10,
+            totalPledges: 20,
+          },
+          reportsDonationHistories: {
+            periods: [
+              {
+                startDate: '2020-11-01',
+                endDate: '2020-11-30',
+                convertedTotal: 30,
+              },
+              {
+                startDate: '2020-12-01',
+                endDate: '2020-12-31',
+                convertedTotal: 40,
+              },
+            ],
+            averageIgnoreCurrent: 50,
+          },
+          healthIndicatorData: [
+            { indicationPeriodBegin: '2020-11-01', staffEnteredGoal: 60 },
+            { indicationPeriodBegin: '2020-12-01', staffEnteredGoal: 200 },
+          ],
+        },
+        variables,
+      },
+    );
+
     it('is zero when data is undefined', () => {
+      data.healthIndicatorData[0].staffEnteredGoal = 100;
+
       expect(
         calculateGraphData({ ...graphOptions, data: undefined }).domainMax,
       ).toBe(0);
     });
 
     it('is the period with the greatest total', () => {
-      const data = gqlMock<
-        GetDonationGraphQuery,
-        GetDonationGraphQueryVariables
-      >(GetDonationGraphDocument, {
-        mocks: {
-          accountList: {
-            monthlyGoal: 10,
-            totalPledges: 20,
-          },
-          reportsDonationHistories: {
-            periods: [
-              { startDate: '2020-11-01', convertedTotal: 100 },
-              { startDate: '2020-12-01', convertedTotal: 40 },
-            ],
-            averageIgnoreCurrent: 50,
-          },
-          healthIndicatorData: [
-            { indicationPeriodBegin: '2020-11-01', staffEnteredGoal: 60 },
-            { indicationPeriodBegin: '2020-12-01', staffEnteredGoal: 200 },
-          ],
-        },
-        variables,
-      });
+      data.reportsDonationHistories.periods[0].convertedTotal = 100;
 
       expect(calculateGraphData({ ...graphOptions, data }).domainMax).toBe(100);
     });
 
     it('is the period with the greatest goal', () => {
-      const data = gqlMock<
-        GetDonationGraphQuery,
-        GetDonationGraphQueryVariables
-      >(GetDonationGraphDocument, {
-        mocks: {
-          accountList: {
-            monthlyGoal: 10,
-            totalPledges: 20,
-          },
-          reportsDonationHistories: {
-            periods: [
-              { startDate: '2020-11-01', convertedTotal: 30 },
-              { startDate: '2020-12-01', convertedTotal: 40 },
-            ],
-            averageIgnoreCurrent: 50,
-          },
-          healthIndicatorData: [
-            { indicationPeriodBegin: '2020-11-01', staffEnteredGoal: 100 },
-            { indicationPeriodBegin: '2020-12-01', staffEnteredGoal: 200 },
-          ],
-        },
-        variables,
-      });
-
       expect(calculateGraphData({ ...graphOptions, data }).domainMax).toBe(100);
     });
 
     it('is the monthly goal', () => {
-      const data = gqlMock<
-        GetDonationGraphQuery,
-        GetDonationGraphQueryVariables
-      >(GetDonationGraphDocument, {
-        mocks: {
-          accountList: {
-            monthlyGoal: 100,
-            totalPledges: 20,
-          },
-          reportsDonationHistories: {
-            periods: [
-              { startDate: '2020-11-01', convertedTotal: 30 },
-              { startDate: '2020-12-01', convertedTotal: 40 },
-            ],
-            averageIgnoreCurrent: 50,
-          },
-          healthIndicatorData: [
-            { indicationPeriodBegin: '2020-11-01', staffEnteredGoal: 60 },
-            { indicationPeriodBegin: '2020-12-01', staffEnteredGoal: 200 },
-          ],
-        },
-        variables,
-      });
+      data.accountList.monthlyGoal = 100;
 
       expect(calculateGraphData({ ...graphOptions, data }).domainMax).toBe(100);
     });
 
     it('is the total pledges', () => {
-      const data = gqlMock<
-        GetDonationGraphQuery,
-        GetDonationGraphQueryVariables
-      >(GetDonationGraphDocument, {
-        mocks: {
-          accountList: {
-            monthlyGoal: 10,
-            totalPledges: 100,
-          },
-          reportsDonationHistories: {
-            periods: [
-              { startDate: '2020-11-01', convertedTotal: 30 },
-              { startDate: '2020-12-01', convertedTotal: 40 },
-            ],
-            averageIgnoreCurrent: 50,
-          },
-          healthIndicatorData: [
-            { indicationPeriodBegin: '2020-11-01', staffEnteredGoal: 60 },
-            { indicationPeriodBegin: '2020-12-01', staffEnteredGoal: 200 },
-          ],
-        },
-        variables,
-      });
+      data.accountList.totalPledges = 100;
 
       expect(calculateGraphData({ ...graphOptions, data }).domainMax).toBe(100);
     });
