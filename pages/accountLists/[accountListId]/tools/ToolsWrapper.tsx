dc146eac1f23e5f698fbddd98884f7427d0d412b
Caleb Cox <canac@users.noreply.github.com>
2025-04-03 12:47:47 -0500

    Remove nested main elements

--- pages/accountLists/[accountListId]/tools/ToolsWrapper.tsx
+++ pages/accountLists/[accountListId]/tools/ToolsWrapper.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import React, { JSXElementConstructor, ReactElement, useState } from 'react';
-import { Box } from '@mui/material';
 import { DynamicContactsRightPanel } from 'src/components/Contacts/ContactsRightPanel/DynamicContactsRightPanel';
 import { navBarHeight } from 'src/components/Layouts/Primary/Primary';
 import { SidePanelsLayout } from 'src/components/Layouts/SidePanelsLayout';
@@ -47,59 +46,57 @@ export const ToolsWrapper: React.FC<ToolsWrapperProps> = ({
       <Head>
         <title>{`${appName} | ${pageTitle}`}</title>
       </Head>
-      <Box component="main">
-        {accountListId ? (
-          <SidePanelsLayout
-            leftOpen={isToolDrawerOpen}
-            isScrollBox={false}
-            leftPanel={
-              <NavToolList
-                selectedId={selectedMenuId || ''}
-                isOpen={isToolDrawerOpen}
-                toggle={setIsToolDrawerOpen}
-              />
-            }
-            leftWidth="290px"
-            mainContent={
-              <>
-                {showToolsHeader && (
-                  <>
-                    <MultiPageHeader
-                      isNavListOpen={isToolDrawerOpen}
-                      onNavListToggle={() =>
-                        setIsToolDrawerOpen(!isToolDrawerOpen)
-                      }
-                      title={pageTitle || ''}
-                      headerType={HeaderTypeEnum.Tools}
-                    />
-                    <PageContentWrapper
-                      maxWidth="lg"
-                      style={{
-                        height: `calc(100vh - ${navBarHeight} - ${multiPageHeaderHeight})`,
-                      }}
-                    >
-                      {children}
-                    </PageContentWrapper>
-                  </>
-                )}
-                {!showToolsHeader && children}
-              </>
-            }
-            rightPanel={
-              selectedContactId ? (
-                <ContactsWrapper>
-                  <DynamicContactsRightPanel onClose={handleCloseContact} />
-                </ContactsWrapper>
-              ) : undefined
-            }
-            rightOpen={typeof selectedContactId !== 'undefined'}
-            rightWidth="60%"
-            headerHeight={'0px'}
-          />
-        ) : (
-          <Loading loading data-testid="ToolsWrapperLoading" />
-        )}
-      </Box>
+      {accountListId ? (
+        <SidePanelsLayout
+          leftOpen={isToolDrawerOpen}
+          isScrollBox={false}
+          leftPanel={
+            <NavToolList
+              selectedId={selectedMenuId || ''}
+              isOpen={isToolDrawerOpen}
+              toggle={setIsToolDrawerOpen}
+            />
+          }
+          leftWidth="290px"
+          mainContent={
+            <>
+              {showToolsHeader && (
+                <>
+                  <MultiPageHeader
+                    isNavListOpen={isToolDrawerOpen}
+                    onNavListToggle={() =>
+                      setIsToolDrawerOpen(!isToolDrawerOpen)
+                    }
+                    title={pageTitle || ''}
+                    headerType={HeaderTypeEnum.Tools}
+                  />
+                  <PageContentWrapper
+                    maxWidth="lg"
+                    style={{
+                      height: `calc(100vh - ${navBarHeight} - ${multiPageHeaderHeight})`,
+                    }}
+                  >
+                    {children}
+                  </PageContentWrapper>
+                </>
+              )}
+              {!showToolsHeader && children}
+            </>
+          }
+          rightPanel={
+            selectedContactId ? (
+              <ContactsWrapper>
+                <DynamicContactsRightPanel onClose={handleCloseContact} />
+              </ContactsWrapper>
+            ) : undefined
+          }
+          rightOpen={typeof selectedContactId !== 'undefined'}
+          rightWidth="60%"
+          headerHeight={'0px'}
+        />
+      ) : (
+        <Loading loading data-testid="ToolsWrapperLoading" />
+      )}
     </>
   );
 };
