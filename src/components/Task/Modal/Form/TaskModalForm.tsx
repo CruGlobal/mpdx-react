aeda3a4d001b36d1c3c14d13384f582bdd8b7d19
wjames111 <william.james@cru.org>
2025-05-16 13:20:12 -0400

    Filter out selectedSuggestedTags from tag options in autocomplete.

--- src/components/Task/Modal/Form/TaskModalForm.tsx
+++ src/components/Task/Modal/Form/TaskModalForm.tsx
@@ -626,6 +626,7 @@ const TaskModalForm = ({
               <Grid item xs={12}>
                 <TagsAutocomplete
                   accountListId={accountListId}
+                  selectedSuggestedTags={selectedSuggestedTags}
                   type={TagTypeEnum.Tag}
                   value={tagList ?? []}
                   onChange={(tagList) => setFieldValue('tagList', tagList)}
