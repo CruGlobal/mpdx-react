628fa698cdb937876a2ca766e80a4ac26be1920d
wjames111 <william.james@cru.org>
2025-05-21 16:20:13 -0400

    Add test to ensure tags are filtered.

--- src/components/Task/Modal/Form/Inputs/TagsAutocomplete/TagsAutocomplete.test.tsx
+++ src/components/Task/Modal/Form/Inputs/TagsAutocomplete/TagsAutocomplete.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { GqlMockedProvider } from '__tests__/util/graphqlMocking';
 import { TagTypeEnum, TagsAutocomplete } from './TagsAutocomplete';
@@ -11,18 +11,24 @@ const mutationSpy = jest.fn();
 interface TestComponentProps {
   type?: TagTypeEnum;
   value?: string[];
+  allPhaseTags?: Set<string>;
+  contactTagList?: string[];
+  taskTagList?: string[];
 }
 
 const TestComponent: React.FC<TestComponentProps> = ({
   type = TagTypeEnum.Contact,
   value = [],
+  allPhaseTags = new Set(),
+  contactTagList = ['ct-1', 'ct-2', 'ct-3'],
+  taskTagList = ['tt-1', 'tt-2', 'tt-3'],
 }) => (
   <GqlMockedProvider<{ TagOptions: TagOptionsQuery }>
     mocks={{
       TagOptions: {
         accountList: {
-          contactTagList: ['ct-1', 'ct-2', 'ct-3'],
-          taskTagList: ['tt-1', 'tt-2', 'tt-3'],
+          contactTagList,
+          taskTagList,
         },
       },
     }}
@@ -33,6 +39,7 @@ const TestComponent: React.FC<TestComponentProps> = ({
       type={type}
       value={value}
       onChange={onChange}
+      allPhaseTags={allPhaseTags}
     />
   </GqlMockedProvider>
 );
@@ -90,5 +97,42 @@ describe('TagsAutocomplete', () => {
     userEvent.tab();
 
     expect(onChange).not.toHaveBeenCalled();
+  });
+
+  it('should not have any phaseTags in the list', async () => {
+    const { queryByRole, getByRole } = render(
+      <TestComponent
+        value={['ct-1']}
+        type={TagTypeEnum.Tag}
+        allPhaseTags={new Set(['tt-2', 'tt-3'])}
+        contactTagList={[]}
+      />,
+    );
+
+    userEvent.click(getByRole('combobox', { name: 'Tags' }));
+    await waitFor(() => {
+      expect(queryByRole('option', { name: 'tt-1' })).toBeInTheDocument();
+      expect(queryByRole('option', { name: 'tt-2' })).not.toBeInTheDocument();
+      expect(queryByRole('option', { name: 'tt-3' })).not.toBeInTheDocument();
+    });
+  });
+
+  it('should default to an empty array of options', async () => {
+    const { getByRole, queryByRole } = render(
+      <TestComponent
+        type={TagTypeEnum.Tag}
+        value={[]}
+        contactTagList={[]}
+        taskTagList={[]}
+      />,
+    );
+
+    userEvent.click(getByRole('combobox', { name: 'Tags' }));
+    expect(
+      await queryByRole('option', { name: 'tt-1' }),
+    ).not.toBeInTheDocument();
+    expect(
+      await queryByRole('option', { name: 'ct-1' }),
+    ).not.toBeInTheDocument();
   });
 });
