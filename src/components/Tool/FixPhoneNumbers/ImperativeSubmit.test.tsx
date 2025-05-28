5fd1573a6aede209fe74a729a56b5a5c9aa983ac
wjames111 <william.james@cru.org>
2025-05-28 11:34:52 -0400

    fixup! Breaks out ImperativeSubmit hook and adds test.

--- src/components/Tool/FixPhoneNumbers/ImperativeSubmit.test.tsx
+++ src/components/Tool/FixPhoneNumbers/ImperativeSubmit.test.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Formik } from 'formik';
-import { DateTime } from 'luxon';
 import { render, waitFor } from '__tests__/util/testingLibraryReactMock';
 import { ImperativeSubmit } from './ImperativeSubmit';
 
@@ -8,27 +7,7 @@ describe('Fix PhoneNumber Contact', () => {
   it('default', async () => {
     const submit = jest.fn();
     render(
-      <Formik
-        initialValues={{
-          numbers: [
-            {
-              id: 'number1',
-              source: 'DataServer',
-              updatedAt: DateTime.fromISO('2021-06-21').toString(),
-              number: '123456',
-              primary: true,
-            },
-            {
-              id: 'number2',
-              source: 'MPDX',
-              updatedAt: DateTime.fromISO('2021-03-23').toString(),
-              number: '7891011',
-              primary: true,
-            },
-          ],
-        }}
-        onSubmit={submit}
-      >
+      <Formik initialValues={{}} onSubmit={submit}>
         <ImperativeSubmit submitAll={true} />
       </Formik>,
     );
