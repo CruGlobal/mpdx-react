93dfa441103c91611c476daff849bd812965f92a
wjames111 <william.james@cru.org>
2025-06-11 15:54:50 -0400

    Adds common component for OrganizationAutocomplete.

---
+++ src/components/common/Autocomplete/OrganizationAutocomplete/OrganizationAutocomplete.tsx
@@ -0,0 +1,44 @@
+import { Autocomplete, AutocompleteProps, TextField } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import { OrganizationsQuery } from 'pages/accountLists/[accountListId]/settings/organizations.generated';
+import { Organizations } from 'src/graphql/types.generated';
+
+interface OrganizationAutocompleteProps
+  extends Partial<AutocompleteProps<Organizations, false, boolean, false>> {
+  organizations: OrganizationsQuery['getOrganizations']['organizations'];
+  textFieldLabel?: string;
+  disableClearable?: boolean;
+}
+
+export const OrganizationAutocomplete = ({
+  organizations,
+  textFieldLabel,
+  ...props
+}: OrganizationAutocompleteProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <Autocomplete
+      style={{
+        width: '250px',
+      }}
+      autoSelect
+      autoHighlight
+      disableClearable
+      {...props}
+      options={
+        organizations?.filter((org): org is Organizations => !!org) || []
+      }
+      getOptionLabel={(org) => org.name}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label={textFieldLabel ?? t('Organization')}
+          InputProps={{
+            ...params.InputProps,
+          }}
+        />
+      )}
+    />
+  );
+};
