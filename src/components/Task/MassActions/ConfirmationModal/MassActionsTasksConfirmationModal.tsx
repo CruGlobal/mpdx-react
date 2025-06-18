158e506749319bdfe40f57454fb6770eea300179
Katelyn Grimes <katelyn.grimes@cru.org>
2025-06-18 15:53:26 -0400

    Confirm users want to mass delete after 5 tasks selected.

--- src/components/Task/MassActions/ConfirmationModal/MassActionsTasksConfirmationModal.tsx
+++ src/components/Task/MassActions/ConfirmationModal/MassActionsTasksConfirmationModal.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
-import { DialogActions, DialogContent, Typography } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  Alert,
+  Checkbox,
+  DialogActions,
+  DialogContent,
+  FormControlLabel,
+  Typography,
+} from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import {
   CancelButton,
@@ -20,6 +27,53 @@ export const MassActionsTasksConfirmationModal: React.FC<
 > = ({ open, idsCount, action, setOpen, onConfirm }) => {
   const { t } = useTranslation();
 
+  // KG: useState for checkbox
+  const [checked, setChecked] = useState(false);
+
+  const shouldConfirmDeletion = action === 'delete' && idsCount > 5;
+
+  const handleChange = (event) => {
+    setChecked(event.target.checked);
+  };
+
+  // KG: get correct task message based on action
+  function renderMessage() {
+    if (action === 'complete' || !shouldConfirmDeletion) {
+      return (
+        <Typography>
+          {t(
+            'Are you sure you wish to {{action}} the {{count}} selected tasks?',
+            { action, count: idsCount },
+          )}
+        </Typography>
+      );
+    } else {
+      return (
+        <Typography>
+          <Alert severity="error">
+            {t(
+              'Deleting these tasks is permanent and cannot be undone. Please make sure you want to permanently delete them before proceeding.',
+            )}
+          </Alert>
+          <FormControlLabel
+            control={
+              <Checkbox
+                //color="error"
+                checked={checked}
+                onChange={handleChange}
+                inputProps={{ 'aria-label': 'controlled' }}
+              />
+            }
+            label={t(
+              'Yes, I want to {{action}} the {{count}} selected tasks.',
+              { action, count: idsCount },
+            )}
+          />
+        </Typography>
+      );
+    }
+  }
+
   return (
     <Modal
       title={action === 'complete' ? t('Complete Tasks') : t('Delete Tasks')}
@@ -27,16 +81,16 @@ export const MassActionsTasksConfirmationModal: React.FC<
       handleClose={() => setOpen(false)}
     >
       <DialogContent dividers data-testid={'CompleteAndDeleteTasksModal'}>
-        <Typography>
-          {t(
-            'Are you sure you wish to {{action}} the {{count}} selected tasks?',
-            { action, count: idsCount },
-          )}
-        </Typography>
+        {renderMessage()}
       </DialogContent>
       <DialogActions>
         <CancelButton onClick={() => setOpen(false)}>{t('No')}</CancelButton>
-        <SubmitButton onClick={onConfirm} type="button">
+        <SubmitButton
+          disabled={!checked && shouldConfirmDeletion}
+          //color="error"
+          onClick={onConfirm}
+          type="button"
+        >
           {t('Yes')}
         </SubmitButton>
       </DialogActions>
