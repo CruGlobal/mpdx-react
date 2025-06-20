a811030f5e14cd139b87e0ef3cde5ceb9035f0ec
Katelyn Grimes <katelyn.grimes@cru.org>
2025-06-20 15:06:16 -0400

    Updated confirm mass task deletion from PR suggestions

--- src/components/Task/MassActions/ConfirmationModal/MassActionsTasksConfirmationModal.tsx
+++ src/components/Task/MassActions/ConfirmationModal/MassActionsTasksConfirmationModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import {
   Alert,
   Checkbox,
@@ -14,9 +14,13 @@ import {
 } from 'src/components/common/Modal/ActionButtons/ActionButtons';
 import Modal from '../../../common/Modal/Modal';
 
+export enum Action {
+  Complete = 'complete',
+  Delete = 'delete',
+}
 interface MassActionsTasksConfirmationModalProps {
   open: boolean;
-  action: 'complete' | 'delete';
+  action: Action;
   idsCount: number;
   setOpen: (open: boolean) => void;
   onConfirm: () => void;
@@ -27,68 +31,60 @@ export const MassActionsTasksConfirmationModal: React.FC<
 > = ({ open, idsCount, action, setOpen, onConfirm }) => {
   const { t } = useTranslation();
 
-  // KG: useState for checkbox
-  const [checked, setChecked] = useState(false);
+  const [hasConfirmedDeletion, setHasConfirmedDeletion] = useState(false);
 
-  const shouldConfirmDeletion = action === 'delete' && idsCount > 5;
+  const shouldConfirmDeletion = useMemo(() => {
+    return action === Action.Delete && idsCount >= 5;
+  }, [action, idsCount]);
 
-  const handleChange = (event) => {
-    setChecked(event.target.checked);
+  const handleChange = () => {
+    setHasConfirmedDeletion(!hasConfirmedDeletion);
   };
-
-  // KG: get correct task message based on action
-  function renderMessage() {
-    if (action === 'complete' || !shouldConfirmDeletion) {
-      return (
-        <Typography>
-          {t(
-            'Are you sure you wish to {{action}} the {{count}} selected tasks?',
-            { action, count: idsCount },
-          )}
-        </Typography>
-      );
-    } else {
-      return (
-        <Typography>
-          <Alert severity="error">
-            {t(
-              'Deleting these tasks is permanent and cannot be undone. Please make sure you want to permanently delete them before proceeding.',
-            )}
-          </Alert>
-          <FormControlLabel
-            control={
-              <Checkbox
-                //color="error"
-                checked={checked}
-                onChange={handleChange}
-                inputProps={{ 'aria-label': 'controlled' }}
-              />
-            }
-            label={t(
-              'Yes, I want to {{action}} the {{count}} selected tasks.',
-              { action, count: idsCount },
-            )}
-            data-testid="confirmDeletionCheckbox"
-          />
-        </Typography>
-      );
-    }
-  }
 
   return (
     <Modal
-      title={action === 'complete' ? t('Complete Tasks') : t('Delete Tasks')}
+      title={
+        action === Action.Complete ? t('Complete Tasks') : t('Delete Tasks')
+      }
       isOpen={open}
       handleClose={() => setOpen(false)}
     >
       <DialogContent dividers data-testid={'CompleteAndDeleteTasksModal'}>
-        {renderMessage()}
+        {action === Action.Complete || !shouldConfirmDeletion ? (
+          <Typography>
+            {t(
+              'Are you sure you wish to {{action}} the {{count}} selected tasks?',
+              { action, count: idsCount },
+            )}
+          </Typography>
+        ) : (
+          <Typography>
+            <Alert severity="error">
+              {t(
+                'Deleting these tasks is permanent and cannot be undone. Please make sure you want to permanently delete them before proceeding.',
+              )}
+            </Alert>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={hasConfirmedDeletion}
+                  onChange={handleChange}
+                  inputProps={{ 'aria-label': 'controlled' }}
+                />
+              }
+              label={t(
+                'Yes, I want to {{action}} the {{count}} selected tasks.',
+                { action, count: idsCount },
+              )}
+              data-testid="confirmDeletionCheckbox"
+            />
+          </Typography>
+        )}
       </DialogContent>
       <DialogActions>
         <CancelButton onClick={() => setOpen(false)}>{t('No')}</CancelButton>
         <SubmitButton
-          disabled={!checked && shouldConfirmDeletion}
-          //color="error"
+          disabled={!hasConfirmedDeletion && shouldConfirmDeletion}
           onClick={onConfirm}
           type="button"
         >
