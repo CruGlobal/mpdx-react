18eb5963a36f73b67129fd80977bec920c7da82b
wjames111 <william.james@cru.org>
2025-06-24 12:26:04 -0400

    fixup! Fix tests; adds test for dateRangeHelper.

--- src/lib/dateRangeHelpers.test.ts
+++ src/lib/dateRangeHelpers.test.ts
@@ -1,48 +1,19 @@
-import { DateTime, Settings } from 'luxon';
+import { Settings } from 'luxon';
 import { getTwelveMonthReportDateRange } from './dateRangeHelpers';
 
 describe('getTwelveMonthReportDateRange', () => {
-  it('should return startDate and endDate as ISO strings', () => {
-    const twelveMonthReportDateRange = getTwelveMonthReportDateRange();
-    expect(typeof twelveMonthReportDateRange).toBe('string');
-    expect(twelveMonthReportDateRange).toMatch(
-      /^\d{4}-\d{2}-\d{2}\.\.\.\d{4}-\d{2}-\d{2}$/,
-    );
-  });
-
-  it('should return a startDate 12 months before the current month and endDate as last day of previous month', () => {
-    const now = DateTime.local();
-    const expectedStart = now
-      .minus({ months: 12 })
-      .startOf('month')
-      .toISODate();
-    const expectedEnd = now.minus({ months: 1 }).endOf('month').toISODate();
-    expect(getTwelveMonthReportDateRange()).toBe(
-      `${expectedStart}...${expectedEnd}`,
-    );
-  });
-
   it('should return correct dates on the first day of the month', () => {
     Settings.now = () => new Date(2025, 5, 1).valueOf();
-    const twelveMonthReportDateRange = getTwelveMonthReportDateRange();
-    const [startDate, endDate] = twelveMonthReportDateRange.split('...');
-    expect(startDate).toBe('2024-06-01');
-    expect(endDate).toBe('2025-05-31');
+    expect(getTwelveMonthReportDateRange()).toBe('2024-06-01...2025-05-31');
   });
 
   it('should return correct dates in the middle of the month', () => {
     Settings.now = () => new Date(2025, 5, 15).valueOf();
-    const twelveMonthReportDateRange = getTwelveMonthReportDateRange();
-    const [startDate, endDate] = twelveMonthReportDateRange.split('...');
-    expect(startDate).toBe('2024-06-01');
-    expect(endDate).toBe('2025-05-31');
+    expect(getTwelveMonthReportDateRange()).toBe('2024-06-01...2025-05-31');
   });
 
   it('should return correct dates at the end of the month', () => {
     Settings.now = () => new Date(2025, 5, 30).valueOf();
-    const twelveMonthReportDateRange = getTwelveMonthReportDateRange();
-    const [startDate, endDate] = twelveMonthReportDateRange.split('...');
-    expect(startDate).toBe('2024-06-01');
-    expect(endDate).toBe('2025-05-31');
+    expect(getTwelveMonthReportDateRange()).toBe('2024-06-01...2025-05-31');
   });
 });
