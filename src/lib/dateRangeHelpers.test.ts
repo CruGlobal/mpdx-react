deb24db4581ff7bc0fbbfce1d2a6fa251481500d
wjames111 <william.james@cru.org>
2025-06-24 11:37:33 -0400

    fixup! Fix tests; adds test for dateRangeHelper.

--- src/lib/dateRangeHelpers.test.ts
+++ src/lib/dateRangeHelpers.test.ts
@@ -1,4 +1,4 @@
-import { DateTime } from 'luxon';
+import { DateTime, Settings } from 'luxon';
 import { getTwelveMonthReportDateRange } from './dateRangeHelpers';
 
 describe('getTwelveMonthReportDateRange', () => {
@@ -20,5 +20,29 @@ describe('getTwelveMonthReportDateRange', () => {
     expect(getTwelveMonthReportDateRange()).toBe(
       `${expectedStart}...${expectedEnd}`,
     );
+  });
+
+  it('should return correct dates on the first day of the month', () => {
+    Settings.now = () => new Date(2025, 5, 1).valueOf();
+    const twelveMonthReportDateRange = getTwelveMonthReportDateRange();
+    const [startDate, endDate] = twelveMonthReportDateRange.split('...');
+    expect(startDate).toBe('2024-06-01');
+    expect(endDate).toBe('2025-05-31');
+  });
+
+  it('should return correct dates in the middle of the month', () => {
+    Settings.now = () => new Date(2025, 5, 15).valueOf();
+    const twelveMonthReportDateRange = getTwelveMonthReportDateRange();
+    const [startDate, endDate] = twelveMonthReportDateRange.split('...');
+    expect(startDate).toBe('2024-06-01');
+    expect(endDate).toBe('2025-05-31');
+  });
+
+  it('should return correct dates at the end of the month', () => {
+    Settings.now = () => new Date(2025, 5, 30).valueOf();
+    const twelveMonthReportDateRange = getTwelveMonthReportDateRange();
+    const [startDate, endDate] = twelveMonthReportDateRange.split('...');
+    expect(startDate).toBe('2024-06-01');
+    expect(endDate).toBe('2025-05-31');
   });
 });
