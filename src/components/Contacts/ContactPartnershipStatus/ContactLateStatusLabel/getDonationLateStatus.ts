c9a161eb19501490fd3fa4b8e49bf5f83282a29d
zachery with an e <45150570+zweatshirt@users.noreply.github.com>
2025-07-11 10:34:58 -0500

    Resolves oversite of getDonationLateStatus calculation and includes minor fixes.

--- src/components/Contacts/ContactPartnershipStatus/ContactLateStatusLabel/getDonationLateStatus.ts
+++ src/components/Contacts/ContactPartnershipStatus/ContactLateStatusLabel/getDonationLateStatus.ts
@@ -11,17 +11,26 @@ const selectLaterDate = (lateAt: string, pledgeStartDate: string): string => {
   return lateAt > pledgeStartDate ? lateAt : pledgeStartDate;
 };
 
-const determineStatusFromDays = (daysDiff: number): ContactLateStatusEnum => {
+const getStatusFromDays = (daysDiff: number): ContactLateStatusEnum => {
   if (daysDiff < 0) {
     return ContactLateStatusEnum.OnTime;
   }
   if (daysDiff < 30) {
     return ContactLateStatusEnum.LateLessThirty;
   }
-  if (daysDiff < 60) {
+  if (daysDiff <= 60) {
     return ContactLateStatusEnum.LateMoreThirty;
   }
   return ContactLateStatusEnum.LateMoreSixty;
+};
+
+const getTotalDaysElapsed = (laterDate: string, daysDiff: number) => {
+  const diffToNow = DateTime.now().diff(
+    DateTime.fromISO(laterDate),
+    'days',
+  ).days;
+  const totalDaysDiff = Math.floor(daysDiff + diffToNow);
+  return totalDaysDiff;
 };
 
 /**
@@ -33,23 +42,44 @@ const determineStatusFromDays = (daysDiff: number): ContactLateStatusEnum => {
  * Both are in ISO 8601 format (e.g., "2020-01-01").
  */
 export const getDonationLateStatus = (
-  lateAt?: string | null | undefined,
-  pledgeStartDate?: string | null | undefined,
+  lateAt?: string | null,
+  pledgeStartDate?: string | null,
 ): ContactLateStatusEnum | undefined => {
   // Determine which date to use
+  if (!lateAt && !pledgeStartDate) {
+    return undefined;
+  }
+
+  // If lateAt is provided and is in the future, return OnTime
+  if (lateAt && DateTime.fromISO(lateAt) > DateTime.now()) {
+    return ContactLateStatusEnum.OnTime;
+  }
+
   const laterDate =
     lateAt && pledgeStartDate
       ? selectLaterDate(lateAt, pledgeStartDate)
-      : pledgeStartDate || lateAt;
+      : pledgeStartDate ?? lateAt;
 
   if (!laterDate) {
     return undefined;
   }
 
-  const daysDiff = DateTime.now().diff(
-    DateTime.fromISO(laterDate),
+  // If only lateAt is provided
+  if (!pledgeStartDate) {
+    const diffToNowSingle = DateTime.now().diff(
+      DateTime.fromISO(laterDate),
+      'days',
+    ).days;
+    return getStatusFromDays(Math.floor(diffToNowSingle));
+  }
+
+  const daysDiff = DateTime.fromISO(laterDate).diff(
+    DateTime.fromISO(pledgeStartDate),
     'days',
   ).days;
 
-  return determineStatusFromDays(daysDiff);
+  // Need to consider the current date in our calculation
+  const totalDaysDiff = getTotalDaysElapsed(laterDate, daysDiff);
+
+  return getStatusFromDays(totalDaysDiff);
 };
