19aeaf88aa0a640cc3b33d45514e8eb27da48b86
Katelyn Grimes <katelyn.grimes@cru.org>
2025-07-25 15:17:15 -0400

    Added category type enum to be used later

--- src/components/Reports/StaffExpenseReport/Helpers/StaffReportEnum.ts
+++ src/components/Reports/StaffExpenseReport/Helpers/StaffReportEnum.ts
@@ -2,3 +2,9 @@ export enum TableType {
   Income = 'income',
   Expenses = 'expenses',
 }
+
+export enum CategoryType {
+  Benefit = 'benefit',
+  Contribution = 'contribution',
+  Salary = 'salary',
+}
