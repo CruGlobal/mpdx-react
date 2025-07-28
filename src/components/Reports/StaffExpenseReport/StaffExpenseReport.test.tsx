d8f13b6bf17f396e109ba17af2a67d4a2cb19746
Katelyn Grimes <katelyn.grimes@cru.org>
2025-07-28 15:52:14 -0400

    Created tests for StaffExpenseReport

---
+++ src/components/Reports/StaffExpenseReport/StaffExpenseReport.test.tsx
@@ -0,0 +1,156 @@
+import React from 'react';
+import { ThemeProvider } from '@mui/material/styles';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterLuxon } from '@mui/x-date-pickers/AdapterLuxon';
+import { render, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { DateTime } from 'luxon';
+import { SnackbarProvider } from 'notistack';
+import TestRouter from '__tests__/util/TestRouter';
+import { GqlMockedProvider } from '__tests__/util/graphqlMocking';
+import theme from 'src/theme';
+import { ReportsStaffExpensesQuery } from './GetStaffExpense.generated';
+import { StaffExpenseReport } from './StaffExpenseReport';
+
+interface TestComponentProps {
+  isEmpty?: boolean;
+  routerMonth?: string;
+}
+
+const mutationSpy = jest.fn();
+const onNavListToggle = jest.fn();
+const setTime = jest.fn();
+const push = jest.fn();
+
+const time = DateTime.fromISO('2025-01-01').startOf('month');
+const title = 'Report title';
+
+const router = {
+  query: { accountListId: 'account-1' },
+  isReady: true,
+  push,
+};
+
+const TestComponent: React.FC<TestComponentProps> = ({
+  isEmpty,
+  routerMonth,
+}) => (
+  <ThemeProvider theme={theme}>
+    <TestRouter
+      router={
+        routerMonth
+          ? {
+              ...router,
+              query: { ...router.query, month: '2025-01-01' },
+            }
+          : router
+      }
+    >
+      <SnackbarProvider>
+        <LocalizationProvider dateAdapter={AdapterLuxon}>
+          <TestRouter router={router}>
+            <GqlMockedProvider<{
+              ReportsStaffExpenses: ReportsStaffExpensesQuery;
+            }>
+              mocks={{
+                ReportsStaffExpenses: {
+                  reportsStaffExpenses: {
+                    accountId: 'account-1',
+                    name: 'Test Account',
+                    status: 'active',
+                    startBalance: 1000,
+                    endBalance: 2000,
+                    funds: isEmpty
+                      ? []
+                      : [
+                          {
+                            fundType: 'Primary',
+                            total: -500,
+                            categories: [
+                              {
+                                category: 'Travel',
+                                total: -300,
+                                averagePerMonth: -100,
+                                subcategories: [
+                                  {
+                                    subCategory: 'Flights',
+                                    total: -200,
+                                    averagePerMonth: -50,
+                                    breakdownByMonth: [
+                                      { month: '2025-01-01', total: -100 },
+                                      { month: '2025-02-01', total: -100 },
+                                    ],
+                                  },
+                                  {
+                                    subCategory: 'Hotels',
+                                    total: -100,
+                                    averagePerMonth: -50,
+                                    breakdownByMonth: [
+                                      { month: '2025-01-01', total: -50 },
+                                      { month: '2025-02-01', total: -50 },
+                                    ],
+                                  },
+                                ],
+                                breakdownByMonth: [
+                                  { month: '2025-01-01', total: -150 },
+                                  { month: '2025-02-01', total: -150 },
+                                ],
+                              },
+                            ],
+                          },
+                        ],
+                  },
+                },
+              }}
+              onCall={mutationSpy}
+            >
+              <StaffExpenseReport
+                accountId={'account-1'}
+                isNavListOpen={true}
+                onNavListToggle={onNavListToggle}
+                title={title}
+                time={time}
+                setTime={setTime}
+              />
+            </GqlMockedProvider>
+          </TestRouter>
+        </LocalizationProvider>
+      </SnackbarProvider>
+    </TestRouter>
+  </ThemeProvider>
+);
+
+describe('StaffExpenseReport', () => {
+  it('renders with data', async () => {
+    const { getByTestId, getByRole } = render(
+      <TestComponent isEmpty={false} />,
+    );
+
+    expect(getByRole('heading', { name: 'Report title' })).toBeInTheDocument();
+    const boxInfo = getByTestId('account-info');
+    await waitFor(() => expect(boxInfo).toHaveTextContent('Test Account'));
+    await waitFor(() => expect(boxInfo).toHaveTextContent('account-1'));
+  });
+
+  it('initializes with month from query', () => {
+    const { getByRole } = render(<TestComponent routerMonth="2025-01-01" />);
+
+    expect(getByRole('heading', { name: 'January 2025' })).toBeInTheDocument();
+  });
+
+  it('renders nav list icon and onclick triggers onNavListToggle', async () => {
+    onNavListToggle.mockClear();
+    const { getByTestId } = render(<TestComponent />);
+
+    expect(getByTestId('ReportsFilterIcon')).toBeInTheDocument();
+    userEvent.click(getByTestId('ReportsFilterIcon'));
+    await waitFor(() => expect(onNavListToggle).toHaveBeenCalled());
+  });
+
+  it('updates the time filter', () => {
+    const { getByRole } = render(<TestComponent />);
+
+    userEvent.click(getByRole('button', { name: 'Previous Month' }));
+    expect(setTime.mock.lastCall[0].toISODate()).toBe('2024-12-01');
+  });
+});
