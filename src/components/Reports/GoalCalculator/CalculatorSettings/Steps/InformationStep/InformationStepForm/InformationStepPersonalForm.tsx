f720befdfd63c79e80507687233a3a277be761be
Caleb Cox <canac@users.noreply.github.com>
2025-07-29 17:02:36 -0500

    Make remaining personal fields selects using enums

--- src/components/Reports/GoalCalculator/CalculatorSettings/Steps/InformationStep/InformationStepForm/InformationStepPersonalForm.tsx
+++ src/components/Reports/GoalCalculator/CalculatorSettings/Steps/InformationStep/InformationStepForm/InformationStepPersonalForm.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
-import { Autocomplete, Grid, TextField, Typography } from '@mui/material';
+import {
+  Autocomplete,
+  FormControl,
+  FormHelperText,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from '@mui/material';
 import { Field, FieldProps } from 'formik';
 import { useTranslation } from 'react-i18next';
+import { Age, BenefitsPlan, FamilySize, Role, Tenure } from './enums';
 import { locations } from './geographicAdjustments';
 
 export const InformationStepPersonalForm: React.FC = () => {
@@ -27,7 +38,7 @@ export const InformationStepPersonalForm: React.FC = () => {
                 renderInput={(params) => (
                   <TextField
                     {...params}
-                    label={t('Location')}
+                    label={t('Geographic Location')}
                     error={meta.touched && Boolean(meta.error)}
                     helperText={meta.touched && meta.error}
                   />
@@ -40,15 +51,16 @@ export const InformationStepPersonalForm: React.FC = () => {
         <Grid item xs={12}>
           <Field name="role">
             {({ field, meta }: FieldProps) => (
-              <TextField
-                {...field}
-                fullWidth
-                size="small"
-                label={t('Role')}
-                error={meta.touched && Boolean(meta.error)}
-                helperText={meta.touched && meta.error}
-                variant="outlined"
-              />
+              <FormControl fullWidth size="small">
+                <InputLabel>{t('Role')}</InputLabel>
+                <Select {...field} label={t('Role')}>
+                  <MenuItem value={Role.Office}>{t('Office')}</MenuItem>
+                  <MenuItem value={Role.Field}>{t('Field')}</MenuItem>
+                </Select>
+                <FormHelperText error={meta.touched && Boolean(meta.error)}>
+                  {meta.touched && meta.error}
+                </FormHelperText>
+              </FormControl>
             )}
           </Field>
         </Grid>
@@ -56,18 +68,56 @@ export const InformationStepPersonalForm: React.FC = () => {
         <Grid item xs={12}>
           <Field name="benefits">
             {({ field, meta }: FieldProps) => (
-              <TextField
-                {...field}
-                fullWidth
-                size="small"
-                label={t('Benefits')}
-                multiline
-                rows={4}
-                error={meta.touched && Boolean(meta.error)}
-                helperText={meta.touched && meta.error}
-                variant="outlined"
-                placeholder={t('Describe your benefits package...')}
-              />
+              <FormControl fullWidth size="small">
+                <InputLabel>{t('Benefits plan')}</InputLabel>
+                <Select {...field} label={t('Benefits plan')}>
+                  <MenuItem value={BenefitsPlan.Select}>{t('Select')}</MenuItem>
+                  <MenuItem value={BenefitsPlan.Plus}>{t('Plus')}</MenuItem>
+                  <MenuItem value={BenefitsPlan.Base}>{t('Base')}</MenuItem>
+                  <MenuItem value={BenefitsPlan.Minimum}>
+                    {t('Minimum')}
+                  </MenuItem>
+                </Select>
+                <FormHelperText error={meta.touched && Boolean(meta.error)}>
+                  {meta.touched && meta.error}
+                </FormHelperText>
+              </FormControl>
+            )}
+          </Field>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Field name="familySize">
+            {({ field, meta }: FieldProps) => (
+              <FormControl fullWidth size="small">
+                <InputLabel>{t('Family size')}</InputLabel>
+                <Select {...field} label={t('Family size')}>
+                  <MenuItem value={FamilySize.SingleOrSpouseNotStaff}>
+                    {t('Single or spouse not staff')}
+                  </MenuItem>
+                  <MenuItem value={FamilySize.MarriedNoChildren}>
+                    {t('Married with no children')}
+                  </MenuItem>
+                  <MenuItem value={FamilySize.Sosa1Dependent}>
+                    {t('SOSA with 1 dependent')}
+                  </MenuItem>
+                  <MenuItem value={FamilySize.Sosa2Or3Dependents}>
+                    {t('SOSA with 2-3 dependents')}
+                  </MenuItem>
+                  <MenuItem value={FamilySize.Sosa4OrMoreDependents}>
+                    {t('SOSA with 4 or more dependents')}
+                  </MenuItem>
+                  <MenuItem value={FamilySize.Married1Or2Children}>
+                    {t('Married with 1-2 children')}
+                  </MenuItem>
+                  <MenuItem value={FamilySize.Married3OrMoreChildren}>
+                    {t('Married with 3 or more children')}
+                  </MenuItem>
+                </Select>
+                <FormHelperText error={meta.touched && Boolean(meta.error)}>
+                  {(meta.touched && meta.error) || t('For benefits plan')}
+                </FormHelperText>
+              </FormControl>
             )}
           </Field>
         </Grid>
@@ -75,17 +125,37 @@ export const InformationStepPersonalForm: React.FC = () => {
         <Grid item xs={12}>
           <Field name="tenure">
             {({ field, meta }: FieldProps) => (
-              <TextField
-                {...field}
-                fullWidth
-                size="small"
-                label={t('Tenure (years)')}
-                type="number"
-                error={meta.touched && Boolean(meta.error)}
-                helperText={meta.touched && meta.error}
-                variant="outlined"
-                inputProps={{ min: 0, max: 50 }}
-              />
+              <FormControl fullWidth size="small">
+                <InputLabel>{t('Tenure (years)')}</InputLabel>
+                <Select {...field} label={t('Tenure (years)')}>
+                  <MenuItem value={Tenure.OneToFour}>{t('1-4')}</MenuItem>
+                  <MenuItem value={Tenure.FiveToNine}>{t('5-9')}</MenuItem>
+                  <MenuItem value={Tenure.TenToFourteen}>{t('10-14')}</MenuItem>
+                  <MenuItem value={Tenure.FifteenToNineteen}>
+                    {t('15-19')}
+                  </MenuItem>
+                  <MenuItem value={Tenure.TwentyToTwentyFour}>
+                    {t('20-24')}
+                  </MenuItem>
+                  <MenuItem value={Tenure.TwentyFiveToTwentyNine}>
+                    {t('25-29')}
+                  </MenuItem>
+                  <MenuItem value={Tenure.ThirtyToThirtyFour}>
+                    {t('30-34')}
+                  </MenuItem>
+                  <MenuItem value={Tenure.ThirtyFiveToThirtyNine}>
+                    {t('35-39')}
+                  </MenuItem>
+                  <MenuItem value={Tenure.FortyToFortyFour}>
+                    {t('40-44')}
+                  </MenuItem>
+                  <MenuItem value={Tenure.FortyFivePlus}>{t('45+')}</MenuItem>
+                </Select>
+                <FormHelperText error={meta.touched && Boolean(meta.error)}>
+                  {(meta.touched && meta.error) ||
+                    t('For New Staff Reference Goal')}
+                </FormHelperText>
+              </FormControl>
             )}
           </Field>
         </Grid>
@@ -93,35 +163,23 @@ export const InformationStepPersonalForm: React.FC = () => {
         <Grid item xs={12}>
           <Field name="age">
             {({ field, meta }: FieldProps) => (
-              <TextField
-                {...field}
-                fullWidth
-                size="small"
-                label={t('Age')}
-                type="number"
-                error={meta.touched && Boolean(meta.error)}
-                helperText={meta.touched && meta.error}
-                variant="outlined"
-                inputProps={{ min: 18, max: 100 }}
-              />
-            )}
-          </Field>
-        </Grid>
-
-        <Grid item xs={12}>
-          <Field name="children">
-            {({ field, meta }: FieldProps) => (
-              <TextField
-                {...field}
-                fullWidth
-                size="small"
-                label={t('Number of Children')}
-                type="number"
-                error={meta.touched && Boolean(meta.error)}
-                helperText={meta.touched && meta.error}
-                variant="outlined"
-                inputProps={{ min: 0, max: 20 }}
-              />
+              <FormControl fullWidth size="small">
+                <InputLabel>{t('Age')}</InputLabel>
+                <Select {...field} label={t('Age')}>
+                  <MenuItem value={Age.Under30}>{t('Under 30')}</MenuItem>
+                  <MenuItem value={Age.ThirtyToThirtyFour}>
+                    {t('30-34')}
+                  </MenuItem>
+                  <MenuItem value={Age.ThirtyFiveToThirtyNine}>
+                    {t('35-39')}
+                  </MenuItem>
+                  <MenuItem value={Age.Over40}>{t('Over 40')}</MenuItem>
+                </Select>
+                <FormHelperText error={meta.touched && Boolean(meta.error)}>
+                  {(meta.touched && meta.error) ||
+                    t('For New Staff Reference Goal')}
+                </FormHelperText>
+              </FormControl>
             )}
           </Field>
         </Grid>
