d0d6ee9ae51b6a11c4376c1364c10f86897e0c07
Caleb Cox <canac@users.noreply.github.com>
2025-08-13 13:23:31 -0500

    Improve printing in reports pages

--- src/components/Reports/GoalCalculator/Shared/GoalCalculatorSection.tsx
+++ src/components/Reports/GoalCalculator/Shared/GoalCalculatorSection.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import InfoIcon from '@mui/icons-material/Info';
-import { IconButton, Typography, styled } from '@mui/material';
+import PrintIcon from '@mui/icons-material/Print';
+import { Button, IconButton, Stack, Typography, styled } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useGoalCalculator } from './GoalCalculatorContext';
 
@@ -11,32 +12,51 @@ const SectionContainer = styled('div')(({ theme }) => ({
 interface GoalCalculatorSectionProps {
   title: string;
   rightPanelContent?: JSX.Element;
+  printable?: boolean;
   children: React.ReactNode;
 }
 
 export const GoalCalculatorSection: React.FC<GoalCalculatorSectionProps> = ({
   title,
   rightPanelContent,
+  printable = false,
   children,
 }) => {
   const { setRightPanelContent } = useGoalCalculator();
   const { t } = useTranslation();
 
+  const handlePrint = () => {
+    window.print();
+  };
+
   return (
     <SectionContainer>
-      <Typography variant="h6">
-        {title}
-        {rightPanelContent && (
-          <IconButton
-            onClick={() => {
-              rightPanelContent && setRightPanelContent(rightPanelContent);
-            }}
-            aria-label={t('Show additional info')}
+      <Stack direction="row" justifyContent="space-between">
+        <Typography variant="h6">
+          {title}
+          {rightPanelContent && (
+            <IconButton
+              className="print-hidden"
+              onClick={() => {
+                rightPanelContent && setRightPanelContent(rightPanelContent);
+              }}
+              aria-label={t('Show additional info')}
+            >
+              <InfoIcon />
+            </IconButton>
+          )}
+        </Typography>
+        {printable && (
+          <Button
+            className="print-hidden"
+            variant="outlined"
+            endIcon={<PrintIcon />}
+            onClick={handlePrint}
           >
-            <InfoIcon />
-          </IconButton>
+            {t('Print')}
+          </Button>
         )}
-      </Typography>
+      </Stack>
       {children}
     </SectionContainer>
   );
