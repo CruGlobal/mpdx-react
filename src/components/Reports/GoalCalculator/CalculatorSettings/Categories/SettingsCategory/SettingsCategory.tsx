57f8077a64400ad59eb4b5e94bce6a09bb809601
zachery with an e <45150570+zweatshirt@users.noreply.github.com>
2025-08-20 14:57:09 -0500

    Add SettingsCategory component for goal title input

---
+++ src/components/Reports/GoalCalculator/CalculatorSettings/Categories/SettingsCategory/SettingsCategory.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { TextField } from '@mui/material';
+import { Field, Form, Formik } from 'formik';
+import { useTranslation } from 'react-i18next';
+import * as yup from 'yup';
+
+export const SettingsCategory: React.FC = () => {
+  const { t } = useTranslation();
+
+  /*
+   * TODO: Check if title exists when fetching goal,
+   * replace below with query data when available.
+   */
+  const goalTitle = '';
+
+  const validationSchema = yup.object({
+    goalTitle: yup
+      .string()
+      .min(2, t('Goal title must be at least 2 characters'))
+      .max(60, t('Goal title must be at most 60 characters'))
+      .matches(/^[a-zA-Z0-9 .,:-]*$/, t('Invalid character in goal title')),
+  });
+
+  const handleSubmit = () => {
+    // TODO: mutate/update goal title here
+  };
+
+  return (
+    <Formik
+      initialValues={{ goalTitle }}
+      validationSchema={validationSchema}
+      onSubmit={handleSubmit}
+    >
+      {({ errors, setFieldTouched, handleChange }) => (
+        <Form>
+          <Field
+            as={TextField}
+            name="goalTitle"
+            fullWidth
+            label={t('Goal Title')}
+            error={!!errors.goalTitle}
+            helperText={errors.goalTitle}
+            onChange={(event: React.SyntheticEvent) => {
+              handleChange(event);
+              setFieldTouched('goalTitle', true, false);
+            }}
+          />
+        </Form>
+      )}
+    </Formik>
+  );
+};
