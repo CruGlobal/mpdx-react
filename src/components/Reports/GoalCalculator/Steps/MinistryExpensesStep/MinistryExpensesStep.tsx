6012a317c9ea5549d92bc816518465fb5f6b65d7
Caleb Cox <canac@users.noreply.github.com>
2025-08-21 15:58:34 -0500

    Refactor step rendering

--- src/components/Reports/GoalCalculator/Steps/MinistryExpensesStep/MinistryExpensesStep.tsx
+++ src/components/Reports/GoalCalculator/Steps/MinistryExpensesStep/MinistryExpensesStep.tsx
@@ -1,67 +1,48 @@
 import React from 'react';
-import { Divider, Link, styled } from '@mui/material';
-import { Stack } from '@mui/system';
+import { Link, Typography, styled } from '@mui/material';
 import { Trans, useTranslation } from 'react-i18next';
-import { HouseholdExpensesHeader } from '../../HouseholdExpenses/HouseholdExpensesHeader';
 import { useGoalCalculator } from '../../Shared/GoalCalculatorContext';
-import { GoalCalculatorLayout } from '../../Shared/GoalCalculatorLayout';
-import { GoalCalculatorSection } from '../../Shared/GoalCalculatorSection';
-import { getFamilySections } from '../../Shared/familySections';
-import { ContinueButton } from '../../SharedComponents/ContinueButton';
-import { GoalCalculatorGrid } from '../../SharedComponents/GoalCalculatorGrid/GoalCalculatorGrid';
-import { SectionList } from '../../SharedComponents/SectionList';
-import { PrimaryCategoryRightPanel } from '../PrimaryCategoryRightPanel/PrimaryCategoryRightPanel';
+import { ExpensesStep } from '../ExpensesStep';
 
-const CategoryContainer = styled('div')(({ theme }) => ({
-  paddingInline: theme.spacing(4),
+const InstructionsWrapper = styled('div')(({ theme }) => ({
+  '.MuiTypography-root': {
+    marginBottom: theme.spacing(1),
+  },
 }));
 
-export const MinistryExpensesStep: React.FC = () => {
-  const { handleContinue, goalCalculationResult } = useGoalCalculator();
+const Instructions: React.FC = () => {
   const { t } = useTranslation();
-  const { data } = goalCalculationResult;
-  const family = data?.goalCalculation.ministryFamily;
 
   return (
-    <GoalCalculatorLayout
-      sectionListPanel={
-        <SectionList sections={family ? getFamilySections(family) : []} />
-      }
-      mainContent={
-        <Stack flex={1} spacing={4} divider={<Divider />}>
-          <CategoryContainer>
-            <Trans t={t}>
-              Enter amounts for the following categories of reimbursable and
-              ministry expenses. The{' '}
-              <Link href="https://staffweb.cru.org/mpd-donations/my-donations/mpga.html">
-                MPGA tool on StaffWeb
-              </Link>{' '}
-              can show you your averages in some of these categories. If you did
-              not take full reimbursements for the entire year, or if your
-              reimbursements were abnormally high (e.g. you had a surgery or
-              bought a new computer), or low (e.g. no summer mission), you will
-              want to adjust the averages from the MPGA to reflect an average
-              year. Click the link above, go to the Income/Expenses tab, and
-              look under the Ministry Expenses section.
-            </Trans>
-            <HouseholdExpensesHeader categoriesTotal={7000} />
-          </CategoryContainer>
-          {family?.primaryBudgetCategories.map((category) => (
-            <GoalCalculatorSection
-              key={category.id}
-              title={category.label}
-              rightPanelContent={
-                <PrimaryCategoryRightPanel category={category.category} />
-              }
-            >
-              <GoalCalculatorGrid categoryName={category.label} />
-            </GoalCalculatorSection>
-          ))}
-          <CategoryContainer>
-            <ContinueButton onClick={handleContinue} />
-          </CategoryContainer>
-        </Stack>
-      }
+    <InstructionsWrapper>
+      <Typography variant="body2">
+        <Trans t={t}>
+          Enter amounts for the following categories of reimbursable and
+          ministry expenses. The{' '}
+          <Link href="https://staffweb.cru.org/mpd-donations/my-donations/mpga.html">
+            MPGA tool on StaffWeb
+          </Link>{' '}
+          can show you your averages in some of these categories. If you did not
+          take full reimbursements for the entire year, or if your
+          reimbursements were abnormally high (e.g. you had a surgery or bought
+          a new computer), or low (e.g. no summer mission), you will want to
+          adjust the averages from the MPGA to reflect an average year. Click
+          the link above, go to the Income/Expenses tab, and look under the
+          Ministry Expenses section.
+        </Trans>
+      </Typography>
+    </InstructionsWrapper>
+  );
+};
+
+export const MinistryExpensesStep: React.FC = () => {
+  const { goalCalculationResult } = useGoalCalculator();
+  const { data } = goalCalculationResult;
+
+  return (
+    <ExpensesStep
+      instructions={<Instructions />}
+      family={data?.goalCalculation.ministryFamily}
     />
   );
 };
--- src/components/Reports/GoalCalculator/Steps/PrimaryCategoryRightPanel/PrimaryCategoryRightPanel.tsx
+++ src/components/Reports/GoalCalculator/Steps/PrimaryCategoryRightPanel/PrimaryCategoryRightPanel.tsx
@@ -1,83 +1,22 @@
 import React from 'react';
-import { Alert, Typography } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import { useTranslation } from 'react-i18next';
 import { PrimaryBudgetCategoryEnum } from 'src/graphql/types.generated';
+import { MedicalPanel } from './MedicalPanel';
+import { MileagePanel } from './MileagePanel';
+import { SavingsPanel } from './SavingsPanel';
+import { UtilitiesPanel } from './UtilitiesPanel';
 
-const MonthlySavingsTable = styled('table')({
-  width: '100%',
-});
-
-interface PrimaryCategoryRightPanelProps {
-  category: PrimaryBudgetCategoryEnum;
-}
-
-export const PrimaryCategoryRightPanel: React.FC<
-  PrimaryCategoryRightPanelProps
-> = ({ category }) => {
-  const { t } = useTranslation();
-
+export const getPrimaryCategoryRightPanel = (
+  category: PrimaryBudgetCategoryEnum,
+) => {
   switch (category) {
     case PrimaryBudgetCategoryEnum.Saving:
-      return (
-        <>
-          <Typography variant="h6" gutterBottom>
-            {t('Emergency Savings Guide')}
-          </Typography>
-          <MonthlySavingsTable>
-            <tbody>
-              <tr>
-                <td>{t('Individuals')}</td>
-                <td>$50/{t('month')}</td>
-              </tr>
-              <tr>
-                <td>{t('Couples')}</td>
-                <td>$100/{t('month')}</td>
-              </tr>
-              <tr>
-                <td>{t('With kids')}</td>
-                <td>$150/{t('month')}</td>
-              </tr>
-            </tbody>
-          </MonthlySavingsTable>
-        </>
-      );
-
+      return <SavingsPanel />;
     case PrimaryBudgetCategoryEnum.Utilities:
-      return (
-        <>
-          <Typography variant="h6" gutterBottom>
-            {t('Utilities')}
-          </Typography>
-          <Alert severity="warning">
-            {t(
-              'For mobile phone and internet expenses, only include the portion not reimbursed as a ministry expense.',
-            )}
-          </Alert>
-        </>
-      );
-
+      return <UtilitiesPanel />;
     case PrimaryBudgetCategoryEnum.Medical:
-      return (
-        <>
-          <Typography variant="h6" gutterBottom>
-            {t('Medical Expenses')}
-          </Typography>
-          <Alert severity="warning">
-            {t(
-              'Only include medical expenses that are not reimbursable through your staff account.',
-            )}
-          </Alert>
-        </>
-      );
-
+      return <MedicalPanel />;
     case PrimaryBudgetCategoryEnum.MinistryAndMedicalMileage:
-      return (
-        <Typography variant="h6" gutterBottom>
-          {t('Mileage Expenses')}
-        </Typography>
-      );
-
+      return <MileagePanel />;
     default:
       return null;
   }
