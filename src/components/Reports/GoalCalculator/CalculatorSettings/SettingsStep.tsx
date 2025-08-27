0b43e49247e98e5250afc6f8bf418efa28340502
wjames111 <william.james@cru.org>
2025-08-27 13:17:16 -0400

    Removes unneaded files for SettingsStep.

--- src/components/Reports/GoalCalculator/CalculatorSettings/SettingsStep.tsx
+++ src/components/Reports/GoalCalculator/CalculatorSettings/SettingsStep.tsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { PrimaryBudgetCategory } from 'src/graphql/types.generated';
 import { useGoalCalculator } from '../Shared/GoalCalculatorContext';
 import { GoalCalculatorLayout } from '../Shared/GoalCalculatorLayout';
 import { GoalCalculatorSection } from '../Shared/GoalCalculatorSection';
 import { GoalCalculatorGrid } from '../SharedComponents/GoalCalculatorGrid/GoalCalculatorGrid';
 import { SectionPage } from '../SharedComponents/SectionPage';
+import { InformationCategory } from './Categories/InformationCategory/InformationCategory';
 import { SettingsSectionList } from './SettingsSectionList';
 
 export const SettingsStep: React.FC = () => {
   const { goalCalculationResult } = useGoalCalculator();
   const { data } = goalCalculationResult;
+  const { t } = useTranslation();
   const specialFamilyPrimaryBudgetCategories =
     data?.goalCalculation?.specialFamily?.primaryBudgetCategories;
 
@@ -18,6 +21,11 @@ export const SettingsStep: React.FC = () => {
       sectionListPanel={<SettingsSectionList />}
       mainContent={
         <SectionPage>
+          <GoalCalculatorSection
+            subtitle={t('Take a moment to verify your information.')}
+          >
+            <InformationCategory />
+          </GoalCalculatorSection>
           {specialFamilyPrimaryBudgetCategories?.map((category) => (
             <GoalCalculatorSection key={category.id} subtitle={category.label}>
               <GoalCalculatorGrid
