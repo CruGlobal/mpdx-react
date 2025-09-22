3c1c9c06853bc768183f4ff5e6e25c618cb0b8ec
Daniel Bisgrove <daniel@bizz-websites.com>
2025-09-22 13:08:36 -0400

    fixup! Improving and updating the constants since the API changed.

--- src/hooks/useGoalCalculatorConstants.test.tsx
+++ src/hooks/useGoalCalculatorConstants.test.tsx
@@ -13,10 +13,10 @@ import { useGoalCalculatorConstants } from './useGoalCalculatorConstants';
 
 const mockData = {
   constant: {
-    __typename: 'Constant',
+    __typename: 'Constant' as const,
     mpdGoalBenefitsConstants: [
       {
-        __typename: 'MpdGoalBenefitsConstant',
+        __typename: 'MpdGoalBenefitsConstant' as const,
         id: '3d0a4925-214c-4e2e-b800-e97e31c727d8',
         size: MpdGoalBenefitsConstantSizeEnum.Single,
         sizeDisplayName: 'Single or spouse not staff',
@@ -25,7 +25,7 @@ const mockData = {
         cost: 1204.45,
       },
       {
-        __typename: 'MpdGoalBenefitsConstant',
+        __typename: 'MpdGoalBenefitsConstant' as const,
         id: '317a7530-1313-4f6a-a756-00520a3c01a3',
         size: MpdGoalBenefitsConstantSizeEnum.MarriedNoChildren,
         sizeDisplayName: 'Married with no children',
@@ -36,13 +36,13 @@ const mockData = {
     ],
     mpdGoalGeographicConstants: [
       {
-        __typename: 'MpdGoalGeographicConstant',
+        __typename: 'MpdGoalGeographicConstant' as const,
         id: 'd1097a97-2a16-4c48-9ab5-5121d8ca129e',
         location: 'None',
         percentageMultiplier: 0,
       },
       {
-        __typename: 'MpdGoalGeographicConstant',
+        __typename: 'MpdGoalGeographicConstant' as const,
         id: '32818f68-59f7-4a06-83c6-6d286ec29bbf',
         location: 'Atlanta, GA',
         percentageMultiplier: 0.12,
@@ -50,7 +50,7 @@ const mockData = {
     ],
     mpdGoalMiscConstants: [
       {
-        __typename: 'MpdGoalMiscConstant',
+        __typename: 'MpdGoalMiscConstant' as const,
         id: '5a92245c-a6ca-4d97-8e7c-b88203573ebd',
         category: MpdGoalMiscConstantCategoryEnum.Rates,
         categoryDisplayName: 'Rates',
@@ -59,7 +59,7 @@ const mockData = {
         fee: 0.12,
       },
       {
-        __typename: 'MpdGoalMiscConstant',
+        __typename: 'MpdGoalMiscConstant' as const,
         id: '8d0d738e-6031-4336-8c27-cb9a52b58b0e',
         category: MpdGoalMiscConstantCategoryEnum.DebtPercentage,
         categoryDisplayName: 'Debt Percentage',
@@ -69,7 +69,7 @@ const mockData = {
       },
     ],
   },
-} as GoalCalculatorConstantsQuery;
+};
 
 describe('useGoalCalculatorConstants', () => {
   it('should show loading state when no data', () => {
