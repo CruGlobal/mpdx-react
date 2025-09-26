210f34f5fc2f2ad05a2c5395f5392d1ad0e9774a
zachery with an e <45150570+zweatshirt@users.noreply.github.com>
2025-09-25 19:24:36 -0500

    Readd PrintTables based on reviews, mostly just using currencyFormat

--- src/components/Reports/StaffExpenseReport/Tables/PrintTables.test.tsx
+++ src/components/Reports/StaffExpenseReport/Tables/PrintTables.test.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterLuxon } from '@mui/x-date-pickers/AdapterLuxon';
 import { render } from '@testing-library/react';
 import { GqlMockedProvider } from '__tests__/util/graphqlMocking';
+import { StaffExpenseCategoryEnum } from 'src/graphql/types.generated';
 import theme from 'src/theme';
 import { ReportsStaffExpensesQuery } from '../GetStaffExpense.generated';
 import { TableType } from '../Helpers/StaffReportEnum';
@@ -16,62 +15,60 @@ interface TestComponentProps {
 }
 
 const TestComponent: React.FC<TestComponentProps> = ({ tableProps }) => (
-  <LocalizationProvider dateAdapter={AdapterLuxon}>
-    <ThemeProvider theme={theme}>
-      <GqlMockedProvider<{
-        StaffExpenseReport: ReportsStaffExpensesQuery;
-      }>
-        mocks={{
-          reportsStaffExpenses: {
-            accountId: 'account-1',
-            name: 'Test Account',
-            status: 'active',
-            startBalance: 1000,
-            endBalance: 2000,
-            funds: [
-              {
-                fundType: 'Primary',
-                total: -500,
-                categories: [
-                  {
-                    category: 'Travel',
-                    total: -300,
-                    averagePerMonth: -100,
-                    subcategories: [
-                      {
-                        subCategory: 'Flights',
-                        total: -200,
-                        averagePerMonth: -50,
-                        breakdownByMonth: [
-                          { month: '2025-01-01', total: -100 },
-                          { month: '2025-02-01', total: -100 },
-                        ],
-                      },
-                    ],
-                  },
-                ],
-              },
-            ],
-          },
-        }}
-        onCall={mutationSpy}
-      >
-        <PrintTables
-          transactions={[
+  <ThemeProvider theme={theme}>
+    <GqlMockedProvider<{
+      ReportsStaffExpenses: ReportsStaffExpensesQuery;
+    }>
+      mocks={{
+        reportsStaffExpenses: {
+          accountId: 'account-1',
+          name: 'Test Account',
+          status: 'active',
+          startBalance: 1000,
+          endBalance: 2000,
+          funds: [
             {
               fundType: 'Primary',
-              month: '2025-01-01',
-              category: 'Travel - Flights',
-              total: -100,
+              total: -500,
+              categories: [
+                {
+                  category: 'Travel',
+                  total: -300,
+                  averagePerMonth: -100,
+                  subcategories: [
+                    {
+                      subCategory: 'Flights',
+                      total: -200,
+                      averagePerMonth: -50,
+                      breakdownByMonth: [
+                        { month: '2025-01-01', total: -100 },
+                        { month: '2025-02-01', total: -100 },
+                      ],
+                    },
+                  ],
+                },
+              ],
             },
-          ]}
-          transactionTotal={0}
-          type={TableType.Expenses}
-          {...tableProps}
-        />
-      </GqlMockedProvider>
-    </ThemeProvider>
-  </LocalizationProvider>
+          ],
+        },
+      }}
+      onCall={mutationSpy}
+    >
+      <PrintTables
+        transactions={[
+          {
+            fundType: 'Primary',
+            month: '2025-01-01',
+            category: StaffExpenseCategoryEnum.Benefits,
+            total: -100,
+          },
+        ]}
+        transactionTotal={0}
+        type={TableType.Expenses}
+        {...tableProps}
+      />
+    </GqlMockedProvider>
+  </ThemeProvider>
 );
 
 describe('PrintTables', () => {
@@ -85,13 +82,15 @@ describe('PrintTables', () => {
     expect(
       await findByRole('columnheader', { name: 'Description' }),
     ).toBeInTheDocument();
-    expect(getByRole('cell', { name: 'Travel - Flights' })).toBeInTheDocument();
+    expect(
+      getByRole('cell', { name: StaffExpenseCategoryEnum.Benefits }),
+    ).toBeInTheDocument();
     expect(
       await findByRole('columnheader', { name: 'Amount' }),
     ).toBeInTheDocument();
-    expect(getByRole('cell', { name: '-$100.00' })).toBeInTheDocument();
+    expect(getByRole('cell', { name: '-$100' })).toBeInTheDocument();
     expect(getByRole('cell', { name: 'Total' })).toBeInTheDocument();
-    expect(getByRole('cell', { name: '$0.00' })).toBeInTheDocument();
+    expect(getByRole('cell', { name: '$0' })).toBeInTheDocument();
   });
 
   it('renders empty table message when no transactions', async () => {
