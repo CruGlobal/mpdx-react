debcdc0e7fcbbf6c3cbfe874d73eb25c026c0b74
zachery with an e <45150570+zweatshirt@users.noreply.github.com>
2025-09-25 19:36:30 -0500

    Add filterTransactions function and corresponding tests for transaction filtering

---
+++ src/components/Reports/StaffExpenseReport/Helpers/filterTransactions.test.tsx
@@ -0,0 +1,109 @@
+import { DateTime } from 'luxon';
+import {
+  Fund,
+  StaffExpenseCategoryEnum,
+  StaffExpensesSubCategoryEnum,
+} from 'src/graphql/types.generated';
+import { filterTransactions } from './filterTransactions';
+
+/* Tests for category filtering need to be written still */
+
+describe('filterTransactions', () => {
+  const mockFund: Fund = {
+    fundType: 'Primary',
+    total: -500,
+    categories: [
+      {
+        category: StaffExpenseCategoryEnum.AdditionalSalary,
+        total: -300,
+        averagePerMonth: -100,
+        subcategories: [
+          {
+            subCategory: StaffExpensesSubCategoryEnum.AdditionalSalary,
+            total: -200,
+            averagePerMonth: -50,
+            breakdownByMonth: [
+              {
+                month: '2025-01-01',
+                total: -100,
+                __typename: 'BreakdownByMonth',
+              },
+              {
+                month: '2025-02-01',
+                total: -100,
+                __typename: 'BreakdownByMonth',
+              },
+            ],
+          },
+          {
+            subCategory: StaffExpensesSubCategoryEnum.OtherAssessment,
+            total: -100,
+            averagePerMonth: -50,
+            breakdownByMonth: [
+              {
+                month: '2025-01-01',
+                total: -50,
+                __typename: 'BreakdownByMonth',
+              },
+              {
+                month: '2025-02-01',
+                total: -50,
+                __typename: 'BreakdownByMonth',
+              },
+            ],
+          },
+        ],
+        breakdownByMonth: [
+          { month: '2025-01-01', total: -150, __typename: 'BreakdownByMonth' },
+          { month: '2025-02-01', total: -150, __typename: 'BreakdownByMonth' },
+        ],
+      },
+    ],
+  };
+
+  it('filters transactions for the target month by default', () => {
+    const targetTime = DateTime.fromISO('2025-01-15');
+    let result = filterTransactions(mockFund, targetTime, {
+      selectedDateRange: null,
+      startDate: null,
+      endDate: null,
+    });
+    expect(result).toHaveLength(2);
+
+    result = filterTransactions(mockFund, targetTime);
+    expect(result).toHaveLength(2);
+  });
+
+  it('filters transactions by custom date range', () => {
+    const targetTime = DateTime.fromISO('2024-06-15');
+    const result = filterTransactions(mockFund, targetTime, {
+      selectedDateRange: null,
+      startDate: DateTime.fromISO('2025-01-01'),
+      endDate: DateTime.fromISO('2025-02-03'),
+    });
+    expect(result).toHaveLength(4);
+  });
+
+  it('returns empty array if no main categories', () => {
+    const emptyFund = { ...mockFund, categories: null };
+    const targetTime = DateTime.fromISO('2024-06-01');
+    const result = filterTransactions(emptyFund, targetTime);
+    expect(result).toEqual([]);
+  });
+
+  it('returns empty array if no transactions in range and custom date range not selected', () => {
+    const targetTime = DateTime.fromISO('2023-01-01');
+    const result = filterTransactions(mockFund, targetTime);
+    expect(result).toEqual([]);
+  });
+
+  it('returns empty array if no transactions in range with custom date range', () => {
+    const targetTime = DateTime.fromISO('2024-06-01');
+    const result = filterTransactions(mockFund, targetTime, {
+      selectedDateRange: null,
+      startDate: DateTime.fromISO('2025-04-01'),
+      endDate: DateTime.fromISO('2025-06-03'),
+    });
+    expect(result).toEqual([]);
+  });
+});
---
+++ src/components/Reports/StaffExpenseReport/Helpers/filterTransactions.tsx
@@ -0,0 +1,75 @@
+import { DateTime } from 'luxon';
+import { Filters } from 'src/components/Reports/StaffExpenseReport/SettingsDialog/SettingsDialog';
+import { Transaction } from 'src/components/Reports/StaffExpenseReport/StaffExpenseReport';
+import { BreakdownByMonth, Fund } from 'src/graphql/types.generated';
+
+export const filterTransactions = (
+  fund: Fund,
+  targetTime: DateTime,
+  filters?: Filters | null,
+): Transaction[] => {
+  // Create a date range filter based on the provided filters or default to the target time
+  const isInRange = createDateRangeFilter(filters, targetTime);
+
+  return (
+    fund.categories?.flatMap((category) => {
+      if (category.subcategories) {
+        return category.subcategories.flatMap(
+          (subcategory) =>
+            subcategory.breakdownByMonth
+              ?.filter((transaction) =>
+                isInRange(DateTime.fromISO(transaction.month)),
+              )
+              .map((transaction) =>
+                mapTransactionToCategory(
+                  transaction,
+                  fund.fundType,
+                  `${category.category} - ${subcategory.subCategory}`,
+                ),
+              ) ?? [],
+        );
+      }
+      return (
+        category.breakdownByMonth
+          ?.filter((transaction) =>
+            isInRange(DateTime.fromISO(transaction.month)),
+          )
+          .map((transaction) =>
+            mapTransactionToCategory(
+              transaction,
+              fund.fundType,
+              category.category,
+            ),
+          ) ?? []
+      );
+    }) ?? []
+  );
+};
+
+const mapTransactionToCategory = (
+  transaction: BreakdownByMonth,
+  fundType: Fund['fundType'],
+  category: string,
+): Transaction => ({
+  ...transaction,
+  fundType,
+  category,
+});
+
+const createDateRangeFilter = (
+  filters: Filters | null | undefined,
+  targetTime: DateTime<boolean>,
+) => {
+  return (transactionDate: DateTime) => {
+    if (filters && (filters.startDate || filters.endDate)) {
+      return (
+        (!filters.startDate || transactionDate >= filters.startDate) &&
+        (!filters.endDate || transactionDate <= filters.endDate)
+      );
+    }
+    return (
+      transactionDate >= targetTime.startOf('month') &&
+      transactionDate <= targetTime.endOf('month')
+    );
+  };
+};
