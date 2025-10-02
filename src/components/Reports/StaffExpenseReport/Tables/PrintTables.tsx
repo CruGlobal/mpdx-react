23601189f463d8d3cad6f3ea8bd592488d74e6f7
zachery with an e <45150570+zweatshirt@users.noreply.github.com>
2025-10-02 12:04:46 -0500

    Adds Income and Expenses tables print functionality

---
+++ src/components/Reports/StaffExpenseReport/Tables/PrintTables.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import { useLocale } from 'src/hooks/useLocale';
+import { currencyFormat } from 'src/lib/intlFormat';
+import { Transaction } from '../StaffExpenseReport';
+
+export interface PrintTablesProps {
+  transactions: Transaction[];
+  transactionTotal: number;
+  type: string;
+}
+
+export const PrintTables: React.FC<PrintTablesProps> = ({
+  transactions,
+  transactionTotal,
+  type,
+}) => {
+  const { t } = useTranslation();
+  const locale = useLocale();
+
+  return (
+    <TableContainer>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell sx={{ width: '20%' }}>
+              <strong>{t('Date')}</strong>
+            </TableCell>
+            <TableCell sx={{ width: '55%' }}>
+              <strong>{t('Description')}</strong>
+            </TableCell>
+            <TableCell sx={{ width: '25%' }}>
+              <strong>{t('Amount')}</strong>
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {transactions.length ? (
+            <>
+              {transactions.map((row) => (
+                <TableRow key={`${row.month}-${row.category}`}>
+                  <TableCell>{row.month}</TableCell>
+                  <TableCell>{row.displayCategory}</TableCell>
+                  <TableCell>
+                    {currencyFormat(row.total, 'USD', locale)}
+                  </TableCell>
+                </TableRow>
+              ))}
+              <TableRow>
+                <TableCell>
+                  <strong>{t('Total')}</strong>
+                </TableCell>
+                <TableCell />
+                <TableCell>
+                  <strong>
+                    <span
+                      style={{ color: transactionTotal < 0 ? 'red' : 'green' }}
+                    >
+                      {currencyFormat(transactionTotal, 'USD', locale)}
+                    </span>
+                  </strong>
+                </TableCell>
+              </TableRow>
+            </>
+          ) : (
+            <>
+              <TableRow>
+                {type === 'income' ? (
+                  <TableCell colSpan={3} align="center">
+                    {t('No Income Transactions Found')}
+                  </TableCell>
+                ) : (
+                  <TableCell colSpan={3} align="center">
+                    {t('No Expense Transactions Found')}
+                  </TableCell>
+                )}
+              </TableRow>
+              <TableRow>
+                <TableCell>
+                  <strong>{t('Total')}</strong>
+                </TableCell>
+                <TableCell />
+                <TableCell>
+                  <strong>
+                    {currencyFormat(transactionTotal, 'USD', locale)}
+                  </strong>
+                </TableCell>
+              </TableRow>
+            </>
+          )}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
