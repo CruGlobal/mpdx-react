81789768aa6e34f8564ec9da3ef76c3a5f1f63bc
zachery with an e <45150570+zweatshirt@users.noreply.github.com>
2025-10-02 12:02:05 -0500

    Adds transaction filtering functions as well as helpers for date formatting and converting category enums to localized values

---
+++ src/components/Reports/StaffExpenseReport/filterTransactions.test.tsx
@@ -0,0 +1,143 @@
+import { DateTime } from 'luxon';
+import {
+  Fund,
+  StaffExpenseCategoryEnum,
+  StaffExpensesSubCategoryEnum,
+} from 'src/graphql/types.generated';
+import i18n from 'src/lib/i18n';
+import { DateRange } from './Helpers/StaffReportEnum';
+import { filterTransactions } from './filterTransactions';
+
+describe('filterTransactions', () => {
+  const mockFund: Fund = {
+    id: 'fund-1',
+    fundType: 'Primary',
+    balance: 1000,
+    deficitLimit: 0,
+    total: -500,
+    categories: [
+      {
+        category: StaffExpenseCategoryEnum.AdditionalSalary,
+        total: -300,
+        averagePerMonth: -100,
+        subcategories: [
+          {
+            subCategory: StaffExpensesSubCategoryEnum.AdditionalSalary,
+            total: -200,
+            averagePerMonth: -50,
+            breakdownByMonth: [
+              {
+                month: '2025-01-01',
+                total: -100,
+              },
+              {
+                month: '2025-02-01',
+                total: -100,
+              },
+            ],
+          },
+          {
+            subCategory: StaffExpensesSubCategoryEnum.OtherAssessment,
+            total: -100,
+            averagePerMonth: -50,
+            breakdownByMonth: [
+              {
+                month: '2025-01-01',
+                total: -50,
+              },
+              {
+                month: '2025-02-01',
+                total: -50,
+              },
+            ],
+          },
+        ],
+        breakdownByMonth: [
+          { month: '2025-01-01', total: -150 },
+          { month: '2025-02-01', total: -150 },
+        ],
+      },
+    ],
+  };
+
+  it('filters transactions for the target month by default', () => {
+    const targetTime = DateTime.fromISO('2025-01-15');
+    const result = filterTransactions({
+      fund: mockFund,
+      targetTime,
+      t: i18n.t,
+      filters: {
+        selectedDateRange: null,
+        startDate: null,
+        endDate: null,
+        categories: [],
+      },
+    });
+    expect(result).toHaveLength(2);
+  });
+
+  it('filters transactions by custom date range', () => {
+    const targetTime = DateTime.fromISO('2024-06-15');
+    const result = filterTransactions({
+      fund: mockFund,
+      targetTime,
+      t: i18n.t,
+      filters: {
+        selectedDateRange: null,
+        startDate: DateTime.fromISO('2025-01-01'),
+        endDate: DateTime.fromISO('2025-02-03'),
+        categories: [],
+      },
+    });
+    expect(result).toHaveLength(4);
+  });
+
+  it('returns empty array if no main categories', () => {
+    const emptyFund = { ...mockFund, categories: null };
+    const targetTime = DateTime.fromISO('2024-06-01');
+    const result = filterTransactions({
+      fund: emptyFund,
+      targetTime,
+      t: i18n.t,
+      filters: {
+        selectedDateRange: null,
+        startDate: DateTime.fromISO('2025-01-01'),
+        endDate: DateTime.fromISO('2025-02-03'),
+        categories: [],
+      },
+    });
+    expect(result).toEqual([]);
+  });
+
+  it('returns empty array if no transactions in range and custom date range not selected', () => {
+    const targetTime = DateTime.fromISO('2023-01-01');
+    const result = filterTransactions({
+      fund: mockFund,
+      targetTime,
+      t: i18n.t,
+      filters: {
+        selectedDateRange: null,
+        startDate: null,
+        endDate: null,
+        categories: [],
+      },
+    });
+    expect(result).toEqual([]);
+  });
+
+  it('returns empty array if no transactions in range with custom date range', () => {
+    const targetTime = DateTime.fromISO('2024-06-01');
+    const result = filterTransactions({
+      fund: mockFund,
+      targetTime,
+      t: i18n.t,
+      filters: {
+        selectedDateRange: DateRange.YearToDate,
+        startDate: DateTime.fromISO('2025-04-01'),
+        endDate: DateTime.fromISO('2025-06-03'),
+        categories: [],
+      },
+    });
+    expect(result).toEqual([]);
+  });
+});
---
+++ src/components/Reports/StaffExpenseReport/filterTransactions.tsx
@@ -0,0 +1,106 @@
+import { TFunction } from 'i18next';
+import { DateTime } from 'luxon';
+import { Filters } from 'src/components/Reports/StaffExpenseReport/SettingsDialog/SettingsDialog';
+import { Transaction } from 'src/components/Reports/StaffExpenseReport/StaffExpenseReport';
+import {
+  BreakdownByMonth,
+  Fund,
+  StaffExpenseCategoryEnum,
+} from 'src/graphql/types.generated';
+import { getLocalizedCategory } from './Helpers/useLocalizedCategory';
+
+interface FilterTransactionsParams {
+  fund: Pick<Fund, 'fundType' | 'total' | 'categories'>;
+  targetTime: DateTime;
+  t: TFunction;
+  filters?: Filters | null;
+  tableType?: 'income' | 'expenses';
+}
+
+export const filterTransactions = ({
+  fund,
+  targetTime,
+  t,
+  filters,
+  tableType,
+}: FilterTransactionsParams): Transaction[] => {
+  // Create a date range filter based on the provided filters or default to the target time
+  const isInRange = createDateRangeFilter(filters, targetTime);
+
+  const transactions =
+    fund.categories?.flatMap((category) => {
+      if (category.subcategories) {
+        return category.subcategories.flatMap(
+          (subcategory) =>
+            subcategory.breakdownByMonth
+              ?.filter((transaction) =>
+                isInRange(DateTime.fromISO(transaction.month)),
+              )
+              .map((transaction) =>
+                mapTransactionToCategory(
+                  transaction,
+                  fund.fundType,
+                  category.category,
+                  `${getLocalizedCategory(
+                    category.category,
+                    t,
+                  )} - ${getLocalizedCategory(subcategory.subCategory, t)}`,
+                ),
+              ) ?? [],
+        );
+      }
+      return (
+        category.breakdownByMonth
+          ?.filter((transaction) =>
+            isInRange(DateTime.fromISO(transaction.month)),
+          )
+          .map((transaction) =>
+            mapTransactionToCategory(
+              transaction,
+              fund.fundType,
+              category.category,
+              getLocalizedCategory(category.category, t),
+            ),
+          ) ?? []
+      );
+    }) ?? [];
+
+  // Filter by positive/negative values BEFORE grouping if tableType is specified
+  const filteredTransactions = tableType
+    ? transactions.filter((transaction) =>
+        tableType === 'income' ? transaction.total > 0 : transaction.total < 0,
+      )
+    : transactions;
+
+  return filteredTransactions;
+};
+
+const mapTransactionToCategory = (
+  transaction: BreakdownByMonth,
+  fundType: Fund['fundType'],
+  category: StaffExpenseCategoryEnum,
+  displayCategory: string,
+): Transaction => ({
+  ...transaction,
+  fundType,
+  category: category,
+  displayCategory,
+});
+
+const createDateRangeFilter = (
+  filters: Filters | null | undefined,
+  targetTime: DateTime<boolean>,
+) => {
+  return (transactionDate: DateTime) => {
+    if (filters && (filters.startDate || filters.endDate)) {
+      return (
+        (!filters.startDate || transactionDate >= filters.startDate) &&
+        (!filters.endDate || transactionDate <= filters.endDate)
+      );
+    }
+    return (
+      transactionDate >= targetTime.startOf('month') &&
+      transactionDate <= targetTime.endOf('month')
+    );
+  };
+};
