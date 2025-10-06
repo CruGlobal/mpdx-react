458fcf1b7890332b2aa1b717be920598cf3a0a44
Caleb Cox <canac@users.noreply.github.com>
2025-10-06 10:26:14 -0500

    Add back Goal Calclator Name field

--- src/components/Reports/GoalCalculator/CalculatorSettings/SettingsStep.tsx
+++ src/components/Reports/GoalCalculator/CalculatorSettings/SettingsStep.tsx
@@ -6,6 +6,7 @@ import { GoalCalculatorSection } from '../Shared/GoalCalculatorSection';
 import { GoalCalculatorGrid } from '../SharedComponents/GoalCalculatorGrid/GoalCalculatorGrid';
 import { SectionPage } from '../SharedComponents/SectionPage';
 import { InformationCategory } from './Categories/InformationCategory/InformationCategory';
+import { SettingsCategory } from './Categories/SettingsCategory/SettingsCategory';
 import { SettingsSectionList } from './SettingsSectionList';
 
 export const SettingsStep: React.FC = () => {
@@ -20,6 +21,12 @@ export const SettingsStep: React.FC = () => {
       sectionListPanel={<SettingsSectionList />}
       mainContent={
         <SectionPage>
+          <GoalCalculatorSection
+            title={t('Settings')}
+            subtitle={t('What do you want to call this goal?')}
+          >
+            <SettingsCategory />
+          </GoalCalculatorSection>
           <GoalCalculatorSection
             title={t('Information')}
             subtitle={t('Take a moment to verify your information.')}
