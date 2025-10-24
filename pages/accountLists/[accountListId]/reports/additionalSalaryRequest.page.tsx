e511201c947e5676629a1dcfab5f04f629f8350a
zachery with an e <45150570+zweatshirt@users.noreply.github.com>
2025-10-24 09:06:42 -0500

    Add SidePanelLayout for designation accounts

--- pages/accountLists/[accountListId]/reports/additionalSalaryRequest.page.tsx
+++ pages/accountLists/[accountListId]/reports/additionalSalaryRequest.page.tsx
@@ -1,13 +1,29 @@
 import Head from 'next/head';
-import React from 'react';
+import React, { useState } from 'react';
+import { Box } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { ensureSessionAndAccountList } from 'pages/api/utils/pagePropsHelpers';
+import { SidePanelsLayout } from 'src/components/Layouts/SidePanelsLayout';
 import { AdditionalSalaryRequest } from 'src/components/Reports/AdditionalSalaryRequest/AdditionalSalaryRequest';
+import {
+  HeaderTypeEnum,
+  MultiPageHeader,
+} from 'src/components/Shared/MultiPageLayout/MultiPageHeader';
+import {
+  MultiPageMenu,
+  NavTypeEnum,
+} from 'src/components/Shared/MultiPageLayout/MultiPageMenu/MultiPageMenu';
 import useGetAppSettings from 'src/hooks/useGetAppSettings';
 
 const AdditionalSalaryRequestPage: React.FC = () => {
   const { appName } = useGetAppSettings();
   const { t } = useTranslation();
+  const [designationAccounts, setDesignationAccounts] = useState<string[]>([]);
+  const [isNavListOpen, setIsNavListOpen] = useState<boolean>(false);
+
+  const handleNavListToggle = () => {
+    setIsNavListOpen(!isNavListOpen);
+  };
 
   return (
     <>
@@ -16,7 +32,34 @@ const AdditionalSalaryRequestPage: React.FC = () => {
           {appName} | {t('Additional Salary Request')}
         </title>
       </Head>
-      <AdditionalSalaryRequest />
+      <SidePanelsLayout
+        isScrollBox={false}
+        leftPanel={
+          <MultiPageMenu
+            isOpen={isNavListOpen}
+            selectedId="salaryRequest"
+            onClose={handleNavListToggle}
+            designationAccounts={designationAccounts}
+            setDesignationAccounts={setDesignationAccounts}
+            navType={NavTypeEnum.Reports}
+          />
+        }
+        leftOpen={isNavListOpen}
+        leftWidth="290px"
+        mainContent={
+          <>
+            <MultiPageHeader
+              isNavListOpen={isNavListOpen}
+              onNavListToggle={handleNavListToggle}
+              title={t('Additional Salary Request')}
+              headerType={HeaderTypeEnum.Report}
+            />
+            <Box>
+              <AdditionalSalaryRequest />
+            </Box>
+          </>
+        }
+      />
     </>
   );
 };
