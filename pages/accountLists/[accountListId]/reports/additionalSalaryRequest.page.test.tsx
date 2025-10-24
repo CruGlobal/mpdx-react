7943bc768d2844bd63d6942d144234c544ad5fc8
zachery with an e <45150570+zweatshirt@users.noreply.github.com>
2025-10-24 15:56:14 -0500

    Add tests for AdditionalSalaryRequest page, context, icon panel layout, and side panel layout

---
+++ pages/accountLists/[accountListId]/reports/additionalSalaryRequest.page.test.tsx
@@ -0,0 +1,45 @@
+import { ThemeProvider } from '@mui/material/styles';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import TestRouter from '__tests__/util/TestRouter';
+import { GqlMockedProvider } from '__tests__/util/graphqlMocking';
+import theme from 'src/theme';
+import AdditionalSalaryRequestPage, {
+  getServerSideProps,
+} from './additionalSalaryRequest.page';
+
+const TestComponent: React.FC = () => (
+  <ThemeProvider theme={theme}>
+    <TestRouter router={{ query: { accountListId: 'account-list-1' } }}>
+      <GqlMockedProvider>
+        <AdditionalSalaryRequestPage />
+      </GqlMockedProvider>
+    </TestRouter>
+  </ThemeProvider>
+);
+
+describe('AdditionalSalaryRequest page', () => {
+  it('renders page', async () => {
+    const { findByRole } = render(<TestComponent />);
+
+    expect(
+      await findByRole('heading', { name: 'About this Form content' }),
+    ).toBeInTheDocument();
+  });
+
+  it('toggles side panel on header click', async () => {
+    const { findByTestId, getByRole, getByTestId } = render(<TestComponent />);
+
+    const leftPanel = getByTestId('SidePanelsLayoutLeftPanel');
+
+    userEvent.click(getByRole('button', { name: 'Toggle Navigation Panel' }));
+    expect(leftPanel).toHaveStyle('transform: none');
+
+    userEvent.click(await findByTestId('CloseIcon'));
+    expect(leftPanel).toHaveStyle('transform: translate(-100%)');
+  });
+
+  it('uses ensureSessionAndAccountList', () => {
+    expect(getServerSideProps).toBeDefined();
+  });
+});
---
+++ src/components/Reports/AdditionalSalaryRequest/AdditionalSalaryRequest.test.tsx
@@ -0,0 +1,28 @@
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { AdditionalSalaryRequest } from './AdditionalSalaryRequest';
+import { AdditionalSalaryRequestTestWrapper } from './AdditionalSalaryRequestTestWrapper';
+
+describe('AdditionalSalaryRequest', () => {
+  it('renders main content based on selected section', async () => {
+    const { getByRole, findByRole } = render(
+      <AdditionalSalaryRequestTestWrapper>
+        <AdditionalSalaryRequest />
+      </AdditionalSalaryRequestTestWrapper>,
+    );
+
+    expect(
+      await findByRole('heading', { name: 'About this Form content' }),
+    ).toBeInTheDocument();
+
+    userEvent.click(getByRole('button', { name: '2. Complete Form' }));
+    expect(
+      await findByRole('heading', { name: 'Complete Form content' }),
+    ).toBeInTheDocument();
+
+    userEvent.click(getByRole('button', { name: '3. Receipt' }));
+    expect(
+      await findByRole('heading', { name: 'Receipt content' }),
+    ).toBeInTheDocument();
+  });
+});
