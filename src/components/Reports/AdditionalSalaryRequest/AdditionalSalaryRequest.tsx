288c1f3ba92f0c60ff4d8bb127fcf583898b14f5
zachery with an e <45150570+zweatshirt@users.noreply.github.com>
2025-10-27 14:16:09 -0500

    Translate placeholders

--- src/components/Reports/AdditionalSalaryRequest/AdditionalSalaryRequest.tsx
+++ src/components/Reports/AdditionalSalaryRequest/AdditionalSalaryRequest.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 import { AdditionalSalaryRequestSectionEnum } from './AdditionalSalaryRequestHelper';
 import { SalaryRequestForm } from './SalaryRequestForm/SalaryRequestForm';
 import {
@@ -11,14 +12,17 @@ import { SectionList } from './Shared/SectionList';
 
 const MainContent: React.FC = () => {
   const { selectedSection } = useAdditionalSalaryRequest();
+  const { t } = useTranslation();
 
   switch (selectedSection) {
     case AdditionalSalaryRequestSectionEnum.AboutForm:
-      return <Typography variant="h5">About this Form content</Typography>;
+      return (
+        <Typography variant="h5">{t('About this Form content')}</Typography>
+      );
     case AdditionalSalaryRequestSectionEnum.CompleteForm:
       return <SalaryRequestForm />;
     case AdditionalSalaryRequestSectionEnum.Receipt:
-      return <Typography variant="h5">Receipt content</Typography>;
+      return <Typography variant="h5">{t('Receipt content')}</Typography>;
   }
 };
 
