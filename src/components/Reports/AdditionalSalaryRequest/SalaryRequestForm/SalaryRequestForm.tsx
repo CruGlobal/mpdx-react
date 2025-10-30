d38b8fc68c735179f074ff9a7c58157a49098aad
zachery with an e <45150570+zweatshirt@users.noreply.github.com>
2025-10-30 16:17:39 -0500

    Change addition salary request form values to init with 0

--- src/components/Reports/AdditionalSalaryRequest/SalaryRequestForm/SalaryRequestForm.tsx
+++ src/components/Reports/AdditionalSalaryRequest/SalaryRequestForm/SalaryRequestForm.tsx
@@ -44,21 +44,21 @@ export const SalaryRequestForm: React.FC = () => {
   const categories = useCompleteFormCategories();
 
   const initialValues: CompleteFormValues = {
-    currentYearSalary: '',
-    previousYearSalary: '',
-    additionalSalary: '',
-    adoption: '',
-    contribution403b: '',
-    counseling: '',
-    healthcareExpenses: '',
-    babysitting: '',
-    childrenMinistryTrip: '',
-    childrenCollege: '',
-    movingExpense: '',
-    seminary: '',
-    housingDownPayment: '',
-    autoPurchase: '',
-    reimbursableExpenses: '',
+    currentYearSalary: '0',
+    previousYearSalary: '0',
+    additionalSalary: '0',
+    adoption: '0',
+    contribution403b: '0',
+    counseling: '0',
+    healthcareExpenses: '0',
+    babysitting: '0',
+    childrenMinistryTrip: '0',
+    childrenCollege: '0',
+    movingExpense: '0',
+    seminary: '0',
+    housingDownPayment: '0',
+    autoPurchase: '0',
+    reimbursableExpenses: '0',
   };
 
   const createCurrencyValidation = (fieldName: string, max?: number) => {
