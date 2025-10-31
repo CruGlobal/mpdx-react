de7b37fa67820946e8a425527cdc350d4ac52058
wjames111 <william.james@cru.org>
2025-10-31 11:55:17 -0400

    fixup! Adds Print button

--- src/components/Reports/PartnerGivingAnalysisReport/Actions/HeaderActions.tsx
+++ src/components/Reports/PartnerGivingAnalysisReport/Actions/HeaderActions.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PrintIcon from '@mui/icons-material/Print';
-import { Button, ButtonGroup, SvgIcon } from '@mui/material';
+import { Button } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
 interface HeaderActionsProps {
@@ -11,17 +11,12 @@ export const HeaderActions: React.FC<HeaderActionsProps> = ({ onPrint }) => {
   const { t } = useTranslation();
 
   return (
-    <ButtonGroup aria-label={t('Report header button group')}>
-      <Button
-        startIcon={
-          <SvgIcon fontSize="small">
-            <PrintIcon titleAccess={t('Print')} />
-          </SvgIcon>
-        }
-        onClick={onPrint}
-      >
-        {t('Print')}
-      </Button>
-    </ButtonGroup>
+    <Button
+      startIcon={<PrintIcon titleAccess={t('Print')} />}
+      onClick={onPrint}
+      variant="outlined"
+    >
+      {t('Print')}
+    </Button>
   );
 };
