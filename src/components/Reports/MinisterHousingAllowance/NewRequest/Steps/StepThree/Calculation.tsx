2957a1e3132b804a935bff4889fac8ead21bd5b8
Katelyn Grimes <katelyn.grimes@cru.org>
2025-11-03 09:47:40 -0500

    Created Step 3: Rent

--- src/components/Reports/MinisterHousingAllowance/NewRequest/Steps/StepThree/Calculation.tsx
+++ src/components/Reports/MinisterHousingAllowance/NewRequest/Steps/StepThree/Calculation.tsx
@@ -1,35 +1,97 @@
+import { useEffect, useState } from 'react';
 import { OpenInNew } from '@mui/icons-material';
-import { Box, Link, Typography } from '@mui/material';
+import {
+  Alert,
+  Box,
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormHelperText,
+  Link,
+  Typography,
+} from '@mui/material';
+import { Formik, useFormikContext } from 'formik';
 import { DateTime } from 'luxon';
 import { Trans, useTranslation } from 'react-i18next';
+import * as yup from 'yup';
 import { useLocale } from 'src/hooks/useLocale';
+import i18n from 'src/lib/i18n';
 import { dateFormatShort } from 'src/lib/intlFormat';
 import { RentOwnEnum } from '../../../Shared/sharedTypes';
+import { FormValues } from '../../NewRequestPage';
 import { DirectionButtons } from '../../Shared/DirectionButtons';
+import { CostOfHome } from './CalcComponents/CostOfHome';
 import { EndingSection } from './CalcComponents/EndingSection';
-import { InitialQuestionsCard } from './CalcComponents/InitialQuestionsCard';
+import { FairRentalValue } from './CalcComponents/FairRentalValue';
 import { RequestSummaryCard } from './CalcComponents/RequestSummaryCard';
 
+//TODO: Get correct links
 interface CalculationProps {
   boardApprovalDate: string;
   availableDate: string;
-  rentOrOwn?: RentOwnEnum;
   handleBack: () => void;
   handleNext: () => void;
 }
+export interface CalculationFormValues {
+  rentalValue?: number | null | undefined;
+  furnitureCostsOne?: number | null | undefined;
+  avgUtilityOne?: number | null | undefined;
+  mortgagePayment?: number | null | undefined;
+  furnitureCostsTwo?: number | null | undefined;
+  repairCosts?: number | null | undefined;
+  avgUtilityTwo?: number | null | undefined;
+  unexpectedExpenses?: number | null | undefined;
+  isChecked?: boolean;
+}
+
+const getValidationSchema = (rentOrOwn?: RentOwnEnum) => {
+  const baseSchema = {
+    mortgagePayment: yup.number().required(i18n.t('Required field.')),
+    furnitureCostsTwo: yup.number().required(i18n.t('Required field.')),
+    repairCosts: yup.number().required(i18n.t('Required field.')),
+    avgUtilityTwo: yup.number().required(i18n.t('Required field.')),
+    unexpectedExpenses: yup.number().required(i18n.t('Required field.')),
+    isChecked: yup
+      .boolean()
+      .oneOf([true], i18n.t('This box must be checked to continue.')),
+  };
+
+  // extra fields for OWN
+  if (rentOrOwn === RentOwnEnum.Own) {
+    return yup.object({
+      ...baseSchema,
+      rentalValue: yup.number().required(i18n.t('Required field.')),
+      furnitureCostsOne: yup.number().required(i18n.t('Required field.')),
+      avgUtilityOne: yup.number().required(i18n.t('Required field.')),
+    });
+  }
+
+  return yup.object(baseSchema);
+};
 
 export const Calculation: React.FC<CalculationProps> = ({
   boardApprovalDate,
   availableDate,
-  rentOrOwn = RentOwnEnum.Own,
   handleBack,
   handleNext,
 }) => {
   const { t } = useTranslation();
   const locale = useLocale();
 
-  //TODO: Get correct links
-  //TODO: get formik value for rent or own to conditionally show fields
+  const { values } = useFormikContext<FormValues>();
+  const { rentOrOwn } = values;
+
+  const initialValues: CalculationFormValues = {
+    rentalValue: undefined,
+    furnitureCostsOne: undefined,
+    avgUtilityOne: undefined,
+    mortgagePayment: undefined,
+    furnitureCostsTwo: undefined,
+    repairCosts: undefined,
+    avgUtilityTwo: undefined,
+    unexpectedExpenses: undefined,
+    isChecked: false,
+  };
 
   const boardDateFormatted = dateFormatShort(
     DateTime.fromISO(boardApprovalDate ?? DateTime.now().toISO()),
@@ -42,51 +104,120 @@ export const Calculation: React.FC<CalculationProps> = ({
   );
 
   return (
-    <>
-      <Box mb={2}>
-        <Typography variant="h5">{t('Calculate Your MHA Request')}</Typography>
-      </Box>
-      <Trans i18nKey="newRequestCalculation">
-        <p style={{ lineHeight: 1.5 }}>
-          Please enter dollar amounts for each category below to calculate your
-          Annual MHA. The board will review this number after{' '}
-          {boardDateFormatted} and you will receive notice of your approval
-          effective {availableDateFormatted}.
-        </p>
-      </Trans>
-      <Box sx={{ mt: 2, mb: 3 }}>
-        <OpenInNew
-          fontSize="medium"
-          sx={{ verticalAlign: 'middle', opacity: 0.56 }}
-        />{' '}
-        <Link href="">What expenses can I claim on my MHA?</Link>
-      </Box>
-      {rentOrOwn === RentOwnEnum.Own && (
-        <Box mb={3}>
-          <InitialQuestionsCard
-            title={t('Fair Rental Value of Your Home')}
-            sectionOneTitle={t('Monthly market rental value of your home.')}
-            sectionFourTitle={t('Total Monthly Fair Rental Value of your Home')}
-          />
-        </Box>
-      )}
-      <InitialQuestionsCard
-        title={t('Cost of Providing a Home')}
-        sectionOneTitle={t(
-          'Monthly mortgage payment, taxes, insurance, and any extra principal you pay.',
-        )}
-        sectionFourTitle={t('Average monthly amount for unexpected expenses.')}
-        isFairRental
-      />
-      <Box mt={3} mb={3}>
-        <RequestSummaryCard rentOrOwn={rentOrOwn} />
-      </Box>
-      <EndingSection />
-      <DirectionButtons
-        handleNext={handleNext}
-        handleBack={handleBack}
-        isCalculate
-      />
-    </>
+    <Formik<CalculationFormValues>
+      initialValues={initialValues}
+      validationSchema={getValidationSchema(rentOrOwn)}
+      validateOnChange
+      validateOnBlur
+      onSubmit={() => {
+        handleNext();
+      }}
+    >
+      {({
+        isValid,
+        submitCount,
+        values,
+        errors,
+        setFieldValue,
+        handleBlur,
+        touched,
+      }) => {
+        const showAlert = submitCount > 0 && (!isValid || !values.isChecked);
+
+        const [closedAt, setClosedAt] = useState<number | null>(null);
+
+        const open = showAlert && closedAt !== submitCount;
+
+        useEffect(() => {
+          if (!showAlert) {
+            setClosedAt(null);
+          }
+        }, [showAlert]);
+
+        return (
+          <form noValidate>
+            <Box mb={2}>
+              <Typography variant="h5">
+                {t('Calculate Your MHA Request')}
+              </Typography>
+            </Box>
+            <Trans i18nKey="newRequestCalculation">
+              <p style={{ lineHeight: 1.5 }}>
+                Please enter dollar amounts for each category below to calculate
+                your Annual MHA. The board will review this number after{' '}
+                {boardDateFormatted} and you will receive notice of your
+                approval effective {availableDateFormatted}.
+              </p>
+            </Trans>
+            <Box sx={{ mt: 2, mb: 3 }}>
+              <OpenInNew
+                fontSize="medium"
+                sx={{ verticalAlign: 'middle', opacity: 0.56 }}
+              />{' '}
+              <Link href="">What expenses can I claim on my MHA?</Link>
+            </Box>
+            {rentOrOwn === RentOwnEnum.Own && (
+              <Box mb={3}>
+                <FairRentalValue />
+              </Box>
+            )}
+            <CostOfHome rentOrOwn={rentOrOwn} />
+            <Box mt={3} mb={3}>
+              <RequestSummaryCard rentOrOwn={rentOrOwn} />
+            </Box>
+            <FormControl error={Boolean(touched.isChecked && errors.isChecked)}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={Boolean(values.isChecked)}
+                    onChange={(e) =>
+                      setFieldValue('isChecked', e.target.checked)
+                    }
+                    onBlur={handleBlur}
+                    name="isChecked"
+                  />
+                }
+                label={t(
+                  'I understand that my approved Annual MHA will be based on the lower of the Annual Fair Rental Value or the Annual Cost of Providing a Home.',
+                )}
+              />
+              <FormHelperText sx={{ ml: 4 }}>
+                {touched.isChecked && errors.isChecked ? (
+                  <i>{errors.isChecked}</i>
+                ) : (
+                  <i>{t('This box must be checked to continue.')}</i>
+                )}
+              </FormHelperText>
+            </FormControl>
+            <EndingSection />
+            {open && (
+              <Alert
+                severity="error"
+                onClose={() => setClosedAt(submitCount)}
+                sx={{ mt: 2, '& ul': { m: 0, pl: 3 } }}
+              >
+                {t('Your form is missing information.')}
+                <ul>
+                  {submitCount > 0 &&
+                    Object.keys(errors).some((k) => k !== 'isChecked') && (
+                      <li>
+                        {t('Please enter a value for all required fields.')}
+                      </li>
+                    )}
+                  {!values.isChecked && (
+                    <li>
+                      {t(
+                        'Please check the box above if you understand how this was calculated.',
+                      )}
+                    </li>
+                  )}
+                </ul>
+              </Alert>
+            )}
+            <DirectionButtons handleBack={handleBack} isCalculate />
+          </form>
+        );
+      }}
+    </Formik>
   );
 };
--- src/components/Reports/MinisterHousingAllowance/NewRequest/Steps/StepTwo/RentOwn.tsx
+++ src/components/Reports/MinisterHousingAllowance/NewRequest/Steps/StepTwo/RentOwn.tsx
@@ -8,111 +8,82 @@ import {
   RadioGroup,
   Typography,
 } from '@mui/material';
-import { Formik } from 'formik';
+import { useFormikContext } from 'formik';
 import { useTranslation } from 'react-i18next';
-import * as yup from 'yup';
-import i18n from 'src/lib/i18n';
 import { RentOwnEnum } from '../../../Shared/sharedTypes';
+import { FormValues } from '../../NewRequestPage';
 import { DirectionButtons } from '../../Shared/DirectionButtons';
 
-export interface FormValues {
-  rentOrOwn: RentOwnEnum | string;
-}
-
-interface RentOwnProps {
-  handleNext: () => void;
-}
-
-const validationSchema = yup.object({
-  rentOrOwn: yup
-    .string()
-    .required(i18n.t('Please select one of the options above to continue.')),
-});
-
-export const RentOwn: React.FC<RentOwnProps> = ({ handleNext }) => {
+export const RentOwn: React.FC = () => {
   const { t } = useTranslation();
-  const initialValues: FormValues = {
-    rentOrOwn: '',
-  };
-
   const [open, setOpen] = useState(true);
 
-  return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={() => {
-        handleNext();
-      }}
-    >
-      {({
-        values: { rentOrOwn },
-        touched,
-        setTouched,
-        errors,
-        handleChange,
-        handleBlur,
-        submitCount,
-        submitForm,
-        isValid,
-      }) => {
-        useEffect(() => {
-          if (submitCount > 0 && !isValid) {
-            setOpen(true);
-          }
-        }, [submitCount, isValid]);
+  const {
+    values,
+    touched,
+    setTouched,
+    errors,
+    handleChange,
+    handleBlur,
+    submitCount,
+    submitForm,
+    isValid,
+  } = useFormikContext<FormValues>();
 
-        return (
-          <form>
-            <Box mb={3}>
-              <Typography variant="h5">{t('Rent or Own?')}</Typography>
-            </Box>
-            <Typography variant="body1" sx={{ mb: 2 }}>
-              {t('Please select the option that applies to you.')}
-            </Typography>
-            <Box>
-              <FormControl
-                component="fieldset"
-                error={touched.rentOrOwn && Boolean(errors.rentOrOwn)}
-              >
-                <RadioGroup
-                  name="rentOrOwn"
-                  value={rentOrOwn}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                >
-                  <FormControlLabel
-                    value={RentOwnEnum.Rent}
-                    control={<Radio />}
-                    label={t('Rent')}
-                  />
-                  <FormControlLabel
-                    value={RentOwnEnum.Own}
-                    control={<Radio />}
-                    label={t('Own')}
-                  />
-                </RadioGroup>
-              </FormControl>
-            </Box>
-            <DirectionButtons
-              handleNext={async () => {
-                setTouched({ rentOrOwn: true });
-                await submitForm();
-              }}
+  useEffect(() => {
+    if (submitCount > 0 && !isValid) {
+      setOpen(true);
+    }
+  }, [submitCount, isValid]);
+
+  return (
+    <>
+      <Box mb={3}>
+        <Typography variant="h5">{t('Rent or Own?')}</Typography>
+      </Box>
+      <Typography variant="body1" sx={{ mb: 2 }}>
+        {t('Please select the option that applies to you.')}
+      </Typography>
+      <Box>
+        <FormControl
+          component="fieldset"
+          error={touched.rentOrOwn && Boolean(errors.rentOrOwn)}
+        >
+          <RadioGroup
+            name="rentOrOwn"
+            value={values.rentOrOwn}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          >
+            <FormControlLabel
+              value={RentOwnEnum.Rent}
+              control={<Radio />}
+              label={t('Rent')}
             />
-            {submitCount > 0 && !isValid && open && (
-              <Alert
-                severity="error"
-                onClose={() => setOpen(false)}
-                sx={{ mt: 2, '& ul': { m: 0, pl: 3 } }}
-              >
-                {t('Your form is missing information.')}
-                <ul>{errors.rentOrOwn && <li>{errors.rentOrOwn}</li>}</ul>
-              </Alert>
-            )}
-          </form>
-        );
-      }}
-    </Formik>
+            <FormControlLabel
+              value={RentOwnEnum.Own}
+              control={<Radio />}
+              label={t('Own')}
+            />
+          </RadioGroup>
+        </FormControl>
+      </Box>
+      <DirectionButtons
+        handleNext={async () => {
+          setTouched({ rentOrOwn: true });
+          await submitForm();
+        }}
+      />
+      {submitCount > 0 && !isValid && open && (
+        <Alert
+          severity="error"
+          onClose={() => setOpen(false)}
+          sx={{ mt: 2, '& ul': { m: 0, pl: 3 } }}
+        >
+          {t('Your form is missing information.')}
+          <ul>{errors.rentOrOwn && <li>{errors.rentOrOwn}</li>}</ul>
+        </Alert>
+      )}
+    </>
   );
 };
