46bb6a766ea9e9090b6be870bec7ba4f221c17d3
wjames111 <william.james@cru.org>
2025-11-11 16:34:09 -0500

    Adds print button

--- src/components/Reports/PartnerGivingAnalysisReport/Actions/HeaderActions.tsx
+++ src/components/Reports/PartnerGivingAnalysisReport/Actions/HeaderActions.tsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import PrintIcon from '@mui/icons-material/Print';
-import { Button } from '@mui/material';
+import { Button, CircularProgress } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 
-export const HeaderActions: React.FC = () => {
+interface HeaderActionsProps {
+  onPrint: () => void;
+  loading?: boolean;
+}
+
+export const HeaderActions: React.FC<HeaderActionsProps> = ({
+  onPrint,
+  loading = false,
+}) => {
   const { t } = useTranslation();
 
-  const onPrint = () => {
-    window.print();
-  };
-
   return (
-    <Button startIcon={<PrintIcon />} onClick={onPrint} variant="outlined">
-      {t('Print')}
+    <Button
+      startIcon={loading ? <CircularProgress size={20} /> : <PrintIcon />}
+      onClick={onPrint}
+      variant="outlined"
+      disabled={loading}
+    >
+      {loading ? t('Loading...') : t('Print')}
     </Button>
   );
 };
