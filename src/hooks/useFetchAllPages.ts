3c1da3c2e4bfd94ddd3ce9d8bab6c1d330c46442
wjames111 <william.james@cru.org>
2025-11-11 13:40:43 -0500

    Fix for controlled pageSize

--- src/hooks/useFetchAllPages.ts
+++ src/hooks/useFetchAllPages.ts
@@ -12,20 +12,23 @@ export const useFetchAllPages = <TData, TVariables extends Variables>({
   fetchMore,
   error,
   pageInfo,
+  pageSize,
 }: {
   fetchMore: QueryResult<TData, TVariables>['fetchMore'];
   error: ApolloError | undefined;
   pageInfo: Pick<PageInfo, 'endCursor' | 'hasNextPage'> | undefined;
+  pageSize?: number;
 }) => {
   useEffect(() => {
     if (pageInfo?.hasNextPage && pageInfo.endCursor) {
       fetchMore({
         variables: {
+          ...(pageSize && { first: pageSize }),
           after: pageInfo.endCursor,
         },
       });
     }
-  }, [pageInfo?.hasNextPage, pageInfo?.endCursor]);
+  }, [pageInfo?.hasNextPage, pageInfo?.endCursor, pageSize]);
 
   const loaded = pageInfo?.hasNextPage === false;
 
