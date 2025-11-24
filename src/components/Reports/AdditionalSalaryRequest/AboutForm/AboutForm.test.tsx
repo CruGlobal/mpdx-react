726bb91339fd1304d1e178680e128d68dca132ad
wjames111 <william.james@cru.org>
2025-11-24 12:11:09 -0500

    Mock only handleContinue

--- src/components/Reports/AdditionalSalaryRequest/AboutForm/AboutForm.test.tsx
+++ src/components/Reports/AdditionalSalaryRequest/AboutForm/AboutForm.test.tsx
@@ -7,42 +7,23 @@ import TestRouter from '__tests__/util/TestRouter';
 import { GqlMockedProvider } from '__tests__/util/graphqlMocking';
 import i18n from 'src/lib/i18n';
 import theme from 'src/theme';
-import { AdditionalSalaryRequestSectionEnum } from '../AdditionalSalaryRequestHelper';
-import {
-  AdditionalSalaryRequestProvider,
-  AdditionalSalaryRequestType,
-} from '../Shared/AdditionalSalaryRequestContext';
+import { AdditionalSalaryRequestProvider } from '../Shared/AdditionalSalaryRequestContext';
 import { AboutForm } from './AboutForm';
 
 const mockHandleContinue = jest.fn();
 
-const mockSectionOrder = [
-  {
-    title: 'About this Form',
-    section: AdditionalSalaryRequestSectionEnum.AboutForm,
-  },
-  {
-    title: 'Complete Form',
-    section: AdditionalSalaryRequestSectionEnum.CompleteForm,
-  },
-  {
-    title: 'Receipt',
-    section: AdditionalSalaryRequestSectionEnum.Receipt,
-  },
-];
-
-jest.mock('../Shared/AdditionalSalaryRequestContext', () => ({
-  ...jest.requireActual('../Shared/AdditionalSalaryRequestContext'),
-  useAdditionalSalaryRequest: (): AdditionalSalaryRequestType => ({
-    sectionOrder: mockSectionOrder,
-    handleContinue: mockHandleContinue,
-    selectedSection: mockSectionOrder[0 /* AboutForm */],
-    isDrawerOpen: false,
-    toggleDrawer: jest.fn(),
-    setSelectedSection: jest.fn(),
-    setIsDrawerOpen: jest.fn(),
-  }),
-}));
+jest.mock('../Shared/AdditionalSalaryRequestContext', () => {
+  const originalModule = jest.requireActual(
+    '../Shared/AdditionalSalaryRequestContext',
+  );
+  return {
+    ...originalModule,
+    useAdditionalSalaryRequest: () => ({
+      ...originalModule.useAdditionalSalaryRequest(),
+      handleContinue: mockHandleContinue,
+    }),
+  };
+});
 
 const router = {
   query: { accountListId: 'account-list-1' },
