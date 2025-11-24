75ee1262f5e73be1e84596f118cad2d3bade94bc
wjames111 <william.james@cru.org>
2025-11-24 11:45:12 -0500

    Use Trans for long text excerpts

--- src/components/Reports/AdditionalSalaryRequest/AboutForm/AboutForm.tsx
+++ src/components/Reports/AdditionalSalaryRequest/AboutForm/AboutForm.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link, List, ListItem, Typography } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 import { useAdditionalSalaryRequest } from '../Shared/AdditionalSalaryRequestContext';
 import { AdditionalSalaryRequestSection } from '../SharedComponents/AdditionalSalaryRequestSection';
 import { AccountInfoCard } from './AccountInfoCard';
@@ -19,72 +19,73 @@ export const AboutForm: React.FC = () => {
 
   return (
     <AdditionalSalaryRequestSection title={selectedSection.title}>
-      <Typography variant="body1" paragraph>
-        {t(
-          'You can use this form to electronically submit additional salary requests. Please note:',
-        )}
-      </Typography>
+      <Trans t={t}>
+        <Typography variant="body1" paragraph>
+          You can use this form to electronically submit additional salary
+          requests. Please note:
+        </Typography>
 
-      <List
-        sx={{
-          mb: theme.spacing(2),
-          listStyleType: 'disc',
-          pl: theme.spacing(2),
-        }}
-      >
-        <ListItem sx={{ display: 'list-item', pl: 0 }}>
-          <Typography variant="body1">
-            {t('You must have')}{' '}
-            <Typography component="span" fontWeight="bold">
-              {t('adequate funds')}
-            </Typography>{' '}
-            {t('in your account to cover this request.')}
-          </Typography>
-        </ListItem>
-        <ListItem sx={{ display: 'list-item', pl: 0 }}>
-          <Typography variant="body1">
-            {t(
-              'Your request will be reviewed and if approved, your request for this year',
-            )}{' '}
-            <Typography component="span" fontWeight="bold">
-              {t('will not exceed your Remaining Allowable Salary')}
+        <List
+          sx={{
+            mb: theme.spacing(2),
+            listStyleType: 'disc',
+            pl: theme.spacing(2),
+          }}
+        >
+          <ListItem sx={{ display: 'list-item', pl: 0 }}>
+            <Typography variant="body1">
+              You must have{' '}
+              <Typography component="span" fontWeight="bold">
+                adequate funds
+              </Typography>{' '}
+              in your account to cover this request.
             </Typography>
-            .
-          </Typography>
-        </ListItem>
-      </List>
+          </ListItem>
+          <ListItem sx={{ display: 'list-item', pl: 0 }}>
+            <Typography variant="body1">
+              Your request will be reviewed and if approved, your request for
+              this year{' '}
+              <Typography component="span" fontWeight="bold">
+                will not exceed your Remaining Allowable Salary
+              </Typography>
+              .
+            </Typography>
+          </ListItem>
+        </List>
+      </Trans>
 
-      <Typography variant="body1" paragraph>
-        {t(
-          'In special cases where requests exceed the remaining allowable salary, we require additional review through our',
-        )}{' '}
-        <Link
-          href="#"
-          underline="always"
-          onClick={(e) => {
-            e.preventDefault();
-            // TODO: Implement Progressive Approvals navigation/modal
-          }}
-        >
-          {t('Progressive Approvals')}
-        </Link>{' '}
-        {t('process. Depending on the request, this can take up to 14 days.')}
-      </Typography>
+      <Trans t={t}>
+        <Typography variant="body1" paragraph>
+          In special cases where requests exceed the remaining allowable salary,
+          we require additional review through our{' '}
+          <Link
+            href="#"
+            underline="always"
+            onClick={(e) => {
+              e.preventDefault();
+              // TODO: Implement Progressive Approvals navigation/modal
+            }}
+          >
+            Progressive Approvals
+          </Link>{' '}
+          process. Depending on the request, this can take up to 14 days.
+        </Typography>
 
-      <Typography variant="body1" paragraph>
-        {t('Alternatively, you may download and submit the')}{' '}
-        <Link
-          href="#"
-          underline="always"
-          onClick={(e) => {
-            e.preventDefault();
-            // TODO: Implement paper version download
-          }}
-        >
-          {t('paper version')}
-        </Link>{' '}
-        {t('of the Additional Salary request if you prefer.')}
-      </Typography>
+        <Typography variant="body1" paragraph>
+          Alternatively, you may download and submit the{' '}
+          <Link
+            href="#"
+            underline="always"
+            onClick={(e) => {
+              e.preventDefault();
+              // TODO: Implement paper version download
+            }}
+          >
+            paper version
+          </Link>{' '}
+          of the Additional Salary request if you prefer.
+        </Typography>
+      </Trans>
 
       <AccountInfoCard
         userName={userName}
