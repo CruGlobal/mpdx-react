8f7a664624591a38c3d641558fccd3574979aa29
zachery with an e <45150570+zweatshirt@users.noreply.github.com>
2026-01-12 14:51:25 -0600

    Run prettier

--- src/components/Reports/MinisterHousingAllowance/MainPages/IneligibleDisplay.tsx
+++ src/components/Reports/MinisterHousingAllowance/MainPages/IneligibleDisplay.tsx
@@ -29,37 +29,38 @@ export const IneligibleDisplay: React.FC = () => {
             <a href="mailto:MHA@cru.org">MHA@cru.org</a>.
           </p>
         </Trans>
-        {!hcmLoading && isMarried && userEligibleForMHA && !spouseEligibleForMHA && (
-          <Box mt={2} data-testid="spouse-ineligible-message">
-            <Trans
-              i18nKey="spouseNotEligibleMha"
-              values={{ preferredName, spousePreferredName }}
-            >
-              <p style={{ lineHeight: 1.5 }}>
-                Completing a Minister&apos;s Housing Allowance calculation form
-                will submit the request for {preferredName}.{' '}
-                {spousePreferredName} has not completed the required IBS
-                courses to meet eligibility criteria.
-              </p>
-            </Trans>
-          </Box>
-        )}
-        {!hcmLoading && (isMarried && !spouseEligibleForMHA || !userEligibleForMHA) && (
-          <Box mt={2} data-testid="user-ineligible-message">
-            <Trans
-              i18nKey="userNotEligibleMha"
-              values={{ preferredName }}
-            >
-              <p style={{ lineHeight: 1.5 }}>
-                Once approved, when you calculate your salary, you will see the
-                approved amount that can be applied to {preferredName}&apos;s
-                salary. If you believe this is incorrect, please contact
-                Personnel Records at 407-826-2236 or{' '}
-                <a href="mailto:MHA@cru.org">MHA@cru.org</a>.
-              </p>
-            </Trans>
-          </Box>
-        )}
+        {!hcmLoading &&
+          isMarried &&
+          userEligibleForMHA &&
+          !spouseEligibleForMHA && (
+            <Box mt={2} data-testid="spouse-ineligible-message">
+              <Trans
+                i18nKey="spouseNotEligibleMha"
+                values={{ preferredName, spousePreferredName }}
+              >
+                <p style={{ lineHeight: 1.5 }}>
+                  Completing a Minister&apos;s Housing Allowance calculation
+                  form will submit the request for {preferredName}.{' '}
+                  {spousePreferredName} has not completed the required IBS
+                  courses to meet eligibility criteria.
+                </p>
+              </Trans>
+            </Box>
+          )}
+        {!hcmLoading &&
+          ((isMarried && !spouseEligibleForMHA) || !userEligibleForMHA) && (
+            <Box mt={2} data-testid="user-ineligible-message">
+              <Trans i18nKey="userNotEligibleMha" values={{ preferredName }}>
+                <p style={{ lineHeight: 1.5 }}>
+                  Once approved, when you calculate your salary, you will see
+                  the approved amount that can be applied to {preferredName}
+                  &apos;s salary. If you believe this is incorrect, please
+                  contact Personnel Records at 407-826-2236 or{' '}
+                  <a href="mailto:MHA@cru.org">MHA@cru.org</a>.
+                </p>
+              </Trans>
+            </Box>
+          )}
       </Box>
     </>
   );
