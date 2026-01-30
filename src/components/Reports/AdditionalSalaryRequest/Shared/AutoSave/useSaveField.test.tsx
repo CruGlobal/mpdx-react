07d0eb6cec7dd87e3292d4ea2fcbe0b4ccb9effa
wjames111 <william.james@cru.org>
2026-01-30 14:23:12 -0500

    Fix type errors

--- src/components/Reports/AdditionalSalaryRequest/Shared/AutoSave/useSaveField.test.tsx
+++ src/components/Reports/AdditionalSalaryRequest/Shared/AutoSave/useSaveField.test.tsx
@@ -47,6 +47,7 @@ const defaultFormValues: CompleteFormValues = {
   expensesNotApprovedWithin90Days: '0',
   deductTwelvePercent: false,
   phoneNumber: '555-1234',
+  emailAddress: 'test@testerson.test',
 };
 
 const mockTrackMutation = jest.fn((mutation) => mutation);
