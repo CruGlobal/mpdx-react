ce693f7f7bd0e6708a045cb676be7a74a6057a2e
wjames111 <william.james@cru.org>
2026-02-11 17:05:13 -0500

    Updates EligibleDisplay with correct notices

--- src/components/Reports/AdditionalSalaryRequest/MainPages/EligibleDisplay.tsx
+++ src/components/Reports/AdditionalSalaryRequest/MainPages/EligibleDisplay.tsx
@@ -1,17 +1,15 @@
+import React from 'react';
 import { Box, Typography } from '@mui/material';
 import { Trans, useTranslation } from 'react-i18next';
+import { AsrStatusEnum } from 'src/graphql/types.generated';
 import { useAdditionalSalaryRequest } from '../Shared/AdditionalSalaryRequestContext';
 
-interface EligibleDisplayProps {
-  allRequestStatus: string;
-}
-
-export const EligibleDisplay: React.FC<EligibleDisplayProps> = ({
-  allRequestStatus,
-}) => {
+export const EligibleDisplay: React.FC = () => {
   const { t } = useTranslation();
-  const { user } = useAdditionalSalaryRequest();
+  const { user, requestData } = useAdditionalSalaryRequest();
   const preferredName = user?.staffInfo?.preferredName;
+  const { status } = requestData?.latestAdditionalSalaryRequest || {};
+
   return (
     <>
       <Box>
@@ -20,24 +18,24 @@ export const EligibleDisplay: React.FC<EligibleDisplayProps> = ({
         </Typography>
       </Box>
       <Box>
-        <Trans i18nKey="pendingAsrRequest">
-          {allRequestStatus === 'None' ? (
-            <Typography sx={{ lineHeight: 1.5 }}>
-              No Additional Salary Request has been created yet. You may create
-              one by clicking the &rdquo;Create ASR&rdquo; button below. Please
-              note that you may only process one additional salary request at a
-              time.
-            </Typography>
-          ) : (
-            <Typography sx={{ lineHeight: 1.5 }}>
-              We have noted that {preferredName} currently has an Additional
-              Salary Request with the status of {allRequestStatus} in our
-              system. You may review the status of your additional salary
-              request below. Please note that you may only process one
-              additional salary request at a time.
-            </Typography>
-          )}
-        </Trans>
+        {status === AsrStatusEnum.Pending && (
+          <Typography sx={{ lineHeight: 1.5 }}>
+            <Trans t={t}>
+              We have noted that {{ preferredName }} currently has a pending
+              request in our system. You may review the status of your
+              additional salary request below. Please note that you may only
+              process one additional salary request at a time.
+            </Trans>
+          </Typography>
+        )}
+        {status === AsrStatusEnum.ActionRequired && (
+          <Typography sx={{ lineHeight: 1.5 }}>
+            <Trans t={t}>
+              Action is required to complete your pending request. Please see
+              the details below.
+            </Trans>
+          </Typography>
+        )}
       </Box>
     </>
   );
